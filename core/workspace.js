fb15ddcc28fc7d25481988601c77e01b24a2bbba
Andrew Mee <primary.edw@gmail.com>
2014-12-02 14:24:41 +1000

    JSON Export Import Take - added other files

--- core/workspace.js
+++ core/workspace.js
@@ -31,6 +31,7 @@ goog.provide('Blockly.Workspace');
 goog.require('Blockly.ScrollbarPair');
 goog.require('Blockly.Trashcan');
 goog.require('Blockly.Xml');
+goog.require('Blockly.JSON');
 goog.require('goog.math');
 goog.require('goog.math.Coordinate');
 

1734b106ead518567d8d108c42a5b48d35275865
Blake Bourque <Techplex.Engineer@gmail.com>
2014-12-23 17:57:58 -0500

    Attempting to run blockly with NodeJS

---
+++ app.js
@@ -0,0 +1,24 @@
+var Blockly = require('./blockly_compressed.js');
+// var blocks = require('./blocks_compressed.js');
+// var python = require('./python_compressed.js');
+// var en = require('./msg/js/en.js');
+
+var fs = require('fs');
+fs.readFile('./input.xml', function (err, data) {
+	if (err) throw err;
+
+	var xmlText = data.toString()
+	try {
+		var xml = Blockly.Xml.textToDom(xmlText)
+	} catch (e) {
+		console.log(e);
+	return;
+	}
+	// Create a headless workspace.
+	var workspace = new Blockly.Workspace();
+	Blockly.Xml.domToWorkspace(workspace, xml);
+	var code = Blockly.Python.workspaceToCode(workspace);
+	document.getElementById('code_output').value = code;
+});
+
+// var xmlText = document.getElementById('xml_input').value;
---
+++ readme-headless.md
@@ -0,0 +1,11 @@
+Blockly Headless
+=====================================
+
+## Dependencies
+[Nodejs](http://nodejs.org/) --
+[xmldom](https://www.npmjs.com/package/xmldom) - Replaces DOMParser
+
+## Getting started
+1. Install NodeJS
+2. Run `npm install` in the project root. This will install the xmldom module.
+3. Run `node app.js`

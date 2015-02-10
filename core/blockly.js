b950e56c0dddfec4231d7e7465d0158873e05d52
Ryan Loader <rani_sputnik@hotmail.com>
2015-02-11 10:20:27 +1300

    Fixed issue with calculating input focus on different input types

--- core/blockly.js
+++ core/blockly.js
@@ -551,7 +551,10 @@ Blockly.removeAllRanges = function() {
  * @private
  */
 Blockly.isTargetInput_ = function(e) {
-  return e.target.type == 'textarea' || e.target.type == 'text';
+  return e.target.type == 'textarea' || e.target.type == 'text' 
+      || e.target.type == 'number' || e.target.type == 'email'
+      || e.target.type == 'password' || e.target.type == 'search'
+      || e.target.type == 'tel' || e.target.type == 'url';
 };
 
 /**

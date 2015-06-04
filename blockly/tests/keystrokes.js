709972c7c12a4bea086df53b92de8ddf45372afb
rkb2265 <rkb2265@rit.edu>
2015-06-04 10:17:13 -0400

    Changed keystrokes.js so all methods will be called in same function.

--- blockly/tests/keystrokes.js
+++ blockly/tests/keystrokes.js
@@ -11,8 +11,8 @@ document.onkeydown = document.onkeyup = function(e){
 		e.preventDefault();
 	}
 	
-	else if(map[17] && map[82]){ //Ctrl R
-		console.log("Control R pressed.");
+	else if(map[17] && map[89]){ //Ctrl Y
+		console.log("Control Y pressed.");
 		//Need to implement Redo
 		e.preventDefault();
 	}
@@ -29,93 +29,88 @@ document.onkeydown = document.onkeyup = function(e){
 		e.preventDefault();
 	}
 	
-
-};
-
-// Function for single keypresses
-document.onkeydown = function(e){
-	
-	e = e || event;
-
-	switch(e.keyCode){
-		//Arrow keys for development purposes.  Switch as needed for proper usage.
-		
-		case 37: //left
-			traverseOut();
-			break;
-		case 38: //up
-			traverseUp();
-			break;
-		case 39: //right
-			traverseIn();
-			break;
-		case 40: //down
-			traverseDown();
-			break;
-		
-		//end of development block
-		
-		
-		case 9: //Tab
-			console.log("Tab key pressed.");
-			e.preventDefault();
-			break;
-		
-		case 65: //A
-			console.log("A key pressed.");
-			//This should initiate a menu to add a block using hotkeys
-			//Hotkey functionality in the menu needs to be determined
-			break;
-			
-		case 78: //N
-			console.log("N key pressed.");
-			//Initiate a navigate search function
-			break;
-			
-		case 79: //O
-			console.log("O key pressed.");
-			//Option to initiate open navigation (this might be thrown out)
-			break;
-		
-		case 67: //C
-			console.log("C key pressed.");
-			//Write a comment on the most recently selected block
-			break;
-			
-		case 71: //G
-			console.log("G key pressed.");
-			//Goto the block the comment that is currently selected is from
-			//Alternatively goto the comment that is connected to the currently selected block
-			break;
-			
-		case 69: //E
-			console.log("E key pressed.");
-			//Edit block of code or edit comment
-			break;
-			
-		case 188: //comma
-			console.log("Comma key pressed.");
-			//Traverse farther into the block layers
-			break;
-		
-		case 190: //period
-			console.log("Period key pressed.");
-			//Traverse up out of block layers
-			break;
-			
-		case 46: //delete
-			console.log("Delete key pressed.");
-			//Delete the currently selected item
-			e.preventDefault();
-			break;
-			
-		case 27: //escape
-			console.log("Escape key pressed.");
-			//Get out of the current menu
-			e.preventDefault();
-			break;
-
+	else if(map[9] && map[16]){ //Tab Shift
+		console.log("Tab Shift pressed.");
+		e.preventDefault();
+		//Go backwards through the same level of code
 	}
 	
-
+	else if(map[9]){ //Tab
+		console.log("Tab key pressed.");
+		e.preventDefault();
+		//Go through the same level of code
+	}
+	
+	else if(map[65]){ //A
+		console.log("A key pressed.");
+		//This should initiate a menu to add a block using hotkeys
+		//Hotkey functionality in the menu needs to be determined
+	}
+	
+	else if(map[78]){ //N
+		console.log("N key pressed.");
+		//Initiate a navigate search function
+	}
+	
+	else if(map[79]){ //O
+		console.log("O key pressed.");
+		//Option to initiate open navigation (this might be thrown out)
+	}
+	
+	else if(map[67]){ //C
+		console.log("C key pressed.");
+		//Write a comment on the most recently selected block
+	}
+	
+	else if(map[71]){ //G
+		console.log("G key pressed.");
+		//Goto the block the comment that is currently selected is from
+		//Alternatively goto the comment that is connected to the currently selected block
+	}
+	
+	else if(map[69]){ //E
+		console.log("E key pressed.");
+		//Edit block of code or edit comment
+	}
+	
+	else if(map[188]){ //comma
+		console.log("Comma key pressed.");
+		//Traverse farther into the block layers
+	}
+	
+	else if(map[190]){ //period
+		console.log("Period key pressed.");
+		//Traverse up out of block layers
+	}
+	
+	else if(map[46]){ //delete
+		console.log("Delete key pressed.");
+		//Delete the currently selected item
+		e.preventDefault();
+	}
+	
+	else if(map[69]){ //escape
+		console.log("Escape key pressed.");
+		//Get out of the current menu
+		e.preventDefault();
+	}
+	
+	//Arrow keys for development purposes.  Switch as needed for proper usage.
+	
+	else if(map[37]){ //left arrow
+		traverseOut();
+	}
+	
+	else if(map[38]){ //up arrow
+		traverseUp();
+	}
+	
+	else if(map[39]){ //right arrow
+		traverseIn();
+	}
+	
+	else if(map[40]){ //down arrow
+		traverseDown();
+	}
+	//End of development block
 };

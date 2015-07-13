de9d6b3fc65fa404c0109a47554d6892b26d1269
Drew Macrae <Drew@Linkitz.com>
2015-07-13 07:42:04 -0700

    Stubbed out 2 more blocks
    these show an interrupt, and an output for the first time. LED output's
    are confusing right now because most petals can light up, but they're
    currently using the LED petal as a symbol for doing so

--- demos/code/linkitzBlocks.js
+++ demos/code/linkitzBlocks.js
@@ -33,3 +33,53 @@ Blockly.JavaScript['getmicdata'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+
+
+
+
+
+Blockly.Blocks['onmotiontrigger'] = {
+  init: function() {
+  	this.appendDummyInput()
+		.appendField(new Blockly.FieldImage("Petal_Top_Motion_Preview.jpg", 64, 64, "GetMotionData"));
+	this.appendDummyInput()
+		.appendField(new Blockly.FieldDropdown([["tap","tap"],["click","click"],["donemoving","done moving"]]),"interruptModes");	
+    this.appendStatementInput("EventCode");
+    this.setColour(210);
+    this.setTooltip('Motion Trigger');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.JavaScript['onmotiontrigger'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+
+
+
+
+Blockly.Blocks['ledoutput'] = {
+  init: function() {
+    this.appendValueInput("ledcolor")
+        .setCheck("Array")
+        .appendField(new Blockly.FieldImage("Petal_Top_LED_Preview.jpg", 50, 50, "LEDLink"));
+    this.setInputsInline(false);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(290);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.JavaScript['ledoutput'] = function(block) {
+  var value_ledcolor = Blockly.JavaScript.valueToCode(block, 'ledcolor', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};

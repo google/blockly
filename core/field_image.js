a43c426986073a937909da837b397c6cc5adb48d
carlosperate <carlosperate@users.noreply.github.com>
2015-08-21 17:25:05 +0100

    Remove additional height to the FieldImage size.

--- core/field_image.js
+++ core/field_image.js
@@ -46,7 +46,7 @@ Blockly.FieldImage = function(src, width, height, opt_alt) {
   // Ensure height and width are numbers.  Strings are bad at math.
   this.height_ = Number(height);
   this.width_ = Number(width);
-  this.size_ = new goog.math.Size(this.height_ + 10, this.width_);
+  this.size_ = new goog.math.Size(this.height_, this.width_);
   this.text_ = opt_alt || '';
   this.setValue(src);
 };

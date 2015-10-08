b39fb209d2ccfb741a2d9e03901170c62cb563cb
Sheldon Pacotti <sheldon.pacotti@frogdesign.com>
2015-10-08 11:25:28 -0500

    custom administrative stuff for working in Ember (remove before raising a PR)

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+
+js-beautify blockly_compressed.js > blockly_compressed_clean.js
+
+cp ./blockly_compressed.js ../px-web-client/vendor/blockly
+cp ./blockly_compressed_clean.js ../px-web-client/vendor/blockly
+cp ./blockly_uncompressed.js ../px-web-client/vendor/blockly
+cp ./javascript_compressed.js ../px-web-client/vendor/blockly
+cp ./blocks_compressed.js ../px-web-client/vendor/blockly

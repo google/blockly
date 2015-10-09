ecdf22d1a7b33a43e908541ef670b3e7f3e03104
toebes-extreme <toebes@extremenetworks.com>
2015-10-09 08:08:59 -0400

    Update to latest type indicator from Hendrik Diel

--- core/typeIndicator.js
+++ core/typeIndicator.js
@@ -1,28 +1,23 @@
 /**
     @license
     Copyright 2015 Hendrik Diel
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
     @fileoverview
     This file adds a indicator to all connections that shows if a dragging block is type compatible
-
     @author
     diel.hendrik@gmail.com (Hendrik Diel)
 */
 
 (function() {
-
+	"use strict";
  // keep in mind if we are dragging right now
  var draggingWorkspace = null;
 
@@ -34,7 +29,7 @@
  // Store google's terminateDrag in a variable so we can expand it.
  var oldTerminateDrag_ = Blockly.BlockSvg.terminateDrag_;
  Blockly.BlockSvg.terminateDrag_ = function() {
-  // Only if the user was
+  // Only if the user was dragging
   if (draggingWorkspace) {
    // Go through all the blocks
    var allBlocks = draggingWorkspace.getAllBlocks();
@@ -59,14 +54,14 @@
 
  /**
   * Creates the indicators on the first mouse move of an drag since all the
-  * blocks have moved then we dont need to worry about moving the indicators
+  * blocks have moved we dont need to worry about moving the indicators
   * along with the blocks.
   * @type {undefined}
   */
  //save old onMouseMove_() for later call
  var oldOnMouseMove_ = Blockly.BlockSvg.prototype.onMouseMove_;
  Blockly.BlockSvg.prototype.onMouseMove_ = function(e) {
-  var this_ = this; // we need to dave the this context so the command beeing created can access it
+  var this_ = this; // we need to save the this context so the command beeing created can access it
   Blockly.doCommand(function() {
    if (Blockly.dragMode_ == 2 && !draggingWorkspace) { // Only on first drag move
     draggingWorkspace = this_.workspace;
@@ -75,17 +70,22 @@
     // or a block without relevant conenctions at all.
     var typ = "dummy";
     var outCon = null;
+		var inCon = null;
     if (this_.outputConnection) {
      outCon = this_.outputConnection;
-     typ = "input";
-    } else if (this_.previousConnection) {
+     typ = "input"; 
+    }if (this_.previousConnection) {
      outCon = this_.previousConnection;
      typ = "statement";
-    }
+	 }if(this_.nextConnection){
+			inCon = this_.nextConnection;
+			typ = "statement";
+		}
     if (outCon) {
      // To get all potential connections by looking up the opposite type
      // and geting all connections of that type from the workspace
      var oppositeType = Blockly.OPPOSITE_TYPE[outCon.type];
+
      var cons = draggingWorkspace.connectionDBList[oppositeType];
 
      cons.forEach(function(otherConn) {
@@ -101,6 +101,23 @@
       }
      });
     }
+		if(inCon){
+		 var oppositeInType = Blockly.OPPOSITE_TYPE[inCon.type];
+		 var consIn = draggingWorkspace.connectionDBList[oppositeInType];
+		 consIn.forEach(function(otherConn) {
+			if (outCon.checkType_(otherConn) && // type must match
+			 !otherConn.typeHighlightSvgPath && // only highlight if not already highlighted
+			 !this_.isParentOf(otherConn.sourceBlock_) // don't highlight childblocks
+			) {
+			 // Add the highlight and save the node so we can remove it later
+			 if (((typ == "statement") || (typ == "input") && !(otherConn.targetConnection)))
+				otherConn.typeHighlightSvgPath = otherConn.typeHighlight();
+			 else
+				otherConn.typeHighlightSvgPath = otherConn.typeHighlight('blocklyOccupiedTypeHighlightedConnectionPath');
+			}
+		 });
+
+		}
    }
   });
   // Call googles onMouseMove_() so it can do the rest

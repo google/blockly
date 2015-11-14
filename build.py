5efe2dd71f929985c16169075c792e29a6f0abdb
Immortalin <immortalin@icloud.com>
2015-11-14 22:23:40 +0800

    fixed npm closure library issue

--- build.py
+++ build.py
@@ -450,9 +450,11 @@ if __name__ == "__main__":
             "Please rename this directory.")
     elif os.path.isdir(os.path.join(os.path.pardir, "google-closure-library")):
       # When Closure is installed by npm, it is named "google-closure-library".
-      print("Error: Closure directory needs to be renamed from"
-            "'google-closure-library' to 'closure-library'.\n"
-            "Please rename this directory.")
+    calcdeps = import_path(os.path.join(
+        os.path.pardir, "closure-library", "closure", "bin", "calcdeps.py"))
+      # print("Error: Closure directory needs to be renamed from"
+      #       "'google-closure-library' to 'closure-library'.\n"
+      #       "Please rename this directory.")
     else:
       print("""Error: Closure not found.  Read this:
 https://developers.google.com/blockly/hacking/closure""")

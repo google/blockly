6edddc6433eaf3fd8df23ebe8299b911650ffc82
Madeeha <madeeha.ghori@google.com>
2016-02-05 14:28:57 -0800

    adding a folder in demos for blind and setting up the package.json file for development. Adding package.json to .gitignore

---
+++ demos/blind/app/boot.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+document.addEventListener('DOMContentLoaded', function() {
+    ng.platform.browser.bootstrap(app.WorkspaceView);
+  });
+
+var option = document.getElementById("selected");
+// (function(app) {
+//   document.addEventListener('DOMContentLoaded', function() {
+//     ng.platform.browser.bootstrap(app.AppComponent);
+//   });
+// })(window.app || (window.app = {}));

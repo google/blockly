6d55e14bfb3c736c47be4fe03b6e64a8c1025baf
Justin Huang <jstn@cs.washington.edu>
2016-03-02 18:19:28 -0800

    Add robot files.

---
+++ blocks/robot.js
@@ -0,0 +1,193 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Robot blocks for Blockly.
+ * @author jstn@cs.washington.edu (Justin Huang)
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.robot');
+
+goog.require('Blockly.Blocks');
+
+Blockly.Blocks['robot_display_message_h1h2'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("display message");
+    this.appendValueInput("H1TEXT")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("big text");
+    this.appendValueInput("H2TEXT")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("regular text");
+    this.appendValueInput("TIMEOUT")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("timeout (s)");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(260);
+    this.setTooltip('Displays a message with both big and regular size text.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['robot_display_ask_multiple_choice'] = {
+  init: function() {
+    this.appendValueInput("QUESTION")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("ask multiple choice question");
+    this.appendValueInput("CHOICES")
+        .setCheck("Array")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("list of choices");
+    this.appendValueInput("TIMEOUT")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("timeout (seconds)");
+    this.setOutput(true, "String");
+    this.setColour(260);
+    this.setTooltip('Asks a multiple choice question. The user\'s choice is returned.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['robot_movement_go_to'] = {
+  init: function() {
+    this.appendValueInput("LOCATION")
+        .setCheck("String")
+        .appendField("go to");
+    this.setOutput(true, "Boolean");
+    this.setColour(160);
+    this.setTooltip('Makes the robot go to a named location.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.robot = Blockly.robot || {};
+Blockly.robot.locations = [['<Select a location>', '<Select a location>']];
+Blockly.robot.getLocations = function() {
+  var options = [];
+  var client = new ROSLIB.Service({
+    ros: ROS,
+    name: '/location_db/list',
+    serviceType : 'location_server/ListPoses'
+  });
+
+  var request = new ROSLIB.ServiceRequest({});
+  client.callService(request, function(result) {
+    for (var i=0; i<result.names.length; ++i) {
+      var name = result.names[i];
+      options.push([name, name]);
+    }
+    Blockly.robot.locations = options;
+    return options;
+  });
+};
+
+Blockly.robot.getLocations();
+
+Blockly.Blocks['robot_movement_locations'] = {
+  init: function() {
+    Blockly.robot.getLocations();
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(Blockly.robot.locations), "NAME");
+    this.setOutput(true, "String");
+    this.setColour(160);
+    this.setTooltip('The list of locations the robot knows about.');
+    this.setHelpUrl('');
+  },
+
+  onchange: function() {
+    if (this.getFieldValue('NAME') === '<Select a location>') {
+      this.setWarningText('Select a location from the list.');
+    } else {
+      this.setWarningText(null);
+    }
+  },
+};
+
+Blockly.Blocks['robot_movement_go_to_dock'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("go to dock");
+    this.setOutput(true, "Boolean");
+    this.setColour(160);
+    this.setTooltip('Makes the robot go to its dock and charge.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['robot_sound_say'] = {
+  init: function() {
+    this.appendValueInput("TEXT")
+        .setCheck("String")
+        .appendField("say");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(65);
+    this.setTooltip('Makes the robot say something.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['robot_head_look_angles'] = {
+  init: function() {
+    Blockly.FieldAngle.CLOCKWISE = false;
+    Blockly.FieldAngle.OFFSET = 90;
+    this.appendDummyInput()
+        .appendField("look up")
+        .appendField(new Blockly.FieldAngle('0'), "UP")
+        .appendField("and left")
+        .appendField(new Blockly.FieldAngle('0'), "LEFT");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(20);
+    this.setTooltip('Look some number of degrees up and/or to the left.');
+    this.setHelpUrl('');
+  }
+};
+
+
+Blockly.Blocks['robot_perception_find_objects'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("look for objects");
+    this.setOutput(true, "Array");
+    this.setColour(230);
+    this.setTooltip('Makes the robot look for objects where it\'s looking.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['robot_object_attributes'] = {
+  init: function() {
+    this.appendValueInput("OBJECT")
+        .appendField(new Blockly.FieldDropdown([["x of object", "X"], ["y of object", "Y"], ["z of object", "Z"], ["long side length of object", "LONGSIDELENGTH"], ["short side length of object", "SHORTSIDELENGTH"], ["height of object", "HEIGHT"]]), "ATTRIBUTE");
+    this.setOutput(true);
+    this.setColour(230);
+    this.setTooltip('Gets an attribute about the given object.');
+    this.setHelpUrl('');
+  }
+};
---
+++ generators/javascript/robot.js
@@ -0,0 +1,103 @@
+/**
+ * @license
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Generating JavaScript for robot blocks.
+ * @author jstn@cs.washington.edu (Justin Huang)
+ */
+'use strict';
+
+goog.provide('Blockly.JavaScript.robot');
+
+goog.require('Blockly.JavaScript');
+
+Blockly.JavaScript['robot_display_message_h1h2'] = function(block) {
+  var value_h1text = Blockly.JavaScript.valueToCode(block, 'H1TEXT', Blockly.JavaScript.ORDER_COMMA) || '\'\'';
+  var value_h2text = Blockly.JavaScript.valueToCode(block, 'H2TEXT', Blockly.JavaScript.ORDER_COMMA) || '\'\'';
+  var value_timeout = Blockly.JavaScript.valueToCode(block, 'TIMEOUT', Blockly.JavaScript.ORDER_COMMA) || 0;
+  var code = 'robot.displayMessage(' + value_h1text + ', ' + value_h2text + ', ' + value_timeout + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['robot_display_ask_multiple_choice'] = function(block) {
+  var value_question = Blockly.JavaScript.valueToCode(block, 'QUESTION', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_choices = Blockly.JavaScript.valueToCode(block, 'CHOICES', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_timeout = Blockly.JavaScript.valueToCode(block, 'TIMEOUT', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'robot.askMultipleChoice(' + value_question + ', ' + value_choices + ', ' + value_timeout + ')';
+  return [code, Blockly.JavaScript.FUNCTION_CALL];
+};
+
+Blockly.JavaScript['robot_movement_go_to'] = function(block) {
+  var value_location = Blockly.JavaScript.valueToCode(block, 'LOCATION', Blockly.JavaScript.ORDER_NONE) || '\'\'';
+  var code = 'robot.goTo(' + value_location + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript['robot_movement_locations'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  var code = Blockly.JavaScript.quote_(dropdown_name);
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['robot_movement_go_to_dock'] = function(block) {
+  var code = 'robot.goToDock()';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript['robot_sound_say'] = function(block) {
+  var value_text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_ATOMIC) || '\'\'';
+  var code = 'robot.say(' + value_text + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['robot_head_look_angles'] = function(block) {
+  var angle_up = block.getFieldValue('UP') || '0';
+  var angle_left = block.getFieldValue('LEFT') || '0';
+  var code = 'robot.lookAtDegrees(' + angle_up + ', ' + angle_left + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['robot_perception_find_objects'] = function(block) {
+  var code = 'robot.findObjects()';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript['robot_object_attributes'] = function(block) {
+  var dropdown_attribute = block.getFieldValue('ATTRIBUTE') || '';
+  var value_object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC) || 'null';
+  var code;
+  if (dropdown_attribute === 'X') {
+    code = value_object + '.pose.pose.position.x';
+  } else if (dropdown_attribute === 'Y') {
+    code = value_object + '.pose.pose.position.y';
+  } else if (dropdown_attribute === 'Z') {
+    code = value_object + '.pose.pose.position.z';
+  } else if (dropdown_attribute === 'LONGSIDEATTRIBUTE') {
+    code = value_object + '.scale.x';
+  } else if (dropdown_attribute === 'SHORTSIDEATTRIBUTE') {
+    code = value_object + '.scale.y';
+  } else if (dropdown_attribute === 'HEIGHT') {
+    code = value_object + '.scale.z';
+  } else {
+    code = 'null'; 
+  }
+
+  return [code, Blockly.JavaScript.ORDER_MEMBER];
+};

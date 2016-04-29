dcd5ed1d9c960c884afdec0a7c1bba80a4a169b0
Madeeha <madeeha.ghori@google.com>
2016-04-29 13:08:30 -0700

    removing all demo stuff for a later pull request

--- demos/blind/tester_demo/app/boot.js
+++
@@ -1,7 +0,0 @@
-var app = app || {};
-
-document.addEventListener('DOMContentLoaded', function() {
-    ng.platform.browser.bootstrap(app.WorkspaceView);
-  });
-
-var option = document.getElementById('selected');

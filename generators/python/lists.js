5f25f7650e9d560724f14070351ecdad6be0567b
kozbial <kozbial@google.com>
2016-06-17 15:03:39 -0700

    Helper function for Python lists
    Adding helper function for casting to int for indices.

--- generators/python/lists.js
+++ generators/python/lists.js
@@ -47,34 +47,34 @@ Blockly.Python['lists_create_with'] = function(block) {
 
 Blockly.Python['lists_repeat'] = function(block) {
   // Create a list with one element repeated.
-  var argument0 = Blockly.Python.valueToCode(block, 'ITEM',
+  var item = Blockly.Python.valueToCode(block, 'ITEM',
       Blockly.Python.ORDER_NONE) || 'None';
-  var argument1 = Blockly.Python.valueToCode(block, 'NUM',
+  var times = Blockly.Python.valueToCode(block, 'NUM',
       Blockly.Python.ORDER_MULTIPLICATIVE) || '0';
-  var code = '[' + argument0 + '] * ' + argument1;
+  var code = '[' + item + '] * ' + times;
   return [code, Blockly.Python.ORDER_MULTIPLICATIVE];
 };
 
 Blockly.Python['lists_length'] = function(block) {
   // String or array length.
-  var argument0 = Blockly.Python.valueToCode(block, 'VALUE',
+  var list = Blockly.Python.valueToCode(block, 'VALUE',
       Blockly.Python.ORDER_NONE) || '[]';
-  return ['len(' + argument0 + ')', Blockly.Python.ORDER_FUNCTION_CALL];
+  return ['len(' + list + ')', Blockly.Python.ORDER_FUNCTION_CALL];
 };
 
 Blockly.Python['lists_isEmpty'] = function(block) {
   // Is the string null or array empty?
-  var argument0 = Blockly.Python.valueToCode(block, 'VALUE',
+  var list = Blockly.Python.valueToCode(block, 'VALUE',
       Blockly.Python.ORDER_NONE) || '[]';
-  var code = 'not len(' + argument0 + ')';
+  var code = 'not len(' + list + ')';
   return [code, Blockly.Python.ORDER_LOGICAL_NOT];
 };
 
 Blockly.Python['lists_indexOf'] = function(block) {
   // Find an item in the list.
-  var argument0 = Blockly.Python.valueToCode(block, 'FIND',
+  var item = Blockly.Python.valueToCode(block, 'FIND',
       Blockly.Python.ORDER_NONE) || '[]';
-  var argument1 = Blockly.Python.valueToCode(block, 'VALUE',
+  var list = Blockly.Python.valueToCode(block, 'VALUE',
       Blockly.Python.ORDER_MEMBER) || '\'\'';
   if (Blockly.Python.ONE_BASED_INDEXING) {
     var exceptionIndex = ' 0';
@@ -92,26 +92,56 @@ Blockly.Python['lists_indexOf'] = function(block) {
          '  try: theIndex = myList.index(elem)' + firstIndexAdjustment,
          '  except: theIndex =' + exceptionIndex,
          '  return theIndex']);
-    var code = functionName + '(' + argument1 + ', ' + argument0 + ')';
-    return [code, Blockly.Python.ORDER_FUNCTION_CALL];
-  } else {
-    var functionName = Blockly.Python.provideFunction_(
-        'last_index',
-        ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(myList, elem):',
-         '  try: theIndex = len(myList) - myList[::-1].index(elem)' +
-           lastIndexAdjustment,
-         '  except: theIndex =' + exceptionIndex,
-         '  return theIndex']);
-    var code = functionName + '(' + argument1 + ', ' + argument0 + ')';
+    var code = functionName + '(' + list + ', ' + item + ')';
     return [code, Blockly.Python.ORDER_FUNCTION_CALL];
   }
+  var functionName = Blockly.Python.provideFunction_(
+      'last_index',
+      ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(myList, elem):',
+       '  try: theIndex = len(myList) - myList[::-1].index(elem)' +
+         lastIndexAdjustment,
+       '  except: theIndex =' + exceptionIndex,
+       '  return theIndex']);
+  var code = functionName + '(' + list + ', ' + item + ')';
+  return [code, Blockly.Python.ORDER_FUNCTION_CALL];
 };
+
+/**
+ *
+ * @param {string} at The index to adjust
+ * @param {number} opt_delta
+ * @param {boolean} opt_negate
+ * @returns {string|number}
+ */
+Blockly.Python.castInt = function(at, opt_delta, opt_negate) {
+  var delta = opt_delta || 0;
+  if (Blockly.isNumber(at)) {
+    // If the index is a naked number, adjust it right now.
+    at = parseInt(at, 10) + delta;
+    if (opt_negate) {
+      at = -at;
+    }
+  } else {
+    // If the index is dynamic, adjust it in code.
+    if (delta > 0) {
+      at = 'int(' + at + ' + ' + delta + ')';
+    } else if (delta < 0) {
+      at = 'int(' + at + ' - ' + -delta + ')';
+    } else {
+      at = 'int(' + at + ')';
+    }
+    if (opt_negate) {
+      at = '-' + at;
+    }
+  }
+  return at;
+};
+
 
 Blockly.Python['lists_getIndex'] = function(block) {
   // Get element at index.
   // Note: Until January 2013 this block did not have MODE or WHERE inputs.
   var mode = block.getFieldValue('MODE') || 'GET';
-  var where = block.getFieldValue('WHERE') || 'FROM_START';
   var defaultAtIndex = (Blockly.Python.ONE_BASED_INDEXING) ? '1' : '0';
   // Special case to avoid wrapping function calls in unneeded parenthesis.
   // func()[0] is prefered over (func())[0]
@@ -120,87 +150,67 @@ Blockly.Python['lists_getIndex'] = function(block) {
       Blockly.Python.ORDER_NONE : Blockly.Python.ORDER_MEMBER;
   var list = Blockly.Python.valueToCode(block, 'VALUE', order) || '[]';
 
-  switch (where) {
+  switch (block.getFieldValue('WHERE') || 'FROM_START') {
     case 'FIRST':
       if (mode == 'GET') {
         var code = list + '[0]';
         return [code, Blockly.Python.ORDER_MEMBER];
-      } else {
+      } else if (mode == 'GET_REMOVE') {
         var code = list + '.pop(0)';
-        if (mode == 'GET_REMOVE') {
-          return [code, Blockly.Python.ORDER_FUNCTION_CALL];
-        } else if (mode == 'REMOVE') {
-          return code + '\n';
-        }
+        return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+      } else if (mode == 'REMOVE') {
+        return code + '.pop(0)\n';
       }
       break;
     case 'LAST':
         if (mode == 'GET') {
           var code = list + '[-1]';
           return [code, Blockly.Python.ORDER_MEMBER];
-        } else {
+        } else if (mode == 'GET_REMOVE') {
           var code = list + '.pop()';
-          if (mode == 'GET_REMOVE') {
-            return [code, Blockly.Python.ORDER_FUNCTION_CALL];
-          } else if (mode == 'REMOVE') {
-            return code + '\n';
-          }
+          return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+        } else if (mode == 'REMOVE') {
+          return code + '.pop()\n';
         }
       break;
     case 'FROM_START':
-        // Adjust index if using one-based indices.
         if (Blockly.Python.ONE_BASED_INDEXING) {
           var at = Blockly.Python.valueToCode(block, 'AT',
               Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-          if (Blockly.isNumber(at)) {
-            // If the index is a naked number, decrement it right now.
-            at = parseInt(at, 10) - 1;
-          } else {
-            // If the index is dynamic, decrement it in code.
-            at = 'int(' + at + ' - 1)';
-          }
+          at = Blockly.Python.castInt(at, -1);
         } else {
           var at = Blockly.Python.valueToCode(block, 'AT',
               Blockly.Python.ORDER_NONE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at);
         }
         if (mode == 'GET') {
           var code = list + '[' + at + ']';
           return [code, Blockly.Python.ORDER_MEMBER];
-        } else {
+        } else if (mode == 'GET_REMOVE') {
           var code = list + '.pop(' + at + ')';
-          if (mode == 'GET_REMOVE') {
-            return [code, Blockly.Python.ORDER_FUNCTION_CALL];
-          } else if (mode == 'REMOVE') {
-            return code + '\n';
-          }
+          return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+        } else if (mode == 'REMOVE') {
+          return list + '.pop(' + at + ')\n';
         }
       break;
     case'FROM_END':
-        // Adjust index if not using one-based indices.
-        if (!Blockly.Python.ONE_BASED_INDEXING) {
+        if (Blockly.Python.ONE_BASED_INDEXING) {
           var at = Blockly.Python.valueToCode(block, 'AT',
-              Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-          if (Blockly.isNumber(at)) {
-            // If the index is a naked number, increment it right now.
-            at = parseInt(at, 10) + 1;
-          } else {
-            // If the index is dynamic, increment it in code.
-            at = 'int(' + at + ' + 1)';
-          }
+                  Blockly.Python.ORDER_NONE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at, 0, true);
         } else {
           var at = Blockly.Python.valueToCode(block, 'AT',
-              Blockly.Python.ORDER_UNARY_SIGN) || defaultAtIndex;
+                  Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at, 1, true);
         }
         if (mode == 'GET') {
           var code = list + '[-' + at + ']';
           return [code, Blockly.Python.ORDER_MEMBER];
-        } else {
+        } else if (mode == 'GET_REMOVE') {
           var code = list + '.pop(-' + at + ')';
-          if (mode == 'GET_REMOVE') {
-            return [code, Blockly.Python.ORDER_FUNCTION_CALL];
-          } else if (mode == 'REMOVE') {
-            return code + '\n';
-          }
+          return [code, Blockly.Python.ORDER_FUNCTION_CALL];
+        } else if (mode == 'REMOVE') {
+          return list + '.pop(-' + at + ')\n';
         }
       break;
     case 'RANDOM':
@@ -264,20 +274,14 @@ Blockly.Python['lists_setIndex'] = function(block) {
         }
       break;
     case 'FROM_START':
-        // Adjust index if using one-based indices.
         if (Blockly.Python.ONE_BASED_INDEXING) {
           var at = Blockly.Python.valueToCode(block, 'AT',
               Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-          if (Blockly.isNumber(at)) {
-            // If the index is a naked number, decrement it right now.
-            at = parseInt(at, 10) - 1;
-          } else {
-            // If the index is dynamic, decrement it in code.
-            at = 'int(' + at + ' - 1)';
-          }
+          at = Blockly.Python.castInt(at, -1);
         } else {
           var at = Blockly.Python.valueToCode(block, 'AT',
               Blockly.Python.ORDER_NONE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at);
         }
         if (mode == 'SET') {
           return list + '[' + at + '] = ' + value + '\n';
@@ -286,25 +290,19 @@ Blockly.Python['lists_setIndex'] = function(block) {
         }
       break;
     case 'FROM_END':
-        // Adjust index if not using one-based indices.
-        if (!Blockly.Python.ONE_BASED_INDEXING) {
+        if (Blockly.Python.ONE_BASED_INDEXING) {
           var at = Blockly.Python.valueToCode(block, 'AT',
-              Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-          if (Blockly.isNumber(at)) {
-            // If the index is a naked number, increment it right now.
-            at = parseInt(at, 10) + 1;
-          } else {
-            // If the index is dynamic, increment it in code.
-            at = 'int(' + at + ' + 1)';
-          }
+                  Blockly.Python.ORDER_NONE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at, 0, true);
         } else {
           var at = Blockly.Python.valueToCode(block, 'AT',
-              Blockly.Python.ORDER_UNARY_SIGN) || defaultAtIndex;
+                  Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
+          at = Blockly.Python.castInt(at, 1, true);
         }
         if (mode == 'SET') {
-          return list + '[-' + at + '] = ' + value + '\n';
+          return list + '[' + at + '] = ' + value + '\n';
         } else if (mode == 'INSERT') {
-          return list + '.insert(-' + at + ', ' + value + ')\n';
+          return list + '.insert(' + at + ', ' + value + ')\n';
         }
       break;
     case 'RANDOM':
@@ -335,48 +333,28 @@ Blockly.Python['lists_getSublist'] = function(block) {
         var at1 = '';
       break;
     case 'FROM_START':
-      // Adjust index based on indexing.
       if (Blockly.Python.ONE_BASED_INDEXING) {
         var at1 = Blockly.Python.valueToCode(block, 'AT1',
             Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-        if (Blockly.isNumber(at1)) {
-          // If the index is a naked number, decrement it right now.
-          at1 = parseInt(at1, 10) - 1;
-        } else {
-          // If the index is dynamic, decrement it in code.
-          at1 = 'int(' + at1 + ' - 1)';
-        }
+        at1 = Blockly.Python.castInt(at1, -1);
       } else {
         var at1 = Blockly.Python.valueToCode(block, 'AT1',
             Blockly.Python.ORDER_NONE) || defaultAtIndex;
-        if (! Blockly.isNumber(at1)) {
-          at1 = 'int(' + at1 + ')';
-        }
+        at1 = Blockly.Python.castInt(at1);
       }
       if (at1 == '0') {
         at1 = '';
       }
       break;
     case 'FROM_END':
-      // Adjust index based on indexing.
       if (Blockly.Python.ONE_BASED_INDEXING) {
         var at1 = Blockly.Python.valueToCode(block, 'AT1',
                 Blockly.Python.ORDER_NONE) || defaultAtIndex;
-        if (Blockly.isNumber(at1)) {
-          at1 = -parseInt(at1, 10);
-        } else {
-          at1 = '-int(' + at1 + ')';
-        }
+        at1 = Blockly.Python.castInt(at1, 0, true);
       } else {
         var at1 = Blockly.Python.valueToCode(block, 'AT1',
                 Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-        if (Blockly.isNumber(at1)) {
-          // If the index is a naked number, increment it right now.
-          at1 = -parseInt(at1, 10) + 1;
-        } else {
-          // If the index is dynamic, increment it in code.
-          at1 = 'int(1 -' + at1 + ')';
-        }
+        at1 = Blockly.Python.castInt(at1, 1, true);
       }
       break;
     default:
@@ -387,48 +365,34 @@ Blockly.Python['lists_getSublist'] = function(block) {
       var at2 = '';
       break;
     case 'FROM_END':
-      // Adjust index based on indexing.
-      if (! Blockly.Python.ONE_BASED_INDEXING) {
+      if (Blockly.Python.ONE_BASED_INDEXING) {
         var at2 = Blockly.Python.valueToCode(block, 'AT2',
-            Blockly.Python.ORDER_NONE) || defaultAtIndex;
-        if (Blockly.isNumber(at2)) {
-          at2 = -parseInt(at2, 10);
-        } else {
-          Blockly.Python.definitions_['import_sys'] = 'import sys';
-          at2 = '-int(' + at2 + ') or sys.maxsize';
-        }
+                Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
+        at2 = Blockly.Python.castInt(at2, -1, true);
       } else {
         var at2 = Blockly.Python.valueToCode(block, 'AT2',
-            Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-        if (Blockly.isNumber(at2)) {
-          // If the index is a naked number, adjust it right now.
-          at2 = 1 - parseInt(at2, 10);
-        } else {
-          // If the index is dynamic, adjust it in code.
-          Blockly.Python.definitions_['import_sys'] = 'import sys';
-          at2 = 'int(1 - ' + at2 + ') or sys.maxsize';
-        }
+                Blockly.Python.ORDER_NONE) || defaultAtIndex;
+        at2 = Blockly.Python.castInt(at2, 0, true);
       }
-      if (at2 == '0') {
+      // Ensure that if the result calculated is 0 that sub-sequence will
+      // include all elements
+      if (!Blockly.isNumber(String(at2))) {
+        Blockly.Python.definitions_['import_sys'] = 'import sys';
+        at2 += ' or sys.maxsize';
+      }
+      else if (at2 == '0') {
         at2 = '';
       }
       break;
     case 'FROM_START':
-      // Adjust index based on indexing.
       if (Blockly.Python.ONE_BASED_INDEXING) {
         var at2 = Blockly.Python.valueToCode(block, 'AT2',
             Blockly.Python.ORDER_NONE) || defaultAtIndex;
-        if (! Blockly.isNumber(at2)) {
-          at2 = 'int(' + at2 + ')';
-        }
+        at2 = Blockly.Python.castInt(at2);
       } else {
         var at2 = Blockly.Python.valueToCode(block, 'AT2',
             Blockly.Python.ORDER_ADDITIVE) || defaultAtIndex;
-        if (Blockly.isNumber(at2)) {
-          at2 = parseInt(at2, 10) + 1;
-        } else {
-          at2 = 'int(' + at2 + ' + 1)';
-        }
+        at2 = Blockly.Python.castInt(at2, 1);
       }
       break;
     default:
@@ -447,13 +411,13 @@ Blockly.Python['lists_sort'] = function(block) {
   var sortFunctionName = Blockly.Python.provideFunction_('lists_sort',
   ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ +
       '(listv, type, reversev):',
-    '  def tryfloat(s):',
+    '  def try_float(s):',
     '    try:',
     '      return float(s)',
     '    except:',
     '      return 0',
     '  keyFuncts = {',
-    '    "NUMERIC": tryfloat,',
+    '    "NUMERIC": try_float,',
     '    "TEXT": str,',
     '    "IGNORE_CASE": lambda s: str(s).lower()',
     '  }',

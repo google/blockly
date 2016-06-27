bbf36c160c39ed0186e335c54718e57c06b1c9b7
kozbial <kozbial@google.com>
2016-06-27 16:53:10 -0700

    Fixing order constant order
    Removing operator () that was incorrect and addiung ~,

--- generators/lua.js
+++ generators/lua.js
@@ -77,7 +77,7 @@ Blockly.Lua.ORDER_ATOMIC = 0;          // literals
 // The next level was not explicit in documentation and inferred by Ellen.
 Blockly.Lua.ORDER_HIGH = 1;            // Function calls, tables[]
 Blockly.Lua.ORDER_EXPONENTIATION = 2;  // ^
-Blockly.Lua.ORDER_UNARY = 3;           // not # - ()
+Blockly.Lua.ORDER_UNARY = 3;           // not # - ~
 Blockly.Lua.ORDER_MULTIPLICATIVE = 4;  // * / %
 Blockly.Lua.ORDER_ADDITIVE = 5;        // + -
 Blockly.Lua.ORDER_CONCATENATION = 6;   // ..

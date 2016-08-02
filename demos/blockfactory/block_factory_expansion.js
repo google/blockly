22c22ede0d27cbf7ca8fdab1cf1ce3f8f09a2be3
quachtina96 <quacht@mit.edu>
2016-08-01 18:35:07 -0700

    working dropdown selection

--- demos/blockfactory/block_factory_expansion.js
+++ demos/blockfactory/block_factory_expansion.js
@@ -140,7 +140,7 @@ BlockFactoryExpansion.prototype.assignLibraryClickHandlers = function() {
       });
 
   var dropdown = document.getElementById('blockLibraryDropdown');
-  dropdown.addEventListener('changed',
+  dropdown.addEventListener('change',
       function() {
         self.onSelectedBlockChanged(dropdown);
       });

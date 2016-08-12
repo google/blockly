70ff4291aff4cb59c5318fdb7636e2ddbe16ee9e
Rachel Fenichel <fenichel@google.com>
2016-08-12 11:09:32 -0700

    Double quotes around variable names

--- msg/messages.js
+++ msg/messages.js
@@ -54,8 +54,6 @@ goog.require('Blockly.Msg');
 Blockly.Msg.VARIABLES_DEFAULT_NAME = 'item';
 /// button text - Button that sets a calendar to today's date.\n{{Identical|Today}}
 Blockly.Msg.TODAY = 'Today';
-/// dropdown choice - Delete the currently selected variable.
-Blockly.Msg.DELETE_VARIABLE = 'Delete the "%1" variable';
 
 // Context menus.
 /// context menu - Make a copy of the selected block (and any blocks it contains).\n{{Identical|Duplicate}}
@@ -106,10 +104,14 @@ Blockly.Msg.NEW_VARIABLE_TITLE = 'New variable name:';
 Blockly.Msg.RENAME_VARIABLE = 'Rename variable...';
 /// prompt - Prompts the user to enter the new name for the selected variable.  See [https://github.com/google/blockly/wiki/Variables#dropdown-menu https://github.com/google/blockly/wiki/Variables#dropdown-menu].\n\nParameters:\n* %1 - the name of the variable to be renamed.
 Blockly.Msg.RENAME_VARIABLE_TITLE = 'Rename all "%1" variables to:';
+
+// Variable deletion.
 /// confirm -  Ask the user to confirm their deletion of multiple uses of a variable.
-Blockly.Msg.DELETE_VARIABLE_CONFIRMATION = 'Delete %1 uses of the %2 variable?';
+Blockly.Msg.DELETE_VARIABLE_CONFIRMATION = 'Delete %1 uses of the "%2" variable?';
 /// alert - Tell the user that they can't delete a variable because it's part of the definition of a procedure.
-Blockly.Msg.CANNOT_DELETE_VARIABLE_PROCEDURE = 'Can\'t delete the variable \'%1\' because it is part of the definition of the procedure \'%2\'';
+Blockly.Msg.CANNOT_DELETE_VARIABLE_PROCEDURE = 'Can\'t delete the variable "%1" because it is part of the definition of the procedure "%2"';
+/// dropdown choice - Delete the currently selected variable.
+Blockly.Msg.DELETE_VARIABLE = 'Delete the "%1" variable';
 
 // Colour Blocks.
 /// url - Information about colour.

46fd252dd3a206d9ab35c3ff8ea7759a9aa397ec
quachtina96 <quacht@mit.edu>
2016-08-15 16:48:43 -0700

    added warning if a block type already exists in blockly standard category

--- demos/blocklyfactory/factory.js
+++ demos/blocklyfactory/factory.js
@@ -60,6 +60,28 @@ BlockFactory.UNNAMED = 'unnamed';
 BlockFactory.oldDir = null;
 
 /**
+ * List of block types in standard categories.
+ * TODO(quachtina96): Get this list from FactoryController since each
+ * FactoryControlelr object has a dictionary containing its standard categories.
+ */
+ BlockFactory.standardBlockTypes = ["controls_if", "logic_compare",
+    "logic_operation", "logic_negate", "logic_boolean", "logic_null",
+    "logic_ternary", "controls_repeat_ext", "controls_whileUntil",
+    "controls_for", "controls_forEach", "controls_flow_statements",
+    "math_number", "math_arithmetic", "math_single", "math_trig",
+    "math_constant", "math_number_property", "math_change", "math_round",
+    "math_on_list", "math_modulo", "math_constrain", "math_random_int",
+    "math_random_float", "text", "text_join", "text_append", "text_length",
+    "text_isEmpty", "text_indexOf", "variables_get", "text_charAt",
+    "text_getSubstring", "text_changeCase", "text_trim", "text_print",
+    "text_prompt_ext", "colour_picker", "colour_random", "colour_rgb",
+    "colour_blend", "lists_create_with", "lists_repeat", "lists_length",
+    "lists_isEmpty", "lists_indexOf", "lists_getIndex", "lists_setIndex",
+    "lists_getSublist", "lists_split", "lists_sort", "variables_set",
+    "procedures_defreturn", "procedures_ifreturn", "procedures_defnoreturn",
+    "procedures_callreturn"];
+
+/**
  * Inject code into a pre tag, with syntax highlighting.
  * Safe from HTML/script injection.
  * @param {string} code Lines of code.
@@ -128,7 +150,8 @@ BlockFactory.updateGenerator = function(block) {
 };
 
 /**
- * Update the preview display.
+ * Update the preview display. Add warning to the factory_base block in the
+ * main workspace if the block type is a standard block.
  */
 BlockFactory.updatePreview = function() {
   // Toggle between LTR/RTL if needed (also used in first display).
@@ -210,8 +233,31 @@ BlockFactory.updatePreview = function() {
     previewBlock.moveBy(15, 10);
     BlockFactory.previewWorkspace.clearUndo();
     BlockFactory.updateGenerator(previewBlock);
+
+    // Warn user if their block type is already exists in Blockly's standard
+    // library.
+    if (BlockFactory.isStandardBlockType(blockType)) {
+      var rootBlock = FactoryUtils.getRootBlock(BlockFactory.mainWorkspace);
+      rootBlock.setWarningText('A standard Blockly.Block already exists ' +
+          'under this name.');
+    }
   } finally {
     Blockly.Blocks = backupBlocks;
+  }
+};
+
+/**
+ * Returns whether or not the block type already exists in Block Factory's list
+ * of standard blocks.
+ *
+ * @param {!string} blockType - Type of block.
+ * @return {boolean} Whether or not the block type is a standard block type.
+ */
+BlockFactory.isStandardBlockType = function(blockType) {
+  for (var i = 0; i < BlockFactory.standardBlockTypes.length; i++) {
+    if (blockType == BlockFactory.standardBlockTypes[i]) {
+      return true;
+    }
   }
 };
 

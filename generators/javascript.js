444f07b1b327f781a9fe5c3592ee50b65d1c5375
wingcd <wingcd@foxmail.com>
2016-08-24 09:47:07 +0800

    Update javascript.js
    add prevent next block  
    so we can do like this:
    define a function in current block
    function foo(){
         //do some thing
    }
    
    and just return code like this: foo();

--- generators/javascript.js
+++ generators/javascript.js
@@ -254,7 +254,8 @@ Blockly.JavaScript.scrub_ = function(block, code) {
       }
     }
   }
-  var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
+  //when prevent next block,we can get just self block code
+  var nextBlock = block.preventNext ? null : block.nextConnection && block.nextConnection.targetBlock();
   var nextCode = Blockly.JavaScript.blockToCode(nextBlock);
   return commentCode + code + nextCode;
 };

dee0cd6e5c70ad997218f0908b1c4fe5621270f7
Emma Dauterman <evd2014@users.noreply.github.com>
2016-08-31 14:25:23 -0700

    Bug fix for adding variables and procedures categories with prompt, removed blocks from category name, check if unsaved changes in workspace factory before leave

--- demos/blocklyfactory/app_controller.js
+++ demos/blocklyfactory/app_controller.js
@@ -625,8 +625,9 @@ AppController.prototype.onresize = function(event) {
  * before actually refreshing.
  */
 AppController.prototype.confirmLeavePage = function() {
-  if (!BlockFactory.isStarterBlock() &&
-      !FactoryUtils.savedBlockChanges(this.blockLibraryController)) {
+  if ((!BlockFactory.isStarterBlock() &&
+      !FactoryUtils.savedBlockChanges(this.blockLibraryController)) ||
+      this.workspaceFactoryController.hasUnsavedChanges()) {
     // When a string is assigned to the returnValue Event property, a dialog box
     // appears, asking the users for confirmation to leave the page.
     return 'You will lose any unsaved changes. Are you sure you want ' +

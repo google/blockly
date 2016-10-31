68f7144d71d15332cf813ea3c3078f7b8c7177fd
Katelyn Mann <picklesrus@users.noreply.github.com>
2016-10-31 11:06:43 -0700

    fix typo. disiplay= display:none to style=display:none

--- core/flyout.js
+++ core/flyout.js
@@ -288,7 +288,7 @@ Blockly.Flyout.prototype.createDom = function() {
   // Setting style to display none to start.  The toolbox and flyout
   // hide/show code  will set up the proper visibility and size later.
   this.svgGroup_ = Blockly.createSvgElement('svg',
-      {'class': 'blocklyFlyout', 'display': 'display:none'}, null);
+      {'class': 'blocklyFlyout', 'style': 'display:none'}, null);
   this.svgBackground_ = Blockly.createSvgElement('path',
       {'class': 'blocklyFlyoutBackground'}, this.svgGroup_);
   this.svgGroup_.appendChild(this.workspace_.createDom());

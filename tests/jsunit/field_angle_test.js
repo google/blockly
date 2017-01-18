e43db1f24ca822e028c77dd451d0ed6c5367c904
Andrew n marshall <Anm@anm.me>
2017-01-18 09:22:37 -0800

    Bad search & replace

--- tests/jsunit/field_angle_test.js
+++ tests/jsunit/field_angle_test.js
@@ -5,7 +5,7 @@
  * Copyright 2017 Google Inc.
  * https://developers.google.com/blockly/
  *
- * Licensed under the Apache License, Version 2.0 (the 'License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *

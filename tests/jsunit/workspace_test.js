34744012d57671b16e85b66aceeec921c57a2412
Andrew n marshall <Anm@anm.me>
2017-01-20 11:29:18 -0800

    Dispose of workspaces and blocks in finally blocks.

--- tests/jsunit/workspace_test.js
+++ tests/jsunit/workspace_test.js
@@ -21,79 +21,107 @@
 
 function test_emptyWorkspace() {
   var workspace = new Blockly.Workspace();
-  assertEquals('Empty workspace (1).', 0, workspace.getTopBlocks(true).length);
-  assertEquals('Empty workspace (2).', 0, workspace.getTopBlocks(false).length);
-  assertEquals('Empty workspace (3).', 0, workspace.getAllBlocks().length);
-  workspace.clear();
-  assertEquals('Empty workspace (4).', 0, workspace.getTopBlocks(true).length);
-  assertEquals('Empty workspace (5).', 0, workspace.getTopBlocks(false).length);
-  assertEquals('Empty workspace (6).', 0, workspace.getAllBlocks().length);
+  try {
+    assertEquals('Empty workspace (1).', 0, workspace.getTopBlocks(true).length);
+    assertEquals('Empty workspace (2).', 0, workspace.getTopBlocks(false).length);
+    assertEquals('Empty workspace (3).', 0, workspace.getAllBlocks().length);
+    workspace.clear();
+    assertEquals('Empty workspace (4).', 0, workspace.getTopBlocks(true).length);
+    assertEquals('Empty workspace (5).', 0, workspace.getTopBlocks(false).length);
+    assertEquals('Empty workspace (6).', 0, workspace.getAllBlocks().length);
+  } finally {
+    workspace.dispose();
+  }
 }
 
 function test_flatWorkspace() {
   var workspace = new Blockly.Workspace();
-  var blockA = workspace.newBlock('');
-  assertEquals('One block workspace (1).', 1, workspace.getTopBlocks(true).length);
-  assertEquals('One block workspace (2).', 1, workspace.getTopBlocks(false).length);
-  assertEquals('One block workspace (3).', 1, workspace.getAllBlocks().length);
-  var blockB = workspace.newBlock('');
-  assertEquals('Two block workspace (1).', 2, workspace.getTopBlocks(true).length);
-  assertEquals('Two block workspace (2).', 2, workspace.getTopBlocks(false).length);
-  assertEquals('Two block workspace (3).', 2, workspace.getAllBlocks().length);
-  blockA.dispose();
-  assertEquals('One block workspace (4).', 1, workspace.getTopBlocks(true).length);
-  assertEquals('One block workspace (5).', 1, workspace.getTopBlocks(false).length);
-  assertEquals('One block workspace (6).', 1, workspace.getAllBlocks().length);
-  workspace.clear();
-  assertEquals('Cleared workspace (1).', 0, workspace.getTopBlocks(true).length);
-  assertEquals('Cleared workspace (2).', 0, workspace.getTopBlocks(false).length);
-  assertEquals('Cleared workspace (3).', 0, workspace.getAllBlocks().length);
+  var blockA, blockB;
+  try {
+    blockA = workspace.newBlock('');
+    assertEquals('One block workspace (1).', 1, workspace.getTopBlocks(true).length);
+    assertEquals('One block workspace (2).', 1, workspace.getTopBlocks(false).length);
+    assertEquals('One block workspace (3).', 1, workspace.getAllBlocks().length);
+    blockB = workspace.newBlock('');
+    assertEquals('Two block workspace (1).', 2, workspace.getTopBlocks(true).length);
+    assertEquals('Two block workspace (2).', 2, workspace.getTopBlocks(false).length);
+    assertEquals('Two block workspace (3).', 2, workspace.getAllBlocks().length);
+    blockA.dispose();
+    assertEquals('One block workspace (4).', 1, workspace.getTopBlocks(true).length);
+    assertEquals('One block workspace (5).', 1, workspace.getTopBlocks(false).length);
+    assertEquals('One block workspace (6).', 1, workspace.getAllBlocks().length);
+    workspace.clear();
+    assertEquals('Cleared workspace (1).', 0, workspace.getTopBlocks(true).length);
+    assertEquals('Cleared workspace (2).', 0, workspace.getTopBlocks(false).length);
+    assertEquals('Cleared workspace (3).', 0, workspace.getAllBlocks().length);
+  } finally {
+    blockB && blockB.dispose();
+    blockA && blockA.dispose();
+    workspace.dispose();
+  }
 }
 
 function test_maxBlocksWorkspace() {
   var workspace = new Blockly.Workspace();
   var blockA = workspace.newBlock('');
   var blockB = workspace.newBlock('');
-  assertEquals('Infinite capacity.', Infinity, workspace.remainingCapacity());
-  workspace.options.maxBlocks = 3;
-  assertEquals('Three capacity.', 1, workspace.remainingCapacity());
-  workspace.options.maxBlocks = 2;
-  assertEquals('Two capacity.', 0, workspace.remainingCapacity());
-  workspace.options.maxBlocks = 1;
-  assertEquals('One capacity.', -1, workspace.remainingCapacity());
-  workspace.options.maxBlocks = 0;
-  assertEquals('Zero capacity.', -2, workspace.remainingCapacity());
-  workspace.clear();
-  assertEquals('Cleared capacity.', 0, workspace.remainingCapacity());
+  try {
+    assertEquals('Infinite capacity.', Infinity, workspace.remainingCapacity());
+    workspace.options.maxBlocks = 3;
+    assertEquals('Three capacity.', 1, workspace.remainingCapacity());
+    workspace.options.maxBlocks = 2;
+    assertEquals('Two capacity.', 0, workspace.remainingCapacity());
+    workspace.options.maxBlocks = 1;
+    assertEquals('One capacity.', -1, workspace.remainingCapacity());
+    workspace.options.maxBlocks = 0;
+    assertEquals('Zero capacity.', -2, workspace.remainingCapacity());
+    workspace.clear();
+    assertEquals('Cleared capacity.', 0, workspace.remainingCapacity());
+  } finally {
+    blockB.dispose();
+    blockA.dispose();
+    workspace.dispose();
+  }
 }
 
 function test_getWorkspaceById() {
   var workspaceA = new Blockly.Workspace();
   var workspaceB = new Blockly.Workspace();
-  assertEquals('Find workspaceA.', workspaceA,
-      Blockly.Workspace.getById(workspaceA.id));
-  assertEquals('Find workspaceB.', workspaceB,
-      Blockly.Workspace.getById(workspaceB.id));
-  assertEquals('No workspace found.', null,
-      Blockly.Workspace.getById('I do not exist.'));
-  workspaceA.dispose();
-  assertEquals('Can\'t find workspaceA.', null,
-      Blockly.Workspace.getById(workspaceA.id));
-  assertEquals('WorkspaceB exists.', workspaceB,
-      Blockly.Workspace.getById(workspaceB.id));
+  try {
+    assertEquals('Find workspaceA.', workspaceA,
+        Blockly.Workspace.getById(workspaceA.id));
+    assertEquals('Find workspaceB.', workspaceB,
+        Blockly.Workspace.getById(workspaceB.id));
+    assertEquals('No workspace found.', null,
+        Blockly.Workspace.getById('I do not exist.'));
+    workspaceA.dispose();
+    assertEquals('Can\'t find workspaceA.', null,
+        Blockly.Workspace.getById(workspaceA.id));
+    assertEquals('WorkspaceB exists.', workspaceB,
+        Blockly.Workspace.getById(workspaceB.id));
+  } finally {
+    workspaceB.dispose();
+    workspaceA.dispose();
+  }
 }
 
 function test_getBlockById() {
   var workspace = new Blockly.Workspace();
   var blockA = workspace.newBlock('');
   var blockB = workspace.newBlock('');
-  assertEquals('Find blockA.', blockA, workspace.getBlockById(blockA.id));
-  assertEquals('Find blockB.', blockB, workspace.getBlockById(blockB.id));
-  assertEquals('No block found.', null,
-      workspace.getBlockById('I do not exist.'));
-  blockA.dispose();
-  assertEquals('Can\'t find blockA.', null, workspace.getBlockById(blockA.id));
-  assertEquals('BlockB exists.', blockB, workspace.getBlockById(blockB.id));
-  workspace.clear();
-  assertEquals('Can\'t find blockB.', null, workspace.getBlockById(blockB.id));
+  try {
+    assertEquals('Find blockA.', blockA, workspace.getBlockById(blockA.id));
+    assertEquals('Find blockB.', blockB, workspace.getBlockById(blockB.id));
+    assertEquals('No block found.', null,
+        workspace.getBlockById('I do not exist.'));
+    blockA.dispose();
+    assertEquals('Can\'t find blockA.', null, workspace.getBlockById(blockA.id));
+    assertEquals('BlockB exists.', blockB, workspace.getBlockById(blockB.id));
+    workspace.clear();
+    assertEquals('Can\'t find blockB.', null, workspace.getBlockById(blockB.id));
+  } finally {
+    blockB.dispose();
+    blockA.dispose();
+    workspace.dispose();
+  }
 }

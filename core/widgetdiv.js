93655e42e2b55319c5e6e62b4c6ac5c1b54dc387
Evan W. Patton <ewpatton@mit.edu>
2017-02-20 18:20:58 -0500

    Adjust WidgetDiv height to fit on screen

--- core/widgetdiv.js
+++ core/widgetdiv.js
@@ -148,5 +148,5 @@ Blockly.WidgetDiv.position = function(anchorX, anchorY, windowSize,
   }
   Blockly.WidgetDiv.DIV.style.left = anchorX + 'px';
   Blockly.WidgetDiv.DIV.style.top = anchorY + 'px';
-  Blockly.WidgetDiv.DIV.style.height = windowSize.height + 'px';
+  Blockly.WidgetDiv.DIV.style.height = (windowSize.height - anchorY) + 'px';
 };

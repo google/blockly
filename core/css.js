541914cad59954976aa01d793d9857c831ea3c45
Rachel Fenichel <fenichel@google.com>
2017-03-01 14:09:04 -0800

    Drag flyout with background

--- core/css.js
+++ core/css.js
@@ -390,6 +390,10 @@ Blockly.Css.CONTENT = [
     'fill-opacity: .8;',
   '}',
 
+  '.blocklyTransparentBackground {',
+    'opacity: 0;',
+  '}',
+
   '.blocklyMainWorkspaceScrollbar {',
     'z-index: 20;',
   '}',

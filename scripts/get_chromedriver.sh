44e39dd4f856516ddf892929828b23b22deba2e4
shirley tan <shirleytan@google.com>
2017-04-04 13:34:53 -0700

    fix path issue again

--- scripts/get_chromedriver.sh
+++ scripts/get_chromedriver.sh
@@ -1,8 +1,10 @@
 s#!/bin/bash
 os_name=`uname`
+export SRC_DIR=`pwd`
+echo "current dir is $SRC_DIR"
 mkdir chromedriver
 if [[ $os_name == 'Linux' ]]; then
-  cd ../ && curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip | tar xz
+  cd chromedriver  && curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip | tar xz
 elif [[ $os_name == 'Darwin' ]]; then
-  cd ../chromedriver  &&  curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip  | tar xz 
+  cd chromedriver  &&  curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip  | tar xz 
 fi

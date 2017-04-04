3f9e099ec15ad66ac489a56bcf223c7e063eec68
Shirley Tan <shirleytan@nestlabs.com>
2017-04-04 08:46:18 -0700

    check java version

--- scripts/selenium_connect.sh
+++ scripts/selenium_connect.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "connect to selenium server"
 ls -l "../webdriverio-test/selenium-server-standalone-3.0.1.jar"
-java -jar -Dwebdriver.gecko.driver=../geckodriver ../webdriverio-test/selenium-server-standalone-3.0.1.jar &
+which java
+java -version
+/usr/local/google/home/shirleytan/opt/java/jdk1.8.0_74/bin/java -jar -Dwebdriver.gecko.driver=../geckodriver ../webdriverio-test/selenium-server-standalone-3.0.1.jar &

516f1264bc99f0fa021ec800363f28abf54820cc
Shirley Tan <shirleytan@nestlabs.com>
2017-04-04 16:38:14 -0700

    minor fix for getting chromedriver

--- scripts/get_chromedriver.sh
+++ scripts/get_chromedriver.sh
@@ -5,7 +5,7 @@ echo "current dir is $SRC_DIR"
 mkdir chromedriver
 ls -l chromedriver
 if [[ $os_name == 'Linux' ]]; then
-  cd chromedriver  && curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip | tar xz
+  cd chromedriver  && curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip > a.zip &&  unzip -o a.zip && rm a.zip 
 elif [[ $os_name == 'Darwin' ]]; then
   cd chromedriver  &&  curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip  | tar xz 
 fi

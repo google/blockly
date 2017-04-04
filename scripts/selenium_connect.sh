cc3092b704bc518ce0aa9cfb1380cd20110f0fa5
shirley tan <shirleytan@google.com>
2017-04-04 08:12:08 -0700

    add more debug info

--- scripts/selenium_connect.sh
+++ scripts/selenium_connect.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "connect to selenium server"
+ls -l "../webdriverio-test/selenium-server-standalone-3.0.1.jar"
 java -jar -Dwebdriver.gecko.driver=../geckodriver ../webdriverio-test/selenium-server-standalone-3.0.1.jar &

d49629750a883e02848550f64c69c33ac68437ac
shirley tan <shirleytan@google.com>
2017-04-04 10:38:58 -0700

    add logic to check if selenium is running or not

--- scripts/selenium_connect.sh
+++ scripts/selenium_connect.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "connect to selenium server"
-ls -l "../webdriverio-test/selenium-server-standalone-3.0.1.jar"
+
+#check if selenium is running, kill it if so
+pid=`lsof -ti tcp:4444`
+if [ $? -eq 0 ]
+then
+ echo "selenium is running and process id is $pid"
+  kill -9 $pid 
+fi
+  
 java -jar -Dwebdriver.gecko.driver=../geckodriver ../webdriverio-test/selenium-server-standalone-3.0.1.jar &

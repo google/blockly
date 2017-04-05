8fd180acc26711567db0cf3c1d059b1cba55eeb6
shirley tan <shirleytan@google.com>
2017-04-04 23:26:43 -0700

    update test summary

--- tests/jsunit/test_runner.js
+++ tests/jsunit/test_runner.js
@@ -21,11 +21,12 @@ browser
    var numOfFailure = regex.exec(result)[1];
   var regex2 = /Unit Tests for Blockly .*]/;
   var testStatus = regex2.exec(result)[0];
-  console.log("================Blockly Unit Test Summary=================");
+  console.log("============Blockly Unit Test Summary=================");
    console.log(testStatus);
    var regex3 = /\d+ passed,\s\d+ failed/;
    var detail = regex3.exec(result)[0];
    console.log(detail);
+  console.log("============Blockly Unit Test Summary=================");
    if ( parseInt(numOfFailure) !== 0) {
      console.log(result);
      process.exit(1);

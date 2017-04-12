e872c6805c2f90ecb58c67603562b5646c740781
shirley tan <shirleytan@google.com>
2017-04-12 11:13:41 -0700

    more debug info

--- scripts/selenium_connect.sh
+++ scripts/selenium_connect.sh
@@ -7,6 +7,6 @@ then
  echo "already running"
   kill -9 $pid 
 fi
-echo "connect to selenium server"
+echo "trying connect to selenium server"
 java -jar -Dwebdriver.gecko.driver=../geckodriver -Dwebdriver.chrome.driver="chromedriver/chromedriver" ../webdriverio-test/selenium-server-standalone-3.0.1.jar  &
 

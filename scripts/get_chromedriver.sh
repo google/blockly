1c1a9da1c7f64425758b1328224561683851eb64
Shirley Tan <shirleytan@nestlabs.com>
2017-04-13 11:04:44 -0700

    cherry pick for get_chromedriver.sh

--- scripts/get_chromedriver.sh
+++ scripts/get_chromedriver.sh
@@ -7,6 +7,10 @@ fi
 
 if [[ $os_name == 'Linux' ]]; then
   cd chromedriver  && curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip > tmp.zip &&  unzip -o tmp.zip && rm tmp.zip 
+  # wait until download finish
+  sleep 5
 elif [[ $os_name == 'Darwin' ]]; then
   cd chromedriver  &&  curl -L https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip  | tar xz 
+  # wait until download finish
+  sleep 5
 fi

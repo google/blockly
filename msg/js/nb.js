a10869a23e4a49fb0e9f12c861d0db2d4ba7513a
Rachel Fenichel <fenichel@google.com>
2017-04-18 14:03:45 -0700

    rebuild for testing

--- msg/js/nb.js
+++ msg/js/nb.js
@@ -7,6 +7,7 @@ goog.provide('Blockly.Msg.nb');
 goog.require('Blockly.Msg');
 
 Blockly.Msg.ADD_COMMENT = "Legg til kommentar";
+Blockly.Msg.CANNOT_DELETE_VARIABLE_PROCEDURE = "Can't delete the variable '%1' because it's part of the definition of the function '%2'";  // untranslated
 Blockly.Msg.CHANGE_VALUE_TITLE = "Bytt verdi:";
 Blockly.Msg.CLEAN_UP = "Rydd opp Blocks";
 Blockly.Msg.COLLAPSE_ALL = "Skjul blokker";
@@ -73,6 +74,19 @@ Blockly.Msg.EXPAND_BLOCK = "Utvid blokk";
 Blockly.Msg.EXTERNAL_INPUTS = "Eksterne kilder";
 Blockly.Msg.HELP = "Hjelp";
 Blockly.Msg.INLINE_INPUTS = "Interne kilder";
+Blockly.Msg.IOS_CANCEL = "Cancel";  // untranslated
+Blockly.Msg.IOS_ERROR = "Error";  // untranslated
+Blockly.Msg.IOS_OK = "OK";  // untranslated
+Blockly.Msg.IOS_PROCEDURES_ADD_INPUT = "+ Add Input";  // untranslated
+Blockly.Msg.IOS_PROCEDURES_ALLOW_STATEMENTS = "Allow statements";  // untranslated
+Blockly.Msg.IOS_PROCEDURES_DUPLICATE_INPUTS_ERROR = "This function has duplicate inputs.";  // untranslated
+Blockly.Msg.IOS_PROCEDURES_INPUTS = "INPUTS";  // untranslated
+Blockly.Msg.IOS_VARIABLES_ADD_BUTTON = "Add";  // untranslated
+Blockly.Msg.IOS_VARIABLES_ADD_VARIABLE = "+ Add Variable";  // untranslated
+Blockly.Msg.IOS_VARIABLES_DELETE_BUTTON = "Delete";  // untranslated
+Blockly.Msg.IOS_VARIABLES_EMPTY_NAME_ERROR = "You can't use an empty variable name.";  // untranslated
+Blockly.Msg.IOS_VARIABLES_RENAME_BUTTON = "Rename";  // untranslated
+Blockly.Msg.IOS_VARIABLES_VARIABLE_NAME = "Variable name";  // untranslated
 Blockly.Msg.LISTS_CREATE_EMPTY_HELPURL = "https://github.com/google/blockly/wiki/Lists#create-empty-list";
 Blockly.Msg.LISTS_CREATE_EMPTY_TITLE = "opprett en tom liste";
 Blockly.Msg.LISTS_CREATE_EMPTY_TOOLTIP = "Returnerer en tom liste, altså med lengde 0";

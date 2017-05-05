f8fa9748e914ba5ca1cfcb73e11271644f1c86a3
shirletan <shirleyqitan@gmail.com>
2017-05-05 13:12:53 -0700

    add osx travis test run job (#1074)

--- scripts/get_selenium.sh
+++ scripts/get_selenium.sh
@@ -6,10 +6,9 @@ if [ ! -d $DIR ]; then
   mkdir $DIR
 fi
 
+echo "downloading selenium jar"
+
 if [ ! -f $DIR/$FILE ]; then
   cd $DIR  && curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
   sleep 5
 fi
-
-
-

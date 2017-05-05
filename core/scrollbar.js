55096aeb168546541b4cc10d5e5fa1224a1d6a0e
Rachel Fenichel <fenichel@google.com>
2017-05-05 09:36:24 -0700

    Fix #1051 (#1084)

--- core/scrollbar.js
+++ core/scrollbar.js
@@ -276,7 +276,6 @@ Blockly.Scrollbar.prototype.containerVisible_ = true;
 /**
  * Width of vertical scrollbar or height of horizontal scrollbar.
  * Increase the size of scrollbars on touch devices.
- * Don't define if there is no document object (e.g. node.js).
  */
 Blockly.Scrollbar.scrollbarThickness = 15;
 if (goog.events.BrowserFeature.TOUCH_ENABLED) {

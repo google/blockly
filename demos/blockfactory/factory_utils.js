e637411343637b33d096cacc64e6a1bd0be81679
Rachel Fenichel <fenichel@google.com>
2017-05-17 14:50:50 -0700

    Fix apostrophe in tooltips and helpurls

--- demos/blockfactory/factory_utils.js
+++ demos/blockfactory/factory_utils.js
@@ -352,8 +352,8 @@ FactoryUtils.formatJavaScript_ = function(blockType, rootBlock, workspace) {
 
   var tooltip = FactoryUtils.getTooltipFromRootBlock_(rootBlock);
   var helpUrl = FactoryUtils.getHelpUrlFromRootBlock_(rootBlock);
-  code.push("    this.setTooltip('" + tooltip + "');");
-  code.push("    this.setHelpUrl('" + helpUrl + "');");
+  code.push(' this.setTooltip(' + JSON.stringify(tooltip) + ');');
+  code.push(' this.setHelpUrl(' + JSON.stringify(helpUrl) + ');');
   code.push('  }');
   code.push('};');
   return code.join('\n');

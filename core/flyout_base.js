35de3c9155b1a8b6c449d2ed1834c6c7e2994278
Rachel Fenichel <fenichel@google.com>
2017-05-24 13:36:30 -0700

    Fix hat block case

--- core/flyout_base.js
+++ core/flyout_base.js
@@ -682,13 +682,6 @@ Blockly.Flyout.prototype.moveRectToBlock_ = function(rect, block) {
   rect.setAttribute('width', blockHW.width);
   rect.setAttribute('height', blockHW.height);
 
-  // Blocks with output tabs are shifted a bit.
-  var tab = block.outputConnection ? Blockly.BlockSvg.TAB_WIDTH : 0;
-  var blockXY = block.getRelativeToSurfaceXY();
-  rect.setAttribute('y', blockXY.y);
-  rect.setAttribute('x',
-      this.RTL ? blockXY.x - blockHW.width + tab : blockXY.x - tab);
-
   // For hat blocks we want to shift them down by the hat height
   // since the y coordinate is the corner, not the top of the hat.
   var hatOffset =
@@ -696,6 +689,13 @@ Blockly.Flyout.prototype.moveRectToBlock_ = function(rect, block) {
   if (hatOffset) {
     block.moveBy(0, hatOffset);
   }
+
+  // Blocks with output tabs are shifted a bit.
+  var tab = block.outputConnection ? Blockly.BlockSvg.TAB_WIDTH : 0;
+  var blockXY = block.getRelativeToSurfaceXY();
+  rect.setAttribute('y', blockXY.y);
+  rect.setAttribute('x',
+      this.RTL ? blockXY.x - blockHW.width + tab : blockXY.x - tab);
 };
 
 /**

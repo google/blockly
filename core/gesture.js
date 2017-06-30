1bbca748bf4e41187aad74318f10bcf6edf95344
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-06-30 14:35:09 -0700

    remove previous fix

--- core/gesture.js
+++ core/gesture.js
@@ -604,7 +604,6 @@ Blockly.Gesture.prototype.doFieldClick_ = function() {
  */
 Blockly.Gesture.prototype.doBlockClick_ = function() {
   // Block click in an autoclosing flyout.
-  if (this.targetBlock_.disabled) return;
   if (this.flyout_ && this.flyout_.autoClose) {
     if (!this.targetBlock_.disabled) {
       if (!Blockly.Events.getGroup()) {

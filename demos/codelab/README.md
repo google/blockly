7f68cdc8bd0e82b712cc714db5e4360c8c24919f
Ewa Gasperowicz <devnook@gmail.com>
2017-08-14 10:57:10 +0200

    Adding licences and comments as per review

--- demos/codelab/README.md
+++ demos/codelab/README.md
@@ -2,7 +2,8 @@
 
 Code for the [Blockly for the Web codelab](https://developers.google.com/TODO).
 
-In this codelab, you'll learn how to use Blockly javascript library to add block code editor to a web application.
+In this codelab, you'll learn how to use Blockly JavaScript library
+to add a block code editor to a web application.
 
 ## What you'll learn
 
@@ -11,4 +12,5 @@ In this codelab, you'll learn how to use Blockly javascript library to add block
 * How to create a new block in Blockly.
 * How to generate and run JavaScript code from blocks.
 
-Example code for each step of the codelab is available from the [completed](completed/) directory.
+Example code for each step of the codelab is available from
+the [completed](completed/) directory.
--- demos/codelab/completed/finish/scripts/main.js
+++ demos/codelab/completed/finish/scripts/main.js
@@ -1,4 +1,20 @@
-(function() {
+/**
+ * Copyright 2017 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ (function() {
 
   let currentButton;
 
@@ -7,6 +23,8 @@
     Blockly.JavaScript.addReservedWords('code');
     var code = Blockly.JavaScript.workspaceToCode(Blockly.getMainWorkspace());
     code += 'MusicMaker.play();';
+    // Eval can be dangerous. For more controlled execution, check
+    // https://github.com/NeilFraser/JS-Interpreter.
     try {
       eval(code);
     } catch (error) {

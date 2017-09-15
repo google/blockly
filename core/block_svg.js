41f12e269995365a94917d6c40a2cb96995d024c
Neil Fraser <fraser@google.com>
2017-09-14 17:18:41 -0700

    Strip licences from compiled code.

--- core/block_svg.js
+++ core/block_svg.js
@@ -30,6 +30,7 @@ goog.require('Blockly.Block');
 goog.require('Blockly.ContextMenu');
 goog.require('Blockly.Grid');
 goog.require('Blockly.RenderedConnection');
+goog.require('Blockly.Tooltip');
 goog.require('Blockly.Touch');
 goog.require('Blockly.utils');
 goog.require('goog.Timer');

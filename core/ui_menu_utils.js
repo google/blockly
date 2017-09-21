6c29876c116feda7d9c931879cc4b59b71e04af3
Rachel Fenichel <fenichel@google.com>
2017-09-21 11:37:56 -0700

    Update copyright date

--- core/ui_menu_utils.js
+++ core/ui_menu_utils.js
@@ -2,7 +2,7 @@
  * @license
  * Visual Blocks Editor
  *
- * Copyright 2012 Google Inc.
+ * Copyright 2017 Google Inc.
  * https://developers.google.com/blockly/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");

3bcc9362cce7b45f1de90badc87cfe1b2d7f6742
Neil Fraser <fraser@google.com>
2017-10-10 19:07:14 -0700

    Be more specific about JS files to compile.

--- tests/compile/test.sh
+++ tests/compile/test.sh
@@ -2,9 +2,10 @@ echo Hello World.
 # Test script for Travis.
 cd tests/compile
 java -jar $(npm root)/google-closure-compiler/compiler.jar --js='main.js' \
-  --js='../../**.js' \
-  --js='!../../externs/**.js' \
-  --js='!../../node_modules/**.js' \
+  --js='../../core/**.js' \
+  --js='../../blocks/**.js' \
+  --js='../../generators/**.js' \
+  --js='../../msg/**.js' \
   --js='!../../msg/messages.js' \
   --js='../../../closure-library/closure/goog/**.js' \
   --js='../../../closure-library/third_party/closure/goog/**.js' \
@@ -13,6 +14,6 @@ java -jar $(npm root)/google-closure-compiler/compiler.jar --js='main.js' \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=Main \
   --js_output_file main_compressed.js
-test -f main_compressed.js || exit 1
+test -s main_compressed.js || exit 1
 ls -lag
 cd ../../

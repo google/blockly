03b5bd5fd7852a8a431ad2698af3c094d66f5e3e
Neil Fraser <fraser@google.com>
2017-10-10 18:37:35 -0700

    Compile main_compressed.js

--- tests/compile/test.sh
+++ tests/compile/test.sh
@@ -1,3 +1,13 @@
 echo Hello World.
-java -jar $(npm root)/google-closure-compiler/compiler.jar --version
-
+java -jar $(npm root)/google-closure-compiler/compiler.jar --js='main.js' \
+  --js='../../**.js' \
+  --js='!../../externs/**.js' \
+  --js='!../../msg/messages.js' \
+  --js='../../../closure-library/closure/goog/**.js' \
+  --js='../../../closure-library/third_party/closure/goog/**.js' \
+  --generate_exports \
+  --externs ../../externs/svg-externs.js \
+  --compilation_level ADVANCED_OPTIMIZATIONS \
+  --dependency_mode=STRICT --entry_point=Main \
+  --js_output_file main_compressed.js
+ls -lag

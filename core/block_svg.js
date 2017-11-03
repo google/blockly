77f577ac4f1168b8475e0f45e7172e231890c954
Rachel Fenichel <fenichel@google.com>
2017-11-03 10:55:55 -0700

    Check workspace.isDragging() instead of Blockly.dragMode_

--- core/block_svg.js
+++ core/block_svg.js
@@ -1458,7 +1458,7 @@ Blockly.BlockSvg.prototype.bumpNeighbours_ = function() {
   if (!this.workspace) {
     return;  // Deleted block.
   }
-  if (Blockly.dragMode_ != Blockly.DRAG_NONE) {
+  if (this.workspace.isDragging()) {
     return;  // Don't bump blocks during a drag.
   }
   var rootBlock = this.getRootBlock();

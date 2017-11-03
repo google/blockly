30716fbd788bd6aca87caf38513b00b58d521015
Rachel Fenichel <fenichel@google.com>
2017-11-03 14:41:57 -0700

    switch to variabletype everywhere

--- tests/jsunit/xml_test.js
+++ tests/jsunit/xml_test.js
@@ -67,7 +67,7 @@ function xmlTest_setUpWithMockBlocks() {
         'name': 'VAR',
         'variable': 'item'
       }
-    ],
+    ]
   }]);
 }
 
@@ -91,7 +91,7 @@ function xmlTest_checkNonVariableField(fieldDom, name, text) {
   assertEquals(text, fieldDom.textContent);
   assertEquals(name, fieldDom.getAttribute('name'));
   assertNull(fieldDom.getAttribute('id'));
-  assertNull(fieldDom.getAttribute('variableType'));
+  assertNull(fieldDom.getAttribute('variabletype'));
 }
 
 /**
@@ -104,7 +104,7 @@ function xmlTest_checkNonVariableField(fieldDom, name, text) {
  */
 function xmlTest_checkVariableFieldDomValues(fieldDom, name, type, id, text) {
   assertEquals(name, fieldDom.getAttribute('name'));
-  assertEquals(type, fieldDom.getAttribute('variableType'));
+  assertEquals(type, fieldDom.getAttribute('variabletype'));
   assertEquals(id, fieldDom.getAttribute('id'));
   assertEquals(text, fieldDom.textContent);
 }
@@ -166,7 +166,7 @@ function test_domToWorkspace_VariablesAtTop() {
         '    <variable type="" id="id3">name3</variable>' +
         '  </variables>' +
         '  <block type="field_variable_test_block">' +
-        '    <field name="VAR" id="id3" variableType="">name3</field>' +
+        '    <field name="VAR" id="id3" variabletype="">name3</field>' +
         '  </block>' +
         '</xml>');
     Blockly.Xml.domToWorkspace(dom, workspace);
@@ -210,7 +210,7 @@ function test_domToWorkspace_VariablesAtTop_MissingType() {
         '    <variable id="id1">name1</variable>' +
         '  </variables>' +
         '  <block type="field_variable_test_block">' +
-        '    <field name="VAR" id="id1" variableType="">name3</field>' +
+        '    <field name="VAR" id="id1" variabletype="">name3</field>' +
         '  </block>' +
         '</xml>');
     Blockly.Xml.domToWorkspace(dom, workspace);
@@ -233,7 +233,7 @@ function test_domToWorkspace_VariablesAtTop_MismatchBlockType() {
         '    <variable type="type1" id="id1">name1</variable>' +
         '  </variables>' +
         '  <block type="field_variable_test_block">' +
-        '    <field name="VAR" id="id1" variableType="">name1</field>' +
+        '    <field name="VAR" id="id1" variabletype="">name1</field>' +
         '  </block>' +
         '</xml>');
     Blockly.Xml.domToWorkspace(dom, workspace);

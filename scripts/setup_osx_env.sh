65ac665b81d817667bca4be2415e2116463ad29e
Rachel Fenichel <fenichel@google.com>
2017-11-10 14:13:06 -0800

    Removed files that were accidentally added when merging from master

--- scripts/setup_osx_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-  
-if [ "${TRAVIS_OS_NAME}" == "osx" ]
-  then
-    brew cask install google-chrome 
-    sudo Xvfb :99 -ac -screen 0 1024x768x8 &
-    export CHROME_BIN="/Applications/Google Chrome.app" 
-fi

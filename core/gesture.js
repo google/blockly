70e2121b2d35bc7b739326cfca9fea8679ecca7f
microsoftsam <samelh@microsoft.com>
2017-11-22 11:52:33 -0800

    minor refactor: remove commented out code

--- core/gesture.js
+++ core/gesture.js
@@ -739,7 +739,6 @@ Blockly.Gesture.prototype.doWorkspaceClick_ = function() {
  * @private
  */
 Blockly.Gesture.prototype.doCommentClick_ = function() {
-  //this.startComment_.select();
   this.startComment_.setFocus();
 };
 

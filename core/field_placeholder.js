acba3c6501a76106400ad90b29e1765f02627a61
ST3tter <st3tter@gmail.com>
2017-12-05 13:21:22 +0100

    changed the opacity of the place holder

--- core/field_placeholder.js
+++ core/field_placeholder.js
@@ -16,6 +16,38 @@ goog.inherits(Blockly.FieldPlaceholder, Blockly.FieldLed);
 Blockly.FieldPlaceholder.prototype.EDITABLE = false;
 
 
+Blockly.Field.prototype.init = function() {
+  if (this.fieldGroup_) {
+    // Field has already been initialized once.
+    return;
+  }
+  // Build the DOM.
+  this.fieldGroup_ = Blockly.utils.createSvgElement('g', {}, null);
+  if (!this.visible_) {
+    this.fieldGroup_.style.display = 'none';
+  }
+  this.borderRect_ = Blockly.utils.createSvgElement('rect',
+      {'rx': 0,
+       'ry': 0,
+       'x': -Blockly.BlockSvg.SEP_SPACE_X / 2,
+       'y': 0,
+       'height': 16,
+       'fill-opacity': 0}, this.fieldGroup_);
+  /** @type {!Element} */
+  this.textElement_ = Blockly.utils.createSvgElement('text',
+      {'class': 'blocklyText', 'y': this.size_.height - 12.5},
+      this.fieldGroup_);
+
+  this.updateEditable();
+  this.sourceBlock_.getSvgRoot().appendChild(this.fieldGroup_);
+  this.mouseDownWrapper_ =
+      Blockly.bindEventWithChecks_(this.fieldGroup_, 'mousedown', this,
+      this.onMouseDown_);
+  // Force a render.
+  this.render_();
+};
+
+
 Blockly.FieldPlaceholder.prototype.updateWidth = function() {
   if (this.borderRect_) {
     this.borderRect_.setAttribute('width',

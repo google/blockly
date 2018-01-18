9a3dc70031237e3c02b67b55cb2b587e2198612a
Rachel Fenichel <fenichel@google.com>
2018-01-17 17:38:26 -0800

    Declare developer variables before user variables.  Rebuild generators.

--- generators/javascript.js
+++ generators/javascript.js
@@ -155,18 +155,18 @@ Blockly.JavaScript.init = function(workspace) {
   Blockly.JavaScript.variableDB_.setVariableMap(workspace.getVariableMap());
 
   var defvars = [];
-  // Add user variables.
-  var variables = workspace.getAllVariables();
-  for (var i = 0; i < variables.length; i++) {
-    defvars[i] = Blockly.JavaScript.variableDB_.getName(variables[i].getId(),
-        Blockly.Variables.NAME_TYPE);
-  }
-
   // Add developer variables (not created or named by the user).
   var devVarList = Blockly.Variables.allDeveloperVariables(workspace);
   for (var i = 0; i < devVarList.length; i++) {
     defvars.push(Blockly.JavaScript.variableDB_.getName(devVarList[i],
         Blockly.Names.DEVELOPER_VARIABLE_TYPE));
+  }
+
+  // Add user variables.
+  var variables = workspace.getAllVariables();
+  for (var i = 0; i < variables.length; i++) {
+    defvars.push(Blockly.JavaScript.variableDB_.getName(variables[i].getId(),
+        Blockly.Variables.NAME_TYPE));
   }
 
   // Declare all of the variables.

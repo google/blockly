f9ce427b69f2e97cbeb04e02ad744059feaa1272
Andrew n marshall <Anm@anm.me>
2018-03-15 12:53:27 -0700

    Custom Dialogs demo: Bumping backdrop Z.

--- demos/custom-dialogs/custom-dialog.js
+++ demos/custom-dialogs/custom-dialog.js
@@ -93,7 +93,8 @@ CustomDialog.show = function(title, message, options) {
     backdropDiv.style.cssText =
         'position: absolute;' +
         'top: 0; left: 0; right: 0; bottom: 0;' +
-        'background-color: rgba(0, 0, 0, .7);';
+        'background-color: rgba(0, 0, 0, .7);' +
+        'z-index: 100;';
     document.body.appendChild(backdropDiv);
 
     dialogDiv = document.createElement('div');

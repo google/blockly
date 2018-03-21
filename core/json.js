45a80111baa3ee0d6d6f28ef4586e8165ea7290e
Josh Lloyd <j.nevercast@gmail.com>
2018-03-21 18:36:42 +1300

    Fix ESLint errors
    Squashed commit:
    [cddc2db7] Fix JSDoc
    [e6d84866] Fix long lines
    [1d76526c] Fix mixed indent of tabs and spaces
    [6b761c16] Fix indent and new lines

--- core/json.js
+++ core/json.js
@@ -30,32 +30,32 @@ goog.provide('Blockly.JSON');
  * Field labels in JSON output
  */
 Blockly.JSON.fieldLabels = {
-	positionX:'x'
-	,positionY:'y'
-	,type:'type'
-	,xmlMutation:'xmlMutation'
-	,jsonMutation:'mutation'
-	,title:'title'
-	,titleValue:'value'
-	,titleName:'name'
-	,comment:'comment'
-	,commentText:'text'
-	,commentPinned:'pinned'
-	,commentHeight:'h'
-	,commentWidth:'w'
-	,inputList:'inputList'
-	,inputName:'name'
-	,inputType:'type'
-	,inputInline:'inline'
-	,inputCollapsed:'collapsed'
-	,inputDisabled:'disabled'
-	,inputDeletable:'deleteable'
-	,inputMovable:'movable'
-	,inputEditable:'editable'
-	,childBlocks:'childBlocks'
-	,nextBlock:'next'
-	
-}
+  positionX:'x'
+  ,positionY:'y'
+  ,type:'type'
+  ,xmlMutation:'xmlMutation'
+  ,jsonMutation:'mutation'
+  ,title:'title'
+  ,titleValue:'value'
+  ,titleName:'name'
+  ,comment:'comment'
+  ,commentText:'text'
+  ,commentPinned:'pinned'
+  ,commentHeight:'h'
+  ,commentWidth:'w'
+  ,inputList:'inputList'
+  ,inputName:'name'
+  ,inputType:'type'
+  ,inputInline:'inline'
+  ,inputCollapsed:'collapsed'
+  ,inputDisabled:'disabled'
+  ,inputDeletable:'deleteable'
+  ,inputMovable:'movable'
+  ,inputEditable:'editable'
+  ,childBlocks:'childBlocks'
+  ,nextBlock:'next'
+  
+};
 /**
  * Encode a block tree as JSON.
  * @param {!Object} workspace The SVG workspace.
@@ -67,7 +67,7 @@ Blockly.JSON.workspaceToObject = function(workspace) {
   var JSON = [];
   for (var i = 0, block; block = blocks[i]; i++) {
     var element = Blockly.JSON.blockToObject_(block);
-	if(!element) continue;
+    if (!element) continue;
     var xy = block.getRelativeToSurfaceXY();
     element[Blockly.JSON.fieldLabels.positionX]= Blockly.RTL ? width - xy.x : xy.x;
     element[Blockly.JSON.fieldLabels.positionY]= xy.y;
@@ -86,11 +86,11 @@ Blockly.JSON.blockToObject_ = function(block) {
   var element = {};
   element[Blockly.JSON.fieldLabels.type]= block.type;
   if (block.mutationToObject) {
-	var mutation = block.mutationToObject();
+    var mutation = block.mutationToObject();
     if (mutation) {
       element[Blockly.JSON.fieldLabels.jsonMutation]=mutation;
     }
-  }else  if (block.mutationToDom) {
+  } else  if (block.mutationToDom) {
     // Custom data for an advanced block.
     var mutation = block.mutationToDom();
     if (mutation) {
@@ -99,12 +99,13 @@ Blockly.JSON.blockToObject_ = function(block) {
   }
   
   function titleToObject(title) {
-	element[Blockly.JSON.fieldLabels.title] = element[Blockly.JSON.fieldLabels.title] ||[]; //maybe multiple???
+    //maybe multiple???
+    element[Blockly.JSON.fieldLabels.title] = element[Blockly.JSON.fieldLabels.title] ||[];
     if (title.name && title.EDITABLE) {
-		var newTitleData = {};
-		newTitleData[Blockly.JSON.fieldLabels.titleValue] = title.getValue();
-		newTitleData[Blockly.JSON.fieldLabels.titleName] = title.name;
-		element[Blockly.JSON.fieldLabels.title].push(newTitleData);
+      var newTitleData = {};
+      newTitleData[Blockly.JSON.fieldLabels.titleValue] = title.getValue();
+      newTitleData[Blockly.JSON.fieldLabels.titleName] = title.name;
+      element[Blockly.JSON.fieldLabels.title].push(newTitleData);
     }
   }
   
@@ -114,13 +115,13 @@ Blockly.JSON.blockToObject_ = function(block) {
     }
   }
   
-  if(element[Blockly.JSON.fieldLabels.title] && !element[Blockly.JSON.fieldLabels.title].length){
-	  delete element[Blockly.JSON.fieldLabels.title];
+  if (element[Blockly.JSON.fieldLabels.title] && !element[Blockly.JSON.fieldLabels.title].length){
+    delete element[Blockly.JSON.fieldLabels.title];
   }
 
   if (block.comment) {
-	var hw = block.comment.getBubbleSize();
-	var newComment = {};
+    var hw = block.comment.getBubbleSize();
+    var newComment = {};
     newComment[Blockly.JSON.fieldLabels.commentText] = block.comment.getText();
     newComment[Blockly.JSON.fieldLabels.commentPinned] = block.comment.isVisible();
     newComment[Blockly.JSON.fieldLabels.commentHeight] = hw.height;
@@ -131,7 +132,7 @@ Blockly.JSON.blockToObject_ = function(block) {
 
   var hasValues = false;
   for (var i = 0, input; input = block.inputList[i]; i++) {
-	if (input.type == Blockly.DUMMY_INPUT) {
+    if (input.type == Blockly.DUMMY_INPUT) {
       continue;
     }
     var container = {};
@@ -139,21 +140,22 @@ Blockly.JSON.blockToObject_ = function(block) {
     
     var empty = true;
      
-	var childBlock = input.connection.targetBlock();
-	if (input.type == Blockly.INPUT_VALUE) {
-	  container[Blockly.JSON.fieldLabels.inputType]='value';
-	  hasValues = true;
-	} else if (input.type == Blockly.NEXT_STATEMENT) {
-	  container[Blockly.JSON.fieldLabels.inputType]='statement';
-	}
-	if (childBlock) {
-	  container[Blockly.JSON.fieldLabels.childBlocks] = Blockly.JSON.blockToObject_(childBlock);
-	  empty = false;
-	}
+    var childBlock = input.connection.targetBlock();
+    if (input.type == Blockly.INPUT_VALUE) {
+      container[Blockly.JSON.fieldLabels.inputType]='value';
+      hasValues = true;
+    } else if (input.type == Blockly.NEXT_STATEMENT) {
+      container[Blockly.JSON.fieldLabels.inputType]='statement';
+    }
+    if (childBlock) {
+      container[Blockly.JSON.fieldLabels.childBlocks] = Blockly.JSON.blockToObject_(childBlock);
+      empty = false;
+    }
     
    
     if (!empty) {
-	  element[Blockly.JSON.fieldLabels.inputList] = element[Blockly.JSON.fieldLabels.inputList] || [];
+      element[Blockly.JSON.fieldLabels.inputList] =
+        element[Blockly.JSON.fieldLabels.inputList] || [];
       element[Blockly.JSON.fieldLabels.inputList].push(container);
     }
   }
@@ -176,15 +178,16 @@ Blockly.JSON.blockToObject_ = function(block) {
   if (!block.isEditable()) {
     element[Blockly.JSON.fieldLabels.inputEditable]= false;
   }
-  try{
-	block.render();
-  }catch (e) {
-	 return; 
+  try {
+    block.render();
+  } catch (e) {
+    return;
   }
   if (block.nextConnection) {
     var nextBlock = block.nextConnection.targetBlock();
     if (nextBlock) {
-	  element[Blockly.JSON.fieldLabels.nextBlock] = element[Blockly.JSON.fieldLabels.nextBlock] || [];
+      element[Blockly.JSON.fieldLabels.nextBlock] =
+        element[Blockly.JSON.fieldLabels.nextBlock] || [];
       var container =Blockly.JSON.blockToObject_(nextBlock);
       element[Blockly.JSON.fieldLabels.nextBlock].push(container);
     }
@@ -216,7 +219,7 @@ Blockly.JSON.objectToText = function(jsonObject) {
  * @return {!Element} A tree of JSON elements.
  */
 Blockly.JSON.textToObject = function(text) {
-   var jsonObject = JSON.parse(text);
+  var jsonObject = JSON.parse(text);
   // The DOM should have one and only one top-level node, an JSON tag.
   if (!jsonObject) {
     // Whatever we got back from the parser is not JSON.
@@ -228,20 +231,20 @@ Blockly.JSON.textToObject = function(text) {
 /**
  * Decode an JSON DOM and create blocks on the workspace.
  * @param {!Blockly.Workspace} workspace The SVG workspace.
- * @param {!Element} JSON JSON DOM.
+ * @param {!Element} json JSON DOM.
  */
 Blockly.JSON.objectToWorkspace = function(workspace, json) {
   var width = Blockly.svgSize().width;
   for (var x = 0, jsonChild; jsonChild = json[x]; x++) {
     //if (JSONChild['eType'] == 'block') {
-      var block = Blockly.JSON.objectToBlock_(workspace, jsonChild);
-	  if(!block)continue;
-      var blockX = parseInt(jsonChild[Blockly.JSON.fieldLabels.positionX]||0,10);
-      var blockY = parseInt(jsonChild[Blockly.JSON.fieldLabels.positionY]||0,10);
-      if (!isNaN(blockX) && !isNaN(blockY)) {
-        block.moveBy(Blockly.RTL ? width - blockX : blockX, blockY);
-      }
-   // }
+    var block = Blockly.JSON.objectToBlock_(workspace, jsonChild);
+    if (!block) continue;
+    var blockX = parseInt(jsonChild[Blockly.JSON.fieldLabels.positionX]||0,10);
+    var blockY = parseInt(jsonChild[Blockly.JSON.fieldLabels.positionY]||0,10);
+    if (!isNaN(blockX) && !isNaN(blockY)) {
+      block.moveBy(Blockly.RTL ? width - blockX : blockX, blockY);
+    }
+    // }
   }
 };
 
@@ -249,17 +252,17 @@ Blockly.JSON.objectToWorkspace = function(workspace, json) {
  * Decode an JSON block tag and create a block (and possibly sub blocks) on the
  * workspace.
  * @param {!Blockly.Workspace} workspace The workspace.
- * @param {!Element} JSONBlock JSON block element.
+ * @param {!Element} jsonBlock JSON block element.
  * @return {!Blockly.Block} The root block created.
  * @private
  */
 Blockly.JSON.objectToBlock_ = function(workspace, jsonBlock) {
   var prototypeName = jsonBlock[Blockly.JSON.fieldLabels.type];
   //var block = new Blockly.Block(workspace, prototypeName);
-  try{
-  var block = Blockly.Block.obtain(workspace, prototypeName);
+  try {
+    var block = Blockly.Block.obtain(workspace, prototypeName);
   } catch (e){
-	  return;
+    return;
   }
   if (!block.svg_) {
     block.initSvg();
@@ -291,82 +294,89 @@ Blockly.JSON.objectToBlock_ = function(workspace, jsonBlock) {
   }
 
   if (jsonBlock[Blockly.JSON.fieldLabels.jsonMutation] && block.objectToMutation) {
-	  block.objectToMutation(jsonBlock[Blockly.JSON.fieldLabels.jsonMutation]);
-  }else if (jsonBlock[Blockly.JSON.fieldLabels.xmlMutation] && block.domToMutation) {
-	  var mutationDom = document.createElement('div');
-	 mutationDom.innerHTML = jsonBlock[Blockly.JSON.fieldLabels.xmlMutation];
-	 block.domToMutation(mutationDom.firstChild);
+    block.objectToMutation(jsonBlock[Blockly.JSON.fieldLabels.jsonMutation]);
+  } else if (jsonBlock[Blockly.JSON.fieldLabels.xmlMutation] && block.domToMutation) {
+    var mutationDom = document.createElement('div');
+    mutationDom.innerHTML = jsonBlock[Blockly.JSON.fieldLabels.xmlMutation];
+    block.domToMutation(mutationDom.firstChild);
   }
   
   if (jsonBlock[Blockly.JSON.fieldLabels.comment]){
-	   block.setCommentText(jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentText]);
-        var visible = jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentPinned];
-        if (visible) {
-          block.comment.setVisible(true);
-        }
-        var bubbleW = jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentWidth];
-        var bubbleH = jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentHeight];
-        if (!isNaN(bubbleW) && !isNaN(bubbleH)) {
-          block.comment.setBubbleSize(bubbleW, bubbleH);
-        }    
+    block.setCommentText(
+        jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentText]
+    );
+    var visible =
+      jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentPinned];
+    if (visible) {
+      block.comment.setVisible(true);
+    }
+    var bubbleW =
+      jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentWidth];
+    var bubbleH =
+      jsonBlock[Blockly.JSON.fieldLabels.comment][Blockly.JSON.fieldLabels.commentHeight];
+    if (!isNaN(bubbleW) && !isNaN(bubbleH)) {
+      block.comment.setBubbleSize(bubbleW, bubbleH);
+    }
   }
   
   if (jsonBlock[Blockly.JSON.fieldLabels.title]){
-	   for(var i=0,jsonTitle; jsonTitle = jsonBlock[Blockly.JSON.fieldLabels.title][i];i++){
-		   try{
-				block.setFieldValue(
-					jsonTitle[Blockly.JSON.fieldLabels.titleValue]
-					, jsonTitle[Blockly.JSON.fieldLabels.titleName]
-					);
-			}catch(e){
-			  //Just ignore the error 
-		   }
-	   }  
+    for (var i=0,jsonTitle; jsonTitle = jsonBlock[Blockly.JSON.fieldLabels.title][i];i++){
+      try {
+        block.setFieldValue(
+            jsonTitle[Blockly.JSON.fieldLabels.titleValue]
+            , jsonTitle[Blockly.JSON.fieldLabels.titleName]
+        );
+      } catch (e){
+        //Just ignore the error
+      }
+    }
   }
   
 
   
   if (jsonBlock[Blockly.JSON.fieldLabels.inputList]){
-	  for(var i=0,jsonInput; jsonInput = jsonBlock[Blockly.JSON.fieldLabels.inputList][i];i++){
-		    var input = block.getInput(jsonInput[Blockly.JSON.fieldLabels.inputName]);
-			if (!input) {
-			  //throw 'Input does not exist: ' + jsonInput['vName'];
-			  continue;
-			}
-			if (jsonInput[Blockly.JSON.fieldLabels.childBlocks]) {	
-			  var blockChild = Blockly.JSON.objectToBlock_(workspace, jsonInput[Blockly.JSON.fieldLabels.childBlocks]);
-			  if (blockChild && blockChild.outputConnection) {
-				 if(input.connection)input.connection.connect(blockChild.outputConnection);
-			  } else if (blockChild && blockChild.previousConnection) {
-				input.connection.connect(blockChild.previousConnection);
-			  } else {
-				console.log('Child block does not have output or previous statement.');
-			  }
-			}
-	   }  
+    for (var i=0,jsonInput; jsonInput = jsonBlock[Blockly.JSON.fieldLabels.inputList][i];i++){
+      var input = block.getInput(jsonInput[Blockly.JSON.fieldLabels.inputName]);
+      if (!input) {
+        //throw 'Input does not exist: ' + jsonInput['vName'];
+        continue;
+      }
+      if (jsonInput[Blockly.JSON.fieldLabels.childBlocks]) {
+        var blockChild = Blockly.JSON.objectToBlock_(workspace,
+            jsonInput[Blockly.JSON.fieldLabels.childBlocks]
+        );
+        if (blockChild && blockChild.outputConnection) {
+          if (input.connection)input.connection.connect(blockChild.outputConnection);
+        } else if (blockChild && blockChild.previousConnection) {
+          input.connection.connect(blockChild.previousConnection);
+        } else {
+          console.log('Child block does not have output or previous statement.');
+        }
+      }
+    }
   }
   
   //If a block has mutation that create new outputs or affect inputs
   //Then you cannot set these input or outputs restriction until both
   //mutationTodata/dom is done and the title/inputList is done.
-  if(block.afterRender) {
-      block.afterRender();
+  if (block.afterRender) {
+    block.afterRender();
   }
   
   if (jsonBlock[Blockly.JSON.fieldLabels.nextBlock]){
-	  for(var i=0,jsonNext; jsonNext = jsonBlock[Blockly.JSON.fieldLabels.nextBlock][i];i++){
-		   if (!block.nextConnection) {
-            throw 'Next statement does not exist.';
-          } else if (block.nextConnection.targetConnection) {
-            // This could happen if there is more than one JSON 'lNext' tag.
-            throw 'Next statement is already connected.';
-          }
-          blockChild = Blockly.JSON.objectToBlock_(workspace, jsonNext);
-          if (!blockChild.previousConnection) {
-            throw 'Next block does not have previous statement.';
-          }
-          block.nextConnection.connect(blockChild.previousConnection);
-	   }  
+    for (var i=0,jsonNext; jsonNext = jsonBlock[Blockly.JSON.fieldLabels.nextBlock][i];i++){
+      if (!block.nextConnection) {
+        throw 'Next statement does not exist.';
+      } else if (block.nextConnection.targetConnection) {
+        // This could happen if there is more than one JSON 'lNext' tag.
+        throw 'Next statement is already connected.';
+      }
+      blockChild = Blockly.JSON.objectToBlock_(workspace, jsonNext);
+      if (!blockChild.previousConnection) {
+        throw 'Next block does not have previous statement.';
+      }
+      block.nextConnection.connect(blockChild.previousConnection);
+    }
   }
   
   

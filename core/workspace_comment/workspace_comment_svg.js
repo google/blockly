4b4bab7ce4a066222062a1eddcf1156aefa37c22
Sam El-Husseini <samelh@microsoft.com>
2018-03-22 12:21:05 -0700

    Focus textarea on select

--- core/workspace_comment/workspace_comment_svg.js
+++ core/workspace_comment/workspace_comment_svg.js
@@ -185,6 +185,7 @@ Blockly.WorkspaceCommentSvg.prototype.unselect = function() {
 Blockly.WorkspaceCommentSvg.prototype.addSelect = function() {
   Blockly.utils.addClass(
       /** @type {!Element} */ (this.svgGroup_), 'blocklySelected');
+  this.setFocus();
 };
 
 /**
@@ -193,6 +194,7 @@ Blockly.WorkspaceCommentSvg.prototype.addSelect = function() {
 Blockly.WorkspaceCommentSvg.prototype.removeSelect = function() {
   Blockly.utils.removeClass(
       /** @type {!Element} */ (this.svgGroup_), 'blocklySelected');
+  this.blurFocus();
 };
 
 /**

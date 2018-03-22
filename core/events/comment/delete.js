9e387ba510f4c7bb06742c55eb0fcbb18b0ce688
Rachel Fenichel <fenichel@google.com>
2018-03-22 14:11:18 -0700

    Add delete events

--- core/events/comment/delete.js
+++ core/events/comment/delete.js
@@ -28,4 +28,66 @@
 goog.provide('Blockly.Events.CommentDelete');
 
 goog.require('Blockly.Events');
-goog.require('Blockly.Events.Abstract');
+goog.require('Blockly.Events.CommentBase');
+
+/**
+ * Class for a comment deletion event.
+ * @param {Blockly.WorkspaceComment} comment The deleted comment.
+ *     Null for a blank event.
+ * @extends {Blockly.Events.CommentBase}
+ * @constructor
+ */
+Blockly.Events.CommentDelete = function(comment) {
+  if (!comment) {
+    return;  // Blank event to be populated by fromJson.
+  }
+  Blockly.Events.CommentDelete.superClass_.constructor.call(this, comment);
+
+  this.xml = comment.toXml();
+};
+goog.inherits(Blockly.Events.CommentDelete, Blockly.Events.CommentBase);
+
+/**
+ * Type of this event.
+ * @type {string}
+ */
+Blockly.Events.CommentDelete.prototype.type = Blockly.Events.COMMENT_DELETE;
+
+/**
+ * Encode the event as JSON.
+ * TODO (#1266): "Full" and "minimal" serialization.
+ * @return {!Object} JSON representation.
+ */
+Blockly.Events.CommentDelete.prototype.toJson = function() {
+  var json = Blockly.Events.CommentDelete.superClass_.toJson.call(this);
+  return json;
+};
+
+/**
+ * Decode the JSON event.
+ * @param {!Object} json JSON representation.
+ */
+Blockly.Events.CommentDelete.prototype.fromJson = function(json) {
+  Blockly.Events.CommentDelete.superClass_.fromJson.call(this, json);
+};
+
+/**
+ * Run a creation event.
+ * @param {boolean} forward True if run forward, false if run backward (undo).
+ */
+Blockly.Events.CommentDelete.prototype.run = function(forward) {
+  var workspace = this.getEventWorkspace_();
+  if (forward) {
+    var comment = workspace.getCommentById(this.commentId);
+    if (comment) {
+      comment.dispose(false, false);
+    } else {
+      // Only complain about root-level block.
+      console.warn("Can't uncreate non-existent comment: " + this.commentId);
+    }
+  } else {
+    var xml = goog.dom.createDom('xml');
+    xml.appendChild(this.xml);
+    Blockly.Xml.domToWorkspace(xml, workspace);
+  }
+};

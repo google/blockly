ed474e8e5afcf8498b8579c4df4f2772dc6a013a
Andrew n marshall <Anm@anm.me>
2018-05-15 16:18:26 -0700

    Correcting paths to files.

--- tests/compile/compile.sh
+++ tests/compile/compile.sh
@@ -13,21 +13,24 @@ else
   exit 1
 fi
 
+# TODO: Find relative path to Blockly root. For now, assume it is pwd.
+# TODO: Find relative path to closure, possibly in node_modules.
+
 echo Using $COMPILER as the compiler.
 rm main_compressed.js 2> /dev/null
 echo Compiling Blockly...
-java -jar $COMPILER --js='main.js' \
-  --js='../../core/**.js' \
-  --js='../../blocks/**.js' \
-  --js='../../generators/**.js' \
-  --js='../../msg/js/**.js' \
-  --js='../../../closure-library/closure/goog/**.js' \
-  --js='../../../closure-library/third_party/closure/goog/**.js' \
+java -jar $COMPILER --js='tests/compile/main.js' \
+  --js='core/**.js' \
+  --js='blocks/**.js' \
+  --js='generators/**.js' \
+  --js='msg/js/**.js' \
+  --js='../closure-library/closure/goog/**.js' \
+  --js='../closure-library/third_party/closure/goog/**.js' \
   --generate_exports \
-  --externs ../../externs/svg-externs.js \
+  --externs externs/svg-externs.js \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=Main \
-  --js_output_file main_compressed.js
+  --js_output_file 'tests/compile/main_compressed.js'
 if [ -s main_compressed.js ]; then
   echo Compilation OK.
 else

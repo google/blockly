f2b6c9a948283199d9fa997f5b3c90ea502df9af
Andrew n marshall <Anm@anm.me>
2018-05-16 14:16:02 -0700

    Work in progress (Needed a Travis run) (#1869)
    Fix advanced compilation on Travis

--- tests/compile/compile.sh
+++ tests/compile/compile.sh
@@ -1,8 +1,12 @@
+echo "Executing compile.sh from $(pwd)"
+
+# TODO: Find relative path to Blockly root. For now, assume it is pwd.
+
 # Find the Closure Compiler.
 if [ -f "$(npm root)/google-closure-compiler/compiler.jar" ]; then
   COMPILER="$(npm root)/google-closure-compiler/compiler.jar"
-elif [ -f *compiler*.jar ]; then
-  COMPILER="*compiler*.jar"
+elif [ -f tests/compile/*compiler*.jar ]; then
+  COMPILER="tests/compile/*compiler*.jar"
   # TODO: Check whether multiple files were found.
 else
   echo "ERROR: Closure Compiler not found."
@@ -12,21 +16,25 @@ else
 fi
 
 echo Using $COMPILER as the compiler.
-rm main_compressed.js 2> /dev/null
+rm tests/compile/main_compressed.js 2> /dev/null
 echo Compiling Blockly...
-java -jar $COMPILER --js='main.js' \
-  --js='../../core/**.js' \
-  --js='../../blocks/**.js' \
-  --js='../../generators/**.js' \
-  --js='../../msg/js/**.js' \
-  --js='../../../closure-library/closure/goog/**.js' \
-  --js='../../../closure-library/third_party/closure/goog/**.js' \
+COMPILATION_COMMAND="java -jar $COMPILER --js='tests/compile/main.js' \
+  --js='core/**.js' \
+  --js='blocks/**.js' \
+  --js='generators/**.js' \
+  --js='msg/js/**.js' \
+  --js='../closure-library/closure/goog/**.js' \
+  --js='../closure-library/third_party/closure/goog/**.js' \
   --generate_exports \
-  --externs ../../externs/svg-externs.js \
+  --externs externs/svg-externs.js \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=Main \
-  --js_output_file main_compressed.js
-if [ -s main_compressed.js ]; then
+  --js_output_file tests/compile/main_compressed.js"
+echo $COMPILATION_COMMAND
+$COMPILATION_COMMAND
+EXIT_CODE=$?
+echo "Compiler exit code: $EXIT_CODE"
+if [ "$EXIT_CODE" -eq 0 ] && [ -s tests/compile/main_compressed.js ]; then
   echo Compilation OK.
 else
   echo Compilation FAIL.

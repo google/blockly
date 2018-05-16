dc6df13e614c1886bfb1b9bf8e8620ec22efc93f
Andrew n marshall <Anm@anm.me>
2018-05-16 13:33:08 -0700

    Correct more paths. Add logging and testing of the compiler exit code. Add TODOs for including other block libraries.

--- tests/compile/compile.sh
+++ tests/compile/compile.sh
@@ -1,10 +1,12 @@
 echo "Executing compile.sh from $(pwd)"
 
+# TODO: Find relative path to Blockly root. For now, assume it is pwd.
+
 # Find the Closure Compiler.
 if [ -f "$(npm root)/google-closure-compiler/compiler.jar" ]; then
   COMPILER="$(npm root)/google-closure-compiler/compiler.jar"
-elif [ -f *compiler*.jar ]; then
-  COMPILER="*compiler*.jar"
+elif [ -f tests/compile/*compiler*.jar ]; then
+  COMPILER="tests/compile/*compiler*.jar"
   # TODO: Check whether multiple files were found.
 else
   echo "ERROR: Closure Compiler not found."
@@ -13,11 +15,8 @@ else
   exit 1
 fi
 
-# TODO: Find relative path to Blockly root. For now, assume it is pwd.
-# TODO: Find relative path to closure, possibly in node_modules.
-
 echo Using $COMPILER as the compiler.
-rm main_compressed.js 2> /dev/null
+rm tests/compile/main_compressed.js 2> /dev/null
 echo Compiling Blockly...
 COMPILATION_COMMAND="java -jar $COMPILER --js='tests/compile/main.js' \
   --js='core/**.js' \
@@ -30,10 +29,12 @@ COMPILATION_COMMAND="java -jar $COMPILER --js='tests/compile/main.js' \
   --externs externs/svg-externs.js \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=Main \
-  --js_output_file 'tests/compile/main_compressed.js'"
+  --js_output_file tests/compile/main_compressed.js"
 echo $COMPILATION_COMMAND
 $COMPILATION_COMMAND
-if [ -s main_compressed.js ]; then
+EXIT_CODE=$?
+echo "Compiler exit code: $EXIT_CODE"
+if [ "$EXIT_CODE" -eq 0 ] && [ -s tests/compile/main_compressed.js ]; then
   echo Compilation OK.
 else
   echo Compilation FAIL.

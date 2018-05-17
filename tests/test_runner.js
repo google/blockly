579a4ffd4fd2ac2cad3e790fe4cc42ca801b1a82
Andrew n marshall <Anm@anm.me>
2018-05-17 15:25:06 -0700

    Add travis_fold support to tests/test_runner.js
    That said, this doesn't do much right now, because the test function
    return relatively quickly, before the test is complete.
    I need to wait for the end of the test.

--- tests/test_runner.js
+++ tests/test_runner.js
@@ -25,20 +25,34 @@ if (require.main !== module) {
   throw __filename + ' must be called directly.';
 }
 
+function travisFold(startOrEnd, i) {
+  if (process.env['TRAVIS'] === 'true') {
+    console.log('travis_fold:' + startOrEnd + ':npm_test_' + i);
+  }
+}
+
 var testFns = [
       require('./jsunit/run_jsunit_tests_in_browser')
     ];
 
-var errored = false;
-testFns.forEach((testFn) => {
+var hadTestFailure = false;
+testFns.forEach((testFn, i) => {
+  travisFold('start', i);
   try {
+    // TODO: Run each test function in a child process to
+    // prevent state leaks between test functions.
+    // Wait for process to complete before starting next test.
+    // Check process exit code as success/failure result.
+    // (OR... Do all of this from shell scripts?)
     testFn();
+    travisFold('end', i);
   } catch (errorStr) {
-    errored = true;
+    hadTestFailure = true;
+    travisFold('end', i);
     console.error(errorStr + '\n\n');
   }
 });
 
-if (errored) {
+if (hadTestFailure) {
   process.exit(1);
 }

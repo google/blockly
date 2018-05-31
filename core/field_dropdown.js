d70fefc49fc8d58e44bf6cf6c7bcaaea75757f03
Andrew n marshall <Anm@anm.me>
2018-05-31 15:24:40 -0700

    Remove logging.

--- core/field_dropdown.js
+++ core/field_dropdown.js
@@ -67,7 +67,6 @@ Blockly.FieldDropdown = function(menuGenerator, opt_validator) {
 };
 goog.inherits(Blockly.FieldDropdown, Blockly.Field);
 
-
 /**
  * Construct a FieldDropdown from a JSON arg object.
  * @param {!Object} options A JSON object with options (options).
@@ -258,10 +257,8 @@ Blockly.FieldDropdown.prototype.positionMenu_ = function(menu) {
   this.createWidget_(menu);
   var menuSize = Blockly.utils.uiMenu.getSize(menu);
 
-  console.log('document.documentElement.clientHeight =', document.documentElement.clientHeight);
   var maxHeightVh = Blockly.FieldDropdown.MAX_MENU_HEIGHT_VH
       * document.documentElement.clientHeight;
-  console.log('maxHeightVh = ', maxHeightVh);
   if (menuSize.height > maxHeightVh) {
     menuSize.height = maxHeightVh;
   }

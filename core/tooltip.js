7d29409c0fe237ef9b82f7edc754276965f18773
Neil Fraser <fraser@google.com>
2018-06-11 00:25:46 -0700

    Remove goog.dom.TagName

--- core/tooltip.js
+++ core/tooltip.js
@@ -36,7 +36,6 @@
 goog.provide('Blockly.Tooltip');
 
 goog.require('goog.dom');
-goog.require('goog.dom.TagName');
 
 
 /**
@@ -132,8 +131,7 @@ Blockly.Tooltip.createDom = function() {
     return;  // Already created.
   }
   // Create an HTML container for popup overlays (e.g. editor widgets).
-  Blockly.Tooltip.DIV =
-      goog.dom.createDom(goog.dom.TagName.DIV, 'blocklyTooltipDiv');
+  Blockly.Tooltip.DIV = goog.dom.createDom('div', 'blocklyTooltipDiv');
   document.body.appendChild(Blockly.Tooltip.DIV);
 };
 

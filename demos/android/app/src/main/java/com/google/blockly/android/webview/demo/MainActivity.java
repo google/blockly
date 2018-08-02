0fb1281cb3b02d3bd6174e861fb2da16c0154bd3
Andrew n marshall <Anm@anm.me>
2018-08-02 11:50:40 -0700

    Android demo initial project creation.

---
+++ demos/android/app/src/main/java/com/google/blockly/android/webview/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.google.blockly.android.webview.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.blocklywebview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

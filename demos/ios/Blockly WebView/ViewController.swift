7e375ce7a765aebd1ef495f35cec8ede59ec56a8
Andrew n marshall <Anm@anm.me>
2018-08-09 15:32:31 -0700

    Load HTML from Resources, into WebView
    The resource file URL appears to be wrong in some way

--- demos/ios/Blockly WebView/ViewController.swift
+++ demos/ios/Blockly WebView/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    /// The name used to reference this iOS object when executing callbacks from the JS code.
+    /// If this value is changed, it should also be changed in the `CODE_GENERATOR_BRIDGE_JS` file.
+    fileprivate static let HOST_HTML = "Blockly/webview.html"
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadWebView()
+    }
+    
+    func loadWebView() {
+        print("loadWebView: Bundle.main = ", Bundle.main); // This prints
+        if let htmlUrl = Bundle.main.url(forResource: "webview", withExtension: "html", subdirectory: "Blockly") {
+            print("htmlUrl = ", htmlUrl)  // Doesn't print
+            webView.load(URLRequest.init(url: htmlUrl))
+        }
     }
 
     override func didReceiveMemoryWarning() {

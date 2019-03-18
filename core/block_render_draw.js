baeb8a3d905437621ea5864f79f9617418681f5f
Rachel Fenichel <fenichel@google.com>
2019-03-18 10:50:05 -0700

    Move highlights to a separate file.

--- core/block_render_draw.js
+++ core/block_render_draw.js
@@ -19,7 +19,7 @@ drawOutline = function(block, info, pathObject) {
     } else if (row.hasExternalInput) {
       drawValueInput(row, pathObject, info, cursorY);
     } else {
-      drawRightSideRow(row, info, pathObject, cursorY);
+      drawRightSideRow(row, info, pathObject);
     }
     cursorY += row.height;
   }
@@ -28,44 +28,15 @@ drawOutline = function(block, info, pathObject) {
   drawLeft(block, info, pathObject, cursorY);
 };
 
-drawRightSideRow = function(row, info, pathObject, cursorY) {
+drawRightSideRow = function(row, info, pathObject) {
+  drawRightSideRowHighlight(row, info, pathObject);
   var steps = pathObject.steps;
   steps.push('H', row.width);
   steps.push('v', row.height);
-
-  var highlightSteps = pathObject.highlightSteps;
-  if (info.RTL) {
-    highlightSteps.push('v', row.height);
-  }
-
-};
-
-drawTopCornerHighlight = function(block, info, pathObject) {
-  var highlightSteps = pathObject.highlightSteps;
-
-  // Position the cursor at the top-left starting point.
-  if (info.squareTopLeftCorner) {
-    highlightSteps.push(BRC.START_POINT_HIGHLIGHT);
-    if (info.startHat) {
-      highlightSteps.push(this.RTL ?
-          Blockly.BlockSvg.START_HAT_HIGHLIGHT_RTL :
-          Blockly.BlockSvg.START_HAT_HIGHLIGHT_LTR);
-    }
-  } else {
-    highlightSteps.push(this.RTL ?
-        Blockly.BlockSvg.TOP_LEFT_CORNER_START_HIGHLIGHT_RTL :
-        Blockly.BlockSvg.TOP_LEFT_CORNER_START_HIGHLIGHT_LTR);
-    highlightSteps.push(Blockly.BlockSvg.TOP_LEFT_CORNER_HIGHLIGHT);
-  }
-
-  // Top edge.
-  if (block.previousConnection) {
-    highlightSteps.push('H', BRC.NOTCH_WIDTH, BRC.NOTCH_PATH_LEFT_HIGHLIGHT);
-  }
-  highlightSteps.push('H', info.maxValueOrDummyWidth - BRC.HIGHLIGHT_OFFSET);
 };
 
 drawTopCorner = function(block, info, pathObject) {
+  drawTopCornerHighlight(block, info, pathObject);
   var steps = pathObject.steps;
   // Position the cursor at the top-left starting point.
   if (info.squareTopLeftCorner) {
@@ -82,8 +53,6 @@ drawTopCorner = function(block, info, pathObject) {
     steps.push('H', BRC.NOTCH_WIDTH, BRC.NOTCH_PATH_LEFT);
   }
   steps.push('H', info.maxValueOrDummyWidth);
-
-  drawTopCornerHighlight(block, info, pathObject);
 };
 
 drawBottom = function(block, info, pathObject) {
@@ -96,47 +65,17 @@ drawBottom = function(block, info, pathObject) {
 };
 
 drawBottomCorner = function(block, info, pathObject) {
-  var highlightSteps = pathObject.highlightSteps;
+  drawBottomCornerHighlight(block, info, pathObject);
   var steps = pathObject.steps;
-  // Should the bottom-left corner be rounded or square?
   if (info.squareBottomLeftCorner) {
     steps.push('H 0');
-    if (!block.RTL) {
-      highlightSteps.push('M',
-          BRC.HIGHLIGHT_OFFSET + ',' + (info.height - BRC.HIGHLIGHT_OFFSET));
-    }
   } else {
     steps.push(BRC.BOTTOM_LEFT_CORNER);
-    if (!block.RTL) {
-      highlightSteps.push(BRC.BOTTOM_LEFT_CORNER_HIGHLIGHT_START +
-          (info.height - Blockly.BlockSvg.DISTANCE_45_INSIDE) +
-          BRC.BOTTOM_LEFT_CORNER_HIGHLIGHT_MID +
-          (info.height - Blockly.BlockSvg.CORNER_RADIUS));
-    }
-  }
-};
-
-drawLeftHighlight = function(block, info, pathObject) {
-  var highlightSteps = pathObject.highlightSteps;
-
-  if (info.hasOutputConnection) {
-    if (block.RTL) {
-      highlightSteps.push(BRC.OUTPUT_CONNECTION_HIGHLIGHT_RTL);
-    } else {
-      highlightSteps.push(BRC.OUTPUT_CONNECTION_HIGHLIGHT_LTR);
-    }
-  }
-
-  if (!block.RTL) {
-    if (info.squareTopLeftCorner) {
-      highlightSteps.push('V', BRC.HIGHLIGHT_OFFSET);
-    } else {
-      highlightSteps.push('V', BRC.CORNER_RADIUS);
-    }
   }
 };
 
 drawLeft = function(block, info, pathObject, cursorY) {
+  drawLeftHighlight(block, info, pathObject);
   var steps = pathObject.steps;
 
   if (info.hasOutputConnection) {
@@ -147,7 +86,6 @@ drawLeft = function(block, info, pathObject, cursorY) {
   // Close off the path.  This draws a vertical line up to the start of the
   // block's path, which may be either a rounded or a sharp corner.
   steps.push('z');
-  drawLeftHighlight(block, info, pathObject);
 };
 
 drawInternals = function(block, info, pathObject) {
@@ -199,67 +137,16 @@ layoutField = function(fieldInfo, cursorX, centerline, rowWidth, RTL) {
   }
 };
 
-highlightValueInput = function(row, pathObject, info, cursorY) {
-  var highlightSteps = pathObject.highlightSteps;
-  //var v = row.height - BRC.TAB_HEIGHT;
-
-  if (info.RTL) {
-    // Highlight around back of tab.
-    // TODO: Unfuck this.
-    highlightSteps.push('v', BRC.TAB_OFFSET_FROM_TOP - 3);
-    highlightSteps.push('m 0,2.5')
-    highlightSteps.push(BRC.TAB_PATH_DOWN_HIGHLIGHT_RTL);
-    highlightSteps.push('v', row.height - BRC.TAB_HEIGHT);
-  } else {
-    // Short highlight glint at bottom of tab.
-    highlightSteps.push('M', (info.maxValueOrDummyWidth - 5) + ',' +
-        (cursorY + BRC.TAB_HEIGHT - 0.7));
-    highlightSteps.push('l', (BRC.TAB_WIDTH * 0.46) + ',-2.1');
-  }
-};
-
 drawValueInput = function(row, pathObject, info, cursorY) {
+  highlightValueInput(row, pathObject, info, cursorY);
   var steps = pathObject.steps;
   steps.push('H', row.width);
   steps.push(BRC.TAB_PATH_DOWN);
   steps.push('v', row.height - BRC.TAB_HEIGHT);
-
-  highlightValueInput(row, pathObject, info, cursorY);
-};
-
-drawInlineInputHighlight = function(pathObject, x, y, input, centerline, isRTL) {
-  var highlightInlineSteps = pathObject.highlightInlineSteps;
-
-  var width = input.width;
-  var height = input.height;
-  var yPos = centerline - height / 2;
-  if (isRTL) {
-    // Highlight right edge, around back of tab, and bottom.
-    highlightInlineSteps.push('M', (x + BRC.TAB_WIDTH - 0.5) +
-      ',' + (yPos + BRC.TAB_OFFSET_FROM_TOP + 5));
-    highlightInlineSteps.push(
-        BRC.TAB_PATH_DOWN_HIGHLIGHT_RTL);
-    highlightInlineSteps.push('v',
-        height - Blockly.BlockSvg.TAB_HEIGHT + 1.5);
-    highlightInlineSteps.push('h',
-        width - BRC.TAB_WIDTH);
-  } else {
-    // Highlight right edge, bottom.
-    highlightInlineSteps.push('M',
-        (x + width + 0.5) + ',' +
-        (yPos + 0.5));
-    highlightInlineSteps.push('v', height);
-    highlightInlineSteps.push('h', BRC.TAB_WIDTH - width);
-    // Short highlight glint at bottom of tab.
-    highlightInlineSteps.push('M',
-        (x + 2.9) + ',' + (yPos + Blockly.BlockSvg.INLINE_PADDING_Y +
-         BRC.TAB_HEIGHT - 0.7));
-    highlightInlineSteps.push('l',
-        (BRC.TAB_WIDTH * 0.46) + ',-2.1');
-  }
 };
 
 drawInlineInput = function(pathObject, x, y, input, centerline, isRTL) {
+  drawInlineInputHighlight(pathObject, x, y, input, centerline, isRTL);
   var inlineSteps = pathObject.inlineSteps;
   var width = input.width;
   var height = input.height;
@@ -272,35 +159,10 @@ drawInlineInput = function(pathObject, x, y, input, centerline, isRTL) {
   inlineSteps.push('h', width - BRC.TAB_WIDTH);
   inlineSteps.push('v', -height);
   inlineSteps.push('z');
-
-  drawInlineInputHighlight(pathObject, x, y, input, centerline, isRTL);
-};
-
-drawStatementInputHighlight = function(row, pathObject, info, cursorY) {
-  var highlightSteps = pathObject.highlightSteps;
-  var x = row.statementEdge;
-  if (info.RTL) {
-    highlightSteps.push('M',
-        (x + BRC.DISTANCE_45_OUTSIDE) +
-        ',' + (cursorY + BRC.DISTANCE_45_OUTSIDE));
-    highlightSteps.push(
-        BRC.INNER_TOP_LEFT_CORNER_HIGHLIGHT_RTL);
-    highlightSteps.push('v',
-        row.height - 2 * BRC.CORNER_RADIUS);
-    highlightSteps.push(
-        BRC.INNER_BOTTOM_LEFT_CORNER_HIGHLIGHT_RTL);
-    highlightSteps.push('H', info.maxValueOrDummyWidth - BRC.HIGHLIGHT_OFFSET);
-  } else {
-    highlightSteps.push('M',
-        (x + BRC.DISTANCE_45_OUTSIDE) + ',' +
-        (cursorY + row.height - BRC.DISTANCE_45_OUTSIDE));
-    highlightSteps.push(
-        BRC.INNER_BOTTOM_LEFT_CORNER_HIGHLIGHT_LTR);
-    highlightSteps.push('H', info.maxValueOrDummyWidth - BRC.HIGHLIGHT_OFFSET);
-  }
 };
 
 drawStatementInput = function(row, pathObject, info, cursorY) {
+  drawStatementInputHighlight(row, pathObject, info, cursorY);
   var steps = pathObject.steps;
   var x = row.statementEdge + BRC.NOTCH_OFFSET;
   steps.push('H', x);
@@ -308,6 +170,4 @@ drawStatementInput = function(row, pathObject, info, cursorY) {
   steps.push('v', row.height - 2 * BRC.CORNER_RADIUS);
   steps.push(BRC.INNER_BOTTOM_LEFT_CORNER);
   steps.push('H', info.maxValueOrDummyWidth);
-
-  drawStatementInputHighlight(row, pathObject, info, cursorY);
 };

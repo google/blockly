807a9f8a431ec410d124a690d8daff16e0043a4d
Rachel Fenichel <fenichel@google.com>
2019-03-18 11:17:54 -0700

    move rendering files into a subdirectory

--- core/block_rendering_rewrite/block_render_draw.js
+++ core/block_rendering_rewrite/block_render_draw.js
@@ -1,5 +1,29 @@
-//goog.require('BlockRenderingConstants');
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2019 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+/**
+ * @fileoverview Methods for graphically rendering a block as SVG.
+ * @author fenichel@google.com (Rachel Fenichel)
+ */
+
+//'use strict';
 renderDraw = function(block, info) {
   var pathObject = new Blockly.BlockSvg.PathObject();
   //drawDebug(block, info, pathObject);

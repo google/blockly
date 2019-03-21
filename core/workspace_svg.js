b7dbf952dac03be6cec879ea6e5c36532baa4913
alschmiedt <aschmiedt@google.com>
2019-03-21 11:24:15 -0700

    Fixing more jsdocs

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -376,7 +376,7 @@ Blockly.WorkspaceSvg.prototype.inverseScreenCTMDirty_ = true;
 
 /**
  * Adds cursor for keyboard navigation.
- * @return{Blockly.CursorSvg} Cursor for keyboard navigation.
+ * @return {Blockly.CursorSvg} Cursor for keyboard navigation.
  */
 Blockly.WorkspaceSvg.prototype.addCursor = function() {
   return new Blockly.CursorSvg(this);

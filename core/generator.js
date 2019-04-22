1ca7d6a28bffd5f9be732c3389d6871677c799f4
alschmiedt <aschmiedt@google.com>
2019-04-22 14:20:05 -0700

    Clarifies what statementToCode does

--- core/generator.js
+++ core/generator.js
@@ -274,7 +274,8 @@ Blockly.Generator.prototype.valueToCode = function(block, name, outerOrder) {
 };
 
 /**
- * Generate code representing the statement.  Indent the code.
+ * Generate a code string representing the blocks attached to the named
+ * statement input. Indent the code.
  * This is mainly used in generators. When trying to generate code to evaluate
  * look at using workspaceToCode or blockToCode.
  * @param {!Blockly.Block} block The block containing the input.

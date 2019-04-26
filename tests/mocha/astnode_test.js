490ec66c4c4a54e07cfb5b146c1b6059c0fd75ba
Rachel Fenichel <fenichel@google.com>
2019-04-26 14:17:33 -0700

    Fix eslint

--- tests/mocha/astnode_test.js
+++ tests/mocha/astnode_test.js
@@ -93,88 +93,88 @@ suite('ASTNode', function() {
   });
 
   suite('HelperFunctions', function() {
-      test('findNextEditableField_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = input.fieldRow[0];
-        var nextField = input.fieldRow[1];
-        var node = Blockly.ASTNode.createFieldNode(field);
-        var editableField = node.findNextEditableField_(field, input);
-        assertEquals(editableField.getLocation(), nextField);
-      });
-
-      test('findNextEditableFieldFirst_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = input.fieldRow[1];
-        var node = Blockly.ASTNode.createFieldNode(field);
-        var editableField = node.findNextEditableField_(field, input, true);
-        assertEquals(editableField.getLocation(), input.fieldRow[0]);
-      });
-
-      test('findPreviousEditableField_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = input.fieldRow[1];
-        var prevField = input.fieldRow[0];
-        var node = Blockly.ASTNode.createFieldNode(prevField);
-        var editableField = node.findPreviousEditableField_(field, input);
-        assertEquals(editableField.getLocation(), prevField);
-      });
-
-      test('findPreviousEditableFieldLast_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = input.fieldRow[0];
-        var node = Blockly.ASTNode.createFieldNode(field);
-        var editableField = node.findPreviousEditableField_(field, input, true);
-        assertEquals(editableField.getLocation(), input.fieldRow[1]);
-      });
-
-      test('findNextForInput_', function() {
-        var input = this.blocks.A.inputList[0];
-        var input2 = this.blocks.A.inputList[1];
-        var connection = input.connection;
-        var node = Blockly.ASTNode.createConnectionNode(connection);
-        var newASTNode = node.findNextForInput_(connection, input);
-        assertEquals(newASTNode.getLocation(), input2.connection);
-      });
-
-      test('findPrevForInput_', function() {
-        var input = this.blocks.A.inputList[0];
-        var input2 = this.blocks.A.inputList[1];
-        var connection = input2.connection;
-        var node = Blockly.ASTNode.createConnectionNode(connection);
-        var newASTNode = node.findPrevForInput_(connection, input2);
-        assertEquals(newASTNode.getLocation(), input.connection);
-      });
-
-      test('findNextForField_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = this.blocks.A.inputList[0].fieldRow[0];
-        var field2 = this.blocks.A.inputList[0].fieldRow[1];
-        var node = Blockly.ASTNode.createFieldNode(field2);
-        var newASTNode = node.findNextForField_(field, input);
-        assertEquals(newASTNode.getLocation(), field2);
-      });
-
-      test('findPrevForField_', function() {
-        var input = this.blocks.A.inputList[0];
-        var field = this.blocks.A.inputList[0].fieldRow[0];
-        var field2 = this.blocks.A.inputList[0].fieldRow[1];
-        var node = Blockly.ASTNode.createFieldNode(field2);
-        var newASTNode = node.findPrevForField_(field2, input);
-        assertEquals(newASTNode.getLocation(), field);
-      });
-
-      test('navigateBetweenStacksForward', function() {
-        var node = new Blockly.ASTNode(Blockly.ASTNode.types.NEXT, this.blocks.A.nextConnection);
-        var newASTNode = node.navigateBetweenStacks_(true);
-        assertEquals(newASTNode.getLocation(), this.blocks.D);
-      });
-
-      test('navigateBetweenStacksBackward', function() {
-        var node = new Blockly.ASTNode(Blockly.ASTNode.types.BLOCK, this.blocks.D);
-        var newASTNode = node.navigateBetweenStacks_(false);
-        assertEquals(newASTNode.getLocation(), this.blocks.A);
-      });
+    test('findNextEditableField_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = input.fieldRow[0];
+      var nextField = input.fieldRow[1];
+      var node = Blockly.ASTNode.createFieldNode(field);
+      var editableField = node.findNextEditableField_(field, input);
+      assertEquals(editableField.getLocation(), nextField);
     });
+
+    test('findNextEditableFieldFirst_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = input.fieldRow[1];
+      var node = Blockly.ASTNode.createFieldNode(field);
+      var editableField = node.findNextEditableField_(field, input, true);
+      assertEquals(editableField.getLocation(), input.fieldRow[0]);
+    });
+
+    test('findPreviousEditableField_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = input.fieldRow[1];
+      var prevField = input.fieldRow[0];
+      var node = Blockly.ASTNode.createFieldNode(prevField);
+      var editableField = node.findPreviousEditableField_(field, input);
+      assertEquals(editableField.getLocation(), prevField);
+    });
+
+    test('findPreviousEditableFieldLast_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = input.fieldRow[0];
+      var node = Blockly.ASTNode.createFieldNode(field);
+      var editableField = node.findPreviousEditableField_(field, input, true);
+      assertEquals(editableField.getLocation(), input.fieldRow[1]);
+    });
+
+    test('findNextForInput_', function() {
+      var input = this.blocks.A.inputList[0];
+      var input2 = this.blocks.A.inputList[1];
+      var connection = input.connection;
+      var node = Blockly.ASTNode.createConnectionNode(connection);
+      var newASTNode = node.findNextForInput_(connection, input);
+      assertEquals(newASTNode.getLocation(), input2.connection);
+    });
+
+    test('findPrevForInput_', function() {
+      var input = this.blocks.A.inputList[0];
+      var input2 = this.blocks.A.inputList[1];
+      var connection = input2.connection;
+      var node = Blockly.ASTNode.createConnectionNode(connection);
+      var newASTNode = node.findPrevForInput_(connection, input2);
+      assertEquals(newASTNode.getLocation(), input.connection);
+    });
+
+    test('findNextForField_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = this.blocks.A.inputList[0].fieldRow[0];
+      var field2 = this.blocks.A.inputList[0].fieldRow[1];
+      var node = Blockly.ASTNode.createFieldNode(field2);
+      var newASTNode = node.findNextForField_(field, input);
+      assertEquals(newASTNode.getLocation(), field2);
+    });
+
+    test('findPrevForField_', function() {
+      var input = this.blocks.A.inputList[0];
+      var field = this.blocks.A.inputList[0].fieldRow[0];
+      var field2 = this.blocks.A.inputList[0].fieldRow[1];
+      var node = Blockly.ASTNode.createFieldNode(field2);
+      var newASTNode = node.findPrevForField_(field2, input);
+      assertEquals(newASTNode.getLocation(), field);
+    });
+
+    test('navigateBetweenStacksForward', function() {
+      var node = new Blockly.ASTNode(Blockly.ASTNode.types.NEXT, this.blocks.A.nextConnection);
+      var newASTNode = node.navigateBetweenStacks_(true);
+      assertEquals(newASTNode.getLocation(), this.blocks.D);
+    });
+
+    test('navigateBetweenStacksBackward', function() {
+      var node = new Blockly.ASTNode(Blockly.ASTNode.types.BLOCK, this.blocks.D);
+      var newASTNode = node.navigateBetweenStacks_(false);
+      assertEquals(newASTNode.getLocation(), this.blocks.A);
+    });
+  });
 
   suite('NavigationFunctions', function() {
     suite('Next', function() {
@@ -247,7 +247,7 @@ suite('ASTNode', function() {
       });
       test('nextConnection', function() {
         var nextConnection = this.blocks.A.nextConnection;
-        var node = Blockly.ASTNode.createConnectionNode(nextConnection)
+        var node = Blockly.ASTNode.createConnectionNode(nextConnection);
         var prevNode = node.prev();
         assertEquals(prevNode.getLocation(), this.blocks.A);
       });

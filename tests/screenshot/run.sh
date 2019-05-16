115769af0325dbef80b739f2412beee4c7e0bd43
Rachel Fenichel <fenichel@google.com>
2019-05-16 14:20:47 -0700

    Delete old files

--- tests/screenshot/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# tests/scripts/test_setup.sh
-node tests/screenshot/sandbox.js
-clear
-./node_modules/.bin/mocha tests/screenshot/test_script.js --opts "./tests/screenshot/mocha.opts"

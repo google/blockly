8994ce12f7c364ca4cf60233d9861a814374d0ba
Rachel Fenichel <fenichel@google.com>
2019-05-16 14:08:10 -0700

    First pass at screenshot diff testing

---
+++ tests/screenshot/gen_screenshots.js
@@ -0,0 +1,92 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2019 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Node.js script to run Mocha tests in Chrome, via webdriver.
+ */
+var webdriverio = require('webdriverio');
+var fs = require('fs');
+
+module.exports = genScreenshots;
+
+/**
+ * Opens two different webdriverio browsers.  One uses the hosted version of
+ * blockly_compressed.js; the other uses the local blockly_uncompressed.js.
+ *
+ * Each playground is a minimal Blockly instance.  This loads the same XML in
+ * both playgrounds and saves a screenshot of each.
+ */
+async function genScreenshots() {
+  // TODOs:
+  // - open the test cases folder
+  // - iterate over all files in the test cases folder
+  // - use the input file name for the output file names
+  var prefix = './tests/screenshot/';
+  var xml_url = prefix + 'test_cases/math_addition';
+  var xml = fs.readFileSync(xml_url, 'utf8');
+  var url_prefix = 'file://' + __dirname + '/playground';
+
+  var browser_new = await buildBrowser(url_prefix + '_new.html');
+  var browser_old = await buildBrowser(url_prefix + '_old.html');
+
+  await genSingleScreenshot(browser_new, 'new', 'math_addition');
+  await genSingleScreenshot(browser_old, 'old', 'math_addition');
+
+  await cleanUp(browser_new, browser_old);
+  return 0;
+}
+
+async function cleanUp(browser_new, browser_old) {
+  await browser_new.deleteSession();
+  await browser_old.deleteSession();
+}
+
+async function buildBrowser(url) {
+  var options = {
+      capabilities: {
+          browserName: 'chrome'
+      }
+  }
+  console.log('Starting webdriverio...');
+  const browser = await webdriverio.remote(options);
+  await browser.setWindowSize(1000, 1000);
+  console.log('Initialized.\nLoading url: ' + url);
+  await browser.url(url);
+  return browser;
+}
+
+async function genSingleScreenshot(browser, dir, test_name) {
+  var prefix = './tests/screenshot/';
+  var xml_url = prefix + 'test_cases/' + test_name;
+  var xml = fs.readFileSync(xml_url, 'utf8');
+
+  var doStuffFn = function(xml_text) {
+    workspace.clear();
+    var xml = Blockly.Xml.textToDom(xml_text);
+    Blockly.Xml.domToWorkspace(xml, workspace);
+  };
+  await browser.execute(doStuffFn, xml);
+  await browser.saveScreenshot(prefix + '/outputs/' + dir + '/' + test_name + '.png');
+}
+
+
+if (require.main === module) {
+  genScreenshots();
+}
---
+++ tests/screenshot/test_script.js
@@ -0,0 +1,41 @@
+var chai = require('chai');
+var fs = require('fs'),
+    PNG = require('pngjs').PNG,
+    pixelmatch = require('pixelmatch');
+
+var old_dir = 'tests/screenshot/outputs/old/';
+var new_dir = 'tests/screenshot/outputs/new/';
+var diff_dir = 'tests/screenshot/outputs/diff/';
+
+// TODOs:
+// - iterate over all test cases
+// - use the name of the test case file as the name of the test
+// - improve doneReading to not rely on globals like this
+// - write or find basic html page that lets you open pairs of outputs + diffs
+
+suite('Rendering', function() {
+  test('math_addition', function(done) {
+    function doneReading() {
+      if (++filesRead < 2) return;
+      var diff = new PNG({width: img1.width, height: img1.height});
+
+      var mismatch_num = pixelmatch(
+          img1.data,
+          img2.data,
+          diff.data,
+          img1.width,
+          img1.height, {threshold: 0.1});
+      console.log('mismatch: ' + mismatch_num);
+
+      diff.pack().pipe(fs.createWriteStream(diff_dir + 'my_screenshot.png'));
+      chai.assert.equal(mismatch_num, 0);
+      done();
+    }
+
+    var img1 = fs.createReadStream(old_dir + 'my_screenshot.png')
+        .pipe(new PNG()).on('parsed', doneReading);
+    var img2 = fs.createReadStream(new_dir + 'my_screenshot.png')
+        .pipe(new PNG()).on('parsed', doneReading);
+    var filesRead = 0;
+  });
+});

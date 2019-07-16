db9835be763b3ee6d220d2ed7920046fbd8c2433
ivml <ivanmladenov91@gmail.com>
2019-07-16 12:53:00 +0200

    Added SSL flag to ignore errors

--- build.py
+++ build.py
@@ -54,6 +54,11 @@
 
 import sys
 
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
 for arg in sys.argv[1:len(sys.argv)]:
   if (arg != 'core' and
       arg != 'accessible' and

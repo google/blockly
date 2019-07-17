41f3d5ba840b0c6f688ec3227d65bc975a3715d0
Beka Westberg <bekawestberg@gmail.com>
2019-07-17 12:49:55 -0700

    Added test blocks.

--- tests/blocks/test_blocks.js
+++ tests/blocks/test_blocks.js
@@ -412,6 +412,104 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
     "tooltip": "A midi note."
   },
   {
+    "type": "test_angles_clockwise",
+    "message0": "clockwise %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "clockwise": true
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_offset",
+    "message0": "offset 90 %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "offset": 90
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_wrap",
+    "message0": "wrap %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "wrap": 180
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_round_30",
+    "message0": "round 30 %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "round": 30
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_round_0",
+    "message0": "no round %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "round": 0
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_protractor",
+    "message0": "protractor %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "mode": "protractor"
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
+    "type": "test_angles_compass",
+    "message0": "compass %1",
+    "args0": [
+      {
+        "type": "field_angle",
+        "name": "FIELDNAME",
+        "angle": 0,
+        "mode": "compass"
+      }
+    ],
+    "style": "math_blocks",
+    "tooltip": "test tooltip"
+  },
+  {
     "type": "test_images_datauri",
     "message0": "Image data: URI %1",
     "args0": [

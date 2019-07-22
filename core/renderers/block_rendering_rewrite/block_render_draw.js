91a8f4de16cb330f5bdf46aad6c1f5868ab7f619
Rachel Fenichel <fenichel@google.com>
2019-07-22 14:10:14 -0700

    Pull all the new rendering files into develop

---
+++ core/renderers/block_rendering_rewrite/block_render_draw.js
@@ -0,0 +1,430 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2019 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Methods for graphically rendering a block as SVG.
+ * @author fenichel@google.com (Rachel Fenichel)
+ */
+
+//'use strict';
+goog.provide('Blockly.BlockRendering.Drawer');
+
+goog.require('Blockly.BlockRendering.Debug');
+goog.require('Blockly.BlockRendering.RenderInfo');
+goog.require('Blockly.BlockRendering.Highlighter');
+goog.require('BRC');
+/* global BRC */
+goog.require('Blockly.BlockRendering.Measurable');
+
+/**
+ * Render the given block.
+ * @param {!Blockly.BlockSvg} block The block to render
+ * @public
+ */
+Blockly.BlockRendering.render = function(block) {
+  if (!block.renderingDebugger) {
+    block.renderingDebugger = new Blockly.BlockRendering.Debug();
+  }
+  new Blockly.BlockRendering.Drawer(block).draw_();
+};
+
+/**
+ * An object that draws a block based on the given rendering information.
+ * @param {!Blockly.BlockSvg} block The block to render
+ * @param {!Blockly.BlockRendering.RenderInfo} info An object containing all
+ *   information needed to render this block.
+ * @private
+ */
+Blockly.BlockRendering.Drawer = function(block) {
+  this.block_ = block;
+  this.topLeft_ = block.getRelativeToSurfaceXY();
+  this.info_ = new Blockly.BlockRendering.RenderInfo(block);
+  this.pathObject_ = new Blockly.BlockSvg.PathObject();
+  this.steps_ = this.pathObject_.steps;
+  this.inlineSteps_ = this.pathObject_.inlineSteps;
+  this.highlighter_ =
+      new Blockly.BlockRendering.Highlighter(this.info_, this.pathObject_);
+};
+
+/**
+ * Draw the block to the workspace. Here "drawing" means setting SVG path
+ * elements and moving fields, icons, and connections on the screen.
+ *
+ * The pieces of the paths are pushed into arrays of "steps", which are then
+ * joined with spaces and set directly on the block.  This guarantees that
+ * the steps are separated by spaces for improved readability, but isn't
+ * required.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.draw_ = function() {
+  this.drawOutline_();
+  this.drawInternals_();
+  this.block_.setPaths_(this.pathObject_);
+  this.block_.renderingDebugger.drawDebug(this.block_, this.info_);
+  this.recordSizeOnBlock_();
+};
+
+/**
+ * Save sizing information back to the block
+ * Most of the rendering information can be thrown away at the end of the render.
+ * Anything that needs to be kept around should be set in this function.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.recordSizeOnBlock_ = function() {
+  // This is used when the block is reporting its size to anyone else.
+  // The dark path adds to the size of the block in both X and Y.
+  this.block_.height = this.info_.height + BRC.DARK_PATH_OFFSET;
+  this.block_.width = this.info_.widthWithChildren + BRC.DARK_PATH_OFFSET;
+  // The flyout uses this information.
+  this.block_.startHat_ = this.info_.topRow.startHat;
+};
+
+/**
+ * Create the outline of the block.  This is a single continuous path.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawOutline_ = function() {
+  this.drawTop_();
+  for (var r = 1; r < this.info_.rows.length - 1; r++) {
+    var row = this.info_.rows[r];
+    if (row.hasStatement) {
+      this.drawStatementInput_(row);
+    } else if (row.hasExternalInput) {
+      this.drawValueInput_(row);
+    } else {
+      this.drawRightSideRow_(row);
+    }
+  }
+  this.drawBottom_();
+  this.drawLeft_();
+};
+
+
+/**
+ * Add steps for the top corner of the block, taking into account
+ * details such as hats and rounded corners.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawTop_ = function() {
+  var topRow = this.info_.topRow;
+  var elements = topRow.elements;
+
+  this.highlighter_.drawTopCorner(topRow);
+  this.highlighter_.drawRightSideRow(topRow);
+  this.positionPreviousConnection_();
+
+  for (var i = 0, elem; elem = elements[i]; i++) {
+    if (elem.type === 'square corner') {
+      this.steps_.push(BRC.START_POINT);
+    } else if (elem.type === 'round corner') {
+      this.steps_.push(BRC.TOP_LEFT_CORNER_START, BRC.TOP_LEFT_CORNER);
+    } else if (elem.type === 'previous connection') {
+      this.steps_.push(BRC.NOTCH_PATH_LEFT);
+    } else if (elem.type === 'hat') {
+      this.steps_.push(BRC.START_HAT_PATH);
+    } else if (elem.isSpacer()) {
+      this.steps_.push('h', elem.width);
+    }
+  }
+  this.steps_.push('v', topRow.height);
+};
+
+
+/**
+ * Add steps for an external value input, rendered as a notch in the side
+ * of the block.
+ * @param {!Blockly.BlockRendering.Row} row The row that this input
+ *     belongs to.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawValueInput_ = function(row) {
+  this.highlighter_.drawValueInput(row);
+  this.steps_.push('H', row.width);
+  this.steps_.push(BRC.TAB_PATH_DOWN);
+  this.steps_.push('v', row.height - BRC.TAB_HEIGHT);
+  this.positionExternalValueConnection_(row);
+};
+
+
+/**
+ * Add steps for a statement input.
+ * @param {!Blockly.BlockRendering.Row} row The row that this input
+ *     belongs to.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawStatementInput_ = function(row) {
+  this.highlighter_.drawStatementInput(row);
+  var x = row.statementEdge + BRC.NOTCH_OFFSET_RIGHT;
+  this.steps_.push('H', x);
+  this.steps_.push(BRC.INNER_TOP_LEFT_CORNER);
+  this.steps_.push('v', row.height - 2 * BRC.CORNER_RADIUS);
+  this.steps_.push(BRC.INNER_BOTTOM_LEFT_CORNER);
+
+  this.positionStatementInputConnection_(row);
+};
+
+/**
+ * Add steps for the right side of a row that does not have value or
+ * statement input connections.
+ * @param {!Blockly.BlockRendering.Row} row The row to draw the
+ *     side of.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawRightSideRow_ = function(row) {
+  this.highlighter_.drawRightSideRow(row);
+  this.steps_.push('H', row.width);
+  this.steps_.push('v', row.height);
+};
+
+
+/**
+ * Add steps for the bottom edge of a block, possibly including a notch
+ * for the next connection
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawBottom_ = function() {
+  var bottomRow = this.info_.bottomRow;
+  var elems = bottomRow.elements;
+  this.highlighter_.drawBottomCorner(bottomRow);
+  this.positionNextConnection_();
+  this.steps_.push('v', bottomRow.height);
+  for (var i = elems.length - 1; i >= 0; i--) {
+    var elem = elems[i];
+    if (elem.type === 'next connection') {
+      this.steps_.push(BRC.NOTCH_PATH_RIGHT);
+    } else if (elem.type === 'square corner') {
+      this.steps_.push('H 0');
+    } else if (elem.type === 'round corner') {
+      this.steps_.push(BRC.BOTTOM_LEFT_CORNER);
+    } else if (elem.isSpacer()) {
+      this.steps_.push('h', elem.width * -1);
+    }
+  }
+};
+
+/**
+ * Add steps for the left side of the block, which may include an output
+ * connection
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawLeft_ = function() {
+  this.highlighter_.drawLeft();
+
+  this.positionOutputConnection_();
+  if (this.info_.hasOutputConnection) {
+    // Draw a line up to the bottom of the tab.
+    this.steps_.push('V', BRC.TAB_OFFSET_FROM_TOP + BRC.TAB_HEIGHT);
+    this.steps_.push(BRC.TAB_PATH_UP);
+  }
+  // Close off the path.  This draws a vertical line up to the start of the
+  // block's path, which may be either a rounded or a sharp corner.
+  this.steps_.push('z');
+};
+
+/**
+ * Draw the internals of the block: inline inputs, fields, and icons.  These do
+ * not depend on the outer path for placement.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawInternals_ = function() {
+  for (var r = 0; r < this.info_.rows.length; r++) {
+    var row = this.info_.rows[r];
+    if (!(row.isSpacer())) {
+      for (var e = 0; e < row.elements.length; e++) {
+        var elem = row.elements[e];
+        if (elem.isInlineInput()) {
+          this.drawInlineInput_(elem);
+        } else if (elem.isIcon() || elem.isField()) {
+          this.layoutField_(elem);
+        }
+      }
+    }
+  }
+};
+
+/**
+ * Some fields are terrible and render offset from where they claim to be
+ * rendered.  This function calculates an x offset for fields that need it.
+ * No one is happy about this.
+ * @param {!Blockly.Field} field The field to find an offset for.
+ * @return {number} How far to offset the field in the x direction.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.dealWithJackassFields_ = function(field) {
+  if (field instanceof Blockly.FieldDropdown
+      || field instanceof Blockly.FieldTextInput
+      || field instanceof Blockly.FieldColour
+      || field instanceof Blockly.FieldCheckbox) {
+    return 5;
+  }
+  return 0;
+};
+
+/**
+ * Push a field or icon's new position to its SVG root.
+ * @param {!Blockly.BlockRendering.Icon|!Blockly.BlockRendering.Field} fieldInfo
+ *     The rendering information for the field or icon.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.layoutField_ = function(fieldInfo) {
+  if (fieldInfo.type == 'field') {
+    var svgGroup = fieldInfo.field.getSvgRoot();
+  } else if (fieldInfo.type == 'icon') {
+    var svgGroup = fieldInfo.icon.iconGroup_;
+  }
+
+  var yPos = fieldInfo.centerline - fieldInfo.height / 2;
+  var xPos = fieldInfo.xPos;
+  if (this.info_.RTL) {
+    xPos = -(xPos + fieldInfo.width);
+  }
+  if (fieldInfo.type == 'icon') {
+    svgGroup.setAttribute('display', 'block');
+    svgGroup.setAttribute('transform', 'translate(' + xPos + ',' + yPos + ')');
+    fieldInfo.icon.computeIconLocation();
+  } else {
+    xPos += this.dealWithJackassFields_(fieldInfo.field);
+
+    svgGroup.setAttribute('transform', 'translate(' + xPos + ',' + yPos + ')');
+  }
+
+  if (this.info_.isInsertionMarker) {
+    // Fields and icons are invisible on insertion marker.  They still have to
+    // be rendered so that the block can be sized correctly.
+    svgGroup.setAttribute('display', 'none');
+  }
+};
+
+/**
+ * Add steps for an inline input.
+ * @param {Blockly.BlockRendering.RenderableInput} input The information about the
+ * input to render.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.drawInlineInput_ = function(input) {
+  this.highlighter_.drawInlineInput(input);
+  var width = input.width;
+  var height = input.height;
+  var yPos = input.centerline - height / 2;
+
+  this.inlineSteps_.push('M', (input.xPos + BRC.TAB_WIDTH) + ',' + yPos);
+  this.inlineSteps_.push('v ', BRC.TAB_OFFSET_FROM_TOP);
+  this.inlineSteps_.push(BRC.TAB_PATH_DOWN);
+  this.inlineSteps_.push('v', height - BRC.TAB_HEIGHT - BRC.TAB_OFFSET_FROM_TOP);
+  this.inlineSteps_.push('h', width - BRC.TAB_WIDTH);
+  this.inlineSteps_.push('v', -height);
+  this.inlineSteps_.push('z');
+
+  this.positionInlineInputConnection_(input);
+
+};
+
+/**
+ * Position the connection on an inline value input, taking into account
+ * RTL and the small gap between the parent block and child block which lets the
+ * parent block's dark path show through.
+ * @param {Blockly.BlockRendering.RenderableInput} input The information about
+ * the input that the connection is on.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionInlineInputConnection_ = function(input) {
+  var yPos = input.centerline - input.height / 2;
+  // Move the connection.
+  if (input.connection) {
+    var connX = input.xPos + BRC.TAB_WIDTH + BRC.DARK_PATH_OFFSET;
+    if (this.info_.RTL) {
+      connX *= -1;
+    }
+    input.connection.setOffsetInBlock(
+        connX, yPos + BRC.TAB_OFFSET_FROM_TOP + BRC.DARK_PATH_OFFSET);
+  }
+};
+
+/**
+ * Position the connection on a statement input, taking into account
+ * RTL and the small gap between the parent block and child block which lets the
+ * parent block's dark path show through.
+ * @param {!Blockly.BlockRendering.Row} row The row that the connection is on.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionStatementInputConnection_ = function(row) {
+  var input = row.getLastInput();
+  if (input.connection) {
+    var connX = row.statementEdge + BRC.NOTCH_OFFSET_LEFT + BRC.DARK_PATH_OFFSET;
+    if (this.info_.RTL) {
+      connX *= -1;
+    }
+    input.connection.setOffsetInBlock(connX, row.yPos + BRC.DARK_PATH_OFFSET);
+  }
+};
+
+/**
+ * Position the connection on an external value input, taking into account
+ * RTL and the small gap between the parent block and child block which lets the
+ * parent block's dark path show through.
+ * @param {!Blockly.BlockRendering.Row} row The row that the connection is on.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionExternalValueConnection_ = function(row) {
+  var input = row.getLastInput();
+  if (input.connection) {
+    var connX = row.width + BRC.DARK_PATH_OFFSET;
+    if (this.info_.RTL) {
+      connX *= -1;
+    }
+    input.connection.setOffsetInBlock(connX, row.yPos);
+  }
+};
+
+/**
+ * Position the previous connection on a block.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionPreviousConnection_ = function() {
+  if (this.info_.topRow.hasPreviousConnection) {
+    var connX = this.info_.RTL ? -BRC.NOTCH_OFFSET_LEFT : BRC.NOTCH_OFFSET_LEFT;
+    this.info_.topRow.connection.setOffsetInBlock(connX, 0);
+  }
+};
+
+/**
+ * Position the next connection on a block.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionNextConnection_ = function() {
+  var bottomRow = this.info_.bottomRow;
+
+  if (bottomRow.hasNextConnection) {
+    var connX = this.info_.RTL ? -BRC.NOTCH_OFFSET_LEFT : BRC.NOTCH_OFFSET_LEFT;
+    bottomRow.connection.setOffsetInBlock(
+        connX, this.info_.height + BRC.DARK_PATH_OFFSET);
+  }
+};
+
+/**
+ * Position the output connection on a block.
+ * @param {!Blockly.BlockRendering.BottomRow} row The bottom row on the block.
+ * @private
+ */
+Blockly.BlockRendering.Drawer.prototype.positionOutputConnection_ = function() {
+  if (this.info_.hasOutputConnection) {
+    this.block_.outputConnection.setOffsetInBlock(0, BRC.TAB_OFFSET_FROM_TOP);
+  }
+};
---
+++ core/renderers/block_rendering_rewrite/block_rendering_constants.js
@@ -0,0 +1,357 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2019 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Methods for graphically rendering a block as SVG.
+ * @author fenichel@google.com (Rachel Fenichel)
+ */
+
+//'use strict';
+goog.provide('BRC');
+/* global BRC */
+
+goog.require('Blockly.utils.Paths');
+
+
+BRC.NO_PADDING = 0;
+BRC.SMALL_PADDING = 3;
+BRC.MEDIUM_PADDING = 5;
+BRC.MEDIUM_LARGE_PADDING = 8;
+BRC.LARGE_PADDING = 10;
+
+// Offset from the top of the row for placing fields on inline input rows
+// and statement input rows.
+// Matches existing rendering (in 2019).
+BRC.TALL_INPUT_FIELD_OFFSET_Y = BRC.MEDIUM_PADDING;
+
+BRC.HIGHLIGHT_OFFSET = 0.5;
+
+// The dark/shadow path in classic rendering is the same as the normal block
+// path, but translated down one and right one.
+BRC.DARK_PATH_OFFSET = 1;
+
+BRC.TAB_HEIGHT = 15;
+
+BRC.TAB_OFFSET_FROM_TOP = 5;
+
+BRC.TAB_VERTICAL_OVERLAP = 2.5;
+
+BRC.TAB_WIDTH = 8;
+
+BRC.NOTCH_WIDTH = 15;
+BRC.NOTCH_HEIGHT = 4;
+
+// This is the minimum width of a block measuring from the end of a rounded
+// corner
+BRC.MIN_BLOCK_WIDTH = 12;
+
+BRC.EMPTY_BLOCK_SPACER_HEIGHT = 16;
+
+
+// Offset from the left side of a block or the inside of a statement input to
+// the left side of the notch.
+BRC.NOTCH_OFFSET_LEFT = BRC.NOTCH_WIDTH;
+
+// This is the width from where a rounded corner ends to where a previous
+// connection starts.
+BRC.NOTCH_OFFSET_ROUNDED_CORNER_PREV = 7;
+
+// This is the offset from the vertical part of a statement input
+// to where to start the notch, which is on the right side in LTR.
+BRC.NOTCH_OFFSET_RIGHT = BRC.NOTCH_OFFSET_LEFT + BRC.NOTCH_WIDTH;
+
+BRC.STATEMENT_BOTTOM_SPACER = 5;
+BRC.STATEMENT_INPUT_PADDING_LEFT = 20;
+BRC.BETWEEN_STATEMENT_PADDING_Y = 4;
+
+// This is the max width of a bottom row that follows a statement input and
+// has inputs inline.
+BRC.MAX_BOTTOM_WIDTH = 66.5;
+
+/**
+ * Rounded corner radius.
+ * @const
+ */
+BRC.CORNER_RADIUS = 8;
+/**
+ * Height of the top hat.
+ * @const
+ */
+BRC.START_HAT_HEIGHT = 15;
+
+BRC.START_HAT_WIDTH = 100;
+
+BRC.SPACER_DEFAULT_HEIGHT = 15;
+
+BRC.MIN_BLOCK_HEIGHT = 24;
+
+BRC.EMPTY_INLINE_INPUT_WIDTH = 22.5;
+
+BRC.EMPTY_INLINE_INPUT_HEIGHT = 26;
+
+BRC.EXTERNAL_VALUE_INPUT_WIDTH = 10;
+
+/**
+ * The height of an empty statement input.  Note that in the old rendering this
+ * varies slightly depending on whether the block has external or inline inputs.
+ * In the new rendering this is consistent.  It seems unlikely that the old
+ * behaviour was intentional.
+ * @const
+ * @type {number}
+ */
+BRC.EMPTY_STATEMENT_INPUT_HEIGHT = BRC.MIN_BLOCK_HEIGHT;
+
+BRC.EMPTY_STATEMENT_INPUT_WIDTH = 32;
+
+BRC.POPULATED_STATEMENT_INPUT_WIDTH = 25;
+
+
+BRC.START_POINT = Blockly.utils.Paths.moveBy(0, 0);
+
+BRC.START_POINT_HIGHLIGHT =
+    Blockly.utils.Paths.moveBy(BRC.HIGHLIGHT_OFFSET, BRC.HIGHLIGHT_OFFSET);
+
+/**
+ * Distance from shape edge to intersect with a curved corner at 45 degrees.
+ * Applies to highlighting on around the inside of a curve.
+ * @const
+ */
+BRC.DISTANCE_45_INSIDE = (1 - Math.SQRT1_2) *
+    (BRC.CORNER_RADIUS - BRC.HIGHLIGHT_OFFSET) + BRC.HIGHLIGHT_OFFSET;
+
+/**
+ * Distance from shape edge to intersect with a curved corner at 45 degrees.
+ * Applies to highlighting on around the outside of a curve.
+ * @const
+ */
+BRC.DISTANCE_45_OUTSIDE = (1 - Math.SQRT1_2) *
+    (BRC.CORNER_RADIUS + BRC.HIGHLIGHT_OFFSET) - BRC.HIGHLIGHT_OFFSET;
+
+/**
+ * SVG path for drawing a horizontal puzzle tab from top to bottom.
+ * @const
+ */
+BRC.TAB_PATH_DOWN =  'c 0,10 -' + BRC.TAB_WIDTH +
+      ',-8 -' + BRC.TAB_WIDTH + ',7.5 s ' +
+      BRC.TAB_WIDTH + ',-2.5 ' + BRC.TAB_WIDTH + ',7.5';
+
+
+/**
+ * SVG path for drawing a horizontal puzzle tab from top to bottom with
+ * highlighting from the upper-right.
+ * @const
+ */
+BRC.TAB_PATH_DOWN_HIGHLIGHT_RTL = 'm -' +
+    (BRC.TAB_WIDTH * 0.97) + ',2.5 q -' +
+    (BRC.TAB_WIDTH * 0.05) + ',10 ' +
+    (BRC.TAB_WIDTH * 0.3) + ',9.5 m ' +
+    (BRC.TAB_WIDTH * 0.67) + ',-1.9';
+
+/**
+ * SVG path for drawing a horizontal puzzle tab from bottom to top.
+ * @const
+ */
+BRC.TAB_PATH_UP =  'c 0,-10 -' + BRC.TAB_WIDTH +
+      ',8 -' + BRC.TAB_WIDTH + ',-7.5 s ' +
+      BRC.TAB_WIDTH + ',2.5 ' + BRC.TAB_WIDTH + ',-7.5';
+
+/**
+ * Path of the top hat's curve.
+ * @const
+ */
+BRC.START_HAT_PATH =
+    Blockly.utils.Paths.curve('c',
+        [
+          Blockly.utils.Paths.point(30, -BRC.START_HAT_HEIGHT),
+          Blockly.utils.Paths.point(70, -BRC.START_HAT_HEIGHT),
+          Blockly.utils.Paths.point(BRC.START_HAT_WIDTH, 0)
+        ]);
+/**
+ * SVG path for drawing next/previous notch from left to right.
+ * @const
+ */
+BRC.NOTCH_PATH_LEFT = 'l 6,4 3,0 6,-4';
+
+/**
+ * SVG path for drawing next/previous notch from left to right with
+ * highlighting.
+ * @const
+ */
+BRC.NOTCH_PATH_LEFT_HIGHLIGHT =
+    'h ' + BRC.HIGHLIGHT_OFFSET + ' ' + BRC.NOTCH_PATH_LEFT;
+
+/**
+ * SVG path for drawing next/previous notch from right to left.
+ * @const
+ */
+BRC.NOTCH_PATH_RIGHT = 'l -6,4 -3,0 -6,-4';
+
+/**
+ * SVG path for drawing the top-left corner of a statement input.
+ * Includes the top notch, a horizontal space, and the rounded inside corner.
+ * @const
+ */
+BRC.INNER_TOP_LEFT_CORNER =
+    BRC.NOTCH_PATH_RIGHT + ' h -' +
+    (BRC.NOTCH_WIDTH - BRC.CORNER_RADIUS) +
+    ' a ' + BRC.CORNER_RADIUS + ',' +
+    BRC.CORNER_RADIUS + ' 0 0,0 -' +
+    BRC.CORNER_RADIUS + ',' +
+    BRC.CORNER_RADIUS;
+
+/**
+ * SVG path for drawing the bottom-left corner of a statement input.
+ * Includes the rounded inside corner.
+ * @const
+ */
+BRC.INNER_BOTTOM_LEFT_CORNER =
+    Blockly.utils.Paths.arc('a', '0 0,0',
+        BRC.CORNER_RADIUS,
+        Blockly.utils.Paths.point(BRC.CORNER_RADIUS, BRC.CORNER_RADIUS));
+
+
+/**
+ * SVG path for drawing highlight on the top-left corner of a statement
+ * input in RTL.
+ * @const
+ */
+BRC.INNER_TOP_LEFT_CORNER_HIGHLIGHT_RTL =
+    Blockly.utils.Paths.arc('a', '0 0,0',
+        BRC.CORNER_RADIUS,
+        Blockly.utils.Paths.point(
+            -BRC.DISTANCE_45_OUTSIDE - BRC.HIGHLIGHT_OFFSET,
+            BRC.CORNER_RADIUS - BRC.DISTANCE_45_OUTSIDE));
+
+/**
+ * SVG path for drawing highlight on the bottom-left corner of a statement
+ * input in RTL.
+ * @const
+ */
+BRC.INNER_BOTTOM_LEFT_CORNER_HIGHLIGHT_RTL =
+    Blockly.utils.Paths.arc('a', '0 0,0',
+        BRC.CORNER_RADIUS + BRC.HIGHLIGHT_OFFSET,
+        Blockly.utils.Paths.point(
+            BRC.CORNER_RADIUS + BRC.HIGHLIGHT_OFFSET,
+            BRC.CORNER_RADIUS + BRC.HIGHLIGHT_OFFSET));
+
+/**
+ * SVG path for drawing highlight on the bottom-left corner of a statement
+ * input in LTR.
+ * @const
+ */
+BRC.INNER_BOTTOM_LEFT_CORNER_HIGHLIGHT_LTR =
+    Blockly.utils.Paths.arc('a', '0 0,0',
+        BRC.CORNER_RADIUS + BRC.HIGHLIGHT_OFFSET,
+        Blockly.utils.Paths.point(
+            BRC.CORNER_RADIUS - BRC.DISTANCE_45_OUTSIDE,
+            BRC.DISTANCE_45_OUTSIDE + BRC.HIGHLIGHT_OFFSET));
+
+
+
+/**
+ * SVG start point for drawing the top-left corner.
+ * @const
+ */
+BRC.TOP_LEFT_CORNER_START =
+    'm 0,' + BRC.CORNER_RADIUS;
+
+/**
+ * SVG path for drawing the rounded top-left corner.
+ * @const
+ */
+BRC.TOP_LEFT_CORNER =
+    'A ' + BRC.CORNER_RADIUS + ',' +
+    BRC.CORNER_RADIUS + ' 0 0,1 ' +
+    BRC.CORNER_RADIUS + ',0';
+
+BRC.BOTTOM_LEFT_CORNER = 'a' + BRC.CORNER_RADIUS + ',' +
+               BRC.CORNER_RADIUS + ' 0 0,1 -' +
+               BRC.CORNER_RADIUS + ',-' +
+               BRC.CORNER_RADIUS;
+
+BRC.BOTTOM_LEFT_CORNER_HIGHLIGHT_START =
+    'M ' + BRC.DISTANCE_45_INSIDE + ', '; // follow with y pos - distance 45 inside
+
+BRC.BOTTOM_LEFT_CORNER_HIGHLIGHT_MID   =
+    'A ' + (BRC.CORNER_RADIUS - BRC.HIGHLIGHT_OFFSET) +
+    ',' + (BRC.CORNER_RADIUS - BRC.HIGHLIGHT_OFFSET) +
+    ' 0 0,1 ' + BRC.HIGHLIGHT_OFFSET + ','; // follow with y pos - corner radius
+
+BRC.OUTPUT_CONNECTION_HIGHLIGHT_LTR =
+    'V ' + (BRC.TAB_HEIGHT + BRC.TAB_OFFSET_FROM_TOP - 1.5) +
+    ' m ' + (BRC.TAB_WIDTH * -0.92) + ',-0.5 ' +
+    'q ' + (BRC.TAB_WIDTH * -0.19) + ',-5.5 0,-11 ' +
+    'm ' + (BRC.TAB_WIDTH * 0.92) + ',1 ' +
+    'V 0.5 H 1';
+
+BRC.OUTPUT_CONNECTION_HIGHLIGHT_RTL =
+    'M ' + (BRC.TAB_WIDTH * -0.25) + ',8.4 l ' +
+    (BRC.TAB_WIDTH * -0.45) + ',-2.1';
+
+/**
+ * SVG start point for drawing the top-left corner's highlight in RTL.
+ * @const
+ */
+BRC.TOP_LEFT_CORNER_START_HIGHLIGHT_RTL =
+    'm ' + BRC.DISTANCE_45_INSIDE + ',' +
+    BRC.DISTANCE_45_INSIDE;
+
+/**
+ * SVG start point for drawing the top-left corner's highlight in LTR.
+ * @const
+ */
+BRC.TOP_LEFT_CORNER_START_HIGHLIGHT_LTR =
+    'm 0.5,' + (BRC.CORNER_RADIUS - 0.5);
+
+/**
+ * SVG path for drawing the highlight on the rounded top-left corner.
+ * @const
+ */
+BRC.TOP_LEFT_CORNER_HIGHLIGHT =
+    'A ' + (BRC.CORNER_RADIUS - 0.5) + ',' +
+    (BRC.CORNER_RADIUS - 0.5) + ' 0 0,1 ' +
+    BRC.CORNER_RADIUS + ',0.5';
+
+/**
+ * Path of the top hat's curve's highlight in LTR.
+ * @const
+ */
+BRC.START_HAT_HIGHLIGHT_LTR =
+    Blockly.utils.Paths.curve('c',
+        [
+          Blockly.utils.Paths.point(17.8, -9.2),
+          Blockly.utils.Paths.point(45.3, -14.9),
+          Blockly.utils.Paths.point(75, -8.7)
+        ]) +
+    Blockly.utils.Paths.moveTo(100.5, 0.5);
+
+/**
+ * Path of the top hat's curve's highlight in RTL.
+ * @const
+ */
+BRC.START_HAT_HIGHLIGHT_RTL =
+    Blockly.utils.Paths.moveBy(25, -8.7) +
+    Blockly.utils.Paths.curve('c',
+        [
+          Blockly.utils.Paths.point(29.7, -6.2),
+          Blockly.utils.Paths.point(57.2, -0.5),
+          Blockly.utils.Paths.point(75, 8.7)
+        ]);
+

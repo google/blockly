29293d901e459e0e91410132e8fc96cfe34c08bc
Abby <aschmiedt@google.com>
2019-07-25 09:25:02 -0700

    Add dispose to cursor

--- core/keyboard_nav/cursor.js
+++ core/keyboard_nav/cursor.js
@@ -13,7 +13,6 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -78,6 +77,12 @@ Blockly.Cursor.prototype.setLocation = function(newNode) {
  * @protected
  */
 Blockly.Cursor.prototype.update_ = function() {};
+
+/**
+ * Dispose method to be overwritten in cursor_svg.
+ * @protected
+ */
+Blockly.Cursor.prototype.dispose = function() {};
 
 /**
  * Find the next connection, field, or block.

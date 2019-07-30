5ce8c68833d0e5946372f3333f7a165478431402
Sam El-Husseini <16690124+samelhusseini@users.noreply.github.com>
2019-07-30 11:13:52 -0700

    Fix node build and add node tests (#2736)
    * Fix node build and add node tests

--- core/utils.js
+++ core/utils.js
@@ -33,6 +33,7 @@
 goog.provide('Blockly.utils');
 
 goog.require('Blockly.Msg');
+goog.require('Blockly.utils.base');
 goog.require('Blockly.utils.Coordinate');
 goog.require('Blockly.utils.string');
 goog.require('Blockly.utils.userAgent');
@@ -621,8 +622,3 @@ Blockly.utils.screenToWsCoordinates = function(ws, screenCoordinates) {
   var finalOffsetMainWs = finalOffsetPixels.scale(1 / ws.scale);
   return finalOffsetMainWs;
 };
-
-/**
- * Reference to the global object.
- */
-Blockly.utils.global = this || self;

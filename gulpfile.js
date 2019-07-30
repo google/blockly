9d593f2cb8656429c71514bb9e87c760e8d816d9
Sam El-Husseini <16690124+samelhusseini@users.noreply.github.com>
2019-07-30 11:17:57 -0700

    TypeScript Definition file (#2738)
    * Add Typescript definition file for Blockly

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,11 @@ gulp.shell = require('gulp-shell');
 gulp.concat = require('gulp-concat');
 var insert = require('gulp-insert');
 
+var path = require('path');
+var fs = require('fs');
+var rimraf = require('rimraf');
+var execSync = require('child_process').execSync;
+
 // Rebuilds Blockly, including the following:
 //  - blockly_compressed.js
 //  - blocks_compressed.js
@@ -107,6 +112,59 @@ function buildWatchTaskFn(concatTask) {
 // Watch Blockly files for changes and trigger automatic rebuilds, including
 // the Node-ready blockly_node_javascript_en.js file.
 gulp.task('watch', buildWatchTaskFn('blockly_javascript_en'));
+
+// Generates the TypeScript definition file (d.ts) for Blockly.
+// As well as generating the typings of each of the files under core/ and msg/,
+// the script also pulls in a number of part files from typings/parts.
+// This includes the header (incl License), additional useful interfaces
+// including Blockly Options and Google Closure typings
+gulp.task('typings', function (cb) {
+  const tmpDir = './typings/tmp';
+  const blocklySrcs = [
+    "core/",
+    "core/keyboard_nav",
+    "core/theme",
+    "core/utils",
+    "msg/"
+  ];
+  // Clean directory if exists
+  if (fs.existsSync(tmpDir)) {
+    rimraf.sync(tmpDir);
+  }
+  fs.mkdirSync(tmpDir);
+
+  // Find all files that will be included in the typings file
+  let files = [];
+  blocklySrcs.forEach((src) => {
+    files = files.concat(fs.readdirSync(src)
+      .filter(fn => fn.endsWith('.js'))
+      .map(fn => path.join(src, fn)));
+  });
+
+  // Generate typings file for each file
+  files.forEach((file) => {
+    const typescriptFileName = `${path.join(tmpDir, file)}.d.ts`;
+    const cmd = `node ./node_modules/typescript-closure-tools/definition-generator/src/main.js ${file} ${typescriptFileName}`;
+    console.log(`Generating typings for ${file}`);
+    execSync(cmd, { stdio: 'inherit' });
+  });
+
+  const srcs = [
+    'typings/parts/blockly-header.d.ts',
+    'typings/parts/blockly-interfaces.d.ts',
+    'typings/parts/goog-closure.d.ts',
+    `${tmpDir}/**/**/**`
+  ];
+  return gulp.src(srcs)
+    .pipe(gulp.concat('blockly.d.ts'))
+    .pipe(gulp.dest('typings'))
+    .on('end', function () {
+      // Clean up tmp directory
+      if (fs.existsSync(tmpDir)) {
+        rimraf.sync(tmpDir);
+      }
+    });
+});
 
 // The default task concatenates files for Node.js, using English language
 // blocks and the JavaScript generator.

a999a389a6efeda6a5a4ec4cf2c66c335d944f6f
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-07-30 06:27:28 +0000

    Minor PR feedback fixes

--- core/utils/size.js
+++ core/utils/size.js
@@ -22,7 +22,7 @@
  * @fileoverview Utility methods for size calculation.
  * These methods are not specific to Blockly, and could be factored out into
  * a JavaScript framework such as Closure.
- * @author fraser@google.com (Neil Fraser)
+ * @author samelh@google.com (Sam El-Husseini)
  */
 'use strict';
 

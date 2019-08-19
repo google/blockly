507a17fba1a4f59c0074cba53f41788e4438ea1c
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-08-19 16:15:59 -0700

    Fix eslint

--- tests/mocha/metrics_test.js
+++ tests/mocha/metrics_test.js
@@ -32,10 +32,8 @@ suite('Metrics', function() {
     assertEquals('Height did not match.', height, toCheck.height);
   }
 
-  /**
-   * Make a mock workspace object with two properties: getBlocksBoundingBox and
-   * scale.
-   */
+  // Make a mock workspace object with two properties:
+  // getBlocksBoundingBox and scale.
   function makeMockWs(scale, x, y, width, height) {
     return {
       getBlocksBoundingBox: function() {
@@ -44,77 +42,77 @@ suite('Metrics', function() {
           bottom: y + height,
           left: x,
           right: x + width
-        }
+        };
       },
       scale: scale
     };
   }
 
   test('GetContentDimensionsExact - empty', function() {
-    var ws = makeMockWs(1, 0, 0, 0, 0)
+    var ws = makeMockWs(1, 0, 0, 0, 0);
     var defaultZoom = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     assertDimensionsMatch(defaultZoom, 0, 0, 0, 0);
   });
   test('GetContentDimensionsExact - empty zoom in', function() {
-    var ws = makeMockWs(2, 0, 0, 0, 0)
+    var ws = makeMockWs(2, 0, 0, 0, 0);
     var zoomIn = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     assertDimensionsMatch(zoomIn, 0, 0, 0, 0);
   });
   test('GetContentDimensionsExact - empty zoom out', function() {
-    var ws = makeMockWs(.5, 0, 0, 0, 0)
+    var ws = makeMockWs(.5, 0, 0, 0, 0);
     var zoomOut = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     assertDimensionsMatch(zoomOut, 0, 0, 0, 0);
   });
   test('GetContentDimensionsExact - non empty at origin', function() {
-    var ws = makeMockWs(1, 0, 0, 100, 100)
+    var ws = makeMockWs(1, 0, 0, 100, 100);
     var defaultZoom = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // Pixel and ws units are the same at default zoom.
     assertDimensionsMatch(defaultZoom, 0, 0, 100, 100);
   });
   test('GetContentDimensionsExact - non empty at origin zoom in', function() {
-    var ws = makeMockWs(2, 0, 0, 100, 100)
+    var ws = makeMockWs(2, 0, 0, 100, 100);
     var zoomIn = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 2 pixels at this zoom level.
     assertDimensionsMatch(zoomIn, 0, 0, 200, 200);
   });
   test('GetContentDimensionsExact - non empty at origin zoom out', function() {
-    var ws = makeMockWs(.5, 0, 0, 100, 100)
+    var ws = makeMockWs(.5, 0, 0, 100, 100);
     var zoomOut = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 0.5 pixels at this zoom level.
     assertDimensionsMatch(zoomOut, 0, 0, 50, 50);
   });
   test('GetContentDimensionsExact - non empty positive origin', function() {
-    var ws = makeMockWs(1, 10, 10, 100, 100)
+    var ws = makeMockWs(1, 10, 10, 100, 100);
     var defaultZoom = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // Pixel and ws units are the same at default zoom.
     assertDimensionsMatch(defaultZoom, 10, 10, 100, 100);
   });
   test('GetContentDimensionsExact - non empty positive origin zoom in', function() {
-    var ws = makeMockWs(2, 10, 10, 100, 100)
+    var ws = makeMockWs(2, 10, 10, 100, 100);
     var zoomIn = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 2 pixels at this zoom level.
     assertDimensionsMatch(zoomIn, 20, 20, 200, 200);
   });
   test('GetContentDimensionsExact - non empty positive origin zoom out', function() {
-    var ws = makeMockWs(.5, 10, 10, 100, 100)
+    var ws = makeMockWs(.5, 10, 10, 100, 100);
     var zoomOut = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 0.5 pixels at this zoom level.
     assertDimensionsMatch(zoomOut, 5, 5, 50, 50);
   });
   test('GetContentDimensionsExact - non empty negative origin', function() {
-    var ws = makeMockWs(1, -10, -10, 100, 100)
+    var ws = makeMockWs(1, -10, -10, 100, 100);
     var defaultZoom = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // Pixel and ws units are the same at default zoom.
     assertDimensionsMatch(defaultZoom, -10, -10, 100, 100);
   });
   test('GetContentDimensionsExact - non empty negative origin zoom in', function() {
-    var ws = makeMockWs(2, -10, -10, 100, 100)
+    var ws = makeMockWs(2, -10, -10, 100, 100);
     var zoomIn = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 2 pixels at this zoom level.
     assertDimensionsMatch(zoomIn, -20, -20, 200, 200);
   });
   test('GetContentDimensionsExact - non empty negative origin zoom out', function() {
-    var ws = makeMockWs(.5, -10, -10, 100, 100)
+    var ws = makeMockWs(.5, -10, -10, 100, 100);
     var zoomOut = Blockly.WorkspaceSvg.getContentDimensionsExact_(ws);
     // 1 ws unit = 0.5 pixels at this zoom level.
     assertDimensionsMatch(zoomOut, -5, -5, 50, 50);

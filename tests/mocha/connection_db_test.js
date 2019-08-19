1c8f73f2e7d17ca21105a0bcca71d937eb29a9cb
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-08-19 16:18:59 -0700

    Fix eslint

--- tests/mocha/connection_db_test.js
+++ tests/mocha/connection_db_test.js
@@ -43,47 +43,86 @@ suite('Connection DB', function() {
     }
   }
 
+  function helper_createConnection(x, y, type, opt_shared_workspace,
+      opt_rendered) {
+    var workspace = opt_shared_workspace ? opt_shared_workspace : {};
+    if (opt_rendered) {
+      var conn = new Blockly.RenderedConnection({workspace: workspace}, type);
+    } else {
+      var conn = new Blockly.Connection({workspace: workspace}, type);
+    }
+    conn.x_ = x;
+    conn.y_ = y;
+    return conn;
+  }
+
+  function helper_getNeighbours(db, x, y, radius) {
+    return db.getNeighbours(helper_createConnection(x, y, Blockly.NEXT_STATEMENT,
+        null, true), radius);
+  }
+
+  function helper_makeSourceBlock(sharedWorkspace) {
+    return {workspace: sharedWorkspace,
+      parentBlock_: null,
+      getParent: function() { return null; },
+      movable_: true,
+      isMovable: function() { return true; },
+      isShadow: function() { return false; },
+      isInsertionMarker: function() { return false; },
+      getFirstStatementConnection: function() { return null; }
+    };
+  }
+
+  function helper_searchDB(db, x, y, radius, shared_workspace) {
+    var tempConn = helper_createConnection(x, y,
+        Blockly.NEXT_STATEMENT, shared_workspace, true);
+    tempConn.sourceBlock_ = helper_makeSourceBlock(shared_workspace);
+    tempConn.sourceBlock_.nextConnection = tempConn;
+    var closest = db.searchForClosest(tempConn, radius, {x: 0, y: 0});
+    return closest.connection;
+  }
+
   test('Add connection', function() {
     var db = new Blockly.ConnectionDB();
     var o2 = {y_: 2, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o2);
     verify_DB_('Adding connection #2', [o2], db);
 
     var o4 = {y_: 4, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o4);
     verify_DB_('Adding connection #4', [o2, o4], db);
 
     var o1 = {y_: 1, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o1);
     verify_DB_('Adding connection #1', [o1, o2, o4], db);
 
     var o3a = {y_: 3, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o3a);
     verify_DB_('Adding connection #3a', [o1, o2, o3a, o4], db);
 
     var o3b = {y_: 3, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o3b);
     verify_DB_('Adding connection #3b', [o1, o2, o3b, o3a, o4], db);
   });
   test('Remove connection', function() {
     var db = new Blockly.ConnectionDB();
     var o1 = {y_: 1, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     var o2 = {y_: 2, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     var o3a = {y_: 3, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     var o3b = {y_: 3, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     var o3c = {y_: 3, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     var o4 = {y_: 4, sourceBlock_: {},
-        getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
+      getSourceBlock: Blockly.Connection.prototype.getSourceBlock};
     db.addConnection(o1);
     db.addConnection(o2);
     db.addConnection(o3c);
@@ -203,24 +242,24 @@ suite('Connection DB', function() {
     }
 
     for (i = 0; i < 10; i++) {
-        assertEquals(i, db.connections_[i].y_);
+      assertEquals(i, db.connections_[i].y_);
     }
 
     // quasi-random
     var xCoords = [-29, -47, -77, 2, 43, 34, -59, -52, -90, -36, -91, 38, 87, -20,
-        60, 4, -57, 65, -37, -81, 57, 58, -96, 1, 67, -79, 34, 93, -90, -99, -62,
-        4, 11, -36, -51, -72, 3, -50, -24, -45, -92, -38, 37, 24, -47, -73, 79,
-        -20, 99, 43, -10, -87, 19, 35, -62, -36, 49, 86, -24, -47, -89, 33, -44,
-        25, -73, -91, 85, 6, 0, 89, -94, 36, -35, 84, -9, 96, -21, 52, 10, -95, 7,
-        -67, -70, 62, 9, -40, -95, -9, -94, 55, 57, -96, 55, 8, -48, -57, -87, 81,
-        23, 65];
+      60, 4, -57, 65, -37, -81, 57, 58, -96, 1, 67, -79, 34, 93, -90, -99, -62,
+      4, 11, -36, -51, -72, 3, -50, -24, -45, -92, -38, 37, 24, -47, -73, 79,
+      -20, 99, 43, -10, -87, 19, 35, -62, -36, 49, 86, -24, -47, -89, 33, -44,
+      25, -73, -91, 85, 6, 0, 89, -94, 36, -35, 84, -9, 96, -21, 52, 10, -95, 7,
+      -67, -70, 62, 9, -40, -95, -9, -94, 55, 57, -96, 55, 8, -48, -57, -87, 81,
+      23, 65];
     var yCoords = [-81, 82, 5, 47, 30, 57, -12, 28, 38, 92, -25, -20, 23, -51, 73,
-        -90, 8, 28, -51, -15, 81, -60, -6, -16, 77, -62, -42, -24, 35, 95, -46,
-        -7, 61, -16, 14, 91, 57, -38, 27, -39, 92, 47, -98, 11, -33, -72, 64, 38,
-        -64, -88, -35, -59, -76, -94, 45, -25, -100, -95, 63, -97, 45, 98, 99, 34,
-        27, 52, -18, -45, 66, -32, -38, 70, -73, -23, 5, -2, -13, -9, 48, 74, -97,
-        -11, 35, -79, -16, -77, 83, -57, -53, 35, -44, 100, -27, -15, 5, 39, 33,
-        -19, -20, -95];
+      -90, 8, 28, -51, -15, 81, -60, -6, -16, 77, -62, -42, -24, 35, 95, -46,
+      -7, 61, -16, 14, 91, 57, -38, 27, -39, 92, 47, -98, 11, -33, -72, 64, 38,
+      -64, -88, -35, -59, -76, -94, 45, -25, -100, -95, 63, -97, 45, 98, 99, 34,
+      27, 52, -18, -45, 66, -32, -38, 70, -73, -23, 5, -2, -13, -9, 48, 74, -97,
+      -11, 35, -79, -16, -77, 83, -57, -53, 35, -44, 100, -27, -15, 5, 39, 33,
+      -19, -20, -95];
     for (i = 0; i < xCoords.length; i++) {
       db.addConnection(helper_createConnection(xCoords[i], yCoords[i],
           Blockly.PREVIOUS_STATEMENT), null, true);
@@ -274,43 +313,4 @@ suite('Connection DB', function() {
     assertEquals(5, result.y_);
   });
 
-  function helper_getNeighbours(db, x, y, radius) {
-    return db.getNeighbours(helper_createConnection(x, y, Blockly.NEXT_STATEMENT,
-      null, true),
-        radius);
-  };
-
-  function helper_searchDB(db, x, y, radius, shared_workspace) {
-    var tempConn = helper_createConnection(x, y,
-        Blockly.NEXT_STATEMENT, shared_workspace, true);
-    tempConn.sourceBlock_ = helper_makeSourceBlock(shared_workspace);
-    tempConn.sourceBlock_.nextConnection = tempConn;
-    var closest = db.searchForClosest(tempConn, radius, {x: 0, y: 0});
-    return closest.connection;
-  };
-
-  function helper_makeSourceBlock(sharedWorkspace) {
-    return {workspace: sharedWorkspace,
-      parentBlock_: null,
-      getParent: function() { return null; },
-      movable_: true,
-      isMovable: function() { return true; },
-      isShadow: function() { return false; },
-      isInsertionMarker: function() { return false; },
-      getFirstStatementConnection: function() { return null; }
-    };
-  };
-
-  function helper_createConnection(x, y, type, opt_shared_workspace,
-      opt_rendered) {
-    var workspace = opt_shared_workspace ? opt_shared_workspace : {};
-    if (opt_rendered) {
-      var conn = new Blockly.RenderedConnection({workspace: workspace}, type);
-    } else {
-      var conn = new Blockly.Connection({workspace: workspace}, type);
-    }
-    conn.x_ = x;
-    conn.y_ = y;
-    return conn;
-  };
 });

fd4f64c78d92969e9a06717f0e88d6dc53784536
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-09-11 14:47:33 -0700

    Try disabling ipv6

--- tests/scripts/setup_linux_env.sh
+++ tests/scripts/setup_linux_env.sh
@@ -5,4 +5,5 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]
     export CHROME_BIN="/usr/bin/google-chrome"
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start &
+    sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6';
 fi

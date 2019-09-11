6e3ecb7d4fb4187a4fef21c319bcc63b2c742673
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-09-11 16:45:06 -0700

    attempt to have travis stop after prepare

--- tests/scripts/setup_linux_env.sh
+++ tests/scripts/setup_linux_env.sh
@@ -3,7 +3,6 @@
 if [ "${TRAVIS_OS_NAME}" == "linux" ]
   then
     export CHROME_BIN="/usr/bin/google-chrome"
-    export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start &
-    sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6';
+    npm run test:prepare > /dev/null &
 fi

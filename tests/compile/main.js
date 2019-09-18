cb64c5e2d00f8a3f8dfb112e0f30ed1714453544
Neil Fraser <fraser@google.com>
2019-09-18 13:19:31 -0700

    Remove now unneeded requires from compile test.

--- tests/compile/main.js
+++ tests/compile/main.js
@@ -2,11 +2,6 @@ goog.provide('Main');
 // Core
 // Either require 'Blockly.requires', or just the components you use:
 goog.require('Blockly');
-goog.require('Blockly.FieldDropdown');
-goog.require('Blockly.FieldImage');
-goog.require('Blockly.FieldNumber');
-goog.require('Blockly.FieldTextInput');
-goog.require('Blockly.FieldVariable');
 goog.require('Blockly.geras.Renderer');
 // Blocks
 goog.require('Blockly.Constants.Logic');

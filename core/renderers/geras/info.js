ba6dfd812552e0823ed75f3dc5133a267cc6fbc9
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-10-28 16:50:21 -0700

    Release v3.20191014.4 #3342 (#3358)
    * Dummy input right alignment (#3342)
    
    * Fix dummy input alignment in geras.
    
    * Rebuild

--- core/renderers/geras/info.js
+++ core/renderers/geras/info.js
@@ -93,7 +93,48 @@ Blockly.geras.RenderInfo.prototype.addInput_ = function(input, activeRow) {
   } else if (input.type == Blockly.DUMMY_INPUT) {
     // Dummy inputs have no visual representation, but the information is still
     // important.
+    activeRow.minHeight = Math.max(activeRow.minHeight,
+        this.constants_.DUMMY_INPUT_MIN_HEIGHT);
     activeRow.hasDummyInput = true;
+  }
+  activeRow.align = input.align;
+};
+
+/**
+ * @override
+ */
+Blockly.geras.RenderInfo.prototype.addElemSpacing_ = function() {
+  var hasExternalInputs = false;
+  for (var i = 0, row; (row = this.rows[i]); i++) {
+    if (row.hasExternalInput) {
+      hasExternalInputs = true;
+    }
+  }
+  for (var i = 0, row; (row = this.rows[i]); i++) {
+    var oldElems = row.elements;
+    row.elements = [];
+    // No spacing needed before the corner on the top row or the bottom row.
+    if (row.startsWithElemSpacer()) {
+      // There's a spacer before the first element in the row.
+      row.elements.push(new Blockly.blockRendering.InRowSpacer(
+          this.constants_, this.getInRowSpacing_(null, oldElems[0])));
+    }
+    for (var e = 0; e < oldElems.length - 1; e++) {
+      row.elements.push(oldElems[e]);
+      var spacing = this.getInRowSpacing_(oldElems[e], oldElems[e + 1]);
+      row.elements.push(
+          new Blockly.blockRendering.InRowSpacer(this.constants_, spacing));
+    }
+    row.elements.push(oldElems[oldElems.length - 1]);
+    if (row.endsWithElemSpacer()) {
+      var spacing = this.getInRowSpacing_(oldElems[oldElems.length - 1], null);
+      if (hasExternalInputs && row.hasDummyInput) {
+        spacing += this.constants_.TAB_WIDTH;
+      }
+      // There's a spacer after the last element in the row.
+      row.elements.push(new Blockly.blockRendering.InRowSpacer(
+          this.constants_, spacing));
+    }
   }
 };
 
@@ -252,22 +293,19 @@ Blockly.geras.RenderInfo.prototype.addAlignmentPadding_ = function(row, missingS
     row.widthWithConnectedBlocks += missingSpace;
   }
 
-  var input = row.getLastInput();
-  if (input) {
-    // Decide where the extra padding goes.
-    if (input.align == Blockly.ALIGN_LEFT) {
-      // Add padding to the end of the row.
-      lastSpacer.width += missingSpace;
-    } else if (input.align == Blockly.ALIGN_CENTRE) {
-      // Split the padding between the beginning and end of the row.
-      firstSpacer.width += missingSpace / 2;
-      lastSpacer.width += missingSpace / 2;
-    } else if (input.align == Blockly.ALIGN_RIGHT) {
-      // Add padding at the beginning of the row.
-      firstSpacer.width += missingSpace;
-    }
+  // Decide where the extra padding goes.
+  if (row.align == Blockly.ALIGN_LEFT) {
+    // Add padding to the end of the row.
+    lastSpacer.width += missingSpace;
+  } else if (row.align == Blockly.ALIGN_CENTRE) {
+    // Split the padding between the beginning and end of the row.
+    firstSpacer.width += missingSpace / 2;
+    lastSpacer.width += missingSpace / 2;
+  } else if (row.align == Blockly.ALIGN_RIGHT) {
+    // Add padding at the beginning of the row.
+    firstSpacer.width += missingSpace;
   } else {
-    // Default to left-aligning if there's no input to say where to align.
+    // Default to left-aligning.
     lastSpacer.width += missingSpace;
   }
   row.width += missingSpace;
@@ -297,6 +335,9 @@ Blockly.geras.RenderInfo.prototype.getSpacerRowHeight_ = function(prev, next) {
     return this.constants_.LARGE_PADDING;
   }
   if (!prev.hasStatement && next.hasDummyInput) {
+    return this.constants_.LARGE_PADDING;
+  }
+  if (prev.hasDummyInput) {
     return this.constants_.LARGE_PADDING;
   }
   return this.constants_.MEDIUM_PADDING;

557d3993682275698b9db85c642e6f5975a304e7
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-10-31 15:39:37 -0700

    Fix tests

--- core/workspace.js
+++ core/workspace.js
@@ -27,6 +27,7 @@ goog.require('Blockly.Cursor');
 goog.require('Blockly.MarkerCursor');
 goog.require('Blockly.Events');
 goog.require('Blockly.ThemeManager');
+goog.require('Blockly.Themes.Classic');
 goog.require('Blockly.utils');
 goog.require('Blockly.utils.math');
 goog.require('Blockly.VariableMap');
@@ -134,7 +135,7 @@ Blockly.Workspace = function(opt_options) {
    */
   this.themeManager_ = this.options.parentWorkspace ?
       this.options.parentWorkspace.getThemeManager() :
-      new Blockly.ThemeManager(this.options.theme);
+      new Blockly.ThemeManager(this.options.theme || Blockly.Themes.Classic);
   
   this.themeManager_.subscribeWorkspace(this);
 

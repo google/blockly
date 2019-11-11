4df003de1f0324ea1b496b5b2828b6e22b01e3ba
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-11-11 13:31:29 -0800

    Remove new line

--- core/renderers/common/constants.js
+++ core/renderers/common/constants.js
@@ -157,7 +157,6 @@ Blockly.blockRendering.ConstantProvider = function() {
    */
   this.FIELD_TEXT_FONTFAMILY = 'sans-serif';
 
-
   /**
    * The ID of the emboss filter, or the empty string if no filter is set.
    * @type {string}

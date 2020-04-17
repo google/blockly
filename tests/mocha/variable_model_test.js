cada0d7d5704b1fd098b81ee206b94e628c0d1da
kozbial <kozbial@google.com>
2020-04-16 17:26:54 -0700

    Updating to use chai syntax.

--- tests/mocha/variable_model_test.js
+++ tests/mocha/variable_model_test.js
@@ -16,43 +16,43 @@ suite('Variable Model', function() {
   test('Trivial', function() {
     var variable = new Blockly.VariableModel(
         this.workspace, 'test', 'test_type', 'test_id');
-    assertEquals('test', variable.name);
-    assertEquals('test_type', variable.type);
-    assertEquals('test_id', variable.id_);
+    chai.assert.equal(variable.name, 'test');
+    chai.assert.equal(variable.type, 'test_type');
+    chai.assert.equal(variable.id_, 'test_id');
   });
 
   test('Null type', function() {
     var variable = new Blockly.VariableModel(
         this.workspace, 'test', null, 'test_id');
-    assertEquals('', variable.type);
+    chai.assert.equal(variable.type, '');
   });
 
   test('Undefined type', function() {
     var variable = new Blockly.VariableModel(
         this.workspace, 'test', undefined, 'test_id');
-    assertEquals('', variable.type);
+    chai.assert.equal(variable.type, '');
   });
 
   test('Null id', function() {
     var variable = new Blockly.VariableModel(
         this.workspace, 'test', 'test_type', null);
-    assertEquals('test', variable.name);
-    assertEquals('test_type', variable.type);
-    assertNotNull(variable.id_);
+    chai.assert.equal(variable.name, 'test');
+    chai.assert.equal(variable.type, 'test_type');
+    chai.assert.exists(variable.id_);
   });
 
   test('Undefined id', function() {
     var variable = new Blockly.VariableModel(
         this.workspace, 'test', 'test_type', undefined);
-    assertEquals('test', variable.name);
-    assertEquals('test_type', variable.type);
-    assertNotNull(variable.id_);
+    chai.assert.equal(variable.name, 'test');
+    chai.assert.equal(variable.type, 'test_type');
+    chai.assert.exists(variable.id_);
   });
 
   test('Only name provided', function() {
     var variable = new Blockly.VariableModel(this.workspace, 'test');
-    assertEquals('test', variable.name);
-    assertEquals('', variable.type);
-    assertNotNull(variable.id_);
+    chai.assert.equal(variable.name, 'test');
+    chai.assert.equal(variable.type, '');
+    chai.assert.exists(variable.id_);
   });
 });

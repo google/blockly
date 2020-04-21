fce5b0a15ddfdff5f3af4b9f3c92d6f375ef52a0
Abby <aschmiedt@google.com>
2020-04-21 16:26:19 -0700

    Pr comments

--- tests/mocha/toolbox_test.js
+++ tests/mocha/toolbox_test.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google LLC
+ * Copyright 2020 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -25,14 +25,14 @@ suite('Toolbox', function() {
     this.toolbox = this.workspace.getToolbox();
     this.toolbox.init();
   });
+  
   teardown(function() {
     this.workspace.dispose();
+    delete Blockly.Blocks['basic_block'];
     sinon.restore();
   });
 
   suite('init', function() {
-    setup(function() {
-    });
     test('HtmlDiv is created', function() {
       chai.assert.isDefined(this.toolbox.HtmlDiv);
     });
@@ -41,11 +41,10 @@ suite('Toolbox', function() {
       chai.assert.equal(toolboxDiv.className, 'blocklyToolboxDiv blocklyNonSelectable');
     });
     test('hideChaff is called when the toolbox is clicked', function() {
-      sinon.stub(Blockly, "hideChaff");
-      var evt = new MouseEvent('pointerdown', {
-      });
+      var hideChaffStub = sinon.stub(Blockly, "hideChaff");
+      var evt = new MouseEvent('pointerdown', {});
       this.toolbox.HtmlDiv.dispatchEvent(evt);
-      chai.assert.isTrue(Blockly.hideChaff.calledOnce);
+      sinon.assert.calledOnce(hideChaffStub);
     });
     test('Flyout is initialized', function() {
       chai.assert.isDefined(this.toolbox.flyout_);
@@ -53,23 +52,16 @@ suite('Toolbox', function() {
   });
 
   suite('renderTree', function() {
-    setup(function() {
-    });
     test('Tree is created and set', function() {
       this.toolbox.renderTree(this.toolboxXml);
       chai.assert.isDefined(this.toolbox.tree_);
     });
     test('Throws error if a toolbox has both blocks and categories at root level', function() {
       var badXml = document.getElementById('toolbox-incorrect');
-      var threwError = false;
-      try {
-        this.toolbox.renderTree(badXml);
-      } catch (Error) {
-        threwError = true;
-        chai.assert.equal(Error.message,
-            'Toolbox cannot have both blocks and categories in the root level.');
-      }
-      chai.assert.isTrue(threwError);
+      var toolbox = this.toolbox;
+      chai.assert.throws(function() {
+        toolbox.renderTree(badXml);
+      }, 'Toolbox cannot have both blocks and categories in the root level.');
     });
     test('Select any open nodes', function() {
       this.toolbox.renderTree(this.toolboxXml);
@@ -93,11 +85,11 @@ suite('Toolbox', function() {
       this.toolbox.handleBeforeTreeSelected_(this.secondChild);
     });
     test('Clear the previously selected category', function() {
-      chai.assert.equal('', this.firstChild.getRowElement().style.backgroundColor);
+      chai.assert.equal(this.firstChild.getRowElement().style.backgroundColor, '');
     });
     test('Set color for new selected category', function() {
-      chai.assert.equal('rgb(85, 119, 238)', this.secondChild
-          .getRowElement().style.backgroundColor);
+      chai.assert.equal(this.secondChild.getRowElement().style.backgroundColor,
+          'rgb(85, 119, 238)');
     });
   });
 
@@ -106,35 +98,35 @@ suite('Toolbox', function() {
       this.toolbox.selectFirstCategory();
       this.firstChild = this.toolbox.tree_.getChildAt(0);
       this.secondChild = this.toolbox.tree_.getChildAt(1);
-      sinon.stub(this.toolbox.flyout_, "show");
+      this.showStub = sinon.stub(this.toolbox.flyout_, "show");
     });
     test('Show the new set of blocks in the flyout', function() {
       this.toolbox.handleAfterTreeSelected_(this.firstChild, this.secondChild);
-      chai.assert.isTrue(this.toolbox.flyout_.show.calledWith(this.secondChild.blocks));
+      sinon.assert.calledWith(this.showStub, this.secondChild.blocks);
     });
     test('Opening the previous selected category does not scroll', function() {
-      sinon.stub(this.toolbox.flyout_, "scrollToStart");
+      var scrollStub = sinon.stub(this.toolbox.flyout_, "scrollToStart");
       this.toolbox.handleAfterTreeSelected_(null, this.firstChild);
-      chai.assert.isFalse(this.toolbox.flyout_.scrollToStart.calledOnce);
+      sinon.assert.notCalled(scrollStub);
     });
     test('Opening new category scrolls to top', function() {
-      sinon.stub(this.toolbox.flyout_, "scrollToStart");
+      var scrollStub = sinon.stub(this.toolbox.flyout_, "scrollToStart");
       this.toolbox.handleAfterTreeSelected_(null, this.secondChild);
-      chai.assert.isTrue(this.toolbox.flyout_.scrollToStart.calledOnce);
+      sinon.assert.calledOnce(scrollStub);
     });
     test('Clicking selected category closes flyout', function() {
-      sinon.stub(this.toolbox.flyout_, "hide");
+      var flyoutHideStub = sinon.stub(this.toolbox.flyout_, "hide");
       this.toolbox.handleAfterTreeSelected_(this.firstChild);
-      chai.assert.isTrue(this.toolbox.flyout_.hide.calledOnce);
+      sinon.assert.calledOnce(flyoutHideStub);
     });
     test('UI Event is fired when new category is selected', function() {
-      sinon.stub(Blockly.Events, 'fire');
+      var eventsFireStub = sinon.stub(Blockly.Events, 'fire');
       this.toolbox.handleAfterTreeSelected_(this.firstChild);
-      chai.assert.isTrue(Blockly.Events.fire.calledOnce);
+      sinon.assert.calledOnce(eventsFireStub);
     });
     test('Last category is updated when there is a new node', function() {
       this.toolbox.handleAfterTreeSelected_(this.firstChild, this.secondChild);
-      chai.assert.equal(this.secondChild, this.toolbox.lastCategory_);
+      chai.assert.equal(this.toolbox.lastCategory_, this.secondChild);
     });
   });
 
@@ -142,17 +134,17 @@ suite('Toolbox', function() {
     setup(function() {
       this.toolbox.init();
     });
-    function checkHorizontal(toolbox) {
+    function checkHorizontalToolbox(toolbox) {
       chai.assert.equal(toolbox.HtmlDiv.style.left, '0px', 'Check left position');
       chai.assert.equal(toolbox.HtmlDiv.style.height, 'auto', 'Check height');
       var svgSize = Blockly.svgSize(toolbox.workspace_.getParentSvg());
       chai.assert.equal(toolbox.HtmlDiv.style.width, svgSize.width + 'px', 'Check width');
-      chai.assert.equal(toolbox.height, toolbox.HtmlDiv.offsetHeight, 'Check width');
+      chai.assert.equal(toolbox.height, toolbox.HtmlDiv.offsetHeight, 'Check height');
     }
-    function checkVertical(toolbox) {
+    function checkVerticalToolbox(toolbox) {
       var svgSize = Blockly.svgSize(toolbox.workspace_.getParentSvg());
-      chai.assert.equal(toolbox.HtmlDiv.style.height, svgSize.height + 'px', 'Check Height');
-      chai.assert.equal(toolbox.width, toolbox.HtmlDiv.offsetWidth, 'Check Width');
+      chai.assert.equal(toolbox.HtmlDiv.style.height, svgSize.height + 'px', 'Check height');
+      chai.assert.equal(toolbox.width, toolbox.HtmlDiv.offsetWidth, 'Check width');
     }
     test('Return if tree is not yet initialized', function() {
       this.toolbox.HtmlDiv = null;
@@ -165,7 +157,7 @@ suite('Toolbox', function() {
       toolbox.toolboxPosition = Blockly.TOOLBOX_AT_TOP;
       toolbox.horizontalLayout_ = true;
       toolbox.position();
-      checkHorizontal(toolbox);
+      checkHorizontalToolbox(toolbox);
       chai.assert.equal(toolbox.HtmlDiv.style.top, '0px', 'Check top');
     });
     test('Position horizontal at bottom', function() {
@@ -173,7 +165,7 @@ suite('Toolbox', function() {
       toolbox.toolboxPosition = Blockly.TOOLBOX_AT_BOTTOM;
       toolbox.horizontalLayout_ = true;
       toolbox.position();
-      checkHorizontal(toolbox);
+      checkHorizontalToolbox(toolbox);
       chai.assert.equal(toolbox.HtmlDiv.style.bottom, '0px', 'Check bottom');
     });
     test('Position Vertical at right', function() {
@@ -182,7 +174,7 @@ suite('Toolbox', function() {
       toolbox.horizontalLayout_ = false;
       toolbox.position();
       chai.assert.equal(toolbox.HtmlDiv.style.right, '0px', 'Check right');
-      checkVertical(toolbox);
+      checkVerticalToolbox(toolbox);
     });
     test('Position Vertical at left ', function() {
       var toolbox = this.toolbox;
@@ -190,7 +182,7 @@ suite('Toolbox', function() {
       toolbox.horizontalLayout_ = false;
       toolbox.position();
       chai.assert.equal(toolbox.HtmlDiv.style.left, '0px', 'Check left');
-      checkVertical(toolbox);
+      checkVerticalToolbox(toolbox);
     });
   });
 
@@ -233,13 +225,13 @@ suite('Toolbox', function() {
       chai.assert.equal(btnString,
           '<button xmlns="http://www.w3.org/1999/xhtml" text="insert" callbackkey="insertConnectionRows"></button>');
     });
-    test('Colours are set using style', function() {
-      sinon.stub(this.toolbox, "setColourFromStyle_");
-      sinon.stub(this.toolbox, "setColour_");
+    test('Colours are set using correct method', function() {
+      var setColourFromStyleStub = sinon.stub(this.toolbox, "setColourFromStyle_");
+      var setColourStub = sinon.stub(this.toolbox, "setColour_");
       this.toolbox.syncTrees_(this.toolboxXml, this.tree,
           this.toolbox.workspace_.options.pathToMedia);
-      chai.assert.isTrue(this.toolbox.setColourFromStyle_.calledOnce, 'Colour set from style');
-      chai.assert.isTrue(this.toolbox.setColour_.called, 'Colour set');
+      sinon.assert.calledOnce(setColourFromStyleStub);
+      sinon.assert.called(setColourStub);
     });
   });
 });

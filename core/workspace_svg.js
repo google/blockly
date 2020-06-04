1735841ec5860205a8b02df845113fb614f36243
Maribeth Bottorff <maribethb@google.com>
2020-06-04 16:08:46 -0700

    Use context menu registry to register functions in the workspace menu

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -16,6 +16,7 @@ goog.require('Blockly.BlockSvg');
 goog.require('Blockly.blockRendering');
 goog.require('Blockly.ConnectionDB');
 goog.require('Blockly.constants');
+goog.require('Blockly.ContextMenuRegistry');
 goog.require('Blockly.Events');
 goog.require('Blockly.Events.BlockCreate');
 goog.require('Blockly.Gesture');
@@ -1685,136 +1686,8 @@ Blockly.WorkspaceSvg.prototype.showContextMenu = function(e) {
   if (this.options.readOnly || this.isFlyout) {
     return;
   }
-  var menuOptions = [];
-  var topBlocks = this.getTopBlocks(true);
-  var eventGroup = Blockly.utils.genUid();
-  var ws = this;
-
-  // Options to undo/redo previous action.
-  var undoOption = {};
-  undoOption.text = Blockly.Msg['UNDO'];
-  undoOption.enabled = this.undoStack_.length > 0;
-  undoOption.callback = this.undo.bind(this, false);
-  menuOptions.push(undoOption);
-  var redoOption = {};
-  redoOption.text = Blockly.Msg['REDO'];
-  redoOption.enabled = this.redoStack_.length > 0;
-  redoOption.callback = this.undo.bind(this, true);
-  menuOptions.push(redoOption);
-
-  // Option to clean up blocks.
-  if (this.isMovable()) {
-    var cleanOption = {};
-    cleanOption.text = Blockly.Msg['CLEAN_UP'];
-    cleanOption.enabled = topBlocks.length > 1;
-    cleanOption.callback = this.cleanUp.bind(this);
-    menuOptions.push(cleanOption);
-  }
-
-  // Add a little animation to collapsing and expanding.
-  var DELAY = 10;
-  if (this.options.collapse) {
-    var hasCollapsedBlocks = false;
-    var hasExpandedBlocks = false;
-    for (var i = 0; i < topBlocks.length; i++) {
-      var block = topBlocks[i];
-      while (block) {
-        if (block.isCollapsed()) {
-          hasCollapsedBlocks = true;
-        } else {
-          hasExpandedBlocks = true;
-        }
-        block = block.getNextBlock();
-      }
-    }
-
-    /**
-     * Option to collapse or expand top blocks.
-     * @param {boolean} shouldCollapse Whether a block should collapse.
-     * @private
-     */
-    var toggleOption = function(shouldCollapse) {
-      var ms = 0;
-      for (var i = 0; i < topBlocks.length; i++) {
-        var block = topBlocks[i];
-        while (block) {
-          setTimeout(block.setCollapsed.bind(block, shouldCollapse), ms);
-          block = block.getNextBlock();
-          ms += DELAY;
-        }
-      }
-    };
-
-    // Option to collapse top blocks.
-    var collapseOption = {enabled: hasExpandedBlocks};
-    collapseOption.text = Blockly.Msg['COLLAPSE_ALL'];
-    collapseOption.callback = function() {
-      toggleOption(true);
-    };
-    menuOptions.push(collapseOption);
-
-    // Option to expand top blocks.
-    var expandOption = {enabled: hasCollapsedBlocks};
-    expandOption.text = Blockly.Msg['EXPAND_ALL'];
-    expandOption.callback = function() {
-      toggleOption(false);
-    };
-    menuOptions.push(expandOption);
-  }
-
-  // Option to delete all blocks.
-  // Count the number of blocks that are deletable.
-  var deleteList = [];
-  function addDeletableBlocks(block) {
-    if (block.isDeletable()) {
-      deleteList = deleteList.concat(block.getDescendants(false));
-    } else {
-      var children = block.getChildren(false);
-      for (var i = 0; i < children.length; i++) {
-        addDeletableBlocks(children[i]);
-      }
-    }
-  }
-  for (var i = 0; i < topBlocks.length; i++) {
-    addDeletableBlocks(topBlocks[i]);
-  }
-
-  function deleteNext() {
-    Blockly.Events.setGroup(eventGroup);
-    var block = deleteList.shift();
-    if (block) {
-      if (block.workspace) {
-        block.dispose(false, true);
-        setTimeout(deleteNext, DELAY);
-      } else {
-        deleteNext();
-      }
-    }
-    Blockly.Events.setGroup(false);
-  }
-
-  var deleteOption = {
-    text: deleteList.length == 1 ? Blockly.Msg['DELETE_BLOCK'] :
-        Blockly.Msg['DELETE_X_BLOCKS'].replace('%1', String(deleteList.length)),
-    enabled: deleteList.length > 0,
-    callback: function() {
-      if (ws.currentGesture_) {
-        ws.currentGesture_.cancel();
-      }
-      if (deleteList.length < 2 ) {
-        deleteNext();
-      } else {
-        Blockly.confirm(
-            Blockly.Msg['DELETE_ALL_BLOCKS'].replace('%1', deleteList.length),
-            function(ok) {
-              if (ok) {
-                deleteNext();
-              }
-            });
-      }
-    }
-  };
-  menuOptions.push(deleteOption);
+  var menuOptions = Blockly.ContextMenuRegistry.getContextMenuOptions(
+      Blockly.ContextMenuRegistry.ScopeType.WORKSPACE, {workspace: this});
 
   // Allow the developer to add or modify menuOptions.
   if (this.configureContextMenu) {
@@ -2721,3 +2594,222 @@ Blockly.WorkspaceSvg.prototype.getAudioManager = function() {
 Blockly.WorkspaceSvg.prototype.getGrid = function() {
   return this.grid_;
 };
+
+
+/**
+ *  {{
+  *    callback: function(*),
+  *    scopeType: !Blockly.ContextMenuRegistry.ScopeType,
+  *    displayText: ((function(!Blockly.ContextMenuRegistry.Scope):string)|string),
+  *    preconditionFn: function(!Blockly.ContextMenuRegistry.Scope):string,
+  *    weight: number,
+  *    id: string,
+  * }}
+ */
+(function() {
+  // Options to undo/redo previous action.
+  var undoOption = {};
+  undoOption.displayText = function() {
+    return Blockly.Msg['UNDO'];
+  };
+  undoOption.preconditionFn = function(scope) {
+    if (scope.workspace.undoStack_.length > 0) {
+      return 'enabled';
+    }
+    return 'disabled';
+  };
+  undoOption.callback = function(scope) {
+    scope.workspace.undo(false);
+  };
+  undoOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  undoOption.id = 'undoWorkspace';
+  undoOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(undoOption);
+
+  var redoOption = {};
+  redoOption.displayText = function() { return Blockly.Msg['REDO']; };
+  redoOption.preconditionFn = function(scope) {
+    if (scope.workspace.redoStack_.length > 0) {
+      return 'enabled';
+    }
+    return 'disabled';
+  };
+  redoOption.callback = function(scope) {
+    scope.workspace.undo(true);
+  };
+  redoOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  redoOption.id = 'redoWorkspace';
+  redoOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(redoOption);
+  
+  // Option to clean up blocks.
+  var cleanOption = {};
+  cleanOption.displayText = function() {
+    return Blockly.Msg['CLEAN_UP'];
+  };
+  cleanOption.preconditionFn = function(scope) {
+    if (scope.workspace.isMovable()) {
+      if (scope.workspace.getTopBlocks(false).length > 1) {
+        return 'enabled';
+      }
+      return 'disabled';
+    }
+    return 'hidden';
+  };
+  cleanOption.callback = function(scope) {
+    scope.workspace.cleanUp();
+  };
+  cleanOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  cleanOption.id = 'cleanWorkspace';
+  cleanOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(cleanOption);
+
+  // Options to collapse and expand top blocks
+
+  /**
+   * Option to collapse or expand top blocks.
+   * @param {boolean} shouldCollapse Whether a block should collapse.
+   * @param {!Array<?>} topBlocks Top blocks in the workspace.
+   * @private
+   */
+  var toggleOption = function(shouldCollapse, topBlocks) {
+    var DELAY = 10;
+    var ms = 0;
+    for (var i = 0; i < topBlocks.length; i++) {
+      var block = topBlocks[i];
+      while (block) {
+        setTimeout(block.setCollapsed.bind(block, shouldCollapse), ms);
+        block = block.getNextBlock();
+        ms += DELAY;
+      }
+    }
+  };
+  var collapseOption = {};
+  collapseOption.displayText = function() {
+    return Blockly.Msg['COLLAPSE_ALL'];
+  };
+  collapseOption.preconditionFn = function(scope) {
+    if (scope.workspace.options.collapse) {
+      var topBlocks = scope.workspace.getTopBlocks(false);
+      for (var i = 0; i < topBlocks.length; i++) {
+        var block = topBlocks[i];
+        while (block) {
+          if (!block.isCollapsed()) {
+            return 'enabled';
+          }
+          block = block.getNextBlock();
+        }
+      }
+      return 'disabled';
+    }
+    return 'hidden';
+  };
+  collapseOption.callback = function(scope) {
+    toggleOption(true, scope.workspace.getTopBlocks(true));
+  };
+  collapseOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  collapseOption.id = 'collapseWorkspace';
+  collapseOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(collapseOption);
+
+  var expandOption = {};
+  expandOption.displayText = function() {
+    return Blockly.Msg['EXPAND_ALL'];
+  };
+  expandOption.preconditionFn = function(scope) {
+    if (scope.workspace.options.collapse) {
+      var topBlocks = scope.workspace.getTopBlocks(false);
+      for (var i = 0; i < topBlocks.length; i++) {
+        var block = topBlocks[i];
+        while (block) {
+          if (block.isCollapsed()) {
+            return 'enabled';
+          }
+          block = block.getNextBlock();
+        }
+      }
+      return 'disabled';
+    }
+    return 'hidden';
+  };
+  expandOption.callback = function(scope) {
+    toggleOption(false, scope.workspace.getTopBlocks(true));
+  };
+  expandOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  expandOption.id = 'toggleWorkspace';
+  expandOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(expandOption);
+  
+  // Option to delete all blocks.
+
+  var addDeletableBlocks = function(block, deleteList) {
+    if (block.isDeletable()) {
+      deleteList = Array.prototype.push.apply(deleteList, block.getDescendants(false));
+    } else {
+      var children = block.getChildren(false);
+      for (var i = 0; i < children.length; i++) {
+        addDeletableBlocks(children[i], deleteList);
+      }
+    }
+  };
+
+  var getDeletableBlocks = function(workspace) {
+    var deleteList = [];
+    var topBlocks = workspace.getTopBlocks(true);
+    for (var i = 0; i < topBlocks.length; i++) {
+      addDeletableBlocks(topBlocks[i], deleteList);
+    }
+    return deleteList;
+  };
+  
+  var eventGroup = Blockly.utils.genUid();
+  function deleteNext(deleteList) {
+    var DELAY = 10;
+    Blockly.Events.setGroup(eventGroup);
+    var block = deleteList.shift();
+    if (block) {
+      if (block.workspace) {
+        block.dispose(false, true);
+        setTimeout(deleteNext, DELAY, deleteList);
+      } else {
+        deleteNext(deleteList);
+      }
+    }
+    Blockly.Events.setGroup(false);
+  }
+
+  var deleteOption = {};
+  deleteOption.displayText = function(scope) {
+    var deletableBlocksLength = getDeletableBlocks(scope.workspace).length;
+    if (deletableBlocksLength == 1) {
+      return Blockly.Msg['DELETE_BLOCK'];
+    } else {
+      return Blockly.Msg['DELETE_X_BLOCKS'].replace('%1', String(deletableBlocksLength));
+    }
+  };
+  deleteOption.preconditionFn = function(scope) {
+    var deletableBlocksLength = getDeletableBlocks(scope.workspace).length;
+    return deletableBlocksLength > 0 ? 'enabled' : 'disabled';
+  };
+  deleteOption.callback = function(scope) {
+    if (scope.workspace.currentGesture_) {
+      scope.workspace.currentGesture_.cancel();
+    }
+    var deletableBlocks = getDeletableBlocks(scope.workspace);
+    if (deletableBlocks.length < 2) {
+      deleteNext(deletableBlocks);
+    } else {
+      Blockly.confirm(
+          Blockly.Msg['DELETE_ALL_BLOCKS'].replace('%1', deletableBlocks.length),
+          function(ok) {
+            if (ok) {
+              deleteNext(deletableBlocks);
+            }
+          });
+    }
+  };
+  deleteOption.scopeType = Blockly.ContextMenuRegistry.ScopeType.WORKSPACE;
+  deleteOption.id = 'workspaceDelete';
+  deleteOption.weight = 0;
+  Blockly.ContextMenuRegistry.register(deleteOption);
+})();

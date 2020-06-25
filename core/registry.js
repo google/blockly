383f5db04d791c6fd5ea957a6d17af7e3c7e44d4
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-06-24 17:00:42 -0700

    Advanced compilation fixes

--- core/registry.js
+++ core/registry.js
@@ -123,7 +123,7 @@ Blockly.registry.register = function(type, name, registryItem) {
 Blockly.registry.validate_ = function(type, registryItem) {
   switch (type) {
     case String(Blockly.registry.Type.FIELD):
-      if (typeof registryItem['fromJson'] != 'function') {
+      if (typeof registryItem.fromJson != 'function') {
         throw Error('Type "' + type + '" must have a fromJson function');
       }
       break;

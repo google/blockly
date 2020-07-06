8a7728f338de119d4469851883ffd6674c322fd2
Abby <aschmiedt@google.com>
2020-07-06 11:28:35 -0700

    Add more jsdocs

--- core/toolbox/category.js
+++ core/toolbox/category.js
@@ -20,20 +20,126 @@ goog.provide('Blockly.ToolboxCategory');
  * @param {!Blockly.IToolbox} toolbox The parent toolbox for the category.
  */
 Blockly.ToolboxCategory = function(categoryDef, toolbox) {
-  this.name = categoryDef['name'];
-  this.parentToolbox = toolbox;
-  // TODO: Fix this.
-  this.workspace_ = this.parentToolbox.workspace_;
-  this.className = categoryDef['className'];
+
+  /**
+   * The name that will be displayed on the category.
+   * @type {string}
+   * @protected
+   */
+  this.name_ = categoryDef['name'];
+
+  /**
+   * The toolbox this category belongs to.
+   * @type {!Blockly.IToolbox}
+   * @protected
+   */
+  this.parentToolbox_ = toolbox;
+
+  /**
+   * The workspace of the parent toolbox.
+   * TODO: This should not be accessing a private variable.
+   * @type {!Blockly.WorkspaceSvg}
+   * @protected
+   */
+  this.workspace_ = this.parentToolbox_.workspace_;
+
+  /**
+   * The contents of the category.
+   * TODO: Figure out where we should be parsing this. Seems like it shouldn't
+   * TODO: have to happen every time someone opens the flyout.
+   * TODO: This would ideally be just IToolboxItem.
+   * @type {Array<Blockly.utils.toolbox.Block|
+   *     Blockly.utils.toolbox.Separator|
+   *     Blockly.utils.toolbox.Button|
+   *     Blockly.utils.toolbox.Label|
+   *     Blockly.utils.toolbox.Category>}
+   */
   this.contents = categoryDef['contents'];
   // TODO: Add dynamic category type
   if (categoryDef['custom']) {
     this.contents = categoryDef['custom'];
   }
 
+  /**
+   * The colour of the category.
+   * @type {string}
+   * @protected
+   */
   this.colour_ = this.getColour_(categoryDef);
+
+  /**
+   * The html container for the category.
+   * @type {HTMLDivElement}
+   */
   this.HtmlDiv = null;
 };
+
+/**
+ * Creates the dom for a toolbox category.
+ * @return {HTMLDivElement} The div for the category.
+ */
+Blockly.ToolboxCategory.prototype.createDom = function() {
+  var toolboxCategory = document.createElement('div');
+  toolboxCategory.classList.add('blocklyToolboxCategory');
+
+  var toolboxCategoryRow = document.createElement('div');
+  toolboxCategoryRow.classList.add('blocklyTreeRow');
+  toolboxCategory.appendChild(toolboxCategoryRow);
+
+  if (this.parentToolbox_.isHorizontal()) {
+    // TODO: Could we use flexbox to more easily accomplish this?
+    var horizontalClass = this.parentToolbox_.isRtl() ?
+        'blocklyHorizontalTreeRtl' : 'blocklyHorizontalTree';
+    toolboxCategory.classList.add(horizontalClass);
+  }
+  var toolboxIcon = this.createIconSpan_();
+  toolboxCategoryRow.appendChild(toolboxIcon);
+
+  var toolboxLabel = this.createLabelSpan_();
+  toolboxCategoryRow.appendChild(toolboxLabel);
+
+  // Create all children categories.
+  if (this.hasCategories()) {
+    for (var i = 0; i < this.contents.length; i++) {
+      // TODO: This should check the type of toolbox item before creating.
+      var child = this.contents[i];
+      var newCategory = new Blockly.ToolboxCategory(child, this.parentToolbox_);
+      var dom = newCategory.createDom();
+      toolboxCategory.appendChild(dom);
+    }
+  }
+
+  Blockly.bindEvent_(
+      toolboxCategoryRow, 'mouseup', this, this.onClick_);
+  this.HtmlDiv = toolboxCategory;
+  return toolboxCategory;
+};
+
+/**
+ * Creates the span that holds the category icon.
+ * @return {HTMLSpanElement} The span that holds the category icon.
+ * @protected
+ */
+Blockly.ToolboxCategory.prototype.createIconSpan_ = function() {
+  var toolboxIcon = document.createElement('span');
+  // TODO: Should get the class name from the config.
+  toolboxIcon.classList.add('blocklyTreeIcon');
+  return toolboxIcon;
+};
+
+/**
+ * Creates the span that holds the category label.
+ * @return {HTMLSpanElement} The span that holds the category label.
+ * @protected
+ */
+Blockly.ToolboxCategory.prototype.createLabelSpan_ = function() {
+  var toolboxLabel = document.createElement('span');
+  toolboxLabel.textContent = this.name_;
+  // TODO: Should get the class name from the config.
+  toolboxLabel.classList.add('blocklyTreeLabel');
+  return toolboxLabel;
+};
+
 
 /**
  * Add either the colour or the style for a category.
@@ -47,7 +153,7 @@ Blockly.ToolboxCategory.prototype.getColour_ = function(categoryInfo) {
   var colour = categoryInfo['colour'];
 
   if (colour && styleName) {
-    console.warn('Toolbox category "' + this.name +
+    console.warn('Toolbox category "' + this.name_ +
         '" must not have both a style and a colour');
   } else if (styleName) {
     return this.getColourfromStyle_(styleName);
@@ -94,14 +200,12 @@ Blockly.ToolboxCategory.prototype.parseColour_ = function(colourValue) {
     var hue = Number(colour);
     if (!isNaN(hue)) {
       return Blockly.hueToHex(hue);
-      // this.hasColours_ = true;
     } else {
       var hex = Blockly.utils.colour.parse(colour);
       if (hex) {
         return Blockly.utils.colour.parse(colour);
-        // this.hasColours_ = true;
       } else {
-        console.warn('Toolbox category "' + this.name +
+        console.warn('Toolbox category "' + this.name_ +
             '" has unrecognized colour attribute: ' + colour);
         return '';
       }
@@ -109,80 +213,37 @@ Blockly.ToolboxCategory.prototype.parseColour_ = function(colourValue) {
   }
 };
 
+/**
+ * Whether or not this category has sub categories.
+ * @return {boolean} True if this category has sub categories, false otherwise.
+ */
 Blockly.ToolboxCategory.prototype.hasCategories = function() {
   return this.contents && this.contents.length &&
     typeof this.contents != 'string' &&
     this.contents[0].kind.toUpperCase() == 'CATEGORY';
 };
 
-Blockly.ToolboxCategory.prototype.show = function() {
-};
-
-Blockly.ToolboxCategory.prototype.hide = function() {
-};
-
 /**
- * Create the dom for the category. If a category has children then update the
- * indentation.
- * TODO: This can have control of adding itself to the toolbox, or the toolbox can have control. Same with the binding the click event.
- * TODO: Could instead do something where the toolbox checks if there is an onClick event, and if there is one then they can add themself.
- * @return {HTMLDivElement} The element for the category.
+ * Set the current category as selected.
+ * @param {boolean} isSelected True if this category is selected, false otherwise.
  */
-Blockly.ToolboxCategory.prototype.createDom = function() {
-  var toolboxCategory = document.createElement('div');
-  toolboxCategory.classList.add('blocklyToolboxCategory');
-
-  var toolboxCategoryRow = document.createElement('div');
-  toolboxCategoryRow.classList.add('blocklyTreeRow');
-  toolboxCategory.appendChild(toolboxCategoryRow);
-
-  if (this.parentToolbox.isHorizontal()) {
-    // TODO: Figure out if there is a better way to be doing this.
-    var horizontalClass = this.parentToolbox.isRtl() ?
-        'blocklyHorizontalTreeRtl' : 'blocklyHorizontalTree';
-    toolboxCategory.classList.add(horizontalClass);
-  }
-  var toolboxIcon = document.createElement('span');
-  toolboxIcon.classList.add('blocklyTreeIcon');
-  toolboxCategoryRow.appendChild(toolboxIcon);
-
-  var toolboxLabel = document.createElement('span');
-  toolboxLabel.textContent = this.name;
-  toolboxLabel.classList.add('blocklyTreeLabel');
-  toolboxCategoryRow.appendChild(toolboxLabel);
-
-
-  if (this.hasCategories()) {
-    for (var i = 0; i < this.contents.length; i++) {
-      var child = this.contents[i];
-      var newCategory = new Blockly.ToolboxCategory(child, this.parentToolbox);
-      var dom = newCategory.createDom();
-      toolboxCategory.appendChild(dom);
-    }
-  }
-
-  Blockly.bindEvent_(
-      toolboxCategoryRow, 'mouseup', this, this.onClick);
-  this.HtmlDiv = toolboxCategory;
-  return toolboxCategory;
-};
-
-
-
 Blockly.ToolboxCategory.prototype.setSelected = function(isSelected) {
   console.log("Setting selected for category");
   if (isSelected) {
-    // TODO: DO not do this. Should be storing the correct htmldiv.
+    // TODO: This should store correct html div.
     this.HtmlDiv.children[0].style.backgroundColor = this.colour_;
   } else {
     this.HtmlDiv.children[0].style.backgroundColor = '';
   }
-
-
 };
 
-Blockly.ToolboxCategory.prototype.onClick = function() {
-  this.parentToolbox.setSelectedItem(this);
+/**
+ * Event listener for when the category is clicked.
+ * @param {Event} e Click event to handle.
+ * @protected
+ */
+Blockly.ToolboxCategory.prototype.onClick_ = function(e) {
+  this.parentToolbox_.setSelectedItem(this);
 };
 
 /**

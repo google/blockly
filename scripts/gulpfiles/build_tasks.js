8bce0ab1d0a740444a034454b1e0e64b6f55ff48
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-08-03 12:43:14 -0700

    Add build strict option that builds with the strict type checker

--- scripts/gulpfiles/build_tasks.js
+++ scripts/gulpfiles/build_tasks.js
@@ -107,8 +107,11 @@ var JSCOMP_ERROR = [
  * @param {boolean=} opt_verbose Optional option for verbose logging
  * @param {boolean=} opt_warnings_as_error Optional option for treating warnings
  *     as errors.
+ * @param {boolean=} opt_strict_typechecker Optional option for enabling strict
+ *     type checking.
  */
-function compile(compilerOptions, opt_verbose, opt_warnings_as_error) {
+function compile(compilerOptions, opt_verbose, opt_warnings_as_error,
+    opt_enable_strict_typechecker) {
   const options = {};
   options.compilation_level = 'SIMPLE_OPTIMIZATIONS';
   options.warning_level = opt_verbose ? 'VERBOSE' : 'DEFAULT';
@@ -116,8 +119,11 @@ function compile(compilerOptions, opt_verbose, opt_warnings_as_error) {
   options.language_out = 'ECMASCRIPT5_STRICT';
   options.rewrite_polyfills = false;
   options.hide_warnings_for = 'node_modules';
-  if (opt_warnings_as_error) {
+  if (opt_warnings_as_error || opt_enable_strict_typechecker) {
     options.jscomp_error = JSCOMP_ERROR;
+    if (opt_enable_strict_typechecker) {
+      options.jscomp_error.push('strictCheckTypes');
+    }
   }
 
   const platform = ['native', 'java', 'javascript'];
@@ -204,7 +210,7 @@ function buildCompressed() {
       language_in:
         argv.closureLibrary ? 'ECMASCRIPT_2015' : 'ECMASCRIPT5_STRICT',
       output_wrapper: outputWrapperUMD('Blockly', [])
-    }, argv.verbose, argv.strict))
+    }, argv.verbose, argv.debug, argv.strict))
     .pipe(gulp.sourcemaps.mapSources(function (sourcePath, file) {
       return sourcePath.replace(/-/g, '/');
     }))
@@ -232,7 +238,7 @@ function buildBlocks() {
         amd: './blockly_compressed.js',
         cjs: './blockly_compressed.js'
       }])
-    }, argv.verbose, argv.strict))
+    }, argv.verbose, argv.debug, argv.strict))
     .pipe(gulp.sourcemaps.write('.', {
       includeContent: false,
       sourceRoot: './'
@@ -258,7 +264,7 @@ function buildGenerator(language, namespace) {
         amd: './blockly_compressed.js',
         cjs: './blockly_compressed.js'
       }])
-    }, argv.verbose, argv.strict))
+    }, argv.verbose, argv.debug, argv.strict))
     .pipe(gulp.sourcemaps.write('.', {
       includeContent: false,
       sourceRoot: './'

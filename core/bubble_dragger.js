f3fdab11fb0964b609f73b62cc05560b92c3d711
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-08-04 17:28:12 -0400

    Add IBubble interface (#4104)
    * Add IBubble interface and make use of it for WorkspaceCommentSvgs

--- core/bubble_dragger.js
+++ core/bubble_dragger.js
@@ -18,20 +18,21 @@ goog.require('Blockly.Events.CommentMove');
 goog.require('Blockly.utils');
 goog.require('Blockly.utils.Coordinate');
 
+goog.requireType('Blockly.IBubble');
+
 
 /**
  * Class for a bubble dragger.  It moves things on the bubble canvas around the
  * workspace when they are being dragged by a mouse or touch.  These can be
  * block comments, mutators, warnings, or workspace comments.
- * @param {!Blockly.Bubble|!Blockly.WorkspaceCommentSvg} bubble The item on the
- *     bubble canvas to drag.
+ * @param {!Blockly.IBubble} bubble The item on the bubble canvas to drag.
  * @param {!Blockly.WorkspaceSvg} workspace The workspace to drag on.
  * @constructor
  */
 Blockly.BubbleDragger = function(bubble, workspace) {
   /**
    * The item on the bubble canvas that is being dragged.
-   * @type {!Blockly.Bubble|!Blockly.WorkspaceCommentSvg}
+   * @type {!Blockly.IBubble}
    * @private
    */
   this.draggingBubble_ = bubble;
@@ -75,7 +76,8 @@ Blockly.BubbleDragger = function(bubble, workspace) {
    */
   this.dragSurface_ =
       Blockly.utils.is3dSupported() && !!workspace.getBlockDragSurface() ?
-      workspace.getBlockDragSurface() : null;
+      workspace.getBlockDragSurface() :
+      null;
 };
 
 /**
@@ -109,7 +111,7 @@ Blockly.BubbleDragger.prototype.startBubbleDrag = function() {
   var toolbox = this.workspace_.getToolbox();
   if (toolbox && typeof toolbox.addStyle == 'function') {
     var style = this.draggingBubble_.isDeletable() ? 'blocklyToolboxDelete' :
-        'blocklyToolboxGrab';
+                                                     'blocklyToolboxGrab';
     toolbox.addStyle(style);
   }
 };
@@ -211,7 +213,7 @@ Blockly.BubbleDragger.prototype.endBubbleDrag = function(
   var toolbox = this.workspace_.getToolbox();
   if (toolbox && typeof toolbox.removeStyle == 'function') {
     var style = this.draggingBubble_.isDeletable() ? 'blocklyToolboxDelete' :
-        'blocklyToolboxGrab';
+                                                     'blocklyToolboxGrab';
     toolbox.removeStyle(style);
   }
   Blockly.Events.setGroup(false);
@@ -238,14 +240,15 @@ Blockly.BubbleDragger.prototype.fireMoveEvent_ = function() {
  * correction for mutator workspaces.
  * This function does not consider differing origins.  It simply scales the
  * input's x and y values.
- * @param {!Blockly.utils.Coordinate} pixelCoord A coordinate with x and y values
- *     in CSS pixel units.
- * @return {!Blockly.utils.Coordinate} The input coordinate divided by the workspace
- *     scale.
+ * @param {!Blockly.utils.Coordinate} pixelCoord A coordinate with x and y
+ *     values in CSS pixel units.
+ * @return {!Blockly.utils.Coordinate} The input coordinate divided by the
+ *     workspace scale.
  * @private
  */
 Blockly.BubbleDragger.prototype.pixelsToWorkspaceUnits_ = function(pixelCoord) {
-  var result = new Blockly.utils.Coordinate(pixelCoord.x / this.workspace_.scale,
+  var result = new Blockly.utils.Coordinate(
+      pixelCoord.x / this.workspace_.scale,
       pixelCoord.y / this.workspace_.scale);
   if (this.workspace_.isMutator) {
     // If we're in a mutator, its scale is always 1, purely because of some

9d2416119cd7b12d62484d0fd147225b27805452
Abby <aschmiedt@google.com>
2020-08-26 09:09:36 -0700

    Fix travis

--- tests/scripts/selenium-config.js
+++ tests/scripts/selenium-config.js
@@ -9,7 +9,7 @@ module.exports = {
     chrome: {
       // check for more recent versions of chrome driver here:
       // https://chromedriver.storage.googleapis.com/index.html
-      version: '83.0.4103.39',
+      version: '85.0.4183.83',
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
     },

122f97c7edb2d4691d5a84357f9cc3b965d7ea18
Rachel Fenichel <fenichel@google.com>
2020-09-24 12:33:41 -0700

    Change order of tests to avoid timing issues

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -52,15 +52,6 @@ run_test_command () {
 # Lint the codebase.
 run_test_command "eslint" "eslint ."
 
-# Run Mocha tests inside a browser.
-run_test_command "mocha" "node tests/mocha/run_mocha_tests_in_browser.js"
-
-# Run Node tests.
-run_test_command "node" "./node_modules/.bin/mocha tests/node --config tests/node/.mocharc.js"
-
-# Run generator tests inside a browser and check the results.
-run_test_command "generators" "tests/scripts/run_generators.sh"
-
 # Run the closure compiler.
 run_test_command "compile" "npm run build"
 
@@ -69,6 +60,15 @@ run_test_command "compile:warnings" "npm run build:debug"
 
 # Generate TypeScript typings and ensure there are no errors.
 run_test_command "typings" "tests/scripts/compile_typings.sh"
+
+# Run Mocha tests inside a browser.
+run_test_command "mocha" "node tests/mocha/run_mocha_tests_in_browser.js"
+
+# Run Node tests.
+run_test_command "node" "./node_modules/.bin/mocha tests/node --config tests/node/.mocharc.js"
+
+# Run generator tests inside a browser and check the results.
+run_test_command "generators" "tests/scripts/run_generators.sh"
 
 # Check the sizes of built files for unexpected growth.
 run_test_command "metadata" "tests/scripts/check_metadata.sh"

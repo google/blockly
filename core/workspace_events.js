647ed426de8820f94225a8d6099ac1cd96296456
kozbial <kozbial@google.com>
2020-10-15 15:31:43 -0700

    Update FinishedLoading event to inherit from Abstract rather than Ui

--- core/workspace_events.js
+++ core/workspace_events.js
@@ -13,7 +13,7 @@
 goog.provide('Blockly.Events.FinishedLoading');
 
 goog.require('Blockly.Events');
-goog.require('Blockly.Events.Ui');
+goog.require('Blockly.Events.Abstract');
 goog.require('Blockly.registry');
 goog.require('Blockly.utils.object');
 
@@ -25,21 +25,69 @@ goog.require('Blockly.utils.object');
  * Finished loading events do not record undo or redo.
  * @param {!Blockly.Workspace=} opt_workspace The workspace that has finished
  *    loading.  Undefined for a blank event.
- * @extends {Blockly.Events.Ui}
+ * @extends {Blockly.Events.Abstract}
  * @constructor
  */
 Blockly.Events.FinishedLoading = function(opt_workspace) {
-  var workspaceId = opt_workspace ? opt_workspace.id : undefined;
-  Blockly.Events.Click.superClass_.constructor.call(this, workspaceId);
+
+  /**
+   * Whether or not the event is blank (to be populated by fromJson).
+   * @type {boolean}
+   */
+  this.isBlank = typeof opt_workspace == 'undefined';
+
+  /**
+   * The workspace identifier for this event.
+   * @type {string}
+   */
+  this.workspaceId = opt_workspace ? opt_workspace.id : '';
+
+  /**
+   * The event group ID for the group this event belongs to. Groups define
+   * events that should be treated as an single action from the user's
+   * perspective, and should be undone together.
+   * @type {string}
+   */
+  this.group = Blockly.Events.getGroup();
+
+  // Workspace events do not undo or redo.
+  this.recordUndo = false;
 };
 Blockly.utils.object.inherits(Blockly.Events.FinishedLoading,
-    Blockly.Events.Ui);
+    Blockly.Events.Abstract);
 
 /**
  * Type of this event.
  * @type {string}
  */
 Blockly.Events.FinishedLoading.prototype.type = Blockly.Events.FINISHED_LOADING;
+
+/**
+ * Encode the event as JSON.
+ * @return {!Object} JSON representation.
+ */
+Blockly.Events.FinishedLoading.prototype.toJson = function() {
+  var json = {
+    'type': this.type,
+  };
+  if (this.group) {
+    json['group'] = this.group;
+  }
+  if (this.workspaceId) {
+    json['workspaceId'] = this.workspaceId;
+  }
+  return json;
+};
+
+/**
+ * Decode the JSON event.
+ * @param {!Object} json JSON representation.
+ */
+Blockly.Events.FinishedLoading.prototype.fromJson = function(json) {
+  this.isBlank = false;
+  this.workspaceId = json['workspaceId'];
+  this.group = json['group'];
+};
 
 Blockly.registry.register(Blockly.registry.Type.EVENT,
     Blockly.Events.FINISHED_LOADING, Blockly.Events.FinishedLoading);

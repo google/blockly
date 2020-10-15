d02ac8ee1ae5200eefc9774ace654c4d805626db
kozbial <kozbial@google.com>
2020-10-15 16:42:28 -0700

    Refactor block drag event

--- core/ui_events.js
+++ core/ui_events.js
@@ -113,5 +113,65 @@ Blockly.Events.Click.prototype.fromJson = function(json) {
   this.blockId = json['blockId'];
 };
 
+/**
+ * Class for a click event.
+ * @param {!Blockly.Block=} opt_block The top block in the stack that is being
+ *    dragged. Undefined for a blank event.
+ * @param {boolean=} opt_isStart Whether this is the start of a block drag.
+ * @param {!Array.<!Blockly.Block>=} opt_blocks The blocks affected by this
+ *    drag. Undefined for a blank event.
+ * @extends {Blockly.Events.Ui}
+ * @constructor
+ */
+Blockly.Events.Drag = function(opt_block, opt_isStart, opt_blocks) {
+  var workspaceId = opt_block ? opt_block.workspace.id : undefined;
+  Blockly.Events.Drag.superClass_.constructor.call(this, workspaceId);
+  this.blockId = opt_block ? opt_block.id : null;
+
+  /**
+   * Whether this is the start of a block drag. Undefined for blank event
+   * @type {boolean|undefined}
+   */
+  this.isStart = opt_isStart;
+  
+  /**
+   * The blocks affected by this drag event. Undefined for blank event
+   * @type {!Array.<!Blockly.Block>|undefined}
+   */
+  this.blocks = opt_blocks;
+};
+Blockly.utils.object.inherits(Blockly.Events.Drag, Blockly.Events.Ui);
+
+/**
+ * Type of this event.
+ * @type {string}
+ */
+Blockly.Events.Drag.prototype.type = Blockly.Events.BLOCK_DRAG;
+
+/**
+ * Encode the event as JSON.
+ * @return {!Object} JSON representation.
+ */
+Blockly.Events.Drag.prototype.toJson = function() {
+  var json = Blockly.Events.Drag.superClass_.toJson.call(this);
+  json['blockId'] = this.blockId;
+  json['blocks'] = this.blocks;
+  json['isStart'] = this.isStart;
+  return json;
+};
+
+/**
+ * Decode the JSON event.
+ * @param {!Object} json JSON representation.
+ */
+Blockly.Events.Drag.prototype.fromJson = function(json) {
+  Blockly.Events.Drag.superClass_.fromJson.call(this, json);
+  this.blockId = json['blockId'];
+  this.blocks = json['blocks'];
+  this.isStart = json['isStart'];
+};
+
 Blockly.registry.register(Blockly.registry.Type.EVENT, Blockly.Events.CLICK,
     Blockly.Events.Click);
+Blockly.registry.register(Blockly.registry.Type.EVENT,
+    Blockly.Events.BLOCK_DRAG, Blockly.Events.Drag);

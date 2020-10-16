d9e4d7ad626d4ddb2cf3b80d442ed633c4816aad
Abby <aschmiedt@google.com>
2020-10-15 17:10:56 -0700

    Test Blockly.onKeyDown method

--- tests/mocha/test_helpers.js
+++ tests/mocha/test_helpers.js
@@ -565,6 +565,36 @@ function dispatchPointerEvent(target, type, properties) {
 }
 
 /**
+ * Creates a key down event used for testing.
+ * @param {number} keyCode The keycode for the event. Use Blockly.utils.KeyCodes enum.
+ * @param {string} type The type of the target. This only matters for the
+ *     Blockly.utils.isTargetInput method.
+ * @param {Array<number>} modifiers A list of modifiers. Use Blockly.utils.KeyCodes enum.
+ * @return {{keyCode: *, getModifierState: (function(): boolean),
+ *     preventDefault: preventDefault, target: {type: *}}} The mocked keydown event.
+ */
+function createKeyDownEvent(keyCode, type, modifiers) {
+  var event = {
+    keyCode: keyCode,
+    target: {
+      type: type
+    },
+    getModifierState: function() {
+      return false;
+    },
+    preventDefault: function() {}
+  };
+  if (modifiers && modifiers.length > 0) {
+    event.altKey = modifiers.indexOf(Blockly.utils.KeyCodes.ALT) > -1;
+    event.ctrlKey = modifiers.indexOf(Blockly.utils.KeyCodes.CTRL) > -1;
+    event.metaKey = modifiers.indexOf(Blockly.utils.KeyCodes.META) > -1;
+    event.shiftKey = modifiers.indexOf(Blockly.utils.KeyCodes.SHIFT) > -1;
+  }
+  return event;
+}
+
+
+/**
  * Simulates mouse click by triggering relevant mouse events.
  * @param {!EventTarget} target The object receiving the event.
  * @param {Object<string, string>=} properties Properties to pass into event

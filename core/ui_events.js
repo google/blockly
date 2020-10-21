7e0f5942ca61ee52f968a7315f5d38b0726e8de3
Monica Kozbial <kozbial@google.com>
2020-10-21 10:57:20 -0700

    Refactor theme event (#4391)
    * Refactor theme event
    
    * Add themeName property to theme event

--- core/ui_events.js
+++ core/ui_events.js
@@ -12,6 +12,7 @@
 
 goog.provide('Blockly.Events.Ui');
 goog.provide('Blockly.Events.Click');
+goog.provide('Blockly.Events.ThemeChange');
 
 goog.require('Blockly.Events');
 goog.require('Blockly.Events.Abstract');
@@ -113,5 +114,51 @@ Blockly.Events.Click.prototype.fromJson = function(json) {
   this.blockId = json['blockId'];
 };
 
+/**
+ * Class for a theme change event.
+ * @param {string=} opt_themeName The theme name. Undefined for a blank event.
+ * @param {string=} opt_workspaceId The workspace identifier for this event.
+ *    event. Undefined for a blank event.
+ * @extends {Blockly.Events.Ui}
+ * @constructor
+ */
+Blockly.Events.ThemeChange = function(opt_themeName, opt_workspaceId) {
+  Blockly.Events.ThemeChange.superClass_.constructor.call(this, opt_workspaceId);
+
+  /**
+   * The theme name.
+   * @type {string|undefined}
+   */
+  this.themeName = opt_themeName;
+};
+Blockly.utils.object.inherits(Blockly.Events.ThemeChange, Blockly.Events.Ui);
+
+/**
+ * Type of this event.
+ * @type {string}
+ */
+Blockly.Events.ThemeChange.prototype.type = Blockly.Events.THEME_CHANGE;
+
+/**
+ * Encode the event as JSON.
+ * @return {!Object} JSON representation.
+ */
+Blockly.Events.ThemeChange.prototype.toJson = function() {
+  var json = Blockly.Events.ThemeChange.superClass_.toJson.call(this);
+  json['themeName'] = this.themeName;
+  return json;
+};
+
+/**
+ * Decode the JSON event.
+ * @param {!Object} json JSON representation.
+ */
+Blockly.Events.ThemeChange.prototype.fromJson = function(json) {
+  Blockly.Events.ThemeChange.superClass_.fromJson.call(this, json);
+  this.themeName = json['themeName'];
+};
+
 Blockly.registry.register(Blockly.registry.Type.EVENT, Blockly.Events.CLICK,
     Blockly.Events.Click);
+Blockly.registry.register(Blockly.registry.Type.EVENT,
+    Blockly.Events.THEME_CHANGE, Blockly.Events.ThemeChange);

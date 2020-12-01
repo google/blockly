f5ef2decd6f86f28ea77d3483a1133ffc947f455
Abby <aschmiedt@google.com>
2020-12-01 10:16:35 -0800

    Actually add the correct fiel

--- core/renderers/measurables/rows.js
+++ core/renderers/measurables/rows.js
@@ -522,9 +522,6 @@ Blockly.blockRendering.InputRow.prototype.measure = function() {
   }
   this.connectedBlockWidths = connectedBlockWidths;
   this.widthWithConnectedBlocks = this.width + connectedBlockWidths;
-  // if (this.hasStatement && this.connectedBlockWidths > 0) {
-  //   this.height -= this.constants_.NOTCH_HEIGHT / 2;
-  // }
 };
 
 /**

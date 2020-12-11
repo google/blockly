e56407fbd4add26ae79f34f9c46b1ae8d2aaf7a6
Pixeljoch <yousifalfraji@gmail.com>
2020-12-11 15:44:59 +0100

    Added Servo Blocks

--- blocks/leaphy/leaphy_extra.js
+++ blocks/leaphy/leaphy_extra.js
@@ -169,4 +169,66 @@ Blockly.Blocks["leaphy_led_strip_demo"] = {
         this.setNextStatement(true, null);
         this.setStyle('leaphy_blocks');
     }
-}
+};
+
+Blockly.Blocks['leaphy_servo_write'] = {
+    /**
+     * Block for writing an angle value into a servo pin.
+     * @this Blockly.Block
+     */
+    init: function() {
+      this.setHelpUrl('http://arduino.cc/en/Reference/ServoWrite');
+      this.setColour(Blockly.Blocks.servo.HUE);
+      this.appendDummyInput()
+          .appendField(Blockly.Msg.ARD_SERVO_WRITE)
+          .appendField(new Blockly.FieldDropdown(
+              Blockly.Arduino.Boards.selected.digitalPins), 'SERVO_PIN');
+      this.setInputsInline(false);
+      this.appendValueInput('SERVO_ANGLE')
+          .setCheck(Blockly.Types.NUMBER.checkList)
+          .appendField(Blockly.Msg.ARD_SERVO_WRITE_TO);
+      this.appendDummyInput()
+          .appendField(Blockly.Msg.ARD_SERVO_WRITE_DEG_180);
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setTooltip(Blockly.Msg.ARD_SERVO_WRITE_TIP);
+    },
+    /**
+     * Updates the content of the the pin related fields.
+     * @this Blockly.Block
+     */
+    updateFields: function() {
+      Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+          this, 'SERVO_PIN', 'digitalPins');
+    }
+  };
+  
+  Blockly.Blocks['leaphy_servo_read'] = {
+    /**
+     * Block for reading an angle value of a servo pin.
+     * @this Blockly.Block
+     */
+    init: function() {
+      this.setHelpUrl('http://arduino.cc/en/Reference/ServoRead');
+      this.setColour(Blockly.Blocks.servo.HUE);
+      this.appendDummyInput()
+          .appendField(Blockly.Msg.ARD_SERVO_READ)
+          .appendField(new Blockly.FieldDropdown(
+              Blockly.Arduino.Boards.selected.digitalPins), 'SERVO_PIN');
+      this.setOutput(true, Blockly.Types.NUMBER.output);
+      this.setTooltip(Blockly.Msg.ARD_SERVO_READ_TIP);
+    },
+    /** @return {string} The type of     return value for the block, an integer. */
+    getBlockType: function() {
+      return Blockly.Types.NUMBER;
+    },
+    /**
+     * Updates the content of the the pin related fields.
+     * @this Blockly.Block
+     */
+    updateFields: function() {
+      Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+          this, 'SERVO_PIN', 'digitalPins');
+    }
+  };
--- generators/arduino/leaphy_extra.js
+++ generators/arduino/leaphy_extra.js
@@ -96,3 +96,49 @@ Blockly.Arduino['leaphy_led_strip_demo'] = function (block){
     var code = 'ledstrip.runFunction(' + dropdownType + ', ' + red + ', ' + green + ', ' + blue + ');\n';
     return code;
 };
+
+Blockly.Arduino['leaphy_servo_write'] = function(block) {
+    var pinKey = block.getFieldValue('SERVO_PIN');
+    var servoAngle = Blockly.Arduino.valueToCode(
+        block, 'SERVO_ANGLE', Blockly.Arduino.ORDER_ATOMIC) || '90';
+    var servoName = 'myServo' + pinKey;
+  
+    Blockly.Arduino.reservePin(
+        block, pinKey, Blockly.Arduino.PinTypes.SERVO, 'Servo Write');
+  
+    Blockly.Arduino.addInclude('servo', '#include <Servo.h>');
+    Blockly.Arduino.addDeclaration('servo_' + pinKey, 'Servo ' + servoName + ';');
+  
+    var setupCode = servoName + '.attach(' + pinKey + ');';
+    Blockly.Arduino.addSetup('servo_' + pinKey, setupCode, true);
+  
+    var code = servoName + '.write(' + servoAngle + ');\n';
+    return code;
+  };
+  
+  /**
+   * Code generator to read an angle value from a servo pin (X).
+   * Arduino code: #include <Servo.h>
+   *               Servo myServoX;
+   *               setup { myServoX.attach(X); }
+   *               loop  { myServoX.read();    }
+   * @param {!Blockly.Block} block Block to generate the code from.
+   * @return {array} Completed code with order of operation.
+   */
+  
+  Blockly.Arduino['leaphy_servo_read'] = function(block) {
+    var pinKey = block.getFieldValue('SERVO_PIN');
+    var servoName = 'myServo' + pinKey;
+  
+    Blockly.Arduino.reservePin(
+        block, pinKey, Blockly.Arduino.PinTypes.SERVO, 'Servo Read');
+  
+    Blockly.Arduino.addInclude('servo', '#include <Servo.h>');
+    Blockly.Arduino.addDeclaration('servo_' + pinKey, 'Servo ' + servoName + ';');
+  
+    var setupCode = servoName + '.attach(' + pinKey + ');';
+    Blockly.Arduino.addSetup('servo_' + pinKey, setupCode, true);
+  
+    var code = servoName + '.read()';
+    return [code, Blockly.Arduino.ORDER_ATOMIC];
+  };

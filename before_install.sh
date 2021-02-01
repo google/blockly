648554c3eb64d08ab5969b94013ea63739453bd9
Abby <aschmiedt@google.com>
2021-02-01 10:51:48 -0800

    update

--- before_install.sh
+++ before_install.sh
@@ -2,7 +2,7 @@
 
 printenv
 export DISPLAY=:99.0
-echo $proces
-if [ "${RUNNER_OS}" == "linux" ]; then ( tests/scripts/setup_linux_env.sh ) fi
+echo $RUNNER_OS
+if [ "${RUNNER_OS}" == "Linux" ]; then ( tests/scripts/setup_linux_env.sh ) fi
 if [ "${RUNNER_OS}" == "osx" ]; then ( tests/scripts/setup_osx_env.sh ) fi
 sleep 2

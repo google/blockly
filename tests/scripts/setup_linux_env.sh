b1239437522f8af17da8af9e50183b474948d27d
Abby <aschmiedt@google.com>
2021-02-01 15:56:29 -0800

    update

--- tests/scripts/setup_linux_env.sh
+++ tests/scripts/setup_linux_env.sh
@@ -4,7 +4,7 @@ echo "IN SETUP LINUX"
 echo $RUNNER_OS
 if [ "${RUNNER_OS}" == "Linux" ]
   then
-    export CHROME_BIN="/usr/bin/google-chrome"
+#    export CHROME_BIN="/usr/bin/google-chrome"
     sh -e /etc/init.d/xvfb start &
     npm run test:prepare > /dev/null &
 fi

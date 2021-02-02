811959d60faa42fcb70dc6a2d9f2970e851884ea
Abby <aschmiedt@google.com>
2021-02-01 16:57:21 -0800

    update

--- before_install.sh
+++
@@ -1,8 +0,0 @@
-  #!/bin/bash
-
-printenv
-export DISPLAY=:99.0
-echo $RUNNER_OS
-if [ "${RUNNER_OS}" == "Linux" ]; then ( tests/scripts/setup_linux_env.sh ) fi
-if [ "${RUNNER_OS}" == "macOS" ]; then ( tests/scripts/setup_osx_env.sh ) fi
-sleep 2

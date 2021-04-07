bdf128d92d19782d83ef98aabec21e095e71992c
Grayson Nix <61979456+nixgc@users.noreply.github.com>
2021-04-07 17:59:36 -0400

    Update lists.js

--- blocks/lists.js
+++ blocks/lists.js
@@ -855,7 +855,6 @@ Blockly.Blocks['lists_split'] = {
    * @param {!Element} xmlElement XML storage element.
    * @this {Blockly.Block}
    */
-  
   domToMutation: function(xmlElement) {
     this.updateType_(xmlElement.getAttribute('mode'));
   }

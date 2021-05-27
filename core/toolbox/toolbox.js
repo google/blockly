da4ea956bfeb3ede092ce46208e571a2544d897d
kozbial <kozbial@google.com>
2021-05-26 17:26:55 -0700

    lint fix

--- core/toolbox/toolbox.js
+++ core/toolbox/toolbox.js
@@ -532,12 +532,12 @@ Blockly.Toolbox.prototype.getClientRect = function() {
 /**
  * Returns whether the provided block would be deleted if dropped on this area.
  * @param {!Blockly.BlockSvg} block The block.
- * @param {boolean} couldConnect Whether the block could could connect to
+ * @param {boolean} _couldConnect Whether the block could could connect to
  *     another.
  * @return {boolean} Whether the block provided would be deleted if dropped on
  *     this area.
  */
-Blockly.Toolbox.prototype.wouldDelete = function(block, couldConnect) {
+Blockly.Toolbox.prototype.wouldDelete = function(block, _couldConnect) {
   // Prefer dragging to the toolbox over connecting to other blocks.
   return !block.getParent() && block.isDeletable();
 };

8f7fb23b908965e03ed4b13b36656bb602074d4f
Christopher Allen <cpcallen+git@google.com>
2021-06-11 13:37:56 +0100

    Update test setup for GitHub Actions
    - Do the (hopefully now) correct test to check we are on macOS.
    - Fix brew command-line syntax for installing google-chrome.

--- tests/scripts/setup_osx_env.sh
+++ tests/scripts/setup_osx_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   
-if [ "${TRAVIS_OS_NAME}" == "osx" ]
+if [ "${RUNNER_OS}" == "macOS" ]
   then
-    brew cask install google-chrome 
+    brew install google-chrome 
     sudo Xvfb :99 -ac -screen 0 1024x768x8 &
     export CHROME_BIN="/Applications/Google Chrome.app"
     npm run test:prepare > /dev/null &

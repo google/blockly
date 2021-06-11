2855c97f03a42db6d5a9b8c4d5a09eea31171eb3
Maribeth Bottorff <maribethb@google.com>
2021-06-11 16:32:01 -0700

    Renames and addresses comments

--- core/block_dragger.js
+++ core/block_dragger.js
@@ -274,7 +274,7 @@ Blockly.BlockDragger.prototype.endDrag = function(e, currentDragDeltaXY) {
   if (preventMove) {
     var newLoc = this.startXY_;
   } else {
-    var newValues = this.getNewLocationAfterMove_(currentDragDeltaXY);
+    var newValues = this.getNewLocationAfterDrag_(currentDragDeltaXY);
     var delta = newValues.delta;
     var newLoc = newValues.newLocation;
   }
@@ -284,11 +284,11 @@ Blockly.BlockDragger.prototype.endDrag = function(e, currentDragDeltaXY) {
     this.dragTarget_.onDrop(this.draggingBlock_);
   }
 
-  if (this.wouldDeleteBlock_) {
-    this.deleteBlockAfterMove_();
-  } else {
+  var deleted = this.maybeDeleteBlock_();
+  if (!deleted) {
+    // These are expensive and don't need to be done if we're deleting.
     this.draggingBlock_.setDragging(false);
-    if (delta) { // !preventMove
+    if (delta) {  // !preventMove
       this.updateBlockAfterMove_(delta);
     } else {
       // Blocks dragged directly from a flyout may need to be bumped into
@@ -308,30 +308,39 @@ Blockly.BlockDragger.prototype.endDrag = function(e, currentDragDeltaXY) {
 
 /**
  * Calculates the drag delta and new location values after a block is dragged.
- * If a block would not be moved because a drag target prevents block moves,
- * then there is no delta, and the new location is the same as the startXY.
  * @param {!Blockly.utils.Coordinate} currentDragDeltaXY How far the pointer has
  *     moved from the start of the drag, in pixel units.
- * @return {{delta: (!Blockly.utils.Coordinate|undefined), newLocation:
+ * @return {{delta: !Blockly.utils.Coordinate, newLocation:
  *     !Blockly.utils.Coordinate}} New location after drag. delta is in
- *     workspace units, and is undefined if a move is prevented. newLocation is
- *     the new coordinate where the block should end up.
+ *     workspace units. newLocation is the new coordinate where the block should
+ *     end up.
  * @protected
  */
-Blockly.BlockDragger.prototype.getNewLocationAfterMove_ = function(
+Blockly.BlockDragger.prototype.getNewLocationAfterDrag_ = function(
     currentDragDeltaXY) {
   var newValues = {};
   newValues.delta = this.pixelsToWorkspaceUnits_(currentDragDeltaXY);
   newValues.newLocation =
-        Blockly.utils.Coordinate.sum(this.startXY_, newValues.delta);
+      Blockly.utils.Coordinate.sum(this.startXY_, newValues.delta);
   return newValues;
 };
 
-Blockly.BlockDragger.prototype.deleteBlockAfterMove_ = function() {
-  // Fire a move event, so we know where to go back to for an undo.
-  this.fireMoveEvent_();
-  this.draggingBlock_.dispose(false, true);
-  Blockly.draggingConnections = [];
+/**
+ * May delete the dragging block, if allowed. If `this.wouldDeleteBlock_` is not
+ * true, the block will not be deleted. This should be called at the end of a
+ * block drag.
+ * @return {boolean} True if the block was deleted.
+ * @protected
+ */
+Blockly.BlockDragger.prototype.maybeDeleteBlock_ = function() {
+  if (this.wouldDeleteBlock_) {
+    // Fire a move event, so we know where to go back to for an undo.
+    this.fireMoveEvent_();
+    this.draggingBlock_.dispose(false, true);
+    Blockly.draggingConnections = [];
+    return true;
+  }
+  return false;
 };
 
 /**

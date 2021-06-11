83c36dda6ae4bbec71f57798a62837324ff7aa58
Christopher Allen <cpcallen+git@google.com>
2021-06-11 16:42:22 +0100

    Remove unneeded macOS setup
    Chrome comes pre-installed on GitHub hosted macOS runners, and while
    `Xvfb` does not seem to be installed neither does it seem to be needed.

--- tests/scripts/setup_osx_env.sh
+++ tests/scripts/setup_osx_env.sh
@@ -2,8 +2,6 @@
   
 if [ "${RUNNER_OS}" == "macOS" ]
   then
-    brew install google-chrome 
-    sudo Xvfb :99 -ac -screen 0 1024x768x8 &
     export CHROME_BIN="/Applications/Google Chrome.app"
     npm run test:prepare > /dev/null &
 fi

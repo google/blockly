4671504d53cc50bd1b2dc967bce31e5f0308b27b
hpnrep6 <hpnrep6@gmail.com>
2021-07-12 23:16:23 -0400

    Fix code executing twice on mobile

--- demos/code/code.js
+++ demos/code/code.js
@@ -187,8 +187,11 @@ Code.bindClick = function(el, func) {
   if (typeof el == 'string') {
     el = document.getElementById(el);
   }
-  el.addEventListener('click', func, true);
-  el.addEventListener('touchend', func, true);
+  if (Blockly.utils.userAgent.MOBILE) {
+    el.addEventListener('touchend', func, true);
+  } else {
+    el.addEventListener('click', func, true);
+  }
 };
 
 /**

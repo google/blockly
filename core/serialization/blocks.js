7c15555e729af6338e713118f033a8b0716ca082
Beka Westberg <bwestberg@google.com>
2021-07-13 16:08:19 +0000

    Add basic block serialization

---
+++ core/serialization/blocks.js
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Handles serializing blocks to plain JavaScript objects only
+ *     containing state.
+ */
+'use strict';
+
+goog.provide('Blockly.serialization.blocks');
+
+Blockly.serialization.blocks.save =
+  function(block, justThisBlock, addCoordinates) {
+    var state = Object.create(null);
+    state['type'] = block.type;
+    state['id'] = block.id;
+
+    Blockly.serialization.blocks.addAttributes_(block, state);
+    Blockly.serialization.blocks.addCoordinates_(block, state);
+
+    return state;
+  };
+
+Blockly.serialization.blocks.addAttributes_ = function(block, state) {
+  if (block.isCollapsed()){
+    state['collapsed'] = true;
+  }
+  if (!block.isEnabled()){
+    state['disabled'] = true;
+  }
+  if (!block.isEditable()){
+    state['editable'] = false;
+  }
+  if (!block.isDeletable() && !block.isShadow()){
+    state['deletable'] = false;
+  }
+  if (!block.isMovable() && !block.isShadow()){
+    state['movable'] = false;
+  }
+
+  if (block.inputsInline !== undefined &&
+      block.inputsInline !== block.inputsInlineDefault) {
+    state['inline'] = block.inputsInline;
+  }
+};
+
+Blockly.serialization.blocks.addCoordinates_ = function(block, state) {
+  var workspace = block.workspace;
+  var xy = block.getRelativeToSurfaceXY();
+  state['x'] = Math.round(workspace.RTL ? workspace.getWidth() - xy.x : xy.x);
+  state['y'] = Math.round(xy.y);
+};
---
+++ core/serialization/serialization.js
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Contains top-level functions for serialization of the workspace
+ *     to JavaScript objects.
+ */
+'use strict';
+
+/**
+ * The top level namespace for JavaScript Object serialization.
+ * @namespace Blockly.blockRendering
+ */
+goog.provide('Blockly.serialization');

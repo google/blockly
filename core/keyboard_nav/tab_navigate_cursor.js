7025f74afd0f7a176446867f9d8d7286e5a97a15
kozbial <kozbial@google.com>
2021-07-20 16:47:41 -0700

    clang-format core/keyboard_nav/tab_navigate_cursor.js

--- core/keyboard_nav/tab_navigate_cursor.js
+++ core/keyboard_nav/tab_navigate_cursor.js
@@ -41,8 +41,8 @@ TabNavigateCursor.prototype.validNode_ = function(node) {
   const type = node && node.getType();
   if (node) {
     const location = /** @type {Field} */ (node.getLocation());
-    if (type == ASTNode.types.FIELD &&
-        location && location.isTabNavigable() && location.isClickable()) {
+    if (type == ASTNode.types.FIELD && location && location.isTabNavigable() &&
+        location.isClickable()) {
       isValid = true;
     }
   }

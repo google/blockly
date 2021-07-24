02f1055c445fb3db78a1dd30695dc2802f94c523
kozbial <kozbial@google.com>
2021-07-23 19:05:24 -0700

    Add eslint disable lines to core/keyboard_nav/ast_node.js

--- core/keyboard_nav/ast_node.js
+++ core/keyboard_nav/ast_node.js
@@ -13,13 +13,20 @@
 goog.module('Blockly.ASTNode');
 goog.module.declareLegacyNamespace();
 
+/* eslint-disable-next-line no-unused-vars */
 const Block = goog.requireType('Blockly.Block');
+/* eslint-disable-next-line no-unused-vars */
 const Connection = goog.requireType('Blockly.Connection');
 const Coordinate = goog.require('Blockly.utils.Coordinate');
+/* eslint-disable-next-line no-unused-vars */
 const Field = goog.requireType('Blockly.Field');
+/* eslint-disable-next-line no-unused-vars */
 const IASTNodeLocation = goog.requireType('Blockly.IASTNodeLocation');
+/* eslint-disable-next-line no-unused-vars */
 const IASTNodeLocationWithBlock = goog.requireType('Blockly.IASTNodeLocationWithBlock');
+/* eslint-disable-next-line no-unused-vars */
 const Input = goog.requireType('Blockly.Input');
+/* eslint-disable-next-line no-unused-vars */
 const Workspace = goog.requireType('Blockly.Workspace');
 const connectionTypes = goog.require('Blockly.connectionTypes');
 

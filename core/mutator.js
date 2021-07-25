e51fca30f125c3b5bc6a7a12512316e03f25ead7
hpnrep6 <hpnrep6@gmail.com>
2021-07-24 22:27:07 -0400

    Fix mutator block positions sometimes being out of place temporarily

--- core/mutator.js
+++ core/mutator.js
@@ -353,7 +353,7 @@ Blockly.Mutator.prototype.setVisible = function(visible) {
     // When the mutator's workspace changes, update the source block.
     this.workspace_.addChangeListener(this.workspaceChanged_.bind(this));
     // Update source block immediately when bubble is visible
-    this.updateWorkspace();
+    this.updateWorkspace_();
     this.applyColour();
   } else {
     // Dispose of the bubble.
@@ -381,7 +381,7 @@ Blockly.Mutator.prototype.workspaceChanged_ = function(e) {
   if (!(e.isUiEvent ||
       (e.type == Blockly.Events.CHANGE && e.element == 'disabled') ||
       e.type == Blockly.Events.CREATE)) {
-    this.updateWorkspace();
+    this.updateWorkspace_();
   }
 };
 
@@ -390,7 +390,7 @@ Blockly.Mutator.prototype.workspaceChanged_ = function(e) {
  * Bump down any block that's too high.
  * @private
  */
-Blockly.Mutator.prototype.updateWorkspace = function() {
+Blockly.Mutator.prototype.updateWorkspace_ = function() {
   if (!this.workspace_.isDragging()) {
     var blocks = this.workspace_.getTopBlocks(false);
     var MARGIN = 20;

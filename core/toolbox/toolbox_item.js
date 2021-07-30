fc828089ff8f65508a84427deb338bbd4e97875f
kozbial <kozbial@google.com>
2021-07-30 15:40:56 -0700

    Fix eslint disable lines

--- core/toolbox/toolbox_item.js
+++ core/toolbox/toolbox_item.js
@@ -18,11 +18,12 @@ const ICollapsibleToolboxItem = goog.requireType('Blockly.ICollapsibleToolboxIte
 const IdGenerator = goog.require('Blockly.utils.IdGenerator');
 /* eslint-disable-next-line no-unused-vars */
 const IToolbox = goog.requireType('Blockly.IToolbox');
+/* eslint-disable-next-line no-unused-vars */
 const IToolboxItem = goog.require('Blockly.IToolboxItem');
+/* eslint-disable-next-line no-unused-vars */
 const WorkspaceSvg = goog.requireType('Blockly.WorkspaceSvg');
 /* eslint-disable-next-line no-unused-vars */
 const toolbox = goog.requireType('Blockly.utils.toolbox');
-/* eslint-disable-next-line no-unused-vars */
 
 
 /**

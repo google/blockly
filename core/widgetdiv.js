af108ec7806531452a6be16a8a5c310033737763
Aaron Dodson <adodson@google.com>
2021-08-09 13:05:00 -0700

    Migrate core/widgetdiv.js to goog.module

--- core/widgetdiv.js
+++ core/widgetdiv.js
@@ -16,7 +16,8 @@
  * @name Blockly.WidgetDiv
  * @namespace
  */
-goog.provide('Blockly.WidgetDiv');
+goog.module('Blockly.WidgetDiv');
+goog.module.declareLegacyNamespace();
 
 goog.require('Blockly.common');
 goog.require('Blockly.utils.dom');
@@ -29,118 +30,131 @@ goog.requireType('Blockly.WorkspaceSvg');
 /**
  * The object currently using this container.
  * @type {Object}
- * @private
  */
-Blockly.WidgetDiv.owner_ = null;
+let owner = null;
 
 /**
  * Optional cleanup function set by whichever object uses the widget.
  * @type {Function}
- * @private
  */
-Blockly.WidgetDiv.dispose_ = null;
+let dispose = null;
 
 /**
  * A class name representing the current owner's workspace renderer.
  * @type {string}
- * @private
  */
-Blockly.WidgetDiv.rendererClassName_ = '';
+let rendererClassName = '';
 
 /**
  * A class name representing the current owner's workspace theme.
  * @type {string}
- * @private
  */
-Blockly.WidgetDiv.themeClassName_ = '';
+let themeClassName = '';
 
-/**
- * Create the widget div and inject it onto the page.
- */
-Blockly.WidgetDiv.createDom = function() {
-  if (Blockly.WidgetDiv.DIV) {
-    return;  // Already created.
-  }
   /**
    * The HTML container for popup overlays (e.g. editor widgets).
    * @type {!Element}
    */
-  Blockly.WidgetDiv.DIV = document.createElement('div');
-  Blockly.WidgetDiv.DIV.className = 'blocklyWidgetDiv';
+let DIV;
+exports.DIV = DIV;
+
+Object.defineProperties(exports, {
+  DIV: {
+    get: function() {
+      return DIV;
+    },
+  }
+});
+
+/**
+ * Create the widget div and inject it onto the page.
+ */
+const createDom = function() {
+  if (DIV) {
+    return;  // Already created.
+  }
+
+  DIV = document.createElement('div');
+  DIV.className = 'blocklyWidgetDiv';
   const container = Blockly.common.getParentContainer() || document.body;
-  container.appendChild(Blockly.WidgetDiv.DIV);
+  container.appendChild(DIV);
 };
+exports.createDom = createDom;
 
 /**
  * Initialize and display the widget div.  Close the old one if needed.
  * @param {!Object} newOwner The object that will be using this container.
  * @param {boolean} rtl Right-to-left (true) or left-to-right (false).
- * @param {Function} dispose Optional cleanup function to be run when the
+ * @param {Function} newDispose Optional cleanup function to be run when the
  *     widget is closed.
  */
-Blockly.WidgetDiv.show = function(newOwner, rtl, dispose) {
-  Blockly.WidgetDiv.hide();
-  Blockly.WidgetDiv.owner_ = newOwner;
-  Blockly.WidgetDiv.dispose_ = dispose;
-  const div = Blockly.WidgetDiv.DIV;
+const show = function(newOwner, rtl, newDispose) {
+  hide();
+  owner = newOwner;
+  dispose = newDispose;
+  const div = DIV;
   div.style.direction = rtl ? 'rtl' : 'ltr';
   div.style.display = 'block';
   const mainWorkspace =
     /** @type {!Blockly.WorkspaceSvg} */ (Blockly.common.getMainWorkspace());
-  Blockly.WidgetDiv.rendererClassName_ =
+  rendererClassName =
       mainWorkspace.getRenderer().getClassName();
-  Blockly.WidgetDiv.themeClassName_ = mainWorkspace.getTheme().getClassName();
-  Blockly.utils.dom.addClass(div, Blockly.WidgetDiv.rendererClassName_);
-  Blockly.utils.dom.addClass(div, Blockly.WidgetDiv.themeClassName_);
+  themeClassName = mainWorkspace.getTheme().getClassName();
+  Blockly.utils.dom.addClass(div, rendererClassName);
+  Blockly.utils.dom.addClass(div, themeClassName);
 };
+exports.show = show;
 
 /**
  * Destroy the widget and hide the div.
  */
-Blockly.WidgetDiv.hide = function() {
-  if (!Blockly.WidgetDiv.isVisible()) {
+const hide = function() {
+  if (!isVisible()) {
     return;
   }
-  Blockly.WidgetDiv.owner_ = null;
+  owner = null;
 
-  const div = Blockly.WidgetDiv.DIV;
+  const div = DIV;
   div.style.display = 'none';
   div.style.left = '';
   div.style.top = '';
-  Blockly.WidgetDiv.dispose_ && Blockly.WidgetDiv.dispose_();
-  Blockly.WidgetDiv.dispose_ = null;
+  dispose && dispose();
+  dispose = null;
   div.textContent = '';
 
-  if (Blockly.WidgetDiv.rendererClassName_) {
-    Blockly.utils.dom.removeClass(div, Blockly.WidgetDiv.rendererClassName_);
-    Blockly.WidgetDiv.rendererClassName_ = '';
+  if (rendererClassName) {
+    Blockly.utils.dom.removeClass(div, rendererClassName);
+    rendererClassName = '';
   }
-  if (Blockly.WidgetDiv.themeClassName_) {
-    Blockly.utils.dom.removeClass(div, Blockly.WidgetDiv.themeClassName_);
-    Blockly.WidgetDiv.themeClassName_ = '';
+  if (themeClassName) {
+    Blockly.utils.dom.removeClass(div, themeClassName);
+    themeClassName = '';
   }
   (/** @type {!Blockly.WorkspaceSvg} */ (
     Blockly.common.getMainWorkspace())).markFocused();
 };
+exports.hide = hide;
 
 /**
  * Is the container visible?
  * @return {boolean} True if visible.
  */
-Blockly.WidgetDiv.isVisible = function() {
-  return !!Blockly.WidgetDiv.owner_;
+const isVisible = function() {
+  return !!owner;
 };
+exports.isVisible = isVisible;
 
 /**
  * Destroy the widget and hide the div if it is being used by the specified
  * object.
  * @param {!Object} oldOwner The object that was using this container.
  */
-Blockly.WidgetDiv.hideIfOwner = function(oldOwner) {
-  if (Blockly.WidgetDiv.owner_ == oldOwner) {
-    Blockly.WidgetDiv.hide();
+const hideIfOwner = function(oldOwner) {
+  if (owner == oldOwner) {
+    hide();
   }
 };
+exports.hideIfOwner = hideIfOwner;
 
 /**
  * Set the widget div's position and height.  This function does nothing clever:
@@ -148,12 +162,11 @@ Blockly.WidgetDiv.hideIfOwner = function(oldOwner) {
  * @param {number} x Horizontal location (window coordinates, not body).
  * @param {number} y Vertical location (window coordinates, not body).
  * @param {number} height The height of the widget div (pixels).
- * @private
  */
-Blockly.WidgetDiv.positionInternal_ = function(x, y, height) {
-  Blockly.WidgetDiv.DIV.style.left = x + 'px';
-  Blockly.WidgetDiv.DIV.style.top = y + 'px';
-  Blockly.WidgetDiv.DIV.style.height = height + 'px';
+const positionInternal = function(x, y, height) {
+  DIV.style.left = x + 'px';
+  DIV.style.top = y + 'px';
+  DIV.style.height = height + 'px';
 };
 
 /**
@@ -169,20 +182,21 @@ Blockly.WidgetDiv.positionInternal_ = function(x, y, height) {
  *     the widget div, in window coordinates.
  * @param {boolean} rtl Whether the workspace is in RTL mode.  This determines
  *     horizontal alignment.
- * @package
  */
-Blockly.WidgetDiv.positionWithAnchor = function(viewportBBox, anchorBBox,
+const positionWithAnchor = function(viewportBBox, anchorBBox,
     widgetSize, rtl) {
-  const y = Blockly.WidgetDiv.calculateY_(viewportBBox, anchorBBox, widgetSize);
-  const x = Blockly.WidgetDiv.calculateX_(viewportBBox, anchorBBox, widgetSize,
+  const y = calculateY(viewportBBox, anchorBBox, widgetSize);
+  const x = calculateX(viewportBBox, anchorBBox, widgetSize,
       rtl);
 
   if (y < 0) {
-    Blockly.WidgetDiv.positionInternal_(x, 0, widgetSize.height + y);
+    positionInternal(x, 0, widgetSize.height + y);
   } else {
-    Blockly.WidgetDiv.positionInternal_(x, y, widgetSize.height);
+    positionInternal(x, y, widgetSize.height);
   }
 };
+/** @package */
+exports.positionWithAnchor = positionWithAnchor;
 
 /**
  * Calculate an x position (in window coordinates) such that the widget will not
@@ -196,9 +210,8 @@ Blockly.WidgetDiv.positionWithAnchor = function(viewportBBox, anchorBBox,
  * @param {boolean} rtl Whether the Blockly workspace is in RTL mode.
  * @return {number} A valid x-coordinate for the top left corner of the widget
  *     div, in window coordinates.
- * @private
  */
-Blockly.WidgetDiv.calculateX_ = function(viewportBBox, anchorBBox, widgetSize,
+const calculateX = function(viewportBBox, anchorBBox, widgetSize,
     rtl) {
   if (rtl) {
     // Try to align the right side of the field and the right side of widget.
@@ -227,9 +240,8 @@ Blockly.WidgetDiv.calculateX_ = function(viewportBBox, anchorBBox, widgetSize,
  *     the widget div.
  * @return {number} A valid y-coordinate for the top left corner of the widget
  *     div, in window coordinates.
- * @private
  */
-Blockly.WidgetDiv.calculateY_ = function(viewportBBox, anchorBBox, widgetSize) {
+const calculateY = function(viewportBBox, anchorBBox, widgetSize) {
   // Flip the widget vertically if off the bottom.
   if (anchorBBox.bottom + widgetSize.height >= viewportBBox.bottom) {
     // The bottom of the widget is at the top of the field.

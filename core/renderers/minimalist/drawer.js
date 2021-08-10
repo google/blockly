19b3a97dc1602cb519dd2d3c3f816ae964140780
kozbial <kozbial@google.com>
2021-08-10 16:27:06 -0700

    Migrate core/renderers/minimalist/drawer.js named requires

--- core/renderers/minimalist/drawer.js
+++ core/renderers/minimalist/drawer.js
@@ -12,26 +12,26 @@
 goog.module('Blockly.minimalist.Drawer');
 goog.module.declareLegacyNamespace();
 
-goog.require('Blockly.blockRendering.Drawer');
-goog.require('Blockly.utils.object');
-
-goog.requireType('Blockly.BlockSvg');
-goog.requireType('Blockly.minimalist.RenderInfo');
+const BaseDrawer = goog.require('Blockly.blockRendering.Drawer');
+/* eslint-disable-next-line no-unused-vars */
+const BlockSvg = goog.requireType('Blockly.BlockSvg');
+/* eslint-disable-next-line no-unused-vars */
+const RenderInfo = goog.requireType('Blockly.minimalist.RenderInfo');
+const object = goog.require('Blockly.utils.object');
 
 
 /**
  * An object that draws a block based on the given rendering information.
- * @param {!Blockly.BlockSvg} block The block to render.
- * @param {!Blockly.minimalist.RenderInfo} info An object containing all
+ * @param {!BlockSvg} block The block to render.
+ * @param {!RenderInfo} info An object containing all
  *   information needed to render this block.
  * @package
  * @constructor
- * @extends {Blockly.blockRendering.Drawer}
+ * @extends {BaseDrawer}
  */
 const Drawer = function(block, info) {
   Drawer.superClass_.constructor.call(this, block, info);
 };
-Blockly.utils.object.inherits(Drawer,
-    Blockly.blockRendering.Drawer);
+object.inherits(Drawer, BaseDrawer);
 
 exports = Drawer;

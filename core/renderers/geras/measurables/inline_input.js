87568c58696df25b8d3933b67badf2cf22f479ea
kozbial <kozbial@google.com>
2021-08-10 14:44:42 -0700

    Split classes from core/renderers/geras/measurables/input.js into multiple files

--- core/renderers/geras/measurables/inline_input.js
+++ core/renderers/geras/measurables/inline_input.js
@@ -5,17 +5,15 @@
  */
 
 /**
- * @fileoverview Objects representing inputs with connections on a rendered
- * block.
+ * @fileoverview Objects representing inline inputs with connections on a
+ * rendered block.
  * @author kozbial@google.com (Monica Kozbial)
  */
 'use strict';
 
 goog.provide('Blockly.geras.InlineInput');
-goog.provide('Blockly.geras.StatementInput');
 
 goog.require('Blockly.blockRendering.InlineInput');
-goog.require('Blockly.blockRendering.StatementInput');
 goog.require('Blockly.utils.object');
 
 goog.requireType('Blockly.blockRendering.ConstantProvider');
@@ -46,27 +44,3 @@ Blockly.geras.InlineInput = function(constants, input) {
 };
 Blockly.utils.object.inherits(Blockly.geras.InlineInput,
     Blockly.blockRendering.InlineInput);
-
-/**
- * An object containing information about the space a statement input takes up
- * during rendering
- * @param {!Blockly.blockRendering.ConstantProvider} constants The rendering
- *   constants provider.
- * @param {!Blockly.Input} input The statement input to measure and store
- *     information for.
- * @package
- * @constructor
- * @extends {Blockly.blockRendering.StatementInput}
- */
-Blockly.geras.StatementInput = function(constants, input) {
-  Blockly.geras.StatementInput.superClass_.constructor.call(
-      this, constants, input);
-
-  if (this.connectedBlock) {
-    // We allow the dark path to show on the parent block so that the child
-    // block looks embossed.  This takes up an extra pixel in both x and y.
-    this.height += this.constants_.DARK_PATH_OFFSET;
-  }
-};
-Blockly.utils.object.inherits(Blockly.geras.StatementInput,
-    Blockly.blockRendering.StatementInput);

b33a0f7677f55e53d52eaee0b3f1dfe090f1cd6b
kozbial <kozbial@google.com>
2021-08-17 12:41:03 -0700

    clang-format core/theme/classic.js

--- core/theme/classic.js
+++ core/theme/classic.js
@@ -20,71 +20,31 @@ const Theme = goog.require('Blockly.Theme');
 let Classic = {};
 
 Classic.defaultBlockStyles = {
-  "colour_blocks": {
-    "colourPrimary": "20"
-  },
-  "list_blocks": {
-    "colourPrimary": "260"
-  },
-  "logic_blocks": {
-    "colourPrimary": "210"
-  },
-  "loop_blocks": {
-    "colourPrimary": "120"
-  },
-  "math_blocks": {
-    "colourPrimary": "230"
-  },
-  "procedure_blocks": {
-    "colourPrimary": "290"
-  },
-  "text_blocks": {
-    "colourPrimary": "160"
-  },
-  "variable_blocks": {
-    "colourPrimary": "330"
-  },
-  "variable_dynamic_blocks": {
-    "colourPrimary": "310"
-  },
-  "hat_blocks": {
-    "colourPrimary": "330",
-    "hat": "cap"
-  }
+  'colour_blocks': {'colourPrimary': '20'},
+  'list_blocks': {'colourPrimary': '260'},
+  'logic_blocks': {'colourPrimary': '210'},
+  'loop_blocks': {'colourPrimary': '120'},
+  'math_blocks': {'colourPrimary': '230'},
+  'procedure_blocks': {'colourPrimary': '290'},
+  'text_blocks': {'colourPrimary': '160'},
+  'variable_blocks': {'colourPrimary': '330'},
+  'variable_dynamic_blocks': {'colourPrimary': '310'},
+  'hat_blocks': {'colourPrimary': '330', 'hat': 'cap'}
 };
 
 Classic.categoryStyles = {
-  "colour_category": {
-    "colour": "20"
-  },
-  "list_category": {
-    "colour": "260"
-  },
-  "logic_category": {
-    "colour": "210"
-  },
-  "loop_category": {
-    "colour": "120"
-  },
-  "math_category": {
-    "colour": "230"
-  },
-  "procedure_category": {
-    "colour": "290"
-  },
-  "text_category": {
-    "colour": "160"
-  },
-  "variable_category": {
-    "colour": "330"
-  },
-  "variable_dynamic_category": {
-    "colour": "310"
-  }
+  'colour_category': {'colour': '20'},
+  'list_category': {'colour': '260'},
+  'logic_category': {'colour': '210'},
+  'loop_category': {'colour': '120'},
+  'math_category': {'colour': '230'},
+  'procedure_category': {'colour': '290'},
+  'text_category': {'colour': '160'},
+  'variable_category': {'colour': '330'},
+  'variable_dynamic_category': {'colour': '310'}
 };
 
 Classic =
-    new Theme('classic', Classic.defaultBlockStyles,
-        Classic.categoryStyles);
+    new Theme('classic', Classic.defaultBlockStyles, Classic.categoryStyles);
 
 exports = Classic;

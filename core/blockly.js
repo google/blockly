a28933e0e0d5c088263d81db4ee074a5b328fc89
Aaron Dodson <adodson@google.com>
2021-09-14 08:54:16 -0700

    Mark Blockly.hideChaff as deprecated

--- core/blockly.js
+++ core/blockly.js
@@ -19,11 +19,9 @@ goog.provide('Blockly');
 goog.require('Blockly.browserEvents');
 goog.require('Blockly.clipboard');
 goog.require('Blockly.common');
-goog.require('Blockly.ComponentManager');
 goog.require('Blockly.connectionTypes');
 goog.require('Blockly.constants');
 goog.require('Blockly.dialog');
-goog.require('Blockly.DropDownDiv');
 goog.require('Blockly.Events');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Events.BlockCreate');
@@ -42,7 +40,6 @@ goog.require('Blockly.internalConstants');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Procedures');
 goog.require('Blockly.ShortcutRegistry');
-goog.require('Blockly.Tooltip');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Touch');
 goog.require('Blockly.utils');
@@ -52,7 +49,6 @@ goog.require('Blockly.utils.Size');
 goog.require('Blockly.utils.toolbox');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Variables');
-goog.require('Blockly.WidgetDiv');
 goog.require('Blockly.WorkspaceSvg');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Xml');
@@ -184,7 +180,7 @@ Blockly.onKeyDown = function(e) {
 Blockly.deleteBlock = function(selected) {
   if (!selected.workspace.isFlyout) {
     Blockly.Events.setGroup(true);
-    Blockly.hideChaff();
+    Blockly.common.getMainWorkspace().hideChaff();
     if (selected.outputConnection) {
       // Do not attempt to heal rows
       // (https://github.com/google/blockly/issues/4832)
@@ -230,23 +226,14 @@ Blockly.onContextMenu_ = function(e) {
   }
 };
 
-/**
- * Close tooltips, context menus, dropdown selections, etc.
- * @param {boolean=} opt_onlyClosePopups Whether only popups should be closed.
- */
-Blockly.hideChaff = function(opt_onlyClosePopups) {
-  Blockly.Tooltip.hide();
-  Blockly.WidgetDiv.hide();
-  Blockly.DropDownDiv.hideWithoutAnimation();
-
-  var onlyClosePopups = !!opt_onlyClosePopups;
-  var workspace = Blockly.common.getMainWorkspace();
-  var autoHideables = workspace.getComponentManager().getComponents(
-      Blockly.ComponentManager.Capability.AUTOHIDEABLE, true);
-  autoHideables.forEach(function(autoHideable) {
-    autoHideable.autoHide(onlyClosePopups);
-  });
-};
+// Add a getter for Blockly.hideChaff, for legacy reasons.
+Object.defineProperty(Blockly, 'hideChaff', {
+  get: function() {
+    Blockly.utils.deprecation.warn(
+        'Blockly.hideChaff', 'September 2021', 'September 2022');
+    return Blockly.common.getMainWorkspace().hideChaff;
+  }
+});
 
 /**
  * Returns the main workspace.  Returns the last used main workspace (based on

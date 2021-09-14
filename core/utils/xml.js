11a7f7bcd280bce107f45b8ecdf81ecb5ce881c7
Christopher Allen <cpcallen+git@google.com>
2021-09-14 15:01:35 +0100

    Migrate core/utils/xml.js to goog.module
    The document() function is renamed to getDocument() so as to avoid
    shadowing the global of the same name.

--- core/utils/xml.js
+++ core/utils/xml.js
@@ -16,65 +16,72 @@
  * @name Blockly.utils.xml
  * @namespace
  */
-goog.provide('Blockly.utils.xml');
+goog.module('Blockly.utils.xml');
+goog.module.declareLegacyNamespace();
 
 
 /**
  * Namespace for Blockly's XML.
  */
-Blockly.utils.xml.NAME_SPACE = 'https://developers.google.com/blockly/xml';
+const NAME_SPACE = 'https://developers.google.com/blockly/xml';
+exports.NAME_SPACE = NAME_SPACE;
 
 /**
  * Get the document object.  This method is overridden in the Node.js build of
  * Blockly. See gulpfile.js, package-blockly-node task.
+ *
+ * Note that this function is named myDocument so as to not shadow the
+ * global of the same name, but (for now) exported as .document to not
+ * break existing importers.
+ *
  * @return {!Document} The document object.
- * @public
  */
-Blockly.utils.xml.document = function() {
+const getDocument = function() {
   return document;
 };
+exports.document = getDocument;
 
 /**
  * Create DOM element for XML.
  * @param {string} tagName Name of DOM element.
  * @return {!Element} New DOM element.
- * @public
  */
-Blockly.utils.xml.createElement = function(tagName) {
-  return Blockly.utils.xml.document().createElementNS(
-      Blockly.utils.xml.NAME_SPACE, tagName);
+const createElement = function(tagName) {
+  return getDocument().createElementNS(
+      NAME_SPACE, tagName);
 };
+exports.createElement = createElement;
 
 /**
  * Create text element for XML.
  * @param {string} text Text content.
  * @return {!Text} New DOM text node.
- * @public
  */
-Blockly.utils.xml.createTextNode = function(text) {
-  return Blockly.utils.xml.document().createTextNode(text);
+const createTextNode = function(text) {
+  return getDocument().createTextNode(text);
 };
+exports.createTextNode = createTextNode;
 
 /**
  * Converts an XML string into a DOM tree.
  * @param {string} text XML string.
  * @return {Document} The DOM document.
  * @throws if XML doesn't parse.
- * @public
  */
-Blockly.utils.xml.textToDomDocument = function(text) {
+const textToDomDocument = function(text) {
   const oParser = new DOMParser();
   return oParser.parseFromString(text, 'text/xml');
 };
+exports.textToDomDocument = textToDomDocument;
 
 /**
  * Converts a DOM structure into plain text.
  * Currently the text format is fairly ugly: all one line with no whitespace.
  * @param {!Node} dom A tree of XML nodes.
  * @return {string} Text representation.
- * @public
  */
-Blockly.utils.xml.domToText = function(dom) {
+const domToText = function(dom) {
   const oSerializer = new XMLSerializer();
   return oSerializer.serializeToString(dom);
 };
+exports.domToText = domToText;

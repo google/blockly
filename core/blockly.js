f6707df94fc79a12db6b7f1c6a3d432ece5a6a07
Maribeth Bottorff <maribethb@google.com>
2021-09-16 10:28:17 -0700

    refactor: remove unused Blockly.onContextMenu_

--- core/blockly.js
+++ core/blockly.js
@@ -42,7 +42,6 @@ goog.require('Blockly.Procedures');
 goog.require('Blockly.ShortcutRegistry');
 /** @suppress {extraRequire} */
 goog.require('Blockly.Touch');
-goog.require('Blockly.utils');
 goog.require('Blockly.utils.colour');
 goog.require('Blockly.utils.deprecation');
 goog.require('Blockly.utils.Size');
@@ -213,18 +212,6 @@ Blockly.paste = Blockly.clipboard.paste;
  * @package
  */
 Blockly.duplicate = Blockly.clipboard.duplicate;
-
-/**
- * Cancel the native context menu, unless the focus is on an HTML input widget.
- * @param {!Event} e Mouse down event.
- * @private
- */
-Blockly.onContextMenu_ = function(e) {
-  if (!Blockly.utils.isTargetInput(e)) {
-    // When focused on an HTML text input widget, don't cancel the context menu.
-    e.preventDefault();
-  }
-};
 
 /**
  * Close tooltips, context menus, dropdown selections, etc.

11d126c49cfbf8d352f1f66484b9f8d9e9fa7277
Aaron Dodson <adodson@google.com>
2021-09-23 15:04:06 -0700

    chore: Remove declareLegacyNamespace from thrasos renderer

---
+++ core/renderers/thrasos/thrasos.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Re-exports of Blockly.thrasos.* modules.
+ */
+'use strict';
+
+goog.module('Blockly.thrasos');
+
+const RenderInfo = goog.require('Blockly.thrasos.RenderInfo');
+const Renderer = goog.require('Blockly.thrasos.Renderer');
+
+exports.RenderInfo = RenderInfo;
+exports.Renderer = Renderer;

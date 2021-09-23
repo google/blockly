2eece4f86ae208c744aec3a6d36e781a6f5fadbe
alschmiedt <aschmiedt@google.com>
2021-09-23 09:54:41 -0700

    chore: cleanup

--- core/events/events.js
+++ core/events/events.js
@@ -43,11 +43,10 @@ const VarCreate = goog.require('Blockly.Events.VarCreate');
 const VarDelete = goog.require('Blockly.Events.VarDelete');
 const VarRename = goog.require('Blockly.Events.VarRename');
 const ViewportChange = goog.require('Blockly.Events.ViewportChange');
-// TODO: Figure out a better name.
 const eventUtils = goog.require('Blockly.Events.utils');
 
+// Events.
 exports.Abstract = Abstract;
-// TODO: is this suppose to be public?
 exports.BlockBase = BlockBase;
 exports.BlockChange = BlockChange;
 exports.BlockCreate = BlockCreate;
@@ -55,7 +54,6 @@ exports.BlockDelete = BlockDelete;
 exports.BlockDrag = BlockDrag;
 exports.BlockMove = BlockMove;
 exports.Click = Click;
-// TODO: Is this suppose to be public?
 exports.CommentBase = CommentBase;
 exports.CommentChange = CommentChange;
 exports.CommentCreate = CommentCreate;
@@ -75,55 +73,49 @@ exports.VarDelete = VarDelete;
 exports.VarRename = VarRename;
 exports.ViewportChange = ViewportChange;
 
-// Export all the public eventUtils.
+// Event utils.
 exports.clearPendingUndo = eventUtils.clearPendingUndo;
-// TODO: What should I do about this private nonsense? What should I be exporting?
-exports.disabled = eventUtils.disabled_;
 exports.disable = eventUtils.disable;
 exports.enable = eventUtils.enable;
 exports.filter = eventUtils.filter;
 exports.fire = eventUtils.fire;
-// TODO: What should I do about this private nonsense?
-exports.fireNow = eventUtils.fireNow_;
 exports.fromJson = eventUtils.fromJson;
 exports.getDescendantIds = eventUtils.getDescendantIds;
 exports.get = eventUtils.get;
 exports.getGroup = eventUtils.getGroup;
 exports.getRecordUndo = eventUtils.getRecordUndo;
 exports.isEnabled = eventUtils.isEnabled;
-// TODO: Same, what to do about this?
 exports.recordUndo = eventUtils.recordUndo;
 exports.setGroup = eventUtils.setGroup;
 exports.setRecordUndo = eventUtils.setRecordUndo;
 exports.disableOrphans = eventUtils.disableOrphans;
 
-//
-exports.CREATE = eventUtils.CREATE;
-exports.BLOCK_CREATE = eventUtils.BLOCK_CREATE;
-exports.DELETE = eventUtils.DELETE;
-exports.BLOCK_DELETE = eventUtils.BLOCK_DELETE;
-exports.CHANGE = eventUtils.CHANGE;
+// Event types.
 exports.BLOCK_CHANGE = eventUtils.BLOCK_CHANGE;
-exports.MOVE = eventUtils.MOVE;
+exports.BLOCK_CREATE = eventUtils.BLOCK_CREATE;
+exports.BLOCK_DELETE = eventUtils.BLOCK_DELETE;
+exports.BLOCK_DRAG = eventUtils.BLOCK_DRAG;
 exports.BLOCK_MOVE = eventUtils.BLOCK_MOVE;
+exports.BUBBLE_OPEN = eventUtils.BUBBLE_OPEN;
+exports.BumpEvent = eventUtils.BumpEvent;
+exports.BUMP_EVENTS = eventUtils.BUMP_EVENTS;
+exports.CHANGE = eventUtils.CHANGE;
+exports.CLICK = eventUtils.CLICK;
+exports.COMMENT_CHANGE = eventUtils.COMMENT_CHANGE;
+exports.COMMENT_CREATE = eventUtils.COMMENT_CREATE;
+exports.COMMENT_DELETE = eventUtils.COMMENT_DELETE;
+exports.COMMENT_MOVE = eventUtils.COMMENT_MOVE;
+exports.CREATE = eventUtils.CREATE;
+exports.DELETE = eventUtils.DELETE;
+exports.FINISHED_LOADING = eventUtils.FINISHED_LOADING;
+exports.MARKER_MOVE = eventUtils.MARKER_MOVE;
+exports.MOVE = eventUtils.MOVE;
+exports.SELECTED = eventUtils.SELECTED;
+exports.THEME_CHANGE = eventUtils.THEME_CHANGE;
+exports.TOOLBOX_ITEM_SELECT = eventUtils.TOOLBOX_ITEM_SELECT;
+exports.TRASHCAN_OPEN = eventUtils.TRASHCAN_OPEN;
+exports.UI = eventUtils.UI;
 exports.VAR_CREATE = eventUtils.VAR_CREATE;
 exports.VAR_DELETE = eventUtils.VAR_DELETE;
 exports.VAR_RENAME = eventUtils.VAR_RENAME;
-exports.UI = eventUtils.UI;
-exports.BLOCK_DRAG = eventUtils.BLOCK_DRAG;
-exports.SELECTED = eventUtils.SELECTED;
-exports.CLICK = eventUtils.CLICK;
-exports.MARKER_MOVE = eventUtils.MARKER_MOVE;
-exports.BUBBLE_OPEN = eventUtils.BUBBLE_OPEN;
-exports.TRASHCAN_OPEN = eventUtils.TRASHCAN_OPEN;
-exports.TOOLBOX_ITEM_SELECT = eventUtils.TOOLBOX_ITEM_SELECT;
-exports.THEME_CHANGE = eventUtils.THEME_CHANGE;
 exports.VIEWPORT_CHANGE = eventUtils.VIEWPORT_CHANGE;
-exports.COMMENT_CREATE = eventUtils.COMMENT_CREATE;
-exports.COMMENT_DELETE = eventUtils.COMMENT_DELETE;
-exports.COMMENT_CHANGE = eventUtils.COMMENT_CHANGE;
-exports.COMMENT_MOVE = eventUtils.COMMENT_MOVE;
-exports.FINISHED_LOADING = eventUtils.FINISHED_LOADING;
-exports.BumpEvent = eventUtils.BumpEvent;
-exports.BUMP_EVENTS = eventUtils.BUMP_EVENTS;
-exports.FIRE_QUEUE_ = eventUtils.FIRE_QUEUE_;

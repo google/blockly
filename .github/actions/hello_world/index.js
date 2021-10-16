af27f666ae35f3b968ee27379fd614d4296b8a34
alschmiedt <aschmiedt@google.com>
2021-10-15 17:04:51 -0700

    testing

--- .github/actions/hello_world/index.js
+++ .github/actions/hello_world/index.js
@@ -2,8 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}`);
+
+  console.log(`Reviewer: ${github.context.payload.pull_request.requested_reviewers}`)
+  const token = core.getInput("repo-token", { required: true });
+  console.log(`Token: ${token}`);
+
+  const octokit = github.getOctokit(token);
+  console.log(octokit);
+  // const result = await octokit.rest.issues.addAssignees({
+  //   owner: github.context.repo.owner,
+  //   repo: github.context.repo.repo,
+  //   issue_number: number,
+  //   assignees: [author]
+  // });
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);

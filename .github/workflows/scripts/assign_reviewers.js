56cca271884026964d35cd48c405dfe1b16e79d4
alschmiedt <aschmiedt@google.com>
2021-10-26 13:41:29 -0700

    chore: general action cleanup

--- .github/workflows/scripts/assign_reviewers.js
+++ .github/workflows/scripts/assign_reviewers.js
@@ -1,23 +1,18 @@
 /**
- * Notes:
- *   It only adds assignees. It does not remove assignees.
- *   Not able to test yet with how it works with auto adding a reviewer.
- *   Presumably it works the same as manually adding a reviewer, but you never know.
+ * This script adds requested reviewers as assignees. If you remove a requested
+ * reviewer, it will not remove them as an assignee.
+ *
+ * See https://github.com/google/blockly/issues/5643 for more information on
+ * why this was added.
  */
 module.exports = ({github, context, core}) => {
   try {
-    console.log(context);
-    console.log("============");
-    console.log(context.payload.pull_request);
     if (context.payload.pull_request === undefined) {
       throw new Error("Can't get pull_request payload. Check a request reviewer event was triggered.");
     }
-
     const reviewers = context.payload.pull_request.requested_reviewers;
-    console.log(`Reviewers: ${reviewers}`);
     // Assignees takes in a list of logins rather than the reviewer object.
     const reviewerNames = reviewers.map(reviewer => reviewer.login);
-    console.log(`ReviewerNames: ${reviewerNames}`);
     const {number:issue_number} = context.payload.pull_request;
 
     github.rest.issues.addAssignees({

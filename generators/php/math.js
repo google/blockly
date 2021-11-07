f701a43c706cc79d31fa3e2d9b6c9c88e598f17c
jeremyjacob123 <43049656+jeremyjacob123@users.noreply.github.com>
2021-11-06 21:52:26 -0400

    Update math.js

--- generators/php/math.js
+++ generators/php/math.js
@@ -150,7 +150,7 @@ Blockly.PHP['math_number_property'] = function(block) {
         Blockly.PHP.ORDER_EQUALITY],
     'ODD': [' % 2 == 1', Blockly.PHP.ORDER_MODULUS,
         Blockly.PHP.ORDER_EQUALITY],
-    'WHOLE': ['is_int(' + number_to_check + ')', Blockly.PHP.ORDER_MODULUS,
+    'WHOLE': ['is_int('')', Blockly.PHP.ORDER_MODULUS,
         Blockly.PHP.ORDER_EQUALITY],
     'POSITIVE': [' > 0', Blockly.PHP.ORDER_RELATIONAL,
         Blockly.PHP.ORDER_RELATIONAL],

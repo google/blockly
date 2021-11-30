35b6c464df0eaa7514cb207eb98404f3fd54ab35
Rachel Fenichel <fenichel@google.com>
2021-11-30 15:21:55 -0800

    chore: run clang-format

--- generators/javascript/procedures.js
+++ generators/javascript/procedures.js
@@ -21,12 +21,10 @@ JavaScript['procedures_defreturn'] = function(block) {
       block.getFieldValue('NAME'), NameType.PROCEDURE);
   let xfix1 = '';
   if (JavaScript.STATEMENT_PREFIX) {
-    xfix1 += JavaScript.injectId(JavaScript.STATEMENT_PREFIX,
-        block);
+    xfix1 += JavaScript.injectId(JavaScript.STATEMENT_PREFIX, block);
   }
   if (JavaScript.STATEMENT_SUFFIX) {
-    xfix1 += JavaScript.injectId(JavaScript.STATEMENT_SUFFIX,
-        block);
+    xfix1 += JavaScript.injectId(JavaScript.STATEMENT_SUFFIX, block);
   }
   if (xfix1) {
     xfix1 = JavaScript.prefixLines(xfix1, JavaScript.INDENT);
@@ -34,12 +32,12 @@ JavaScript['procedures_defreturn'] = function(block) {
   let loopTrap = '';
   if (JavaScript.INFINITE_LOOP_TRAP) {
     loopTrap = JavaScript.prefixLines(
-        JavaScript.injectId(JavaScript.INFINITE_LOOP_TRAP,
-        block), JavaScript.INDENT);
+        JavaScript.injectId(JavaScript.INFINITE_LOOP_TRAP, block),
+        JavaScript.INDENT);
   }
   const branch = JavaScript.statementToCode(block, 'STACK');
-  let returnValue = JavaScript.valueToCode(block, 'RETURN',
-      JavaScript.ORDER_NONE) || '';
+  let returnValue =
+      JavaScript.valueToCode(block, 'RETURN', JavaScript.ORDER_NONE) || '';
   let xfix2 = '';
   if (branch && returnValue) {
     // After executing the function body, revisit this block for the return.
@@ -51,11 +49,10 @@ JavaScript['procedures_defreturn'] = function(block) {
   const args = [];
   const variables = block.getVars();
   for (let i = 0; i < variables.length; i++) {
-    args[i] = JavaScript.nameDB_.getName(variables[i],
-        NameType.VARIABLE);
+    args[i] = JavaScript.nameDB_.getName(variables[i], NameType.VARIABLE);
   }
-  let code = 'function ' + funcName + '(' + args.join(', ') + ') {\n' +
-      xfix1 + loopTrap + branch + xfix2 + returnValue + '}';
+  let code = 'function ' + funcName + '(' + args.join(', ') + ') {\n' + xfix1 +
+      loopTrap + branch + xfix2 + returnValue + '}';
   code = JavaScript.scrub_(block, code);
   // Add % so as not to collide with helper functions in definitions list.
   JavaScript.definitions_['%' + funcName] = code;
@@ -64,8 +61,7 @@ JavaScript['procedures_defreturn'] = function(block) {
 
 // Defining a procedure without a return value uses the same generator as
 // a procedure with a return value.
-JavaScript['procedures_defnoreturn'] =
-    JavaScript['procedures_defreturn'];
+JavaScript['procedures_defnoreturn'] = JavaScript['procedures_defreturn'];
 
 JavaScript['procedures_callreturn'] = function(block) {
   // Call a procedure with a return value.
@@ -74,8 +70,8 @@ JavaScript['procedures_callreturn'] = function(block) {
   const args = [];
   const variables = block.getVars();
   for (let i = 0; i < variables.length; i++) {
-    args[i] = JavaScript.valueToCode(block, 'ARG' + i,
-        JavaScript.ORDER_NONE) || 'null';
+    args[i] = JavaScript.valueToCode(block, 'ARG' + i, JavaScript.ORDER_NONE) ||
+        'null';
   }
   const code = funcName + '(' + args.join(', ') + ')';
   return [code, JavaScript.ORDER_FUNCTION_CALL];
@@ -91,8 +87,9 @@ JavaScript['procedures_callnoreturn'] = function(block) {
 
 JavaScript['procedures_ifreturn'] = function(block) {
   // Conditionally return value from a procedure.
-  const condition = JavaScript.valueToCode(block, 'CONDITION',
-      JavaScript.ORDER_NONE) || 'false';
+  const condition =
+      JavaScript.valueToCode(block, 'CONDITION', JavaScript.ORDER_NONE) ||
+      'false';
   let code = 'if (' + condition + ') {\n';
   if (JavaScript.STATEMENT_SUFFIX) {
     // Inject any statement suffix here since the regular one at the end
@@ -102,8 +99,8 @@ JavaScript['procedures_ifreturn'] = function(block) {
         JavaScript.INDENT);
   }
   if (block.hasReturnValue_) {
-    const value = JavaScript.valueToCode(block, 'VALUE',
-        JavaScript.ORDER_NONE) || 'null';
+    const value =
+        JavaScript.valueToCode(block, 'VALUE', JavaScript.ORDER_NONE) || 'null';
     code += JavaScript.INDENT + 'return ' + value + ';\n';
   } else {
     code += JavaScript.INDENT + 'return;\n';

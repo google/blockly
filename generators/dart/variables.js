577569270a421da4a280474f2d52552f30dc844c
Rachel Fenichel <fenichel@google.com>
2021-11-30 19:04:03 -0800

    chore: run clang-format

--- generators/dart/variables.js
+++ generators/dart/variables.js
@@ -16,16 +16,16 @@ const {NameType} = goog.require('Blockly.Names');
 
 Dart['variables_get'] = function(block) {
   // Variable getter.
-  const code = Dart.nameDB_.getName(block.getFieldValue('VAR'),
-      NameType.VARIABLE);
+  const code =
+      Dart.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
   return [code, Dart.ORDER_ATOMIC];
 };
 
 Dart['variables_set'] = function(block) {
   // Variable setter.
-  const argument0 = Dart.valueToCode(block, 'VALUE',
-      Dart.ORDER_ASSIGNMENT) || '0';
-  const varName = Dart.nameDB_.getName(block.getFieldValue('VAR'),
-      NameType.VARIABLE);
+  const argument0 =
+      Dart.valueToCode(block, 'VALUE', Dart.ORDER_ASSIGNMENT) || '0';
+  const varName =
+      Dart.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
   return varName + ' = ' + argument0 + ';\n';
 };

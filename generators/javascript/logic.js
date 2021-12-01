e11fe348b20a69da89835c9eb968663019aef9c3
Rachel Fenichel <fenichel@google.com>
2021-11-30 17:31:55 -0800

    refactor: convert some js block generators to goog.module (#5756)
    * refactor: convert generators/javascript/colour.js to goog.module
    
    * refactor: convert generators/javascript/colour.js to named requires
    
    * chore: run clang-format
    
    * refactor: convert generators/javascript/lists.js to goog.module
    
    * refactor: convert generators/javascript/lists.js to named requires
    
    * chore: run clang-format
    
    * refactor: convert generators/javascript/logic.js to goog.module
    
    * refactor: convert generators/javascript/logic.js to named requires
    
    * chore: run clang-format
    
    * refactor: convert genereators/javascript/math.js to goog.module
    
    * refactor: convert generators/javascript/math.js to named requires
    
    * chore: rebuild deps.js

--- generators/javascript/logic.js
+++ generators/javascript/logic.js
@@ -9,74 +9,71 @@
  */
 'use strict';
 
-goog.provide('Blockly.JavaScript.logic');
+goog.module('Blockly.JavaScript.logic');
 
-goog.require('Blockly.JavaScript');
+const JavaScript = goog.require('Blockly.JavaScript');
 
 
-Blockly.JavaScript['controls_if'] = function(block) {
+JavaScript['controls_if'] = function(block) {
   // If/elseif/else condition.
   let n = 0;
   let code = '';
-  if (Blockly.JavaScript.STATEMENT_PREFIX) {
+  if (JavaScript.STATEMENT_PREFIX) {
     // Automatic prefix insertion is switched off for this block.  Add manually.
-    code += Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_PREFIX,
-        block);
+    code += JavaScript.injectId(JavaScript.STATEMENT_PREFIX, block);
   }
   do {
-    const conditionCode = Blockly.JavaScript.valueToCode(block, 'IF' + n,
-        Blockly.JavaScript.ORDER_NONE) || 'false';
-    let branchCode = Blockly.JavaScript.statementToCode(block, 'DO' + n);
-    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
-      branchCode = Blockly.JavaScript.prefixLines(
-          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
-          block), Blockly.JavaScript.INDENT) + branchCode;
+    const conditionCode =
+        JavaScript.valueToCode(block, 'IF' + n, JavaScript.ORDER_NONE) ||
+        'false';
+    let branchCode = JavaScript.statementToCode(block, 'DO' + n);
+    if (JavaScript.STATEMENT_SUFFIX) {
+      branchCode = JavaScript.prefixLines(
+                       JavaScript.injectId(JavaScript.STATEMENT_SUFFIX, block),
+                       JavaScript.INDENT) +
+          branchCode;
     }
-    code += (n > 0 ? ' else ' : '') +
-        'if (' + conditionCode + ') {\n' + branchCode + '}';
+    code += (n > 0 ? ' else ' : '') + 'if (' + conditionCode + ') {\n' +
+        branchCode + '}';
     n++;
   } while (block.getInput('IF' + n));
 
-  if (block.getInput('ELSE') || Blockly.JavaScript.STATEMENT_SUFFIX) {
-    let branchCode = Blockly.JavaScript.statementToCode(block, 'ELSE');
-    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
-      branchCode = Blockly.JavaScript.prefixLines(
-          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
-          block), Blockly.JavaScript.INDENT) + branchCode;
+  if (block.getInput('ELSE') || JavaScript.STATEMENT_SUFFIX) {
+    let branchCode = JavaScript.statementToCode(block, 'ELSE');
+    if (JavaScript.STATEMENT_SUFFIX) {
+      branchCode = JavaScript.prefixLines(
+                       JavaScript.injectId(JavaScript.STATEMENT_SUFFIX, block),
+                       JavaScript.INDENT) +
+          branchCode;
     }
     code += ' else {\n' + branchCode + '}';
   }
   return code + '\n';
 };
 
-Blockly.JavaScript['controls_ifelse'] = Blockly.JavaScript['controls_if'];
+JavaScript['controls_ifelse'] = JavaScript['controls_if'];
 
-Blockly.JavaScript['logic_compare'] = function(block) {
+JavaScript['logic_compare'] = function(block) {
   // Comparison operator.
-  const OPERATORS = {
-    'EQ': '==',
-    'NEQ': '!=',
-    'LT': '<',
-    'LTE': '<=',
-    'GT': '>',
-    'GTE': '>='
-  };
+  const OPERATORS =
+      {'EQ': '==', 'NEQ': '!=', 'LT': '<', 'LTE': '<=', 'GT': '>', 'GTE': '>='};
   const operator = OPERATORS[block.getFieldValue('OP')];
   const order = (operator === '==' || operator === '!=') ?
-      Blockly.JavaScript.ORDER_EQUALITY : Blockly.JavaScript.ORDER_RELATIONAL;
-  const argument0 = Blockly.JavaScript.valueToCode(block, 'A', order) || '0';
-  const argument1 = Blockly.JavaScript.valueToCode(block, 'B', order) || '0';
+      JavaScript.ORDER_EQUALITY :
+      JavaScript.ORDER_RELATIONAL;
+  const argument0 = JavaScript.valueToCode(block, 'A', order) || '0';
+  const argument1 = JavaScript.valueToCode(block, 'B', order) || '0';
   const code = argument0 + ' ' + operator + ' ' + argument1;
   return [code, order];
 };
 
-Blockly.JavaScript['logic_operation'] = function(block) {
+JavaScript['logic_operation'] = function(block) {
   // Operations 'and', 'or'.
   const operator = (block.getFieldValue('OP') === 'AND') ? '&&' : '||';
-  const order = (operator === '&&') ? Blockly.JavaScript.ORDER_LOGICAL_AND :
-      Blockly.JavaScript.ORDER_LOGICAL_OR;
-  let argument0 = Blockly.JavaScript.valueToCode(block, 'A', order);
-  let argument1 = Blockly.JavaScript.valueToCode(block, 'B', order);
+  const order = (operator === '&&') ? JavaScript.ORDER_LOGICAL_AND :
+                                      JavaScript.ORDER_LOGICAL_OR;
+  let argument0 = JavaScript.valueToCode(block, 'A', order);
+  let argument1 = JavaScript.valueToCode(block, 'B', order);
   if (!argument0 && !argument1) {
     // If there are no arguments, then the return value is false.
     argument0 = 'false';
@@ -95,34 +92,36 @@ Blockly.JavaScript['logic_operation'] = function(block) {
   return [code, order];
 };
 
-Blockly.JavaScript['logic_negate'] = function(block) {
+JavaScript['logic_negate'] = function(block) {
   // Negation.
-  const order = Blockly.JavaScript.ORDER_LOGICAL_NOT;
-  const argument0 = Blockly.JavaScript.valueToCode(block, 'BOOL', order) ||
-      'true';
+  const order = JavaScript.ORDER_LOGICAL_NOT;
+  const argument0 = JavaScript.valueToCode(block, 'BOOL', order) || 'true';
   const code = '!' + argument0;
   return [code, order];
 };
 
-Blockly.JavaScript['logic_boolean'] = function(block) {
+JavaScript['logic_boolean'] = function(block) {
   // Boolean values true and false.
   const code = (block.getFieldValue('BOOL') === 'TRUE') ? 'true' : 'false';
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  return [code, JavaScript.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['logic_null'] = function(block) {
+JavaScript['logic_null'] = function(block) {
   // Null data type.
-  return ['null', Blockly.JavaScript.ORDER_ATOMIC];
+  return ['null', JavaScript.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['logic_ternary'] = function(block) {
+JavaScript['logic_ternary'] = function(block) {
   // Ternary operator.
-  const value_if = Blockly.JavaScript.valueToCode(block, 'IF',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'false';
-  const value_then = Blockly.JavaScript.valueToCode(block, 'THEN',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'null';
-  const value_else = Blockly.JavaScript.valueToCode(block, 'ELSE',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'null';
+  const value_if =
+      JavaScript.valueToCode(block, 'IF', JavaScript.ORDER_CONDITIONAL) ||
+      'false';
+  const value_then =
+      JavaScript.valueToCode(block, 'THEN', JavaScript.ORDER_CONDITIONAL) ||
+      'null';
+  const value_else =
+      JavaScript.valueToCode(block, 'ELSE', JavaScript.ORDER_CONDITIONAL) ||
+      'null';
   const code = value_if + ' ? ' + value_then + ' : ' + value_else;
-  return [code, Blockly.JavaScript.ORDER_CONDITIONAL];
+  return [code, JavaScript.ORDER_CONDITIONAL];
 };

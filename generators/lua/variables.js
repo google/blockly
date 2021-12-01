ca6e376bb2718c0fa1312428b45d82de3f910551
Rachel Fenichel <fenichel@google.com>
2021-12-01 12:53:09 -0800

    chore: run clang-format

--- generators/lua/variables.js
+++ generators/lua/variables.js
@@ -17,16 +17,15 @@ const {NameType} = goog.require('Blockly.Names');
 
 Lua['variables_get'] = function(block) {
   // Variable getter.
-  const code = Lua.nameDB_.getName(block.getFieldValue('VAR'),
-      NameType.VARIABLE);
+  const code =
+      Lua.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
   return [code, Lua.ORDER_ATOMIC];
 };
 
 Lua['variables_set'] = function(block) {
   // Variable setter.
-  const argument0 = Lua.valueToCode(block, 'VALUE',
-      Lua.ORDER_NONE) || '0';
-  const varName = Lua.nameDB_.getName(
-      block.getFieldValue('VAR'), NameType.VARIABLE);
+  const argument0 = Lua.valueToCode(block, 'VALUE', Lua.ORDER_NONE) || '0';
+  const varName =
+      Lua.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
   return varName + ' = ' + argument0 + '\n';
 };

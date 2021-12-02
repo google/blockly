22ee041a73f712db1eaa978009efc7e232779c5e
Rachel Fenichel <fenichel@google.com>
2021-12-01 17:15:52 -0800

    chore: run clang-format

--- generators/php/variables.js
+++ generators/php/variables.js
@@ -16,17 +16,17 @@ const {NameType} = goog.require('Blockly.Names');
 
 
 PHP['variables_get'] = function(block) {
-    // Variable getter.
-    const code = PHP.nameDB_.getName(block.getFieldValue('VAR'),
-        NameType.VARIABLE);
-    return [code, PHP.ORDER_ATOMIC];
+  // Variable getter.
+  const code =
+      PHP.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
+  return [code, PHP.ORDER_ATOMIC];
 };
 
 PHP['variables_set'] = function(block) {
-    // Variable setter.
-    const argument0 = PHP.valueToCode(block, 'VALUE',
-            PHP.ORDER_ASSIGNMENT) || '0';
-    const varName = PHP.nameDB_.getName(
-        block.getFieldValue('VAR'), NameType.VARIABLE);
-    return varName + ' = ' + argument0 + ';\n';
+  // Variable setter.
+  const argument0 =
+      PHP.valueToCode(block, 'VALUE', PHP.ORDER_ASSIGNMENT) || '0';
+  const varName =
+      PHP.nameDB_.getName(block.getFieldValue('VAR'), NameType.VARIABLE);
+  return varName + ' = ' + argument0 + ';\n';
 };

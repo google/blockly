a42fae7a8afeb4564144958e3ab0fc23c38d4f59
Christopher Allen <cpcallen+git@google.com>
2021-12-03 17:51:44 +0000

    chore(build): Reenable advanced compilation test
    Fixes #5602.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -76,8 +76,8 @@ run_test_command "package" "npm run package"
 # Run Node tests.
 run_test_command "node" "./node_modules/.bin/mocha tests/node --config tests/node/.mocharc.js"
 
-# # Attempt advanced compilation of a Blockly app.
-# run_test_command "advanced_compile" "npm run test:compile:advanced"
+# Attempt advanced compilation of a Blockly app.
+run_test_command "advanced_compile" "npm run test:compile:advanced"
 
 
 # End of tests.

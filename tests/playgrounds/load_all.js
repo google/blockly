6a354150ac60a17ab761da6a314c3e091183f143
alschmiedt <aschmiedt@google.com>
2021-12-20 14:29:20 -0800

    chore: run clang-format

--- tests/playgrounds/load_all.js
+++ tests/playgrounds/load_all.js
@@ -5,23 +5,28 @@
  */
 
 /**
- * @fileoverview Loads uncompressed Blockly when running locally. Loads compressed otherwise.
+ * @fileoverview Loads uncompressed Blockly when running locally. Loads
+ * compressed otherwise.
  */
 'use strict';
 
 
 /**
- * Loads all the compressed or uncompressed dependencies necessary to run the playground.
- * This is necessary since the goog.module conversion. Please see issue #5557 for more information.
+ * Loads all the compressed or uncompressed dependencies necessary to run the
+ * playground. This is necessary since the goog.module conversion. Please see
+ * issue #5557 for more information.
  */
 
 (function() {
-    if (location.hostname  === 'localhost' || location.hostname === "127.0.0.1" || location.hostname === '[::1]') {
-        document.write(`<script src="../blockly_uncompressed.js" id="blockly-uncompressed-script"></script>`);
-        document.write(`<script src="../msg/messages.js"></script>`);
-        document.write(`<script src="../tests/themes/test_themes.js"></script>`);
-        document.write(`<script src="../node_modules/@blockly/block-test/dist/index.js"></script>`);
-        document.write(`<script>
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' ||
+    location.hostname === '[::1]') {
+  document.write(
+      `<script src="../blockly_uncompressed.js" id="blockly-uncompressed-script"></script>`);
+  document.write(`<script src="../msg/messages.js"></script>`);
+  document.write(`<script src="../tests/themes/test_themes.js"></script>`);
+  document.write(
+      `<script src="../node_modules/@blockly/block-test/dist/index.js"></script>`);
+  document.write(`<script>
                 // Custom requires for the playground.
                 goog.require('Blockly.Dart.all');
                 goog.require('Blockly.JavaScript.all');
@@ -31,14 +36,15 @@
                 goog.require('Blockly.WorkspaceCommentSvg');
                 goog.require('Blockly.blocks.all');
             </script>`);
-    } else {
-        document.write(`<script src="../blockly_compressed.js" id="blockly-compressed-script"></script>`);
-        document.write(`<script src="../dart_compressed.js"></script>`);
-        document.write(`<script src="../javascript_compressed.js"></script>`);
-        document.write(`<script src="../lua_compressed.js"></script>`);
-        document.write(`<script src="../php_compressed.js"></script>`);
-        document.write(`<script src="../python_compressed.js"></script>`);
-        document.write(`<script src="../blocks_compressed.js"></script>`);
-        document.write(`<script src="../msg/messages.js"></script>`);
-    }
+} else {
+  document.write(
+      `<script src="../blockly_compressed.js" id="blockly-compressed-script"></script>`);
+  document.write(`<script src="../dart_compressed.js"></script>`);
+  document.write(`<script src="../javascript_compressed.js"></script>`);
+  document.write(`<script src="../lua_compressed.js"></script>`);
+  document.write(`<script src="../php_compressed.js"></script>`);
+  document.write(`<script src="../python_compressed.js"></script>`);
+  document.write(`<script src="../blocks_compressed.js"></script>`);
+  document.write(`<script src="../msg/messages.js"></script>`);
+}
 })();

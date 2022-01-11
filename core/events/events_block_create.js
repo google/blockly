08a8d42495c343d9b20893eb856c7f851c05580c
Rachel Fenichel <fenichel@google.com>
2022-01-11 14:44:21 -0800

    refactor: convert events/events_block_create.js to ES6 class

--- core/events/events_block_create.js
+++ core/events/events_block_create.js
@@ -18,7 +18,6 @@ goog.module('Blockly.Events.BlockCreate');
 const Xml = goog.require('Blockly.Xml');
 const blocks = goog.require('Blockly.serialization.blocks');
 const eventUtils = goog.require('Blockly.Events.utils');
-const object = goog.require('Blockly.utils.object');
 const registry = goog.require('Blockly.registry');
 const {BlockBase} = goog.require('Blockly.Events.BlockBase');
 /* eslint-disable-next-line no-unused-vars */
@@ -27,91 +26,93 @@ const {Block} = goog.requireType('Blockly.Block');
 
 /**
  * Class for a block creation event.
- * @param {!Block=} opt_block The created block.  Undefined for a blank
- *     event.
  * @extends {BlockBase}
- * @constructor
- * @alias Blockly.Events.BlockCreate
  */
-const BlockCreate = function(opt_block) {
-  BlockCreate.superClass_.constructor.call(this, opt_block);
+class BlockCreate extends BlockBase {
+  /**
+   * @param {!Block=} opt_block The created block.  Undefined for a blank
+   *     event.
+   * @alias Blockly.Events.BlockCreate
+   */
+  constructor(opt_block) {
+    super(opt_block);
+
+    /**
+     * Type of this event.
+     * @type {string}
+     */
+    this.type = eventUtils.BLOCK_CREATE;
+
+    if (!opt_block) {
+      return;  // Blank event to be populated by fromJson.
+    }
+    if (opt_block.isShadow()) {
+      // Moving shadow blocks is handled via disconnection.
+      this.recordUndo = false;
+    }
+
+    this.xml = Xml.blockToDomWithXY(opt_block);
+    this.ids = eventUtils.getDescendantIds(opt_block);
+
+    /**
+     * JSON representation of the block that was just created.
+     * @type {!blocks.State}
+     */
+    this.json = /** @type {!blocks.State} */ (
+        blocks.save(opt_block, {addCoordinates: true}));
+  }
 
   /**
-   * Type of this event.
-   * @type {string}
+   * Encode the event as JSON.
+   * @return {!Object} JSON representation.
    */
-  this.type = eventUtils.BLOCK_CREATE;
-
-  if (!opt_block) {
-    return;  // Blank event to be populated by fromJson.
+  toJson() {
+    const json = super.toJson();
+    json['xml'] = Xml.domToText(this.xml);
+    json['ids'] = this.ids;
+    json['json'] = this.json;
+    if (!this.recordUndo) {
+      json['recordUndo'] = this.recordUndo;
+    }
+    return json;
   }
-  if (opt_block.isShadow()) {
-    // Moving shadow blocks is handled via disconnection.
-    this.recordUndo = false;
-  }
-
-  this.xml = Xml.blockToDomWithXY(opt_block);
-  this.ids = eventUtils.getDescendantIds(opt_block);
 
   /**
-   * JSON representation of the block that was just created.
-   * @type {!blocks.State}
+   * Decode the JSON event.
+   * @param {!Object} json JSON representation.
    */
-  this.json = /** @type {!blocks.State} */ (
-      blocks.save(opt_block, {addCoordinates: true}));
-};
-object.inherits(BlockCreate, BlockBase);
-
-/**
- * Encode the event as JSON.
- * @return {!Object} JSON representation.
- */
-BlockCreate.prototype.toJson = function() {
-  const json = BlockCreate.superClass_.toJson.call(this);
-  json['xml'] = Xml.domToText(this.xml);
-  json['ids'] = this.ids;
-  json['json'] = this.json;
-  if (!this.recordUndo) {
-    json['recordUndo'] = this.recordUndo;
+  fromJson(json) {
+    super.fromJson(json);
+    this.xml = Xml.textToDom(json['xml']);
+    this.ids = json['ids'];
+    this.json = /** @type {!blocks.State} */ (json['json']);
+    if (json['recordUndo'] !== undefined) {
+      this.recordUndo = json['recordUndo'];
+    }
   }
-  return json;
-};
 
-/**
- * Decode the JSON event.
- * @param {!Object} json JSON representation.
- */
-BlockCreate.prototype.fromJson = function(json) {
-  BlockCreate.superClass_.fromJson.call(this, json);
-  this.xml = Xml.textToDom(json['xml']);
-  this.ids = json['ids'];
-  this.json = /** @type {!blocks.State} */ (json['json']);
-  if (json['recordUndo'] !== undefined) {
-    this.recordUndo = json['recordUndo'];
-  }
-};
-
-/**
- * Run a creation event.
- * @param {boolean} forward True if run forward, false if run backward (undo).
- */
-BlockCreate.prototype.run = function(forward) {
-  const workspace = this.getEventWorkspace_();
-  if (forward) {
-    blocks.append(this.json, workspace);
-  } else {
-    for (let i = 0; i < this.ids.length; i++) {
-      const id = this.ids[i];
-      const block = workspace.getBlockById(id);
-      if (block) {
-        block.dispose(false);
-      } else if (id === this.blockId) {
-        // Only complain about root-level block.
-        console.warn('Can\'t uncreate non-existent block: ' + id);
+  /**
+   * Run a creation event.
+   * @param {boolean} forward True if run forward, false if run backward (undo).
+   */
+  run(forward) {
+    const workspace = this.getEventWorkspace_();
+    if (forward) {
+      blocks.append(this.json, workspace);
+    } else {
+      for (let i = 0; i < this.ids.length; i++) {
+        const id = this.ids[i];
+        const block = workspace.getBlockById(id);
+        if (block) {
+          block.dispose(false);
+        } else if (id === this.blockId) {
+          // Only complain about root-level block.
+          console.warn('Can\'t uncreate non-existent block: ' + id);
+        }
       }
     }
   }
-};
+}
 
 registry.register(registry.Type.EVENT, eventUtils.CREATE, BlockCreate);
 

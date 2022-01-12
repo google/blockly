927596ffe8a4d4932ddc639ff54e1faba0442969
Rachel Fenichel <fenichel@google.com>
2022-01-11 17:29:06 -0800

    refactor: convert events/events_toolbox_item_select.js to ES6 class

--- core/events/events_toolbox_item_select.js
+++ core/events/events_toolbox_item_select.js
@@ -16,66 +16,67 @@
 goog.module('Blockly.Events.ToolboxItemSelect');
 
 const eventUtils = goog.require('Blockly.Events.utils');
-const object = goog.require('Blockly.utils.object');
 const registry = goog.require('Blockly.registry');
 const {UiBase} = goog.require('Blockly.Events.UiBase');
 
 
 /**
  * Class for a toolbox item select event.
- * @param {?string=} opt_oldItem The previously selected toolbox item. Undefined
- *    for a blank event.
- * @param {?string=} opt_newItem The newly selected toolbox item. Undefined for
- *    a blank event.
- * @param {string=} opt_workspaceId The workspace identifier for this event.
- *    Undefined for a blank event.
  * @extends {UiBase}
- * @constructor
- * @alias Blockly.Events.ToolboxItemSelect
  */
-const ToolboxItemSelect = function(opt_oldItem, opt_newItem, opt_workspaceId) {
-  ToolboxItemSelect.superClass_.constructor.call(this, opt_workspaceId);
+class ToolboxItemSelect extends UiBase {
+  /**
+   * @param {?string=} opt_oldItem The previously selected toolbox item. Undefined
+   *    for a blank event.
+   * @param {?string=} opt_newItem The newly selected toolbox item. Undefined for
+   *    a blank event.
+   * @param {string=} opt_workspaceId The workspace identifier for this event.
+   *    Undefined for a blank event.
+   * @alias Blockly.Events.ToolboxItemSelect
+   */
+  constructor(opt_oldItem, opt_newItem, opt_workspaceId) {
+    super(opt_workspaceId);
+
+    /**
+     * The previously selected toolbox item.
+     * @type {?string|undefined}
+     */
+    this.oldItem = opt_oldItem;
+
+    /**
+     * The newly selected toolbox item.
+     * @type {?string|undefined}
+     */
+    this.newItem = opt_newItem;
+
+    /**
+     * Type of this event.
+     * @type {string}
+     */
+    this.type = eventUtils.TOOLBOX_ITEM_SELECT;
+  }
 
   /**
-   * The previously selected toolbox item.
-   * @type {?string|undefined}
+   * Encode the event as JSON.
+   * @return {!Object} JSON representation.
    */
-  this.oldItem = opt_oldItem;
+  toJson() {
+    const json = super.toJson();
+    json['oldItem'] = this.oldItem;
+    json['newItem'] = this.newItem;
+    return json;
+  }
 
   /**
-   * The newly selected toolbox item.
-   * @type {?string|undefined}
+   * Decode the JSON event.
+   * @param {!Object} json JSON representation.
    */
-  this.newItem = opt_newItem;
-
-  /**
-   * Type of this event.
-   * @type {string}
-   */
-  this.type = eventUtils.TOOLBOX_ITEM_SELECT;
-};
-object.inherits(ToolboxItemSelect, UiBase);
-
-/**
- * Encode the event as JSON.
- * @return {!Object} JSON representation.
- */
-ToolboxItemSelect.prototype.toJson = function() {
-  const json = ToolboxItemSelect.superClass_.toJson.call(this);
-  json['oldItem'] = this.oldItem;
-  json['newItem'] = this.newItem;
-  return json;
-};
-
-/**
- * Decode the JSON event.
- * @param {!Object} json JSON representation.
- */
-ToolboxItemSelect.prototype.fromJson = function(json) {
-  ToolboxItemSelect.superClass_.fromJson.call(this, json);
-  this.oldItem = json['oldItem'];
-  this.newItem = json['newItem'];
-};
+  fromJson(json) {
+    super.fromJson(json);
+    this.oldItem = json['oldItem'];
+    this.newItem = json['newItem'];
+  }
+}
 
 registry.register(
     registry.Type.EVENT, eventUtils.TOOLBOX_ITEM_SELECT, ToolboxItemSelect);

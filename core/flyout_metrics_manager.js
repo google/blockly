c11897fa23a64ee9af2fe743f4d7b872e90b2fc1
Rachel Fenichel <fenichel@google.com>
2022-01-12 11:49:51 -0800

    refactor: convert flyout_metrics_manager.js to ES6 class

--- core/flyout_metrics_manager.js
+++ core/flyout_metrics_manager.js
@@ -15,7 +15,6 @@
  */
 goog.module('Blockly.FlyoutMetricsManager');
 
-const object = goog.require('Blockly.utils.object');
 /* eslint-disable-next-line no-unused-vars */
 const {IFlyout} = goog.requireType('Blockly.IFlyout');
 const {MetricsManager} = goog.require('Blockly.MetricsManager');
@@ -26,81 +25,83 @@ const {WorkspaceSvg} = goog.requireType('Blockly.WorkspaceSvg');
 /**
  * Calculates metrics for a flyout's workspace.
  * The metrics are mainly used to size scrollbars for the flyout.
- * @param {!WorkspaceSvg} workspace The flyout's workspace.
- * @param {!IFlyout} flyout The flyout.
  * @extends {MetricsManager}
- * @constructor
- * @alias Blockly.FlyoutMetricsManager
  */
-const FlyoutMetricsManager = function(workspace, flyout) {
+class FlyoutMetricsManager extends MetricsManager {
   /**
-   * The flyout that owns the workspace to calculate metrics for.
-   * @type {!IFlyout}
-   * @protected
+   * @param {!WorkspaceSvg} workspace The flyout's workspace.
+   * @param {!IFlyout} flyout The flyout.
+   * @alias Blockly.FlyoutMetricsManager
    */
-  this.flyout_ = flyout;
+  constructor(workspace, flyout) {
+    super(workspace);
 
-  FlyoutMetricsManager.superClass_.constructor.call(this, workspace);
-};
-object.inherits(FlyoutMetricsManager, MetricsManager);
-
-/**
- * Gets the bounding box of the blocks on the flyout's workspace.
- * This is in workspace coordinates.
- * @return {!SVGRect|{height: number, y: number, width: number, x: number}} The
- *     bounding box of the blocks on the workspace.
- * @private
- */
-FlyoutMetricsManager.prototype.getBoundingBox_ = function() {
-  let blockBoundingBox;
-  try {
-    blockBoundingBox = this.workspace_.getCanvas().getBBox();
-  } catch (e) {
-    // Firefox has trouble with hidden elements (Bug 528969).
-    // 2021 Update: It looks like this was fixed around Firefox 77 released in
-    // 2020.
-    blockBoundingBox = {height: 0, y: 0, width: 0, x: 0};
+    /**
+     * The flyout that owns the workspace to calculate metrics for.
+     * @type {!IFlyout}
+     * @protected
+     */
+    this.flyout_ = flyout;
   }
-  return blockBoundingBox;
-};
 
-/**
- * @override
- */
-FlyoutMetricsManager.prototype.getContentMetrics = function(
-    opt_getWorkspaceCoordinates) {
-  // The bounding box is in workspace coordinates.
-  const blockBoundingBox = this.getBoundingBox_();
-  const scale = opt_getWorkspaceCoordinates ? 1 : this.workspace_.scale;
+  /**
+   * Gets the bounding box of the blocks on the flyout's workspace.
+   * This is in workspace coordinates.
+   * @return {!SVGRect|{height: number, y: number, width: number, x: number}} The
+   *     bounding box of the blocks on the workspace.
+   * @private
+   */
+  getBoundingBox_() {
+    let blockBoundingBox;
+    try {
+      blockBoundingBox = this.workspace_.getCanvas().getBBox();
+    } catch (e) {
+      // Firefox has trouble with hidden elements (Bug 528969).
+      // 2021 Update: It looks like this was fixed around Firefox 77 released in
+      // 2020.
+      blockBoundingBox = {height: 0, y: 0, width: 0, x: 0};
+    }
+    return blockBoundingBox;
+  }
 
-  return {
-    height: blockBoundingBox.height * scale,
-    width: blockBoundingBox.width * scale,
-    top: blockBoundingBox.y * scale,
-    left: blockBoundingBox.x * scale,
-  };
-};
+  /**
+   * @override
+   */
+  getContentMetrics(
+      opt_getWorkspaceCoordinates) {
+    // The bounding box is in workspace coordinates.
+    const blockBoundingBox = this.getBoundingBox_();
+    const scale = opt_getWorkspaceCoordinates ? 1 : this.workspace_.scale;
 
-/**
- * @override
- */
-FlyoutMetricsManager.prototype.getScrollMetrics = function(
-    opt_getWorkspaceCoordinates, opt_viewMetrics, opt_contentMetrics) {
-  const contentMetrics = opt_contentMetrics || this.getContentMetrics();
-  const margin = this.flyout_.MARGIN * this.workspace_.scale;
-  const scale = opt_getWorkspaceCoordinates ? this.workspace_.scale : 1;
+    return {
+      height: blockBoundingBox.height * scale,
+      width: blockBoundingBox.width * scale,
+      top: blockBoundingBox.y * scale,
+      left: blockBoundingBox.x * scale,
+    };
+  }
 
-  // The left padding isn't just the margin. Some blocks are also offset by
-  // tabWidth so that value and statement blocks line up.
-  // The contentMetrics.left value is equivalent to the variable left padding.
-  const leftPadding = contentMetrics.left;
+  /**
+   * @override
+   */
+  getScrollMetrics(
+      opt_getWorkspaceCoordinates, opt_viewMetrics, opt_contentMetrics) {
+    const contentMetrics = opt_contentMetrics || this.getContentMetrics();
+    const margin = this.flyout_.MARGIN * this.workspace_.scale;
+    const scale = opt_getWorkspaceCoordinates ? this.workspace_.scale : 1;
 
-  return {
-    height: (contentMetrics.height + 2 * margin) / scale,
-    width: (contentMetrics.width + leftPadding + margin) / scale,
-    top: 0,
-    left: 0,
-  };
-};
+    // The left padding isn't just the margin. Some blocks are also offset by
+    // tabWidth so that value and statement blocks line up.
+    // The contentMetrics.left value is equivalent to the variable left padding.
+    const leftPadding = contentMetrics.left;
+
+    return {
+      height: (contentMetrics.height + 2 * margin) / scale,
+      width: (contentMetrics.width + leftPadding + margin) / scale,
+      top: 0,
+      left: 0,
+    };
+  }
+}
 
 exports.FlyoutMetricsManager = FlyoutMetricsManager;

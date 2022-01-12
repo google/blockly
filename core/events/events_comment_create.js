b402db77efaefcf85cd0f7d807ad4dcd623c812b
Rachel Fenichel <fenichel@google.com>
2022-01-11 16:59:45 -0800

    refactor: convert events/events_comment_create.js to ES6 class

--- core/events/events_comment_create.js
+++ core/events/events_comment_create.js
@@ -17,7 +17,6 @@ goog.module('Blockly.Events.CommentCreate');
 
 const Xml = goog.require('Blockly.Xml');
 const eventUtils = goog.require('Blockly.Events.utils');
-const object = goog.require('Blockly.utils.object');
 const registry = goog.require('Blockly.registry');
 const {CommentBase} = goog.require('Blockly.Events.CommentBase');
 /* eslint-disable-next-line no-unused-vars */
@@ -26,56 +25,58 @@ const {WorkspaceComment} = goog.requireType('Blockly.WorkspaceComment');
 
 /**
  * Class for a comment creation event.
- * @param {!WorkspaceComment=} opt_comment The created comment.
- *     Undefined for a blank event.
  * @extends {CommentBase}
- * @constructor
- * @alias Blockly.Events.CommentCreate
  */
-const CommentCreate = function(opt_comment) {
-  CommentCreate.superClass_.constructor.call(this, opt_comment);
-
+class CommentCreate extends CommentBase {
   /**
-   * Type of this event.
-   * @type {string}
+   * @param {!WorkspaceComment=} opt_comment The created comment.
+   *     Undefined for a blank event.
+   * @alias Blockly.Events.CommentCreate
    */
-  this.type = eventUtils.COMMENT_CREATE;
+  constructor(opt_comment) {
+    super(opt_comment);
 
-  if (!opt_comment) {
-    return;  // Blank event to be populated by fromJson.
+    /**
+     * Type of this event.
+     * @type {string}
+     */
+    this.type = eventUtils.COMMENT_CREATE;
+
+    if (!opt_comment) {
+      return;  // Blank event to be populated by fromJson.
+    }
+
+    this.xml = opt_comment.toXmlWithXY();
   }
 
-  this.xml = opt_comment.toXmlWithXY();
-};
-object.inherits(CommentCreate, CommentBase);
+  // TODO (#1266): "Full" and "minimal" serialization.
+  /**
+   * Encode the event as JSON.
+   * @return {!Object} JSON representation.
+   */
+  toJson() {
+    const json = super.toJson();
+    json['xml'] = Xml.domToText(this.xml);
+    return json;
+  }
 
-/**
- * Encode the event as JSON.
- * @return {!Object} JSON representation.
- */
-// TODO (#1266): "Full" and "minimal" serialization.
-CommentCreate.prototype.toJson = function() {
-  const json = CommentCreate.superClass_.toJson.call(this);
-  json['xml'] = Xml.domToText(this.xml);
-  return json;
-};
+  /**
+   * Decode the JSON event.
+   * @param {!Object} json JSON representation.
+   */
+  fromJson(json) {
+    super.fromJson(json);
+    this.xml = Xml.textToDom(json['xml']);
+  }
 
-/**
- * Decode the JSON event.
- * @param {!Object} json JSON representation.
- */
-CommentCreate.prototype.fromJson = function(json) {
-  CommentCreate.superClass_.fromJson.call(this, json);
-  this.xml = Xml.textToDom(json['xml']);
-};
-
-/**
- * Run a creation event.
- * @param {boolean} forward True if run forward, false if run backward (undo).
- */
-CommentCreate.prototype.run = function(forward) {
-  CommentBase.CommentCreateDeleteHelper(this, forward);
-};
+  /**
+   * Run a creation event.
+   * @param {boolean} forward True if run forward, false if run backward (undo).
+   */
+  run(forward) {
+    CommentBase.CommentCreateDeleteHelper(this, forward);
+  }
+}
 
 registry.register(
     registry.Type.EVENT, eventUtils.COMMENT_CREATE, CommentCreate);

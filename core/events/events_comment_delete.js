ea0cb809613240fc6d7ef6699edfe52b1ad92fdb
Rachel Fenichel <fenichel@google.com>
2022-01-11 17:03:56 -0800

    refactor: convert events/events_comment_delete.js to ES6 class

--- core/events/events_comment_delete.js
+++ core/events/events_comment_delete.js
@@ -16,7 +16,6 @@
 goog.module('Blockly.Events.CommentDelete');
 
 const eventUtils = goog.require('Blockly.Events.utils');
-const object = goog.require('Blockly.utils.object');
 const registry = goog.require('Blockly.registry');
 const {CommentBase} = goog.require('Blockly.Events.CommentBase');
 /* eslint-disable-next-line no-unused-vars */
@@ -25,54 +24,56 @@ const {WorkspaceComment} = goog.requireType('Blockly.WorkspaceComment');
 
 /**
  * Class for a comment deletion event.
- * @param {!WorkspaceComment=} opt_comment The deleted comment.
- *     Undefined for a blank event.
  * @extends {CommentBase}
- * @constructor
- * @alias Blockly.Events.CommentDelete
  */
-const CommentDelete = function(opt_comment) {
-  CommentDelete.superClass_.constructor.call(this, opt_comment);
-
+class CommentDelete extends CommentBase {
   /**
-   * Type of this event.
-   * @type {string}
+   * @param {!WorkspaceComment=} opt_comment The deleted comment.
+   *     Undefined for a blank event.
+   * @alias Blockly.Events.CommentDelete
    */
-  this.type = eventUtils.COMMENT_DELETE;
+  constructor(opt_comment) {
+    super(opt_comment);
 
-  if (!opt_comment) {
-    return;  // Blank event to be populated by fromJson.
+    /**
+     * Type of this event.
+     * @type {string}
+     */
+    this.type = eventUtils.COMMENT_DELETE;
+
+    if (!opt_comment) {
+      return;  // Blank event to be populated by fromJson.
+    }
+
+    this.xml = opt_comment.toXmlWithXY();
   }
 
-  this.xml = opt_comment.toXmlWithXY();
-};
-object.inherits(CommentDelete, CommentBase);
+  // TODO (#1266): "Full" and "minimal" serialization.
+  /**
+   * Encode the event as JSON.
+   * @return {!Object} JSON representation.
+   */
+  toJson() {
+    const json = super.toJson();
+    return json;
+  }
 
-/**
- * Encode the event as JSON.
- * @return {!Object} JSON representation.
- */
-// TODO (#1266): "Full" and "minimal" serialization.
-CommentDelete.prototype.toJson = function() {
-  const json = CommentDelete.superClass_.toJson.call(this);
-  return json;
-};
+  /**
+   * Decode the JSON event.
+   * @param {!Object} json JSON representation.
+   */
+  fromJson(json) {
+    super.fromJson(json);
+  }
 
-/**
- * Decode the JSON event.
- * @param {!Object} json JSON representation.
- */
-CommentDelete.prototype.fromJson = function(json) {
-  CommentDelete.superClass_.fromJson.call(this, json);
-};
-
-/**
- * Run a creation event.
- * @param {boolean} forward True if run forward, false if run backward (undo).
- */
-CommentDelete.prototype.run = function(forward) {
-  CommentBase.CommentCreateDeleteHelper(this, !forward);
-};
+  /**
+   * Run a creation event.
+   * @param {boolean} forward True if run forward, false if run backward (undo).
+   */
+  run(forward) {
+    CommentBase.CommentCreateDeleteHelper(this, !forward);
+  }
+}
 
 registry.register(
     registry.Type.EVENT, eventUtils.COMMENT_DELETE, CommentDelete);

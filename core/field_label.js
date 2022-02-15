14241f350413ebefb8fde918ece4d9c89dc442f2
Beka Westberg <bwestberg@google.com>
2022-02-15 20:52:04 +0000

    refact: run conversion script on misc fields

--- core/field_label.js
+++ core/field_label.js
@@ -26,107 +26,110 @@ const {Field} = goog.require('Blockly.Field');
 
 /**
  * Class for a non-editable, non-serializable text field.
- * @param {string=} opt_value The initial value of the field. Should cast to a
- *    string. Defaults to an empty string if null or undefined.
- * @param {string=} opt_class Optional CSS class for the field's text.
- * @param {Object=} opt_config A map of options used to configure the field.
- *    See the [field creation documentation]{@link
- * https://developers.google.com/blockly/guides/create-custom-blocks/fields/built-in-fields/label#creation}
- *    for a list of properties this parameter supports.
- * @extends {Field}
- * @constructor
- * @alias Blockly.FieldLabel
  */
-const FieldLabel = function(opt_value, opt_class, opt_config) {
-  FieldLabel.superClass_.constructor.call(this, opt_value, null, opt_config);
+var FieldLabel = class {
+  /**
+   * @param {string=} opt_value The initial value of the field. Should cast to a
+   *    string. Defaults to an empty string if null or undefined.
+   * @param {string=} opt_class Optional CSS class for the field's text.
+   * @param {Object=} opt_config A map of options used to configure the field.
+   *    See the [field creation documentation]{@link
+   * https://developers.google.com/blockly/guides/create-custom-blocks/fields/built-in-fields/label#creation}
+   *    for a list of properties this parameter supports.
+   * @extends {Field}
+   * @alias Blockly.FieldLabel
+   */
+  constructor(opt_value, opt_class, opt_config) {
+    FieldLabel.superClass_.constructor.call(this, opt_value, null, opt_config);
 
-  if (!this.value_) this.value_ = '';
+    if (!this.value_) this.value_ = '';
+
+    /**
+     * The html class name to use for this field.
+     * @type {?string}
+     * @private
+     */
+    this.class_ = null;
+
+    /**
+     * Editable fields usually show some sort of UI indicating they are
+     * editable. This field should not.
+     * @type {boolean}
+     */
+    this.EDITABLE = false;
+
+    if (!opt_config) {  // If the config was not passed use old configuration.
+      this.class_ = opt_class || null;
+    }
+  }
 
   /**
-   * The html class name to use for this field.
-   * @type {?string}
-   * @private
+   * @override
    */
-  this.class_ = null;
+  configure_(config) {
+    FieldLabel.superClass_.configure_.call(this, config);
+    this.class_ = config['class'];
+  }
 
   /**
-   * Editable fields usually show some sort of UI indicating they are
-   * editable. This field should not.
-   * @type {boolean}
+   * Create block UI for this label.
+   * @package
    */
-  this.EDITABLE = false;
+  initView() {
+    this.createTextElement_();
+    if (this.class_) {
+      dom.addClass(
+          /** @type {!SVGTextElement} */ (this.textElement_), this.class_);
+    }
+  }
 
-  if (!opt_config) {  // If the config was not passed use old configuration.
-    this.class_ = opt_class || null;
+  /**
+   * Ensure that the input value casts to a valid string.
+   * @param {*=} opt_newValue The input value.
+   * @return {?string} A valid string, or null if invalid.
+   * @protected
+   */
+  doClassValidation_(opt_newValue) {
+    if (opt_newValue === null || opt_newValue === undefined) {
+      return null;
+    }
+    return String(opt_newValue);
+  }
+
+  /**
+   * Set the CSS class applied to the field's textElement_.
+   * @param {?string} cssClass The new CSS class name, or null to remove.
+   */
+  setClass(cssClass) {
+    if (this.textElement_) {
+      // This check isn't necessary, but it's faster than letting removeClass
+      // figure it out.
+      if (this.class_) {
+        dom.removeClass(this.textElement_, this.class_);
+      }
+      if (cssClass) {
+        dom.addClass(this.textElement_, cssClass);
+      }
+    }
+    this.class_ = cssClass;
+  }
+
+  /**
+   * Construct a FieldLabel from a JSON arg object,
+   * dereferencing any string table references.
+   * @param {!Object} options A JSON object with options (text, and class).
+   * @return {!FieldLabel} The new field instance.
+   * @package
+   * @nocollapse
+   */
+  static fromJson(options) {
+    const text = parsing.replaceMessageReferences(options['text']);
+    // `this` might be a subclass of FieldLabel if that class doesn't override
+    // the static fromJson method.
+    return new this(text, undefined, options);
   }
 };
 object.inherits(FieldLabel, Field);
-
-/**
- * Construct a FieldLabel from a JSON arg object,
- * dereferencing any string table references.
- * @param {!Object} options A JSON object with options (text, and class).
- * @return {!FieldLabel} The new field instance.
- * @package
- * @nocollapse
- */
-FieldLabel.fromJson = function(options) {
-  const text = parsing.replaceMessageReferences(options['text']);
-  // `this` might be a subclass of FieldLabel if that class doesn't override
-  // the static fromJson method.
-  return new this(text, undefined, options);
-};
-
-/**
- * @override
- */
-FieldLabel.prototype.configure_ = function(config) {
-  FieldLabel.superClass_.configure_.call(this, config);
-  this.class_ = config['class'];
-};
-
-/**
- * Create block UI for this label.
- * @package
- */
-FieldLabel.prototype.initView = function() {
-  this.createTextElement_();
-  if (this.class_) {
-    dom.addClass(
-        /** @type {!SVGTextElement} */ (this.textElement_), this.class_);
-  }
-};
-
-/**
- * Ensure that the input value casts to a valid string.
- * @param {*=} opt_newValue The input value.
- * @return {?string} A valid string, or null if invalid.
- * @protected
- */
-FieldLabel.prototype.doClassValidation_ = function(opt_newValue) {
-  if (opt_newValue === null || opt_newValue === undefined) {
-    return null;
-  }
-  return String(opt_newValue);
-};
-
-/**
- * Set the CSS class applied to the field's textElement_.
- * @param {?string} cssClass The new CSS class name, or null to remove.
- */
-FieldLabel.prototype.setClass = function(cssClass) {
-  if (this.textElement_) {
-    // This check isn't necessary, but it's faster than letting removeClass
-    // figure it out.
-    if (this.class_) {
-      dom.removeClass(this.textElement_, this.class_);
-    }
-    if (cssClass) {
-      dom.addClass(this.textElement_, cssClass);
-    }
-  }
-  this.class_ = cssClass;
-};
 
 fieldRegistry.register('field_label', FieldLabel);
 

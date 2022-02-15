e693ecb8481f57ada4ca0f5f66bca5cd75c8e1b8
alschmiedt <aschmiedt@google.com>
2022-02-15 11:25:12 -0800

    chore: ran format

--- core/renderers/common/debug.js
+++ core/renderers/common/debug.js
@@ -7,51 +7,54 @@
 /**
  * @fileoverview Block rendering debugging functionality.
  */
- 'use strict';
+'use strict';
 
- /**
-  * Block rendering debugging functionality.
-  * @namespace Blockly.blockRendering.debug
-  */
- goog.module('Blockly.blockRendering.debug');
- 
- 
- /**
-  * Whether or not the debugger is turned on.
-  * @type {boolean}
-  */
- let useDebugger = false;
- /**
-  * Returns whether the debugger is turned on.
-  * @return {boolean} Whether the debugger is turned on.
-  * @alias Blockly.blockRendering.debug.isDebuggerEnabled
-  * @package
-  * @deprecated Remove September 2022.
-  */
- const isDebuggerEnabled = function() {
-   console.warn('Blockly.blockRendering.isDebuggerEnabled() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
-   return useDebugger;
- };
- exports.isDebuggerEnabled = isDebuggerEnabled;
- 
- /**
-  * Turn on the blocks debugger.
-  * @package
-  * @alias Blockly.blockRendering.debug.startDebugger
-  */
- const startDebugger = function() {
-   console.warn('Blockly.blockRendering.startDebugger() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
-   useDebugger = true;
- };
- exports.startDebugger = startDebugger;
- 
- /**
-  * Turn off the blocks debugger.
-  * @package
-  * @alias Blockly.blockRendering.debug.stopDebugger
-  */
- const stopDebugger = function() {
-   console.warn('Blockly.blockRendering.stopDebugger() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
-   useDebugger = false;
- };
- exports.stopDebugger = stopDebugger;
+/**
+ * Block rendering debugging functionality.
+ * @namespace Blockly.blockRendering.debug
+ */
+goog.module('Blockly.blockRendering.debug');
+
+
+/**
+ * Whether or not the debugger is turned on.
+ * @type {boolean}
+ */
+let useDebugger = false;
+/**
+ * Returns whether the debugger is turned on.
+ * @return {boolean} Whether the debugger is turned on.
+ * @alias Blockly.blockRendering.debug.isDebuggerEnabled
+ * @package
+ * @deprecated Remove September 2022.
+ */
+const isDebuggerEnabled = function() {
+  console.warn(
+      'Blockly.blockRendering.isDebuggerEnabled() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
+  return useDebugger;
+};
+exports.isDebuggerEnabled = isDebuggerEnabled;
+
+/**
+ * Turn on the blocks debugger.
+ * @package
+ * @alias Blockly.blockRendering.debug.startDebugger
+ */
+const startDebugger = function() {
+  console.warn(
+      'Blockly.blockRendering.startDebugger() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
+  useDebugger = true;
+};
+exports.startDebugger = startDebugger;
+
+/**
+ * Turn off the blocks debugger.
+ * @package
+ * @alias Blockly.blockRendering.debug.stopDebugger
+ */
+const stopDebugger = function() {
+  console.warn(
+      'Blockly.blockRendering.stopDebugger() functionality has been removed. Please see https://www.npmjs.com/package/@blockly/dev-tools for more information on how you can use the debug renderer.');
+  useDebugger = false;
+};
+exports.stopDebugger = stopDebugger;

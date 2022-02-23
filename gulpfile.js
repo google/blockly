672985bfbf0a74cb88ea22d9a012461febf8fdda
Christopher Allen <cpcallen+git@google.com>
2022-02-23 21:49:30 +0000

    refactor: Be more selective in eslint ignorance
    Rather than ignoring blockly_uncompressed.js, gulpfile.js and
    core/utils/global.js entirely, add suitable eslint-disable directives
    to those files so that they lint cleanly (and migrate gulpfile to
    use const instead of var).

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,17 @@
  * @fileoverview Gulp script to build Blockly for Node & NPM.
  * Run this script by calling "npm install" in this directory.
  */
+/* eslint-env node */
 
-var gulp = require('gulp');
+const gulp = require('gulp');
 
-var buildTasks = require('./scripts/gulpfiles/build_tasks');
-var packageTasks = require('./scripts/gulpfiles/package_tasks');
-var gitTasks = require('./scripts/gulpfiles/git_tasks');
-var licenseTasks = require('./scripts/gulpfiles/license_tasks');
-var appengineTasks = require('./scripts/gulpfiles/appengine_tasks');
-var releaseTasks = require('./scripts/gulpfiles/release_tasks');
-var cleanupTasks = require('./scripts/gulpfiles/cleanup_tasks');
+const buildTasks = require('./scripts/gulpfiles/build_tasks');
+const packageTasks = require('./scripts/gulpfiles/package_tasks');
+const gitTasks = require('./scripts/gulpfiles/git_tasks');
+const licenseTasks = require('./scripts/gulpfiles/license_tasks');
+const appengineTasks = require('./scripts/gulpfiles/appengine_tasks');
+const releaseTasks = require('./scripts/gulpfiles/release_tasks');
+const cleanupTasks = require('./scripts/gulpfiles/cleanup_tasks');
 
 module.exports = {
   deployDemos: appengineTasks.deployDemos,

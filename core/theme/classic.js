e791aeafd68d17f6f718bca4c37f28bcd8a05944
oboshto <oboshto@gmail.com>
2022-04-18 16:17:03 +0200

    fix argument_local_blocks colour fix zoomToFit in ZoomControls.prototype.resetZoom_ update package version

--- core/theme/classic.js
+++ core/theme/classic.js
@@ -31,7 +31,7 @@ const defaultBlockStyles = {
   'variable_blocks': {'colourPrimary': '330'},
   'variable_dynamic_blocks': {'colourPrimary': '310'},
   'hat_blocks': {'colourPrimary': '330', 'hat': 'cap'},
-  'argument_local_blocks': {'colourPrimary': '#a55b5b'},
+  'argument_local_blocks': {'colourPrimary': '#d37e0a'},
 };
 
 const categoryStyles = {

4fcb45ca5ccdbbcf0d0d20f452c08b7d9572a8ff
Beka Westberg <bwestberg@google.com>
2022-06-28 21:18:02 +0000

    fix: temporarily get the project to build

--- core/blockly.ts
+++ core/blockly.ts
@@ -179,17 +179,6 @@ import * as Xml from './xml.js';
 import {ZoomControls} from './zoom_controls.js';
 
 
-/**
- * Blockly core version.
- * This constant is overridden by the build script (npm run build) to the value
- * of the version in package.json. This is done by the Closure Compiler in the
- * buildCompressed gulp task.
- * For local builds, you can pass --define='Blockly.VERSION=X.Y.Z' to the
- * compiler to override this constant.
- * @define {string}
- * @alias Blockly.VERSION
- */
-export const VERSION = 'uncompiled';
 
 /*
  * Top-level functions and properties on the Blockly namespace.

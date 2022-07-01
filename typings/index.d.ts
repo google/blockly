a6b6adaf5c8e274b0a976ef5b07e13c75c83634b
Beka Westberg <bwestberg@google.com>
2022-07-01 21:14:34 +0000

    chore: make handcrafted files reference generated

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,6 +9,6 @@
  * @author samelh@google.com (Sam El-Husseini)
  */
 
-export * from './core';
-export * as libraryBlocks from './blocks';
+export * from './declarations/core/blockly';
+export * as libraryBlocks from './blocks';  // Handcrafted file.
 export const JavaScript: any;

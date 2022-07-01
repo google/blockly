0847e3af0eb0b9f384d634e9381c8c37afd4aaa0
Beka Westberg <bwestberg@google.com>
2022-07-01 19:27:53 +0000

    chore: copy generated type defs into dist dir

--- scripts/gulpfiles/package_tasks.js
+++ scripts/gulpfiles/package_tasks.js
@@ -389,8 +389,9 @@ function packageDTS() {
   const handwrittenSrcs = [
     'typings/*.d.ts',
     'typings/msg/msg.d.ts',
+    `${BUILD_DIR}/ts/**/*.d.ts`
   ];
-  return gulp.src(handwrittenSrcs, {base: 'typings'})
+  return gulp.src(handwrittenSrcs)
       .pipe(gulp.dest(RELEASE_DIR));
 };
 

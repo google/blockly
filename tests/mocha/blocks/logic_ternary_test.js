e9920a54e0b55fc654156b3765eb5a545d1804ab
Beka Westberg <bwestberg@google.com>
2022-08-16 22:49:40 +0000

    feat!: allow blocks to receive their own delete events (#6337)
    * feat!: allow blocks to receive their own delete events
    
    * fix: move block tests back into main directory
    
    * chore: add a test for disposing of callers
    
    * chore: add test for delete being received
    
    * chore: add comment about why we have to run the clock twice
    
    * chore: fix whitespace
    
    * chore: fix whitespace
    
    * chore: fix imports in tests
    
    * chore: bump mocha timeout
    
    * chore: bump timeout again?
    
    * chore: eliminate the possibility that tests are actually timing out
    
    * chore: change timeout back
    
    * chore: remove tests that might be the problematic ones
    
    * chore: attempt enabling delete event test
    
    * chore: enable lists tests
    
    * chore: try ternary test as well
    
    * chore: actually add block test files
    
    * chore: enable remaining tests

--- tests/mocha/blocks/logic_ternary_test.js
+++ tests/mocha/blocks/logic_ternary_test.js
@@ -6,7 +6,7 @@
 
 goog.declareModuleId('Blockly.test.logicTernary');
 
-import * as eventUtils from '../../build/src/core/events/utils.js';
+import * as eventUtils from '../../../build/src/core/events/utils.js';
 import {runSerializationTestSuite} from '../test_helpers/serialization.js';
 import {sharedTestSetup, sharedTestTeardown} from '../test_helpers/setup_teardown.js';
 

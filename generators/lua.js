224af0aa6bb17b3b6e2c7cc7c01daae75ccc7122
Maribeth Bottorff <maribethb@google.com>
2022-10-06 13:05:00 -0700

    fix: remove deprecated use of objectUtils from generators (#6499)

--- generators/lua.js
+++ generators/lua.js
@@ -13,7 +13,6 @@
 
 goog.module('Blockly.Lua');
 
-const objectUtils = goog.require('Blockly.utils.object');
 const stringUtils = goog.require('Blockly.utils.string');
 const {Block} = goog.requireType('Blockly.Block');
 const {Generator} = goog.require('Blockly.Generator');
@@ -115,7 +114,7 @@ Lua.init = function(workspace) {
  */
 Lua.finish = function(code) {
   // Convert the definitions dictionary into a list.
-  const definitions = objectUtils.values(this.definitions_);
+  const definitions = Object.values(this.definitions_);
   // Call Blockly.Generator's finish.
   code = Object.getPrototypeOf(this).finish.call(this, code);
   this.isInitialized = false;

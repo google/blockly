c773c210a98cdb99d109f0f74c793a686cfa0d53
Beka Westberg <bwestberg@google.com>
2022-10-11 22:18:33 +0000

    chore: update validatedBlockStyle_ to use Partial

--- core/renderers/common/constants.ts
+++ core/renderers/common/constants.ts
@@ -658,12 +658,7 @@ export class ConstantProvider {
    * @param blockStyle A full or partial block style object.
    * @returns A full block style object, with all required properties populated.
    */
-  protected validatedBlockStyle_(blockStyle: {
-    colourPrimary: string,
-    colourSecondary?: string,
-    colourTertiary?: string,
-    hat?: string
-  }): BlockStyle {
+  protected validatedBlockStyle_(blockStyle: Partial<BlockStyle>): BlockStyle {
     // Make a new object with all of the same properties.
     const valid = {} as BlockStyle;
     if (blockStyle) {

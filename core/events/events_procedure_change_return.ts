dd61a1d3c5ff3c4ac5e3a35f0093e457774be0ba
Beka Westberg <bwestberg@google.com>
2022-11-01 23:05:26 +0000

    feat: add running procedure change return events

--- core/events/events_procedure_change_return.ts
+++ core/events/events_procedure_change_return.ts
@@ -14,10 +14,28 @@ import * as eventUtils from './utils.js';
 
 
 export class ProcedureChangeReturn extends ProcedureBase {
+  private newTypes: string[]|null;
+
   constructor(
       workpace: Workspace, model: IProcedureModel,
       public readonly oldTypes: string[]|null) {
     super(workpace, model);
+
+    this.newTypes = model.getReturnTypes();
+  }
+
+  run(forward: boolean) {
+    const procedureModel =
+        this.getEventWorkspace_().getProcedureMap().get(this.model.getId());
+    if (!procedureModel) {
+      throw new Error(
+          'Cannot rename a procedure that does not exist in the procedure map');
+    }
+    if (forward) {
+      procedureModel.setReturnTypes(this.newTypes);
+    } else {
+      procedureModel.setReturnTypes(this.oldTypes);
+    }
   }
 }
 

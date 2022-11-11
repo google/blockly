98116935795dfb8c25254a787744c9ca757b98fd
Blake T. Williams <blaketw@google.com>
2022-11-11 16:26:39 +0000

    chore: added pass-through constructor to field_textinput

--- core/field_textinput.ts
+++ core/field_textinput.ts
@@ -18,10 +18,31 @@ import './events/events_block_change.js';
 import {FieldInput, FieldInputConfig, FieldInputValidator} from './field_input.js';
 import * as fieldRegistry from './field_registry.js';
 import * as parsing from './utils/parsing.js';
+import type {Sentinel} from './utils/sentinel.js';
 
 export type FieldTextInputValidator = FieldInputValidator<string>;
 
 export class FieldTextInput extends FieldInput<string> {
+  /**
+   * @param opt_value The initial value of the field. Should cast to a string.
+   *     Defaults to an empty string if null or undefined. Also accepts
+   *     Field.SKIP_SETUP if you wish to skip setup (only used by subclasses
+   *     that want to handle configuration and setting the field value after
+   *     their own constructors have run).
+   * @param opt_validator A function that is called to validate changes to the
+   *     field's value. Takes in a string & returns a validated string, or null
+   *     to abort the change.
+   * @param opt_config A map of options used to configure the field.
+   *     See the [field creation documentation]{@link
+   * https://developers.google.com/blockly/guides/create-custom-blocks/fields/built-in-fields/text-input#creation}
+   * for a list of properties this parameter supports.
+   */
+  constructor(
+      opt_value?: string|Sentinel, opt_validator?: FieldTextInputValidator|null,
+      opt_config?: FieldInputConfig) {
+    super(opt_value, opt_validator, opt_config);
+  }
+
   /**
    * Construct a FieldTextInput from a JSON arg object,
    * dereferencing any string table references.

f2f9d77b06074d81128e0b393b220a927651a02b
Beka Westberg <bwestberg@google.com>
2022-11-11 19:48:01 +0000

    feat: add serialization to procedure base event

--- core/events/events_procedure_base.ts
+++ core/events/events_procedure_base.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {Abstract as AbstractEvent} from './events_abstract.js';
+import {Abstract as AbstractEvent, AbstractEventJson} from './events_abstract.js';
 import type {IProcedureModel} from '../interfaces/i_procedure_model.js';
 import type {Workspace} from '../workspace.js';
 
@@ -12,11 +12,26 @@ import type {Workspace} from '../workspace.js';
 /**
  * The base event for an event associated with a procedure.
  */
-export class ProcedureBase extends AbstractEvent {
+export abstract class ProcedureBase extends AbstractEvent {
   isBlank = false;
 
   constructor(workspace: Workspace, public readonly model: IProcedureModel) {
     super();
     this.workspaceId = workspace.id;
   }
+
+  /**
+   * Encode the event as JSON.
+   *
+   * @returns JSON representation.
+   */
+  toJson(): ProcedureBaseJson {
+    const json = super.toJson() as ProcedureBaseJson;
+    json['procedureId'] = this.model.getId();
+    return json;
+  }
+}
+
+export interface ProcedureBaseJson extends AbstractEventJson {
+  procedureId: string;
 }

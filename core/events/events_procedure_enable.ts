6ddbd1628db7bfd70e5355cd84624e9be0c425e0
Beka Westberg <bwestberg@google.com>
2022-11-11 20:12:47 +0000

    feat: add serialization of procedure enable events

--- core/events/events_procedure_enable.ts
+++ core/events/events_procedure_enable.ts
@@ -9,7 +9,7 @@ import type {IProcedureModel} from '../interfaces/i_procedure_model.js';
 import * as registry from '../registry.js';
 import type {Workspace} from '../workspace.js';
 
-import {ProcedureBase} from './events_procedure_base.js';
+import {ProcedureBase, ProcedureBaseJson} from './events_procedure_base.js';
 import * as eventUtils from './utils.js';
 
 /**
@@ -43,7 +43,34 @@ export class ProcedureEnable extends ProcedureBase {
       procedureModel.setEnabled(this.oldState);
     }
   }
+
+  /**
+   * Encode the event as JSON.
+   *
+   * @returns JSON representation.
+   */
+  toJson(): ProcedureEnableJson {
+    return super.toJson() as ProcedureEnableJson;
+  }
+
+  /**
+   * Deserializes the JSON event.
+   *
+   * @internal
+   */
+  static fromJson(json: ProcedureEnableJson, workspace: Workspace):
+      ProcedureEnable {
+    const model = workspace.getProcedureMap().get(json['procedureId']);
+    if (!model) {
+      throw new Error(
+          'Cannot deserialize procedure enable event because the ' +
+          'target procedure does not exist');
+    }
+    return new ProcedureEnable(workspace, model);
+  }
 }
+
+export interface ProcedureEnableJson extends ProcedureBaseJson { }
 
 registry.register(
     registry.Type.EVENT, eventUtils.PROCEDURE_ENABLE, ProcedureEnable);

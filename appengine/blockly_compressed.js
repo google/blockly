42f722b48f0e6899f6a4383cce1505fdee5ff747
Neil Fraser <fraser@google.com>
2022-11-30 00:59:52 +0100

    docs: Add notice to use unpkg instead of demos.
    Logs show about a dozen orgs actively using Blockly instances that are hot-linked to blockly-demo.appspot.com
    
    These files disappeared on 25 November 2022, breaking a bunch of people.  This alert should point developers in the right direction.

---
+++ appengine/blockly_compressed.js
@@ -0,0 +1,5 @@
+// Added November 2022 after discovering that a number of orgs were hot-linking
+// their Blockly applications to https://blockly-demo.appspot.com/
+// Delete this file in early 2024.
+alert('Compiled Blockly files should be loaded from https://unpkg.com/blockly/\n' +
+    'For help, contact https://groups.google.com/g/blockly');

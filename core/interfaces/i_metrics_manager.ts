84f67f302903cd6df37bb16854e871c559fe2867
Maribeth Bottorff <maribethb@google.com>
2023-01-10 13:43:47 -0800

    fix: correct docs for flyout metrics

--- core/interfaces/i_metrics_manager.ts
+++ core/interfaces/i_metrics_manager.ts
@@ -47,11 +47,14 @@ export interface IMetricsManager {
       opt_contentMetrics?: ContainerRegion): ContainerRegion;
 
   /**
-   * Gets the width and the height of the flyout on the workspace in pixel
-   * coordinates. Returns 0 for the width and height if the workspace has a
-   * category toolbox instead of a simple toolbox.
+   * Gets the width and the height of the flyout in pixel
+   * coordinates. By default, will get metrics for either a simple flyout (owned
+   * directly by the workspace) or for the flyout owned by the toolbox. If you
+   * pass `opt_own` as `true` then only metrics for the simple flyout will be
+   * returned, and it will return 0 for the width and height if the workspace
+   * has a category toolbox instead of a simple toolbox.
    *
-   * @param opt_own Whether to only return the workspace's own flyout.
+   * @param opt_own Whether to only return the workspace's own flyout metrics.
    * @returns The width and height of the flyout.
    */
   getFlyoutMetrics(opt_own?: boolean): ToolboxMetrics;
@@ -60,8 +63,7 @@ export interface IMetricsManager {
    * Gets the width, height and position of the toolbox on the workspace in
    * pixel coordinates. Returns 0 for the width and height if the workspace has
    * a simple toolbox instead of a category toolbox. To get the width and height
-   * of a
-   * simple toolbox @see {@link IMetricsManager#getFlyoutMetrics}.
+   * of a simple toolbox, see {@link IMetricsManager#getFlyoutMetrics}.
    *
    * @returns The object with the width, height and position of the toolbox.
    */

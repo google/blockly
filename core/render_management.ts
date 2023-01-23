808a77bae22f2a71a4fafff3e271bbd74f0c7a42
Beka Westberg <bwestberg@google.com>
2023-01-23 22:20:59 +0000

    feat: add implementation of the block tree

--- core/render_management.ts
+++ core/render_management.ts
@@ -4,33 +4,71 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as goog from '../closure/goog/goog.js';
+goog.declareModuleId('Blockly.renderManagement');
+
 import {BlockSvg} from './block_svg';
+
 
 /**
  * @internal
  */
 export class BlockTree {
-  add(block: BlockSvg) {}
+  private root: Node = new Node();
+  private map: Map<string, Node> = new Map();
 
-  remove(block: BlockSvg) {}
-
-  has(block: BlockSvg): boolean {
-    return false;
+  add(block: BlockSvg) {
+    this.addRec(block);
   }
 
-  getNodeFor(block: BlockSvg): Node {
-    return new Node();
+  private addRec(block: BlockSvg): Node {
+    if (this.map.has(block.id)) return this.map.get(block.id)!;
+    const parent = block.getParent();
+    const parentNode = parent ? this.addRec(parent) : this.root;
+    const node = new Node(block, parentNode);
+    parentNode.children.push(node);
+    this.map.set(block.id, node);
+    return node;
+  }
+
+  remove(block: BlockSvg) {
+    const node = this.getNodeFor(block);
+    if (!node) return;
+    this.removeRec(node);
+  }
+  
+  private removeRec(node: Node) {
+    if (node.block) this.map.delete(node.block.id);
+
+    const parentNode = node.parent;
+    // Should not happen, because only the root has no parent.
+    if (!parentNode) return;
+    parentNode.children.splice(parentNode.children.indexOf(node), 1);
+
+    for (const child of node.children) {
+      this.removeRec(child);
+    }
+  }
+
+  has(block: BlockSvg): boolean {
+    return this.map.has(block.id);
+  }
+
+  getNodeFor(block: BlockSvg): Node|undefined {
+    return this.map.get(block.id);
   }
 
   getRootNode(): Node {
-    return new Node();
+    return this.root;
   }
 
-  clear() {}
+  clear() {
+    this.root = new Node();
+  }
 }
 
 export class Node {
   constructor(
       public readonly block?: BlockSvg, public readonly parent?: Node,
-      public readonly children: [] = []) {}
+      public readonly children: Node[] = []) {}
 }

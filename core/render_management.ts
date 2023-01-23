b6fe10c265e17d593e5b753023b4fec83daee476
Beka Westberg <bwestberg@google.com>
2023-01-23 22:28:08 +0000

    chore: add inline docs

--- core/render_management.ts
+++ core/render_management.ts
@@ -11,16 +11,21 @@ import {BlockSvg} from './block_svg';
 
 
 /**
+ * Constructs a subforest of blocks on the workspace when blocks are
+ * added to it.
+ *
  * @internal
  */
 export class BlockTree {
   private root: Node = new Node();
   private map: Map<string, Node> = new Map();
 
+  /** Adds the given block and all parent blocks to the tree. */
   add(block: BlockSvg) {
     this.addRec(block);
   }
 
+  /** Recursively adds the given block and all parent blocks to the tree. */
   private addRec(block: BlockSvg): Node {
     if (this.map.has(block.id)) return this.map.get(block.id)!;
     const parent = block.getParent();
@@ -31,12 +36,14 @@ export class BlockTree {
     return node;
   }
 
+  /** Removes the given block and all children from the tree. */
   remove(block: BlockSvg) {
     const node = this.getNodeFor(block);
     if (!node) return;
     this.removeRec(node);
   }
   
+  /** Recursively removes the given node and all children from the tree. */
   private removeRec(node: Node) {
     if (node.block) this.map.delete(node.block.id);
 
@@ -50,23 +57,30 @@ export class BlockTree {
     }
   }
 
+  /** @returns True if the tree contains the given block, false otherwise. */
   has(block: BlockSvg): boolean {
     return this.map.has(block.id);
   }
 
+  /** @returns The node associated with the given block. */
   getNodeFor(block: BlockSvg): Node|undefined {
     return this.map.get(block.id);
   }
 
+  /** @returns The root node of the tree. */
   getRootNode(): Node {
     return this.root;
   }
 
+  /** Removes all nodes from the tree. */
   clear() {
     this.root = new Node();
   }
 }
 
+/**
+ * @internal
+ */
 export class Node {
   constructor(
       public readonly block?: BlockSvg, public readonly parent?: Node,

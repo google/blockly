f6a18109678b9f393d5d085856459b2132550b5f
Beka Westberg <bwestberg@google.com>
2023-03-21 22:18:17 +0000

    chore: actually fix node

--- core/events/utils.ts
+++ core/events/utils.ts
@@ -220,14 +220,15 @@ function fireInternal(event: Abstract) {
   }
   if (!FIRE_QUEUE.length) {
     // First event added; schedule a firing of the event queue.
-    if (requestAnimationFrame) {
+    try {
       // If we are in a browser context, we want to make sure that the event
       // fires after blocks have been rerendered this frame.
       requestAnimationFrame(() => {
         setTimeout(fireNow, 0);
       });
-    } else {
+    } catch (e) {
       // Otherwise we just want to delay so events can be coallesced.
+      // requestAnimationFrame will error triggering this.
       setTimeout(fireNow, 0);
     }
   }

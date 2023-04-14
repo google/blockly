f2ed3b67a914f0f285cf6e3cfaaf44eb3079ba9a
Beka Westberg <bwestberg@google.com>
2023-04-14 16:22:37 +0000

    chore: add tests for resolving render management promises

---
+++ tests/mocha/render_management_test.js
@@ -0,0 +1,64 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+goog.declareModuleId('Blockly.test.renderManagement');
+
+import {sharedTestSetup, sharedTestTeardown} from './test_helpers/setup_teardown.js';
+
+
+suite('Render Management', function() {
+  setup(function() {
+    this.clock = sharedTestSetup.call(this).clock;
+  });
+
+  teardown(function() {
+    sharedTestTeardown.call(this);
+  });
+
+  suite('after queued renders callback', function() {
+    function createMockBlock() {
+      return {
+        hasRendered: false,
+        renderEfficiently: function() {this.hasRendered = true;},
+
+        // All of the APIs the render management system needs.
+        getParent: () => null,
+        getChildren: () => [],
+        isDisposed: () => false,
+        getConnections_: () => [],
+        getRelativeToSurfaceXY: () => ({x: 0, y: 0}),
+        workspace: {
+          resizeContents: () => {},
+        },
+      };
+    }
+
+    test('the queueRender promise is properly resolved after rendering',
+        function() {
+          const block = createMockBlock();
+          const promise = Blockly.renderManagement.queueRender(block)
+            .then(() => {
+              chai.assert.isTrue(
+                  block.hasRendered, 'Expected block to be rendered');
+             });
+          this.clock.runAll();
+          return promise;
+        });
+
+    test(
+        'the after queued renders promise is properly resolved after rendering',
+        function() {
+          const block = createMockBlock();
+          Blockly.renderManagement.queueRender(block);
+          const promise = Blockly.renderManagement.afterQueuedRenders(() => {
+              chai.assert.isTrue(
+                  block.hasRendered, 'Expected block to be rendered');
+             });
+          this.clock.runAll();
+          return promise;
+        });
+  });
+});

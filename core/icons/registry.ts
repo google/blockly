0cfe57786368df5ad5a9d6f8cd40495f6b33a7b1
Beka Westberg <bwestberg@google.com>
2023-05-09 23:34:12 +0000

    chore: fix doc error

--- core/icons/registry.ts
+++ core/icons/registry.ts
@@ -13,7 +13,7 @@ import * as registry from '../registry.js';
  *
  * @param type The type of the icon to register. This should be the same string
  *     that is returned from its `getType` method.
- * @param icon The icon to register.
+ * @param iconConstructor The icon class/constructor to register.
  */
 export function register(
   type: string,

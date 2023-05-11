ac2ef6d12154db54121470910912b8c7de556dd3
Rachel Fenichel <fenichel@google.com>
2023-05-11 13:45:06 -0700

    chore: remove mixin deprecation

--- core/utils/object.ts
+++ core/utils/object.ts
@@ -7,27 +7,6 @@
 import * as goog from '../../closure/goog/goog.js';
 goog.declareModuleId('Blockly.utils.object');
 
-import * as deprecation from './deprecation.js';
-
-/**
- * Copies all the members of a source object to a target object.
- *
- * @param target Target.
- * @param source Source.
- * @deprecated Use the built-in **Object.assign** instead.
- */
-export function mixin(target: AnyDuringMigration, source: AnyDuringMigration) {
-  deprecation.warn(
-    'Blockly.utils.object.mixin',
-    'May 2022',
-    'May 2023',
-    'Object.assign'
-  );
-  for (const x in source) {
-    target[x] = source[x];
-  }
-}
-
 /**
  * Complete a deep merge of all members of a source object with a target object.
  *

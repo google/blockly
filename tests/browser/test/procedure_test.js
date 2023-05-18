628a7a22c93ebbd46675aeeb8a8c2bcc13fc0729
Eric Blackmon <ericblackmon@google.com>
2023-05-18 13:53:33 -0400

    chore: test for procedures

--- tests/browser/test/procedure_test.js
+++ tests/browser/test/procedure_test.js
@@ -46,7 +46,9 @@ suite('Testing Connecting Blocks', function (done) {
     }
     // Use Selenium to bring up the page
     const url =
-      'https://rachel-fenichel.github.io/blockly/demos/code/index.html';
+      'file://' +
+      posixPath(path.join(__dirname, '..', '..', '..', 'demos', 'code')) +
+      '/index.html';
     console.log(url);
     console.log('Starting webdriverio...');
     browser = await webdriverio.remote(options);
@@ -57,50 +59,50 @@ suite('Testing Connecting Blocks', function (done) {
 
   test('Testing Procedure', async function () {
     // Drag out first function
-    const functionButton = await browser.$('#blockly\\:9 > div.blocklyTreeRow');
+    const functionCategory = await browser.$('#blockly-8');
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-    functionButton.click();
-    let toReturnBlock = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(4)'
+    functionCategory.click();
+    let proceduresDefReturn = await browser.$(
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(5)'
     );
-    await toReturnBlock.dragAndDrop({x: 50, y: 20});
+    await proceduresDefReturn.dragAndDrop({x: 50, y: 20});
 
     // Drag out second function
-    functionButton.click();
+    functionCategory.click();
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-    toReturnBlock = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(4)'
+    proceduresDefReturn = await browser.$(
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(5)'
     );
-    await toReturnBlock.dragAndDrop({x: 300, y: 200});
+    await proceduresDefReturn.dragAndDrop({x: 300, y: 200});
 
     // Drag out numeric
-    const mathButton = await browser.$('#blockly\\:3 > div.blocklyTreeRow');
-    mathButton.click();
+    const mathCategory = await browser.$('#blockly-2');
+    mathCategory.click();
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-    const numericBlock = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(2)'
+    const mathNumeric = await browser.$(
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(3)'
     );
-    await numericBlock.dragAndDrop({x: 50, y: 20});
-
+    await mathNumeric.dragAndDrop({x: 50, y: 20});
     // Connect numeric to first procedure
     const numericWorkspace = await browser.$(
       '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected'
     );
     const doSomething = await browser.$(
-      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(1)'
+      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(2)'
     );
     await numericWorkspace.dragAndDrop(doSomething);
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-    await numericWorkspace.dragAndDrop({x: 90, y: 20});
+    await numericWorkspace.dragAndDrop({x: 100, y: 25});
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
 
     // Drag out doSomething from flyout and connect it to doSomething2
     const doSomething2 = await browser.$(
-      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(1)'
+      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(2)'
     );
-    functionButton.click();
+    functionCategory.click();
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
     const doSomethingFlyout = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(8)'
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(9)'
     );
     await doSomethingFlyout.dragAndDrop(doSomething2);
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
@@ -110,18 +112,18 @@ suite('Testing Connecting Blocks', function (done) {
     await doSomethingFlyoutWorkspace.dragAndDrop({x: 130, y: 20});
 
     // Drag out print from flyout and connect it with doSomething 2
-    const textButton = await browser.$('#blockly\\:4 > div.blocklyTreeRow');
+    const textButton = await browser.$('#blockly-3');
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
     textButton.click();
     const printFlyout = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(22)'
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(23)'
     );
     await printFlyout.dragAndDrop({x: 50, y: 20});
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-    functionButton.click();
+    functionCategory.click();
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
     const doSomething2Flyout = await browser.$(
-      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(10)'
+      '#content_blocks > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(11)'
     );
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
     await doSomething2Flyout.dragAndDrop({x: 130, y: 20});
@@ -130,7 +132,7 @@ suite('Testing Connecting Blocks', function (done) {
       '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected'
     );
     const printWorkSpace = await browser.$(
-      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(3)'
+      '#content_blocks > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g:nth-child(4)'
     );
     await doSomething2FlyoutWorkspace.dragAndDrop(printWorkSpace);
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec

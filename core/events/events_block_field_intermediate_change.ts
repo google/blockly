0eca869eef05103a929e427017455659183244ed
John Nesky <nesky@google.com>
2023-05-19 17:08:43 -0700

    New intermediate event type for field edits.

--- core/events/events_block_field_intermediate_change.ts
+++ core/events/events_block_field_intermediate_change.ts
@@ -1,16 +1,17 @@
 /**
  * @license
- * Copyright 2018 Google LLC
+ * Copyright 2023 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /**
- * Class for a block change event.
+ * Class for an event representing an intermediate change to a block's field's
+ * value.
  *
  * @class
  */
 import * as goog from '../../closure/goog/goog.js';
-goog.declareModuleId('Blockly.Events.BlockChange');
+goog.declareModuleId('Blockly.Events.BlockFieldIntermediateChange');
 
 import type {Block} from '../block.js';
 import type {BlockSvg} from '../block_svg.js';
@@ -19,22 +20,22 @@ import * as utilsXml from '../utils/xml.js';
 import {Workspace} from '../workspace.js';
 import * as Xml from '../xml.js';
 
-import {BlockBase, BlockBaseJson} from './events_block_base.js';
+import {AbstractEventJson} from './events_abstract.js';
+import {UiBase} from './events_ui_base.js';
 import * as eventUtils from './utils.js';
 
 /**
- * Notifies listeners when some element of a block has changed (e.g.
- * field values, comments, etc).
+ * Notifies listeners when the value of a block's field has changed but the
+ * change is not yet complete, and is expected to be followed by a block change
+ * event.
  */
-export class BlockChange extends BlockBase {
-  override type = eventUtils.BLOCK_CHANGE;
-  /**
-   * The element that changed; one of 'field', 'comment', 'collapsed',
-   * 'disabled', 'inline', or 'mutation'
-   */
-  element?: string;
+export class BlockFieldIntermediateChange extends UiBase {
+  override type = eventUtils.BLOCK_FIELD_INTERMEDIATE_CHANGE;
 
-  /** The name of the field that changed, if this is a change to a field. */
+  /** The ID of the block associated with this event. */
+  blockId?: string;
+
+  /** The name of the field that changed. */
   name?: string;
 
   /** The original value of the element. */
@@ -44,26 +45,25 @@ export class BlockChange extends BlockBase {
   newValue: unknown;
 
   /**
-   * @param opt_block The changed block.  Undefined for a blank event.
-   * @param opt_element One of 'field', 'comment', 'disabled', etc.
-   * @param opt_name Name of input or field affected, or null.
+   * @param opt_block The changed block. Undefined for a blank event.
+   * @param opt_name Name of the field affected.
    * @param opt_oldValue Previous value of element.
    * @param opt_newValue New value of element.
    */
   constructor(
     opt_block?: Block,
-    opt_element?: string,
-    opt_name?: string | null,
+    opt_name?: string,
     opt_oldValue?: unknown,
     opt_newValue?: unknown
   ) {
-    super(opt_block);
-
+    const workspaceId = opt_block ? opt_block.workspace.id : undefined;
+    super(workspaceId);
     if (!opt_block) {
       return; // Blank event to be populated by fromJson.
     }
-    this.element = opt_element;
-    this.name = opt_name || undefined;
+    
+    this.blockId = opt_block.id;
+    this.name = opt_name;
     this.oldValue = opt_oldValue;
     this.newValue = opt_newValue;
   }
@@ -73,15 +73,21 @@ export class BlockChange extends BlockBase {
    *
    * @returns JSON representation.
    */
-  override toJson(): BlockChangeJson {
-    const json = super.toJson() as BlockChangeJson;
-    if (!this.element) {
+  override toJson(): BlockFieldIntermediateChangeJson {
+    const json = super.toJson() as BlockFieldIntermediateChangeJson;
+    if (!this.blockId) {
       throw new Error(
-        'The changed element is undefined. Either pass an ' +
-          'element to the constructor, or call fromJson'
+        'The block ID is undefined. Either pass a block to ' +
+          'the constructor, or call fromJson.'
       );
     }
-    json['element'] = this.element;
+    if (!this.name) {
+      throw new Error(
+        'The changed field name is undefined. Either pass a ' +
+          'name to the constructor, or call fromJson.'
+      );
+    }
+    json['blockId'] = this.blockId;
     json['name'] = this.name;
     json['oldValue'] = this.oldValue;
     json['newValue'] = this.newValue;
@@ -92,22 +98,22 @@ export class BlockChange extends BlockBase {
    * Deserializes the JSON event.
    *
    * @param event The event to append new properties to. Should be a subclass
-   *     of BlockChange, but we can't specify that due to the fact that
-   *     parameters to static methods in subclasses must be supertypes of
-   *     parameters to static methods in superclasses.
+   *     of BlockFieldIntermediateChange, but we can't specify that due to the
+   *     fact that parameters to static methods in subclasses must be supertypes
+   *     of parameters to static methods in superclasses.
    * @internal
    */
   static fromJson(
-    json: BlockChangeJson,
+    json: BlockFieldIntermediateChangeJson,
     workspace: Workspace,
     event?: any
-  ): BlockChange {
+  ): BlockFieldIntermediateChange {
     const newEvent = super.fromJson(
       json,
       workspace,
-      event ?? new BlockChange()
-    ) as BlockChange;
-    newEvent.element = json['element'];
+      event ?? new BlockFieldIntermediateChange()
+    ) as BlockFieldIntermediateChange;
+    newEvent.blockId = json['blockId'];
     newEvent.name = json['name'];
     newEvent.oldValue = json['oldValue'];
     newEvent.newValue = json['newValue'];
@@ -122,102 +128,16 @@ export class BlockChange extends BlockBase {
   override isNull(): boolean {
     return this.oldValue === this.newValue;
   }
-
-  /**
-   * Run a change event.
-   *
-   * @param forward True if run forward, false if run backward (undo).
-   */
-  override run(forward: boolean) {
-    const workspace = this.getEventWorkspace_();
-    if (!this.blockId) {
-      throw new Error(
-        'The block ID is undefined. Either pass a block to ' +
-          'the constructor, or call fromJson'
-      );
-    }
-    const block = workspace.getBlockById(this.blockId);
-    if (!block) {
-      throw new Error(
-        'The associated block is undefined. Either pass a ' +
-          'block to the constructor, or call fromJson'
-      );
-    }
-    // Assume the block is rendered so that then we can check.
-    const blockSvg = block as BlockSvg;
-    if (blockSvg.mutator) {
-      // Close the mutator (if open) since we don't want to update it.
-      blockSvg.mutator.setVisible(false);
-    }
-    const value = forward ? this.newValue : this.oldValue;
-    switch (this.element) {
-      case 'field': {
-        const field = block.getField(this.name!);
-        if (field) {
-          field.setValue(value);
-        } else {
-          console.warn("Can't set non-existent field: " + this.name);
-        }
-        break;
-      }
-      case 'comment':
-        block.setCommentText((value as string) || null);
-        break;
-      case 'collapsed':
-        block.setCollapsed(!!value);
-        break;
-      case 'disabled':
-        block.setEnabled(!value);
-        break;
-      case 'inline':
-        block.setInputsInline(!!value);
-        break;
-      case 'mutation': {
-        const oldState = BlockChange.getExtraBlockState_(block as BlockSvg);
-        if (block.loadExtraState) {
-          block.loadExtraState(JSON.parse((value as string) || '{}'));
-        } else if (block.domToMutation) {
-          block.domToMutation(
-            utilsXml.textToDom((value as string) || '<mutation/>')
-          );
-        }
-        eventUtils.fire(
-          new BlockChange(block, 'mutation', null, oldState, value)
-        );
-        break;
-      }
-      default:
-        console.warn('Unknown change type: ' + this.element);
-    }
-  }
-
-  // TODO (#5397): Encapsulate this in the BlocklyMutationChange event when
-  //    refactoring change events.
-  /**
-   * Returns the extra state of the given block (either as XML or a JSO,
-   * depending on the block's definition).
-   *
-   * @param block The block to get the extra state of.
-   * @returns A stringified version of the extra state of the given block.
-   * @internal
-   */
-  static getExtraBlockState_(block: BlockSvg): string {
-    if (block.saveExtraState) {
-      const state = block.saveExtraState();
-      return state ? JSON.stringify(state) : '';
-    } else if (block.mutationToDom) {
-      const state = block.mutationToDom();
-      return state ? Xml.domToText(state) : '';
-    }
-    return '';
-  }
 }
 
-export interface BlockChangeJson extends BlockBaseJson {
-  element: string;
-  name?: string;
+export interface BlockFieldIntermediateChangeJson extends AbstractEventJson {
+  blockId: string;
+  name: string;
   newValue: unknown;
   oldValue: unknown;
 }
 
-registry.register(registry.Type.EVENT, eventUtils.CHANGE, BlockChange);
+registry.register(
+    registry.Type.EVENT,
+    eventUtils.BLOCK_FIELD_INTERMEDIATE_CHANGE,
+    BlockFieldIntermediateChange);
--- core/field_input.ts
+++ core/field_input.ts
@@ -72,6 +72,13 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
    */
   protected isTextValid_ = false;
 
+  /**
+   * The intial value of the field when the user opened an editor to change its
+   * value. When the editor is disposed, an event will be fired that uses this
+   * as the event's oldValue.
+   */
+  protected valueWhenEditorWasOpened_: string | T | null = null;
+
   /** Key down event data. */
   private onKeyDownWrapper_: browserEvents.Data | null = null;
 
@@ -328,6 +335,7 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
     WidgetDiv.show(this, block.RTL, this.widgetDispose_.bind(this));
     this.htmlInput_ = this.widgetCreate_() as HTMLInputElement;
     this.isBeingEdited_ = true;
+    this.valueWhenEditorWasOpened_ = this.value_;
 
     if (!quietInput) {
       (this.htmlInput_ as HTMLElement).focus({
@@ -410,6 +418,20 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
     // Make sure the field's node matches the field's internal value.
     this.forceRerender();
     this.onFinishEditing_(this.value_);
+
+    if (this.sourceBlock_ && eventUtils.isEnabled() &&
+        this.valueWhenEditorWasOpened_ !== null &&
+        this.valueWhenEditorWasOpened_ !== this.value_) {
+      // When closing a field input widget, fire an event indicating that the
+      // user has completed a sequence of changes. The value may have changed
+      // multiple times while the editor was open, but this will fire an event
+      // containing the value when the editor was opened as well as the new one.
+      eventUtils.fire(new (eventUtils.get(eventUtils.BLOCK_CHANGE))(
+          this.sourceBlock_, 'field', this.name || null, this.valueWhenEditorWasOpened_,
+          this.value_));
+      this.valueWhenEditorWasOpened_ = null;
+    }
+
     eventUtils.setGroup(false);
 
     // Actual disposal.
@@ -499,7 +521,20 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
    * @param _e Keyboard event.
    */
   private onHtmlInputChange_(_e: Event) {
+    // Intermediate value changes from user input are not confirmed until the
+    // user closes the editor, and may be numerous. Inhibit reporting these as
+    // normal block change events, and instead report them as special
+    // intermediate changes that do not get recorded in undo history.
+    const oldValue = this.value_;
+    eventUtils.disable();
     this.setValue(this.getValueFromEditorText_(this.htmlInput_!.value));
+    eventUtils.enable();
+    if (this.sourceBlock_ && eventUtils.isEnabled() &&
+        this.value_ !== oldValue) {
+      eventUtils.fire(
+          new (eventUtils.get(eventUtils.BLOCK_FIELD_INTERMEDIATE_CHANGE))(
+              this.sourceBlock_, this.name || null, oldValue, this.value_));
+    }
 
     // Resize the widget div after the block has finished rendering.
     renderManagement.finishQueuedRenders().then(() => {

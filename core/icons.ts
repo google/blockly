c43cdd9eff3d4acc04cb14543199d65144483776
Beka Westberg <bwestberg@google.com>
2023-05-25 17:13:47 +0000

    chore: delete old comment icon

--- core/icons.ts
+++ core/icons.ts
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import {CommentIcon} from './icons/comment_icon.js';
 import * as exceptions from './icons/exceptions.js';
 import * as registry from './icons/registry.js';
 
-export {exceptions, registry};
+export {CommentIcon, exceptions, registry};

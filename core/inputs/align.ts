d7ccf8a5eeacc76fc61b0089003dff8a34a68e67
Beka Westberg <bwestberg@google.com>
2023-06-16 11:27:46 -0700

    fix: input exports (#7165)
    * fix: input exports
    
    * chore: fix build
    
    * chore: attempt to fix build
    
    * chore: attempt to fix build
    
    * chore: create new align enum to replace old one
    
    * chore: format
    
    * fix: Tweak renamings entries
    
    It appears that the goal is to map:
    
    Blockly.Input.Align -> Blockly.inputs.Align
    Blockly.Align -> Blockly.inputs.Align
    Blockly.ALIGN_* -> Blockly.inputs.Align.*
    
    I believe this commit achieves that in a more minimal (and correct)
    way—but if I have misunderstood the intention then this will not
    be a useful correction.
    
    ---------
    
    Co-authored-by: Christopher Allen <cpcallen+git@google.com>

---
+++ core/inputs/align.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2012 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Enum for alignment of inputs.
+ */
+export enum Align {
+  LEFT = -1,
+  CENTRE = 0,
+  RIGHT = 1,
+}

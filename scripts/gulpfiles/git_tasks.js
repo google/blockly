828056da516efe36be0fe8e509bd544c96811701
Christopher Allen <cpcallen+git@google.com>
2023-06-19 10:58:17 +0100

    refactor(build): Modernise git_tasks.js
    - Various style updates:
      - Use CONSTANT_CASE.
      - Use /** JSDoc comments */
      - Use `template ${literals}`.
    - Use git switch instead of git checkout.
    - Try to avoid use of remote names; use URLs where possible.

--- scripts/gulpfiles/git_tasks.js
+++ scripts/gulpfiles/git_tasks.js
@@ -14,7 +14,7 @@ const execSync = require('child_process').execSync;
 const buildTasks = require('./build_tasks');
 const packageTasks = require('./package_tasks');
 
-const upstream_url = "https://github.com/google/blockly.git";
+const UPSTREAM_URL = 'https://github.com/google/blockly.git';
 
 /**
  * Extra paths to include in the gh_pages branch (beyond the normal
@@ -27,81 +27,97 @@ const EXTRAS = [
   'node_modules/@blockly',
 ];
 
-// Stash current state, check out the named branch, and sync with
-// google/blockly.
+/**
+ * Stash current state, check out the named branch, and sync with
+ * google/blockly.
+ */
 function syncBranch(branchName) {
   return function(done) {
     execSync('git stash save -m "Stash for sync"', { stdio: 'inherit' });
     checkoutBranch(branchName);
-    execSync('git pull ' + upstream_url + ' ' + branchName,
-        { stdio: 'inherit' });
-    execSync('git push origin ' + branchName, { stdio: 'inherit' });
+    execSync(`git pull ${UPSTREAM_URL} ${branchName}`, { stdio: 'inherit' });
+    execSync(`git push origin ${branchName}`, { stdio: 'inherit' });
     done();
-  }
+  };
 }
 
-// Stash current state, check out develop, and sync with google/blockly.
+/**
+ * Stash current state, check out develop, and sync with
+ * google/blockly.
+ */
 function syncDevelop() {
   return syncBranch('develop');
 };
 
-// Stash current state, check out master, and sync with google/blockly.
+/**
+ * Stash current state, check out master, and sync with
+ * google/blockly.
+ */
 function syncMaster() {
   return syncBranch('master');
 };
 
-// Helper function: get a name for a rebuild branch. Format: rebuild_mm_dd_yyyy.
+/**
+ * Helper function: get a name for a rebuild branch. Format:
+ * rebuild_mm_dd_yyyy.
+ */
 function getRebuildBranchName() {
   const date = new Date();
   const mm = date.getMonth() + 1;  // Month, 0-11
   const dd = date.getDate();  // Day of the month, 1-31
   const yyyy = date.getFullYear();
-  return 'rebuild_' + mm + '_' + dd + '_' + yyyy;
+  return `rebuild_${mm}_${dd}_${yyyy}`;
 };
 
-// Helper function: get a name for a rebuild branch. Format: rebuild_yyyy_mm.
+/**
+ * Helper function: get a name for a rebuild branch. Format:
+ * rebuild_yyyy_mm.
+ */
 function getRCBranchName() {
   const date = new Date();
   const mm = date.getMonth() + 1;  // Month, 0-11
   const yyyy = date.getFullYear();
-  return 'rc_' + yyyy + '_' + mm;
+  return `rc_${yyyy}_${mm}`;
 };
 
-// If branch does not exist then create the branch.
-// If branch exists switch to branch.
+/**
+ * If branch does not exist then create the branch.
+ * If branch exists switch to branch.
+ */
 function checkoutBranch(branchName) {
-  execSync('git checkout ' + branchName + ' || git checkout -b ' + branchName,
-   { stdio: 'inherit' });
+  execSync(`git switch -c ${branchName}`,
+      { stdio: 'inherit' });
 }
 
-// Create and push an RC branch.
-// Note that this pushes to google/blockly.
+/**
+ * Create and push an RC branch.
+ * Note that this pushes to google/blockly.
+ */
 const createRC = gulp.series(
   syncDevelop(),
   function(done) {
     const branchName = getRCBranchName();
-    execSync('git checkout -b ' + branchName, { stdio: 'inherit' });
-    execSync('git push ' + upstream_url + ' ' + branchName,
-        { stdio: 'inherit' });
+    execSync(`git switch -C ${branchName}`, { stdio: 'inherit' });
+    execSync(`git push ${UPSTREAM_URL} ${branchName}`, { stdio: 'inherit' });
     done();
   }
 );
 
-// Create the rebuild branch.
+/** Create the rebuild branch. */
 function createRebuildBranch(done) {
   const branchName = getRebuildBranchName();
-  console.log('make-rebuild-branch: creating branch ' + branchName);
-  execSync('git checkout -b ' + branchName, { stdio: 'inherit' });
+  console.log(`make-rebuild-branch: creating branch ${branchName}`);
+  execSync(`git switch -C ${branchName}`, { stdio: 'inherit' });
   done();
 }
 
-// Push the rebuild branch to origin.
+/** Push the rebuild branch to origin. */
 function pushRebuildBranch(done) {
   console.log('push-rebuild-branch: committing rebuild');
   execSync('git commit -am "Rebuild"', { stdio: 'inherit' });
   const branchName = getRebuildBranchName();
-  execSync('git push origin ' + branchName, { stdio: 'inherit' });
-  console.log('Branch ' + branchName + ' pushed to GitHub.');
+  execSync(`git push origin ${branchName}`, { stdio: 'inherit' });
+  console.log(`Branch ${branchName} pushed to GitHub.`);
   console.log('Next step: create a pull request against develop.');
   done();
 }
@@ -114,8 +130,8 @@ function pushRebuildBranch(done) {
 const updateGithubPages = gulp.series(
   function(done) {
     execSync('git stash save -m "Stash for sync"', { stdio: 'inherit' });
-    execSync('git checkout gh-pages || git checkout -b gh-pages', { stdio: 'inherit' });
-    execSync('git fetch upstream', { stdio: 'inherit' });
+    execSync('git switch -C gh-pages', { stdio: 'inherit' });
+    execSync(`git fetch ${UPSTREAM_URL}`, { stdio: 'inherit' });
     execSync('git reset --hard upstream/develop', { stdio: 'inherit' });
     done();
   },
@@ -127,8 +143,7 @@ const updateGithubPages = gulp.series(
     // so we have to force add.
     execSync(`git add -f ${EXTRAS.join(' ')}`, {stdio: 'inherit'});
     execSync('git commit -am "Rebuild"', {stdio: 'inherit'});
-    execSync('git push ' + upstream_url + ' gh-pages --force',
-        {stdio: 'inherit'});
+    execSync(`git push ${UPSTREAM_URL} gh-pages --force`, {stdio: 'inherit'});
     done();
   }
 );

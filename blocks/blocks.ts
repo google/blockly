8328413569379f4dbdb77839d41642d4727ac4fd
Christopher Allen <cpcallen+git@google.com>
2023-06-21 12:02:25 +0100

    fix(blocks): Remove lint

--- blocks/blocks.ts
+++ blocks/blocks.ts
@@ -4,11 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * @fileoverview All the blocks.  (Entry point for blocks_compressed.js.)
- * @suppress {extraRequire}
- */
-
 import * as goog from '../closure/goog/goog.js';
 goog.declareModuleId('Blockly.libraryBlocks');
 

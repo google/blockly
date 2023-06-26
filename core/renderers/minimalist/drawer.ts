b189b1989c2304c00c2edb09ccfb9aad21b5cbf7
Beka Westberg <bwestberg@google.com>
2023-06-26 14:30:06 -0700

    chore: deprecate minimalist renderer (#7210)
    * chore: deprecate the minimalist renderer
    
    * chore: add renamings

--- core/renderers/minimalist/drawer.ts
+++ core/renderers/minimalist/drawer.ts
@@ -9,19 +9,32 @@ goog.declareModuleId('Blockly.minimalist.Drawer');
 
 import type {BlockSvg} from '../../block_svg.js';
 import {Drawer as BaseDrawer} from '../common/drawer.js';
+import * as deprecation from '../../utils/deprecation.js';
 
 import type {RenderInfo} from './info.js';
 
 /**
  * An object that draws a block based on the given rendering information.
+ *
+ * @deprecated Use Blockly.blockRendering.Drawer instead.
+ *     To be removed in v11.
  */
 export class Drawer extends BaseDrawer {
   /**
    * @param block The block to render.
    * @param info An object containing all information needed to render this
    *     block.
+   *
+   * @deprecated Use Blockly.blockRendering.Drawer instead.
+   *     To be removed in v11.
    */
   constructor(block: BlockSvg, info: RenderInfo) {
     super(block, info);
+    deprecation.warn(
+      'Blockly.minimalist.Drawer',
+      'v10',
+      'v11',
+      'Blockly.blockRendering.Drawer'
+    );
   }
 }

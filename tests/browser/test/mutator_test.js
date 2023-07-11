0e1db5b569712294beb8afd81612528a7a4e1e62
Eric Blackmon <ericblackmon@google.com>
2023-07-11 11:17:15 -0400

    chore: create test for undo block movements and editing a field

--- tests/browser/test/mutator_test.js
+++ tests/browser/test/mutator_test.js
@@ -12,10 +12,7 @@ const chai = require('chai');
 const {
   testSetup,
   testFileLocations,
-  getSelectedBlockElement,
-  getNthBlockOfCategory,
   getBlockTypeFromCategory,
-  connect,
   switchRTL,
 } = require('./test_setup');
 
@@ -33,7 +30,6 @@ suite('Testing Field Edits', function (done) {
     await testingMutator(1);
   });
 
-
   // Teardown entire suite after test are done running
   suiteTeardown(async function () {
     await browser.deleteSession();
@@ -41,43 +37,43 @@ suite('Testing Field Edits', function (done) {
 });
 
 async function testingMutator(delta) {
-      // Drag out print from flyout.
-      const controlIfFlyout = await getBlockTypeFromCategory(
-        browser,
-        'Logic',
-        'controls_if'
-      );
-      await controlIfFlyout.dragAndDrop({x: (delta*50), y: 50});
-      //Get the number of transform elements on the if do block
-      const ifDo = await browser.$(
-        '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected'
-      );
-   let  ifDoNumHTML= await ifDo.getHTML();
-    let ifDoTransformCountBefore=(ifDoNumHTML.match(/transform/g) || []).length;
-      console.log(ifDoTransformCountBefore);
-      //Click mutator 
-      const mutatorWheel = await browser.$(
-        '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected > g.blocklyIconGroup'
-      );
-      
-      await mutatorWheel.click();
+  // Drag out print from flyout.
+  const controlIfFlyout = await getBlockTypeFromCategory(
+    browser,
+    'Logic',
+    'controls_if'
+  );
+  await controlIfFlyout.dragAndDrop({x: delta * 50, y: 50});
+  // Get the number of transform elements on the if do block
+  const ifDo = await browser.$(
+    '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected'
+  );
+  let ifDoNumHTML = await ifDo.getHTML();
+  const ifDoTransformCountBefore = (ifDoNumHTML.match(/transform/g) || []).length;
+  console.log(ifDoTransformCountBefore);
+  // Click mutator
+  const mutatorWheel = await browser.$(
+    '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBlockCanvas > g.blocklyDraggable.blocklySelected > g.blocklyIconGroup'
+  );
 
-      const elseIfFlyout = await browser.$(
-        '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBubbleCanvas > g > g:nth-child(2) > svg:nth-child(1) > g > g.blocklyFlyout > g > g.blocklyBlockCanvas > g:nth-child(3)'
-      );
+  await mutatorWheel.click();
+  // Create the new configuration
+  const elseIfFlyout = await browser.$(
+    '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBubbleCanvas > g > g:nth-child(2) > svg:nth-child(1) > g > g.blocklyFlyout > g > g.blocklyBlockCanvas > g:nth-child(3)'
+  );
 
-      elseIfFlyout.dragAndDrop({x: (delta*50), y: 42})
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
+  elseIfFlyout.dragAndDrop({x: delta * 50, y: 42});
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
 
-      const elseIf = await browser.$(
-        '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBubbleCanvas > g > g:nth-child(2) > svg:nth-child(1) > g > g.blocklyBlockCanvas > g.blocklyDraggable'
-      );
+  const elseIf = await browser.$(
+    '#blocklyDiv > div > svg.blocklySvg > g > g.blocklyBubbleCanvas > g > g:nth-child(2) > svg:nth-child(1) > g > g.blocklyBlockCanvas > g.blocklyDraggable'
+  );
 
-      elseIf.dragAndDrop({x: (delta)*-25, y: -10})
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
-      ifDoNumHTML= await ifDo.getHTML();
-      let ifDoTransformCountAfter=(ifDoNumHTML.match(/transform/g) || []).length;
+  elseIf.dragAndDrop({x: delta * -25, y: -10});
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 sec
+  // Check to see that the new configuration has more transform elements
+  ifDoNumHTML = await ifDo.getHTML();
+  const ifDoTransformCountAfter = (ifDoNumHTML.match(/transform/g) || []).length;
 
-      chai.assert.isTrue(ifDoTransformCountAfter>ifDoTransformCountBefore);
-      
+  chai.assert.isTrue(ifDoTransformCountAfter > ifDoTransformCountBefore);
 }

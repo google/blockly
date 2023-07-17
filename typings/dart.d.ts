bd84ffaecda628a546bfcdf155ca81084404f5a9
Christopher Allen <cpcallen+git@google.com>
2023-07-17 20:40:27 +0100

    fix(generators): Changes to exports and access controls for TypeScript compatibility (#7295)
    * fix(generators): Add missing declarations for Order enums
    
    * chore(generators): Remove spurious whitespace
    
    * fix(generators): Make provideFunction_ etc. public
    
      Remove the protected declaration on provideFunction_ and
      FUNCTION_NAME_PLACEHOLDER_ so they can be used from generator
      functions written in TypeScript.
    
      Not strictly part of #7283, but closely related and required to
      fixing the related issue google/blockly-samples#1785.
    
    * chore(generators): format
    
    (cherry picked from commit d503fbb409e58d80a3a4db89ec603532505e8663)

--- typings/dart.d.ts
+++ typings/dart.d.ts
@@ -4,4 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export enum Order {
+  ATOMIC = 0,         // 0 "" ...
+  UNARY_POSTFIX = 1,  // expr++ expr-- () [] . ?.
+  UNARY_PREFIX = 2,   // -expr !expr ~expr ++expr --expr
+  MULTIPLICATIVE = 3, // * / % ~/
+  ADDITIVE = 4,       // + -
+  SHIFT = 5,          // << >>
+  BITWISE_AND = 6,    // &
+  BITWISE_XOR = 7,    // ^
+  BITWISE_OR = 8,     // |
+  RELATIONAL = 9,     // >= > <= < as is is!
+  EQUALITY = 10,      // == !=
+  LOGICAL_AND = 11,   // &&
+  LOGICAL_OR = 12,    // ||
+  IF_NULL = 13,       // ??
+  CONDITIONAL = 14,   // expr ? expr : expr
+  CASCADE = 15,       // ..
+  ASSIGNMENT = 16,    // = *= /= ~/= %= += -= <<= >>= &= ^= |=
+  NONE = 99,          // (...)
+}
+
 export declare const dartGenerator: any;

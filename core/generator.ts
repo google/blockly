b79c582fb6fdabece77520bf1265ca9c02b05ff2
Beka Westberg <bwestberg@google.com>
2023-08-14 16:56:20 +0000

    feat: make quoting methods public

--- core/generator.ts
+++ core/generator.ts
@@ -37,7 +37,7 @@ export type BlockGenerator = (
 /**
  * Class for a code generator that translates the blocks into a language.
  */
-export class CodeGenerator {
+export abstract class CodeGenerator {
   name_: string;
 
   /** A dictionary of block generator functions keyed by block type. */
@@ -169,10 +169,6 @@ export class CodeGenerator {
     codeString = codeString.replace(/[ \t]+\n/g, '\n');
     return codeString;
   }
-
-  // The following are some helpful functions which can be used by multiple
-
-  // languages.
 
   /**
    * Prepend a common prefix onto each line of code.
@@ -440,6 +436,10 @@ export class CodeGenerator {
     const id = block.id.replace(/\$/g, '$$$$'); // Issue 251.
     return msg.replace(/%1/g, "'" + id + "'");
   }
+
+  abstract quote_(toQuote: string): string;
+
+  abstract multiline_quote_(toQuote: string): string;
 
   /**
    * Add one or more words to the list of reserved words for this language.

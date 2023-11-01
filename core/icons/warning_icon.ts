5c3319457c8a2b55b1875eca5296dd23e3ad5a91
Siddheya Kulkarni <115717746+Asymtode712@users.noreply.github.com>
2023-11-01 23:30:27 +0530

    Update warning_icon.ts

--- core/icons/warning_icon.ts
+++ core/icons/warning_icon.ts
@@ -88,7 +88,7 @@ export class WarningIcon extends Icon implements IHasBubble {
       },
       this.svgRoot,
     );
-    dom.addClass(this.svgRoot, 'blockly-icon-warning');
+    dom.addClass(this.svgRoot!, 'blockly-icon-warning');
   }
 
   override dispose() {

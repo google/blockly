9fec824aa0b829dc8942d92e33ce82c298d31f44
Christopher Allen <cpcallen+git@google.com>
2023-11-13 13:26:50 +0000

    refactor(generators): Migrate dart_generator.js to TypeScript

--- generators/php/php_generator.ts
+++ generators/php/php_generator.ts
@@ -5,71 +5,67 @@
  */
 
 /**
- * @fileoverview Helper functions for generating PHP for blocks.
- * @suppress {checkTypes|globalThis}
+ * @file PHP code generator class, including helper methods for
+ * generating PHP for blocks.
  */
 
 // Former goog.module ID: Blockly.PHP
 
 import * as stringUtils from '../../core/utils/string.js';
-// import type {Block} from '../../core/block.js';
+import type {Block} from '../../core/block.js';
 import {CodeGenerator} from '../../core/generator.js';
 import {Names} from '../../core/names.js';
-// import type {Workspace} from '../../core/workspace.js';
+import type {Workspace} from '../../core/workspace.js';
 import {inputTypes} from '../../core/inputs/input_types.js';
 
 
 /**
  * Order of operation ENUMs.
  * http://php.net/manual/en/language.operators.precedence.php
- * @enum {number}
  */
-export const Order = {
-  ATOMIC: 0,             // 0 "" ...
-  CLONE: 1,              // clone
-  NEW: 1,                // new
-  MEMBER: 2.1,           // []
-  FUNCTION_CALL: 2.2,    // ()
-  POWER: 3,              // **
-  INCREMENT: 4,          // ++
-  DECREMENT: 4,          // --
-  BITWISE_NOT: 4,        // ~
-  CAST: 4,               // (int) (float) (string) (array) ...
-  SUPPRESS_ERROR: 4,     // @
-  INSTANCEOF: 5,         // instanceof
-  LOGICAL_NOT: 6,        // !
-  UNARY_PLUS: 7.1,       // +
-  UNARY_NEGATION: 7.2,   // -
-  MULTIPLICATION: 8.1,   // *
-  DIVISION: 8.2,         // /
-  MODULUS: 8.3,          // %
-  ADDITION: 9.1,         // +
-  SUBTRACTION: 9.2,      // -
-  STRING_CONCAT: 9.3,    // .
-  BITWISE_SHIFT: 10,     // << >>
-  RELATIONAL: 11,        // < <= > >=
-  EQUALITY: 12,          // == != === !== <> <=>
-  REFERENCE: 13,         // &
-  BITWISE_AND: 13,       // &
-  BITWISE_XOR: 14,       // ^
-  BITWISE_OR: 15,        // |
-  LOGICAL_AND: 16,       // &&
-  LOGICAL_OR: 17,        // ||
-  IF_NULL: 18,           // ??
-  CONDITIONAL: 19,       // ?:
-  ASSIGNMENT: 20,        // = += -= *= /= %= <<= >>= ...
-  LOGICAL_AND_WEAK: 21,  // and
-  LOGICAL_XOR: 22,       // xor
-  LOGICAL_OR_WEAK: 23,   // or
-  NONE: 99,              // (...)
-};
+export enum Order {
+  ATOMIC = 0,             // 0 "" ...
+  CLONE = 1,              // clone
+  NEW = 1,                // new
+  MEMBER = 2.1,           // []
+  FUNCTION_CALL = 2.2,    // ()
+  POWER = 3,              // **
+  INCREMENT = 4,          // ++
+  DECREMENT = 4,          // --
+  BITWISE_NOT = 4,        // ~
+  CAST = 4,               // (int) (float) (string) (array) ...
+  SUPPRESS_ERROR = 4,     // @
+  INSTANCEOF = 5,         // instanceof
+  LOGICAL_NOT = 6,        // !
+  UNARY_PLUS = 7.1,       // +
+  UNARY_NEGATION = 7.2,   // -
+  MULTIPLICATION = 8.1,   // *
+  DIVISION = 8.2,         // /
+  MODULUS = 8.3,          // %
+  ADDITION = 9.1,         // +
+  SUBTRACTION = 9.2,      // -
+  STRING_CONCAT = 9.3,    // .
+  BITWISE_SHIFT = 10,     // << >>
+  RELATIONAL = 11,        // < <= > >=
+  EQUALITY = 12,          // == != === !== <> <=>
+  REFERENCE = 13,         // &
+  BITWISE_AND = 13,       // &
+  BITWISE_XOR = 14,       // ^
+  BITWISE_OR = 15,        // |
+  LOGICAL_AND = 16,       // &&
+  LOGICAL_OR = 17,        // ||
+  IF_NULL = 18,           // ??
+  CONDITIONAL = 19,       // ?:
+  ASSIGNMENT = 20,        // = += -= *= /= %= <<= >>= ...
+  LOGICAL_AND_WEAK = 21,  // and
+  LOGICAL_XOR = 22,       // xor
+  LOGICAL_OR_WEAK = 23,   // or
+  NONE = 99,              // (...)
+}
 
 export class PhpGenerator extends CodeGenerator {
-  /**
-   * List of outer-inner pairings that do NOT require parentheses.
-   * @type {!Array<!Array<number>>}
-   */
-  ORDER_OVERRIDES = [
+  /** List of outer-inner pairings that do NOT require parentheses. */
+  ORDER_OVERRIDES: [Order, Order][] = [
     // (foo()).bar() -> foo().bar()
     // (foo())[0] -> foo()[0]
     [Order.MEMBER, Order.FUNCTION_CALL],
@@ -88,8 +84,9 @@ export class PhpGenerator extends CodeGenerator {
     [Order.LOGICAL_OR, Order.LOGICAL_OR]
   ];
 
-  constructor(name) {
-    super(name ?? 'PHP');
+  /** @param name Name of the language the generator is for. */
+  constructor(name = 'PHP') {
+    super(name);
     this.isInitialized = false;
 
     // Copy Order values onto instance for backwards compatibility
@@ -100,7 +97,16 @@ export class PhpGenerator extends CodeGenerator {
     // replace data properties with get accessors that call
     // deprecate.warn().)
     for (const key in Order) {
-      this['ORDER_' + key] = Order[key];
+      // Must assign Order[key] to a temporary to get the type guard to work;
+      // see https://github.com/microsoft/TypeScript/issues/10530.
+      const value = Order[key];
+      // Skip reverse-lookup entries in the enum.  Due to
+      // https://github.com/microsoft/TypeScript/issues/55713 this (as
+      // of TypeScript 5.5.2) actually narrows the type of value to
+      // never - but that still allows the following assignment to
+      // succeed.
+      if (typeof value === 'string') continue;
+      (this as unknown as Record<string, Order>)['ORDER_' + key] = value;
     }
 
     // List of illegal variable names.  This is not intended to be a
@@ -134,9 +140,10 @@ export class PhpGenerator extends CodeGenerator {
 
   /**
    * Initialise the database of variable names.
-   * @param {!Workspace} workspace Workspace to generate code from.
+   *
+   * @param workspace Workspace to generate code from.
    */
-  init(workspace) {
+  init(workspace: Workspace) {
     super.init(workspace);
 
     if (!this.nameDB_) {
@@ -154,37 +161,40 @@ export class PhpGenerator extends CodeGenerator {
 
   /**
    * Prepend the generated code with the variable definitions.
-   * @param {string} code Generated code.
-   * @return {string} Completed code.
+   *
+   * @param code Generated code.
+   * @returns Completed code.
    */
-  finish(code) {
+  finish(code: string): string {
     // Convert the definitions dictionary into a list.
     const definitions = Object.values(this.definitions_);
     // Call Blockly.CodeGenerator's finish.
     code = super.finish(code);
     this.isInitialized = false;
 
-    this.nameDB_.reset();
+    this.nameDB_!.reset();
     return definitions.join('\n\n') + '\n\n\n' + code;
   };
 
   /**
    * Naked values are top-level blocks with outputs that aren't plugged into
-   * anything.  A trailing semicolon is needed to make this legal.
-   * @param {string} line Line of generated code.
-   * @return {string} Legal line of code.
+   * anything.
+   *
+   * @param line Line of generated code.
+   * @returns Legal line of code.
    */
-  scrubNakedValue(line) {
+  scrubNakedValue(line: string): string {
     return line + ';\n';
   };
 
   /**
    * Encode a string as a properly escaped PHP string, complete with
    * quotes.
-   * @param {string} string Text to encode.
-   * @return {string} PHP string.
+   *
+   * @param string Text to encode.
+   * @returns PHP string.
    */
-  quote_(string) {
+  quote_(string: string): string {
     string = string.replace(/\\/g, '\\\\')
         .replace(/\n/g, '\\\n')
         .replace(/'/g, '\\\'');
@@ -194,10 +204,10 @@ export class PhpGenerator extends CodeGenerator {
   /**
    * Encode a string as a properly escaped multiline PHP string, complete with
    * quotes.
-   * @param {string} string Text to encode.
-   * @return {string} PHP string.
+   * @param string Text to encode.
+   * @returns PHP string.
    */
-  multiline_quote_(string) {
+  multiline_quote_(string: string): string {
     const lines = string.split(/\n/g).map(this.quote_);
     // Join with the following, plus a newline:
     // . "\n" .
@@ -209,14 +219,13 @@ export class PhpGenerator extends CodeGenerator {
    * Common tasks for generating PHP from blocks.
    * Handles comments for the specified block and any connected value blocks.
    * Calls any statements following this block.
-   * @param {!Block} block The current block.
-   * @param {string} code The PHP code created for this block.
-   * @param {boolean=} opt_thisOnly True to generate code for only this
-   *     statement.
-   * @return {string} PHP code with comments and subsequent blocks added.
-   * @protected
+   *
+   * @param block The current block.
+   * @param code The PHP code created for this block.
+   * @param thisOnly True to generate code for only this statement.
+   * @returns PHP code with comments and subsequent blocks added.
    */
-  scrub_(block, code, opt_thisOnly) {
+  scrub_(block: Block, code: string, thisOnly = false): string {
     let commentCode = '';
     // Only collect comments for blocks that aren't inline.
     if (!block.outputConnection || !block.outputConnection.targetConnection) {
@@ -230,7 +239,7 @@ export class PhpGenerator extends CodeGenerator {
       // Don't collect comments for nested statements.
       for (let i = 0; i < block.inputList.length; i++) {
         if (block.inputList[i].type === inputTypes.VALUE) {
-          const childBlock = block.inputList[i].connection.targetBlock();
+          const childBlock = block.inputList[i].connection!.targetBlock();
           if (childBlock) {
             comment = this.allNestedComments(childBlock);
             if (comment) {
@@ -242,45 +251,45 @@ export class PhpGenerator extends CodeGenerator {
     }
     const nextBlock =
         block.nextConnection && block.nextConnection.targetBlock();
-    const nextCode = opt_thisOnly ? '' : this.blockToCode(nextBlock);
+    const nextCode = thisOnly ? '' : this.blockToCode(nextBlock);
     return commentCode + code + nextCode;
   };
 
   /**
    * Gets a property and adjusts the value while taking into account indexing.
-   * @param {!Block} block The block.
-   * @param {string} atId The property ID of the element to get.
-   * @param {number=} opt_delta Value to add.
-   * @param {boolean=} opt_negate Whether to negate the value.
-   * @param {number=} opt_order The highest order acting on this value.
-   * @return {string|number}
+   *
+   * @param block The block.
+   * @param atId The ID of the input block to get (and adjust) the value of.
+   * @param delta Value to add.
+   * @param negate Whether to negate the value.
+   * @param order The highest order acting on this value.
+   * @returns The adjusted value.
    */
-  getAdjusted(block, atId, opt_delta, opt_negate, opt_order) {
-    let delta = opt_delta || 0;
-    let order = opt_order || this.ORDER_NONE;
+  getAdjusted(block: Block, atId: string, delta = 0, negate = false, order = Order.NONE): string | number {
     if (block.workspace.options.oneBasedIndex) {
       delta--;
     }
     let defaultAtIndex = block.workspace.options.oneBasedIndex ? '1' : '0';
-    let outerOrder = order;
-    let innerOrder;
+    let outerOrder: Order = order;
+    let innerOrder: Order | undefined;
     if (delta > 0) {
-      outerOrder = this.ORDER_ADDITION;
-      innerOrder = this.ORDER_ADDITION;
+      outerOrder = Order.ADDITION;
+      innerOrder = Order.ADDITION;
     } else if (delta < 0) {
-      outerOrder = this.ORDER_SUBTRACTION;
-      innerOrder = this.ORDER_SUBTRACTION;
-    } else if (opt_negate) {
-      outerOrder = this.ORDER_UNARY_NEGATION;
-      innerOrder = this.ORDER_UNARY_NEGATION;
+      outerOrder = Order.SUBTRACTION;
+      innerOrder = Order.SUBTRACTION;
+    } else if (negate) {
+      outerOrder = Order.UNARY_NEGATION;
+      innerOrder = Order.UNARY_NEGATION;
     }
+
     let at = this.valueToCode(block, atId, outerOrder) || defaultAtIndex;
 
     if (stringUtils.isNumber(at)) {
       // If the index is a naked number, adjust it right now.
-      at = Number(at) + delta;
-      if (opt_negate) {
-        at = -at;
+      at = String(Number(at) + delta);
+      if (negate) {
+        at = String(-Number(at));
       }
     } else {
       // If the index is dynamic, adjust it in code.
@@ -289,14 +298,14 @@ export class PhpGenerator extends CodeGenerator {
       } else if (delta < 0) {
         at = at + ' - ' + -delta;
       }
-      if (opt_negate) {
+      if (negate) {
         if (delta) {
           at = '-(' + at + ')';
         } else {
           at = '-' + at;
         }
       }
-      innerOrder = Math.floor(innerOrder);
+      innerOrder = innerOrder === undefined ? NaN : Math.floor(innerOrder);
       order = Math.floor(order);
       if (innerOrder && order >= innerOrder) {
         at = '(' + at + ')';

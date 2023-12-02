d19965da38f68ff12197c529b353c927b90b2826
truongductri01 <truongductri01@gmail.com>
2023-12-02 10:17:56 -0500

    fix: update test statements

--- tests/mocha/event_block_field_intermediate_change_test.js
+++ tests/mocha/event_block_field_intermediate_change_test.js
@@ -37,7 +37,7 @@ suite('Field Intermediate Change Event', function () {
   });
 
   suite('Change Value', function () {
-    test("this tests that running forward changes the block's value to new value", function () {
+    test("running forward changes the block's value to new value", function () {
       const block = this.workspace.newBlock('text', 'block_id');
       const origEvent = new Blockly.Events.BlockFieldIntermediateChange(
         block,
@@ -53,7 +53,7 @@ suite('Field Intermediate Change Event', function () {
       );
     });
 
-    test("this tests that running backward changes the block's value to old value", function () {
+    test("running backward changes the block's value to old value", function () {
       const block = this.workspace.newBlock('text', 'block_id');
       const origEvent = new Blockly.Events.BlockFieldIntermediateChange(
         block,

50e9293f77fd5c1431de3c43c42f1672d0368d3c
Christopher Allen <cpcallen+git@google.com>
2024-01-08 16:56:39 +0000

    fix(generators): Fix generator type declarations
    Add a missing ./ prefix to the imports in the generator wrapper
    .d.ts files.
    
    Fixes: #7741

--- typings/javascript.d.ts
+++ typings/javascript.d.ts
@@ -4,4 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from 'generators/javascript';
+export * from './generators/javascript';
--- typings/php.d.ts
+++ typings/php.d.ts
@@ -4,4 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from 'generators/php';
+export * from './generators/php';

a512a854c4315e5e94d83513f3e9c5107ebf9303
Beka Westberg <bwestberg@google.com>
2024-02-29 19:43:07 +0000

    chore: add text area to comment view

--- core/comments/comment_view.ts
+++ core/comments/comment_view.ts
@@ -14,24 +14,15 @@ import {Size} from '../utils.js';
 
 export class CommentView implements IRenderedElement {
   private svgRoot: SVGGElement;
-  private backgroundRect: SVGRectElement;
   private topBar: SVGRectElement;
   private deleteIcon: SVGImageElement;
   private foldoutIcon: SVGImageElement;
   private resizeHandle: SVGImageElement;
+  private foreignObject: SVGForeignObjectElement;
+  private textarea: HTMLTextAreaElement;
 
   constructor(private readonly workspace: WorkspaceSvg) {
     this.svgRoot = dom.createSvgElement(Svg.G, {'class': 'blocklyComment'});
-
-    this.backgroundRect = dom.createSvgElement(
-      Svg.RECT,
-      {
-        'class': 'blocklyCommentRect',
-        'x': 0,
-        'y': 0,
-      },
-      this.svgRoot,
-    );
 
     this.topBar = dom.createSvgElement(
       Svg.RECT,
@@ -60,6 +51,27 @@ export class CommentView implements IRenderedElement {
       },
       this.svgRoot,
     );
+
+    this.foreignObject = dom.createSvgElement(
+      Svg.FOREIGNOBJECT,
+      {
+        'class': 'blocklyCommentForeignObject',
+      },
+      this.svgRoot,
+    );
+    const body = document.createElementNS(dom.HTML_NS, 'body');
+    body.setAttribute('xmlns', dom.HTML_NS);
+    body.className = 'blocklyMinimalBody';
+    this.textarea = document.createElementNS(
+      dom.HTML_NS,
+      'textarea',
+    ) as HTMLTextAreaElement;
+    this.textarea.className = 'blocklyTextarea blocklyText';
+    // TODO: Handle RTL.
+    // this.textarea.setAttribute('dir', this.workspace.RTL ? 'RTL' : 'LTR');
+    body.appendChild(this.textarea);
+    this.foreignObject.appendChild(body);
+
     this.resizeHandle = dom.createSvgElement(
       Svg.IMAGE,
       {
@@ -85,15 +97,18 @@ export class CommentView implements IRenderedElement {
     const deleteSize = this.deleteIcon.getBBox();
     const foldoutSize = this.foldoutIcon.getBBox();
     const resizeSize = this.resizeHandle.getBBox();
-    console.log(resizeSize);
 
     this.svgRoot.setAttribute('height', `${size.height}`);
     this.svgRoot.setAttribute('width', `${size.width}`);
 
-    this.backgroundRect.setAttribute('height', `${size.height}`);
-    this.backgroundRect.setAttribute('width', `${size.width}`);
-
     this.topBar.setAttribute('width', `${size.width}`);
+
+    this.foreignObject.setAttribute(
+      'height',
+      `${size.height - topBarSize.height}`,
+    );
+    this.foreignObject.setAttribute('y', `${topBarSize.height}`);
+    this.foreignObject.setAttribute('width', `${size.width}`);
 
     const deleteMargin = (topBarSize.height - deleteSize.height) / 2;
     this.deleteIcon.setAttribute('y', `${deleteMargin}`);
@@ -116,6 +131,18 @@ css.register(`
   --commentFillColour: #FFFCC7;
   --commentBorderColour: #F2E49B;
   --commentIconColour: #1A1A1A
+}
+
+.blocklyComment .blocklyTextarea {
+  background-color: var(--commentFillColour);
+  border: 1px solid var(--commentBorderColour);
+  outline: 0;
+  resize: none;
+  overflow: hidden;
+  box-sizing: border-box;
+  padding: 8px;
+  width: 100%;
+  height: 100%;
 }
 
 .blocklyDeleteIcon {

141be453876479348f1e55980fa68521acc5cdea
Beka Westberg <bwestberg@google.com>
2024-03-08 17:27:56 +0000

    feat: implement constructor

--- core/comments/workspace_comment.ts
+++ core/comments/workspace_comment.ts
@@ -7,9 +7,23 @@
 import {Workspace} from '../workspace.js';
 import {Size} from '../utils/size.js';
 import {Coordinate} from '../utils/coordinate.js';
+import * as idGenerator from '../utils/idgenerator.js';
 
-class WorkspaceComment {
-  constructor(workspace: Workspace, id?: string) {}
+export class WorkspaceComment {
+  public readonly id: string;
+
+  constructor(
+    protected readonly workspace: Workspace,
+    id?: string,
+  ) {
+    // TODO: Before merging, file issue to update getCommentById.
+    this.id = id && !workspace.getCommentById(id) ? id : idGenerator.genUid();
+
+    // TODO: Before merging, file issue to add top comment.
+    // workspace.addTopComment(this);
+
+    // TODO(7909): Fire events.
+  }
 
   setText(text: string) {}
 

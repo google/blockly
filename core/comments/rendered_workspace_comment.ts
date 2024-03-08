e25759db967b593f135848b2691421aa0bc06271
Beka Westberg <bwestberg@google.com>
2024-03-08 20:42:07 +0000

    feat: add updating the view and model

--- core/comments/rendered_workspace_comment.ts
+++ core/comments/rendered_workspace_comment.ts
@@ -7,6 +7,7 @@
 import {WorkspaceComment} from './workspace_comment.js';
 import {WorkspaceSvg} from '../workspace_svg.js';
 import {CommentView} from './comment_view.js';
+import {Size} from '../utils.js';
 
 export class RenderedWorkspaceComment extends WorkspaceComment {
   private view: CommentView;
@@ -17,5 +18,37 @@ export class RenderedWorkspaceComment extends WorkspaceComment {
     this.view = new CommentView(workspace);
     // Set the size to the default size as defined in the superclass.
     this.view.setSize(this.getSize());
+
+    this.addModelUpdateBindings();
+  }
+
+  private addModelUpdateBindings() {
+    this.view.addTextChangeListener(
+      (_, newText: string) => void super.setText(newText),
+    );
+    this.view.addSizeChangeListener(
+      (_, newSize: Size) => void super.setSize(newSize),
+    );
+    this.view.addOnCollapseListener(
+      () => void super.setCollapsed(this.view.isCollapsed()),
+    );
+  }
+
+  setText(text: string): void {
+    // setText will trigger the change listener that updates
+    // the model aka superclass.
+    this.view.setText(text);
+  }
+
+  setSize(size: Size) {
+    // setSize will trigger the change listener that updates
+    // the model aka superclass.
+    this.view.setSize(size);
+  }
+
+  setCollapsed(collapsed: boolean) {
+    // setCollapsed will trigger the change listener that updates
+    // the model aka superclass.
+    this.view.setCollapsed(collapsed);
   }
 }

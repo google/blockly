3f88daf4f4905b13134c29552e91bf9f4e5757be
Beka Westberg <bwestberg@google.com>
2024-03-08 21:10:37 +0000

    chore: add tsdoc

--- core/comments/rendered_workspace_comment.ts
+++ core/comments/rendered_workspace_comment.ts
@@ -10,8 +10,10 @@ import {CommentView} from './comment_view.js';
 import {Coordinate, Size} from '../utils.js';
 
 export class RenderedWorkspaceComment extends WorkspaceComment {
+  /** The class encompassing the svg elements making up the workspace comment. */
   private view: CommentView;
 
+  /** Constructs the workspace comment, including the view. */
   constructor(workspace: WorkspaceSvg, id?: string) {
     super(workspace, id);
 
@@ -23,6 +25,10 @@ export class RenderedWorkspaceComment extends WorkspaceComment {
     this.addModelUpdateBindings();
   }
 
+  /**
+   * Adds listeners to the view that updates the model (i.e. the superclass)
+   * when changes are made to the view.
+   */
   private addModelUpdateBindings() {
     this.view.addTextChangeListener(
       (_, newText: string) => void super.setText(newText),
@@ -35,36 +41,42 @@ export class RenderedWorkspaceComment extends WorkspaceComment {
     );
   }
 
-  setText(text: string): void {
+  /** Sets the text of the comment. */
+  override setText(text: string): void {
     // setText will trigger the change listener that updates
     // the model aka superclass.
     this.view.setText(text);
   }
 
-  setSize(size: Size) {
+  /** Sets the size of the comment. */
+  override setSize(size: Size) {
     // setSize will trigger the change listener that updates
     // the model aka superclass.
     this.view.setSize(size);
   }
 
-  setCollapsed(collapsed: boolean) {
+  /** Sets whether the comment is collapsed or not. */
+  override setCollapsed(collapsed: boolean) {
     // setCollapsed will trigger the change listener that updates
     // the model aka superclass.
     this.view.setCollapsed(collapsed);
   }
 
-  setEditable(editable: boolean): void {
+  /** Sets whether the comment is editable or not. */
+  override setEditable(editable: boolean): void {
     super.setEditable(editable);
     // Use isEditable rather than isOwnEditable to account for workspace state.
     this.view.setEditable(this.isEditable());
   }
 
-  moveTo(location: Coordinate): void {
+  /** Moves the comment to the given location in workspace coordinates. */
+  override moveTo(location: Coordinate): void {
     super.moveTo(location);
     this.view.moveTo(location);
   }
 
-  dispose() {
+  /** Disposes of the view. */
+  override dispose() {
     this.view.dispose();
     super.dispose();
   }

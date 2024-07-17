ae80adfe9cb51890241cff435c30416804da401a
Arun Chandran <53257113+Arun-cn@users.noreply.github.com>
2024-07-17 22:48:14 +0530

    fix!: Replace Closure UI CSS classes with Blockly CSS classes (#8339)
    * fix!: Replace Closure UI CSS classes with Blockly CSS classes
    
    * chore: remove comments about deprecated goog-x class
    
    * chore: remove deprecated goog-x classes
    
    * fix: correct coding format to pass CI checks

--- core/menu.ts
+++ core/menu.ts
@@ -85,8 +85,7 @@ export class Menu {
    */
   render(container: Element): HTMLDivElement {
     const element = document.createElement('div');
-    // goog-menu is deprecated, use blocklyMenu.  May 2020.
-    element.className = 'blocklyMenu goog-menu blocklyNonSelectable';
+    element.className = 'blocklyMenu blocklyNonSelectable';
     element.tabIndex = 0;
     if (this.roleName) {
       aria.setRole(element, this.roleName);

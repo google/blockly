66a713ead46e3542bf4f6db7659125a993711ac6
TicTacToe_sst <paliwalshreshtha@gmail.com>
2024-07-21 14:15:36 +0530

    feat: added blocklynotdetetable class to block_svg

--- core/block_svg.ts
+++ core/block_svg.ts
@@ -1070,6 +1070,20 @@ export class BlockSvg
   }
 
   /**
+   * Add blocklyNotDeletable class when block is not deletable
+   * Or remove class when block is deletable
+   */
+  override setDeletable(deletable: boolean) {
+    super.setDeletable(deletable);
+
+    if(deletable) {
+      dom.removeClass(this.svgGroup_, 'blocklyNotDeletable');
+    } else {
+      dom.addClass(this.svgGroup_, 'blocklyNotDeletable');
+    }
+  }
+
+  /**
    * Set whether the block is highlighted or not.  Block highlighting is
    * often used to visually mark blocks currently being executed.
    *

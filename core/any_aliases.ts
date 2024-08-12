04b070a46b67f864dcd8ebb402bc9522967215e8
Tay754 <t7547347@gmail.com>
2024-08-12 12:12:58 -0400

    Delete core directory
    Fraud

--- core/any_aliases.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// eslint-disable-next-line
-type AnyDuringMigration = any;
--- core/comments.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2024 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export {CommentView} from './comments/comment_view.js';
-export {WorkspaceComment} from './comments/workspace_comment.js';
-export {RenderedWorkspaceComment} from './comments/rendered_workspace_comment.js';

32571e53e9339d569ac0d6aefc0c8aa3cafbd73f
Tay754 <t7547347@gmail.com>
2024-08-13 19:48:17 -0400

    Delete tests directory
    Fraud

--- tests/browser/.mocharc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  ui: 'tdd',
-  require: __dirname + '/test/hooks.mjs',
-};
--- tests/mocha/.mocharc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  ui: 'tdd',
-  reporter: 'landing',
-};

4e084951817b8cfd37d5b08ccb0890762b0822de
Christopher Allen <cpcallen+git@google.com>
2024-08-13 14:53:45 +0100

    refactor(events): Use "export ... from" where applicable

--- core/events/events.ts
+++ core/events/events.ts
@@ -6,109 +6,50 @@
 
 // Former goog.module ID: Blockly.Events
 
-import {Abstract, AbstractEventJson} from './events_abstract.js';
-import {BlockBase, BlockBaseJson} from './events_block_base.js';
-import {BlockChange, BlockChangeJson} from './events_block_change.js';
-import {BlockCreate, BlockCreateJson} from './events_block_create.js';
-import {BlockDelete, BlockDeleteJson} from './events_block_delete.js';
-import {BlockDrag, BlockDragJson} from './events_block_drag.js';
-import {
+import * as eventUtils from './utils.js';
+
+// Events.
+export {Abstract, AbstractEventJson} from './events_abstract.js';
+export {BlockBase, BlockBaseJson} from './events_block_base.js';
+export {BlockChange, BlockChangeJson} from './events_block_change.js';
+export {BlockCreate, BlockCreateJson} from './events_block_create.js';
+export {BlockDelete, BlockDeleteJson} from './events_block_delete.js';
+export {BlockDrag, BlockDragJson} from './events_block_drag.js';
+export {
   BlockFieldIntermediateChange,
   BlockFieldIntermediateChangeJson,
 } from './events_block_field_intermediate_change.js';
-import {BlockMove, BlockMoveJson} from './events_block_move.js';
-import {BubbleOpen, BubbleOpenJson, BubbleType} from './events_bubble_open.js';
-import {Click, ClickJson, ClickTarget} from './events_click.js';
-import {CommentBase, CommentBaseJson} from './events_comment_base.js';
-import {CommentChange, CommentChangeJson} from './events_comment_change.js';
-import {
+export {BlockMove, BlockMoveJson} from './events_block_move.js';
+export {BubbleOpen, BubbleOpenJson, BubbleType} from './events_bubble_open.js';
+export {Click, ClickJson, ClickTarget} from './events_click.js';
+export {CommentBase, CommentBaseJson} from './events_comment_base.js';
+export {CommentChange, CommentChangeJson} from './events_comment_change.js';
+export {
   CommentCollapse,
   CommentCollapseJson,
 } from './events_comment_collapse.js';
-import {CommentCreate, CommentCreateJson} from './events_comment_create.js';
-import {CommentDelete} from './events_comment_delete.js';
-import {CommentDrag, CommentDragJson} from './events_comment_drag.js';
-import {CommentMove, CommentMoveJson} from './events_comment_move.js';
-import {CommentResize, CommentResizeJson} from './events_comment_resize.js';
-import {MarkerMove, MarkerMoveJson} from './events_marker_move.js';
-import {Selected, SelectedJson} from './events_selected.js';
-import {ThemeChange, ThemeChangeJson} from './events_theme_change.js';
-import {
+export {CommentCreate, CommentCreateJson} from './events_comment_create.js';
+export {CommentDelete} from './events_comment_delete.js';
+export {CommentDrag, CommentDragJson} from './events_comment_drag.js';
+export {CommentMove, CommentMoveJson} from './events_comment_move.js';
+export {CommentResize, CommentResizeJson} from './events_comment_resize.js';
+export {MarkerMove, MarkerMoveJson} from './events_marker_move.js';
+export {Selected, SelectedJson} from './events_selected.js';
+export {ThemeChange, ThemeChangeJson} from './events_theme_change.js';
+export {
   ToolboxItemSelect,
   ToolboxItemSelectJson,
 } from './events_toolbox_item_select.js';
-import {TrashcanOpen, TrashcanOpenJson} from './events_trashcan_open.js';
-import {UiBase} from './events_ui_base.js';
-import {VarBase, VarBaseJson} from './events_var_base.js';
-import {VarCreate, VarCreateJson} from './events_var_create.js';
-import {VarDelete, VarDeleteJson} from './events_var_delete.js';
-import {VarRename, VarRenameJson} from './events_var_rename.js';
-import {ViewportChange, ViewportChangeJson} from './events_viewport.js';
-import * as eventUtils from './utils.js';
-import {FinishedLoading} from './workspace_events.js';
+export {TrashcanOpen, TrashcanOpenJson} from './events_trashcan_open.js';
+export {UiBase} from './events_ui_base.js';
+export {VarBase, VarBaseJson} from './events_var_base.js';
+export {VarCreate, VarCreateJson} from './events_var_create.js';
+export {VarDelete, VarDeleteJson} from './events_var_delete.js';
+export {VarRename, VarRenameJson} from './events_var_rename.js';
+export {ViewportChange, ViewportChangeJson} from './events_viewport.js';
+export {FinishedLoading} from './workspace_events.js';
 
-// Events.
-export {
-  Abstract,
-  AbstractEventJson,
-  BlockBase,
-  BlockBaseJson,
-  BlockChange,
-  BlockChangeJson,
-  BlockCreate,
-  BlockCreateJson,
-  BlockDelete,
-  BlockDeleteJson,
-  BlockDrag,
-  BlockDragJson,
-  BlockFieldIntermediateChange,
-  BlockFieldIntermediateChangeJson,
-  BlockMove,
-  BlockMoveJson,
-  BubbleOpen,
-  BubbleOpenJson,
-  BubbleType,
-  Click,
-  ClickJson,
-  ClickTarget,
-  CommentBase,
-  CommentBaseJson,
-  CommentChange,
-  CommentChangeJson,
-  CommentCollapse,
-  CommentCollapseJson,
-  CommentCreate,
-  CommentCreateJson,
-  CommentDelete,
-  CommentDrag,
-  CommentDragJson,
-  CommentMove,
-  CommentMoveJson,
-  CommentResize,
-  CommentResizeJson,
-  FinishedLoading,
-  MarkerMove,
-  MarkerMoveJson,
-  Selected,
-  SelectedJson,
-  ThemeChange,
-  ThemeChangeJson,
-  ToolboxItemSelect,
-  ToolboxItemSelectJson,
-  TrashcanOpen,
-  TrashcanOpenJson,
-  UiBase,
-  VarBase,
-  VarBaseJson,
-  VarCreate,
-  VarCreateJson,
-  VarDelete,
-  VarDeleteJson,
-  VarRename,
-  VarRenameJson,
-  ViewportChange,
-  ViewportChangeJson,
-};
+export type {BumpEvent} from './utils.js';
 
 // Event types.
 export const BLOCK_CHANGE = eventUtils.BLOCK_CHANGE;
@@ -119,7 +60,6 @@ export const BLOCK_MOVE = eventUtils.BLOCK_MOVE;
 export const BLOCK_FIELD_INTERMEDIATE_CHANGE =
   eventUtils.BLOCK_FIELD_INTERMEDIATE_CHANGE;
 export const BUBBLE_OPEN = eventUtils.BUBBLE_OPEN;
-export type BumpEvent = eventUtils.BumpEvent;
 export const BUMP_EVENTS = eventUtils.BUMP_EVENTS;
 export const CHANGE = eventUtils.CHANGE;
 export const CLICK = eventUtils.CLICK;
@@ -145,17 +85,19 @@ export const VAR_RENAME = eventUtils.VAR_RENAME;
 export const VIEWPORT_CHANGE = eventUtils.VIEWPORT_CHANGE;
 
 // Event utils.
-export const clearPendingUndo = eventUtils.clearPendingUndo;
-export const disable = eventUtils.disable;
-export const enable = eventUtils.enable;
-export const filter = eventUtils.filter;
-export const fire = eventUtils.fire;
-export const fromJson = eventUtils.fromJson;
-export const getDescendantIds = eventUtils.getDescendantIds;
-export const get = eventUtils.get;
-export const getGroup = eventUtils.getGroup;
-export const getRecordUndo = eventUtils.getRecordUndo;
-export const isEnabled = eventUtils.isEnabled;
-export const setGroup = eventUtils.setGroup;
-export const setRecordUndo = eventUtils.setRecordUndo;
-export const disableOrphans = eventUtils.disableOrphans;
+export {
+  clearPendingUndo,
+  disable,
+  disableOrphans,
+  enable,
+  filter,
+  fire,
+  fromJson,
+  get,
+  getDescendantIds,
+  getGroup,
+  getRecordUndo,
+  isEnabled,
+  setGroup,
+  setRecordUndo,
+} from './utils.js';

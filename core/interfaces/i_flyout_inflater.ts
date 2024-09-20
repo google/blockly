fb6d3ee4938eee1cea260a0015d1c96e14b66d56
Aaron Dodson <adodson@google.com>
2024-09-20 15:05:08 -0700

    refactor: Add the gapForElement method.

--- core/interfaces/i_flyout_inflater.ts
+++ core/interfaces/i_flyout_inflater.ts
@@ -5,7 +5,7 @@ export interface IFlyoutInflater {
   /**
    * Loads the object represented by the given state onto the workspace.
    *
-   * Note that this method's interface is indentical to that in ISerializer, to
+   * Note that this method's interface is identical to that in ISerializer, to
    * allow for code reuse.
    *
    * @param state A JSON representation of an element to inflate on the flyout.
@@ -17,6 +17,16 @@ export interface IFlyoutInflater {
    * @returns The newly inflated flyout element.
    */
   load(state: Object, flyoutWorkspace: WorkspaceSvg): IBoundedElement;
+
+  /**
+   * Returns the amount of spacing that should follow the element corresponding
+   * to the given JSON representation.
+   *
+   * @param state A JSON representation of the element preceding the gap.
+   * @param defaultGap The default gap for elements in this flyout.
+   * @returns The gap that should follow the given element.
+   */
+  gapForElement(state: Object, defaultGap: number): number;
 
   /**
    * Disposes of the given element.

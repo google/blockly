246362152d146ec19b715741c39768074924d442
Aaron Dodson <adodson@google.com>
2024-09-25 15:24:14 -0700

    feat: Add inflaters for flyout labels and buttons.

---
+++ core/button_flyout_inflater.ts
@@ -0,0 +1,35 @@
+import type {IFlyoutInflater} from './interfaces/i_flyout_inflater.js';
+import type {IBoundedElement} from './interfaces/i_bounded_element.js';
+import type {WorkspaceSvg} from './workspace_svg.js';
+import {FlyoutButton} from './flyout_button.js';
+import {ButtonOrLabelInfo} from './utils/toolbox.js';
+import * as registry from './registry.js';
+
+export class ButtonFlyoutInflater implements IFlyoutInflater {
+  load(state: Object, flyoutWorkspace: WorkspaceSvg): IBoundedElement {
+    const button = new FlyoutButton(
+      flyoutWorkspace,
+      flyoutWorkspace.targetWorkspace!,
+      state as ButtonOrLabelInfo,
+      false,
+    );
+    button.show();
+    return button;
+  }
+
+  gapForElement(state: Object, defaultGap: number): number {
+    return defaultGap;
+  }
+
+  disposeElement(element: IBoundedElement): void {
+    if (element instanceof FlyoutButton) {
+      element.dispose();
+    }
+  }
+}
+
+registry.register(
+  registry.Type.FLYOUT_INFLATER,
+  'button',
+  ButtonFlyoutInflater,
+);
---
+++ core/label_flyout_inflater.ts
@@ -0,0 +1,31 @@
+import type {IFlyoutInflater} from './interfaces/i_flyout_inflater.js';
+import type {IBoundedElement} from './interfaces/i_bounded_element.js';
+import type {WorkspaceSvg} from './workspace_svg.js';
+import {FlyoutButton} from './flyout_button.js';
+import {ButtonOrLabelInfo} from './utils/toolbox.js';
+import * as registry from './registry.js';
+
+export class LabelFlyoutInflater implements IFlyoutInflater {
+  load(state: Object, flyoutWorkspace: WorkspaceSvg): IBoundedElement {
+    const label = new FlyoutButton(
+      flyoutWorkspace,
+      flyoutWorkspace.targetWorkspace!,
+      state as ButtonOrLabelInfo,
+      true,
+    );
+    label.show();
+    return label;
+  }
+
+  gapForElement(state: Object, defaultGap: number): number {
+    return defaultGap;
+  }
+
+  disposeElement(element: IBoundedElement): void {
+    if (element instanceof FlyoutButton) {
+      element.dispose();
+    }
+  }
+}
+
+registry.register(registry.Type.FLYOUT_INFLATER, 'label', LabelFlyoutInflater);

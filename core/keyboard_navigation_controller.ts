47efeb2a5d8c13aa0e4a0865f61ec08fe6da8b28
Maribeth Moffatt <maribethb@google.com>
2025-05-28 16:46:51 -0700

    chore: fix tsdoc

--- core/keyboard_navigation_controller.ts
+++ core/keyboard_navigation_controller.ts
@@ -16,10 +16,23 @@ export class KeyboardNavigationController {
   private activeClassName = 'blocklyKeyboardNavigation';
 
   /**
-   * Sets whether we think the user is actively using keyboard navigation.
+   * Sets whether a user is actively using keyboard navigation.
    *
-   * If they are, we'll apply a css class to the entire page so that
+   * If they are, apply a css class to the entire page so that
    * focused items can apply additional styling for keyboard users.
+   *
+   * Note that since enabling keyboard navigation presents significant UX changes
+   * (such as cursor visualization and move mode), callers should take care to
+   * only set active keyboard navigation when they have a high confidence in that
+   * being the correct state. In general, in any given mouse or key input situation
+   * callers can choose one of three paths:
+   * 1. Do nothing. This should be the choice for neutral actions that don't
+   *    predominantly imply keyboard or mouse usage (such as clicking to select a block).
+   * 2. Disable keyboard navigation. This is the best choice when a user is definitely
+   *    predominantly using the mouse (such as using a right click to open the context menu).
+   * 3. Enable keyboard navigation. This is the best choice when there's high confidence
+   *    a user actually intends to use it (such as attempting to use the arrow keys to move
+   *    around).
    *
    * @param isUsing
    */
@@ -29,7 +42,7 @@ export class KeyboardNavigationController {
   }
 
   /**
-   * @returns true if we think the user is actively using keyboard navigation
+   * @returns true if the user is actively using keyboard navigation
    * (e.g., has recently taken some action that is only relevant to keyboard users)
    */
   getIsActive(): boolean {
